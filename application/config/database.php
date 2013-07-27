42c5ec283ea28c071223b00fdcf52145e556c264
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2013-07-27 15:07:29 +0000

    Resolved issue 22 and introduced dev/prod database details.

--- application/config/database.php
+++ application/config/database.php
@@ -2,84 +2,75 @@
 
 defined('SYSPATH') OR die('No direct access allowed.');
 
-return array
-    (
+// Determine which database's we should be using.
+$useDB = (Kohana::$environment !== Kohana::PRODUCTION) ? "dev" : "prod";
+
+// Differing DB settings
+$dbOvr = array (
+        "dev" => array (
+            'connection' => array (
+                'hostname' => 'localhost',
+                'database' => 'dev_core',
+                'username' => 'dev',
+                'password' => 'Monk1esW111T4k3ov3R!',
+                'persistent' => TRUE,
+            ),
+        ),
+        "prod" => array (
+            'connection' => array (
+                'hostname' => 'localhost',
+                'database' => 'prod_core',
+                'username' => 'prod_core',
+                'password' => 'zaJerNGdEwXfPnqD',
+                'persistent' => TRUE,
+            ),
+        ),
+    );
+
+// Default DB settings
+$dbDefs = array(
     'default' => array(
         'type' => 'MySQL',
-        'connection' => array(
-            'hostname' => 'localhost',
-            'database' => 'core',
-            'username' => "core",
-            'password' => "rww2ZnyePqQ9FJyR",
-            'persistent' => TRUE,
-        ),
+        'connection' => $dbOvr[$useDB]['connection'],
         'table_prefix' => '',
         'charset' => 'utf8',
         'caching' => TRUE,
     ),
     'site' => array(
         'type' => 'MySQL',
-        'connection' => array(
-            'hostname' => 'localhost',
-            'database' => 'core',
-            'username' => "core",
-            'password' => "rww2ZnyePqQ9FJyR",
-            'persistent' => TRUE,
-        ),
+        'connection' => $dbOvr[$useDB]['connection'],
         'table_prefix' => 'site_',
         'charset' => 'utf8',
         'caching' => TRUE,
     ),
     'mship' => array(
         'type' => 'MySQL',
-        'connection' => array(
-            'hostname' => 'localhost',
-            'database' => 'core',
-            'username' => "core",
-            'password' => "rww2ZnyePqQ9FJyR",
-            'persistent' => TRUE,
-        ),
+        'connection' => $dbOvr[$useDB]['connection'],
         'table_prefix' => 'mship_',
         'charset' => 'utf8',
         'caching' => TRUE,
     ),
     'sso' => array(
         'type' => 'MySQL',
-        'connection' => array(
-            'hostname' => 'localhost',
-            'database' => 'core',
-            'username' => "core",
-            'password' => "rww2ZnyePqQ9FJyR",
-            'persistent' => TRUE,
-        ),
+        'connection' => $dbOvr[$useDB]['connection'],
         'table_prefix' => 'sso_',
         'charset' => 'utf8',
         'caching' => TRUE,
     ),
     'train' => array(
         'type' => 'MySQL',
-        'connection' => array(
-            'hostname' => 'localhost',
-            'database' => 'core',
-            'username' => "core",
-            'password' => "rww2ZnyePqQ9FJyR",
-            'persistent' => TRUE,
-        ),
+        'connection' => $dbOvr[$useDB]['connection'],
         'table_prefix' => 'train_',
         'charset' => 'utf8',
         'caching' => TRUE,
     ),
     'sys' => array(
         'type' => 'MySQL',
-        'connection' => array(
-            'hostname' => 'localhost',
-            'database' => 'core',
-            'username' => "core",
-            'password' => "rww2ZnyePqQ9FJyR",
-            'persistent' => TRUE,
-        ),
+        'connection' => $dbOvr[$useDB]['connection'],
         'table_prefix' => 'sys_',
         'charset' => 'utf8',
         'caching' => TRUE,
     ),
 );
+
+return $dbDefs;
