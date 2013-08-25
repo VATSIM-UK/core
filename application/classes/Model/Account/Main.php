4c8fa6501a95a6a49db4ddd57b204333fe9b29c6
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2013-08-26 00:48:43 +0100

    Moved the name formatting feature from a helper to the Account_Main model. Addresses issue #33.

--- application/classes/Model/Account/Main.php
+++ application/classes/Model/Account/Main.php
@@ -87,12 +87,12 @@ class Model_Account_Main extends Model_Master {
             'name_first' => array(
                 array('trim'),
                 array(array("UTF8", "clean"), array(":value")),
-                array(array("Helper_Account_Main", "name_formatter"), array(":value", "f")),
+                array(array($this, "formatName"), array(":value", "f")),
             ),
             'name_last' => array(
                 array('trim'),
                 array(array("UTF8", "clean"), array(":value")),
-                array(array("Helper_Account_Main", "name_formatter"), array(":value", "s")),
+                array(array($this, "formatName"), array(":value", "s")),
             ),
             'password' => array(
                 array("sha1"),
@@ -152,6 +152,95 @@ class Model_Account_Main extends Model_Master {
         
         // Return the count.
         return $ipCheck->reset(FALSE)->count_all();
+    }
+    
+    /**
+     * This helper formats the name of a person to conform with expected output.
+     * 
+     * @param string $name The name to format.
+     * @param string $type 'f' for forename, 's' for surname.
+     * @return string The formatted name.
+     */
+    public static function formatName($name, $type='f'){
+        //Firstname
+        if($type == 'f'){
+
+            $name = trim($name);
+            $name = ucfirst(strtolower($name));
+            $name = addslashes($name);
+
+            return $name;
+
+        ///Surname
+        } elseif($type == 's') {
+
+            $name = trim($name);
+
+            ///Test for spaces- eg Le Bargy
+            $space = explode(' ', $name);
+            if(count($space) > 1){
+
+                $name = '';
+                foreach($space as $k => $v){
+                    $name .= ucfirst(strtolower($v)).' ';
+                }
+
+                $name = addslashes(trim($name));
+                return $name;
+
+            } else {
+                if(strlen($name) <= 2){
+                    return $name;
+                }
+                
+                ///Check for Mc - eg McTighe
+                $name = strtolower($name);
+                            $first_two = $name{0} . (isset($name{1}) ? $name{1} : "");
+                $therest = '';
+
+                if($first_two == 'mc'){
+                    for($i = 2; $i < strlen($name); $i++){
+                        $therest .= $name{$i};
+                    }
+
+                    $name = "Mc".ucfirst($therest);
+                    $name = addslashes(trim($name));
+                    return $name;
+
+                } else {
+
+                    ///Check for hyphon seperated surnames
+                    $hyphon = explode('-', $name);
+                    if(count($hyphon) > 1){
+
+                        $name = '';
+                        $numh = 0;
+                        foreach($hyphon as $k => $v){
+
+                            $numh = $numh+1;
+                            $name .= ucfirst(strtolower($v));
+
+                            ///Dont append extra -
+                            if($numh != count($hyphon)){
+                                $name .= '-';
+                            }
+
+                        }
+
+                        $name = addslashes(trim($name));
+                        return $name;
+
+                    } else {
+                        ///Any other surname
+                        $name = ucfirst(strtolower($name));
+                        $name = addslashes(trim($name));
+                        return $name;
+                    }
+                }
+            }
+        } else {
+            return '';
+        }
     }
 }
 
