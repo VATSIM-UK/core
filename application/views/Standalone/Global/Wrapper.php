135944b1abbc473f117a50631933d6df6a9c0aa6
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2013-08-26 01:07:53 +0100

    Modified the Standalone view to follow the Wrapper model.

---
+++ application/views/Standalone/Global/Wrapper.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.2" />
+        <title><?= Arr::get($_config, "site.name.long", "") ?></title>
+
+        <!-- CSS -->
+        <?= HTML::style("http://code.jquery.com/ui/1.10.3/themes/cupertino/jquery-ui.css"); ?>
+        <?= HTML::style("media/bootstrap/css/bootstrap.min.css"); ?>
+        <?= HTML::style("media/bootstrap/css/bootstrap-responsive.min.css"); ?>
+        <?= HTML::style("media/style/Standalone/design.css"); ?>
+        <?= HTML::style("http://fonts.googleapis.com/css?family=Yellowtail"); ?>
+
+        <!-- Javascript -->
+        <?= HTML::script("http://code.jquery.com/jquery-1.9.1.min.js"); ?>
+        <?= HTML::script("http://code.jquery.com/ui/1.10.1/jquery-ui.js"); ?>
+        <?= HTML::script("media/bootstrap/js/bootstrap.min.js"); ?>
+        <?= HTML::script("media/scripts/general.js"); ?>
+    </head>
+    <body>
+        <div class="container container-header">
+            <div class="row-fluid">
+                <div class="span4 header-left">
+                    <p align="left"><?= HTML::image("media/style/global/images/logo.png") ?></p>
+                </div>
+                <div class="span8 header-right">
+                    <p align="right"><?= HTML::image("media/style/global/images/slogan.png"); ?></p>
+                </div>
+            </div>
+        </div>
+        <div class="container container-content">
+            <div class="content">
+                <?=$_content?>
+            </div>
+        </div>
+        <div class="container container-footer">
+            <div class="footer">
+                <div class="row-fluid">
+                    <p>
+                        VATSIM-UK &copy; 2013 - Version <?=exec("git describe --abbrev=0 --tags")?>
+                        &bull;
+                        Got a problem? Email us: <?= Html::anchor('http://helpdesk.vatsim-uk.co.uk/index.php?act=tickets&code=open&step=2&department=2', 'web-support][at][vatsim-uk.co.uk', array('target' => '_blank')) ?>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
