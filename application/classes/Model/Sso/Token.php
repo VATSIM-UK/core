bf6fae41ca35b11041f1db228844c0f7a3790d46
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2013-08-26 01:11:08 +0100

    Moved the token authentication from Controller_Sso_Auth to Controller_Sso_Token, and the logic from Controller_Sso_Auth to Model_Sso_Token.  Addresses issue #33.

--- application/classes/Model/Sso/Token.php
+++ application/classes/Model/Sso/Token.php
@@ -43,6 +43,66 @@ class Model_Sso_Token extends Model_Master {
     public function filters(){
         return array();
     }
+    
+    /**
+     * Load the current or requested token.
+     * 
+     * @param string $token If set, this token will be loaded.
+     * @return Sso_Token ORM Object.
+     */
+    public function get_current_token($token=null){
+        if($token == null){
+            $token = Session::instance()->get(ORM::factory("Setting")->getValue("sso.token.key"), null);
+            if($token == NULL){
+                return $this;
+            }
+        }
+        
+        // Load it!
+        $token = ORM::factory("Sso_Token")
+                    ->where("token", "=", $token)
+                    ->where("expires", ">", gmdate("Y-m-d H:i:s"))
+                    ->limit(1)
+                    ->find();
+        
+        // Now, load THIS model properly!
+        $this->__construct($token->id);
+        return $this;
+    }
+    
+    /**
+     * Start a new token for the current request.
+     * 
+     * This will also kill all old tokens if the session is currently set.
+     * 
+     * @param string $token The token to start.
+     * @param string $key The section/area/module key.
+     * @param string $returnURL The URL to return the member to.
+     * @param boolean True on success, false otherwise.
+     */
+    public function set_current_token($token, $key, $returnURL=null){
+        // First, let's kill the old token if it's currently set.
+        $oldToken = $this->get_current_token();
+        if($oldToken->loaded()){
+            $oldToken->expires = gmdate("Y-m-d H:i:s", strtotime("-30 seconds"));
+            $oldToken->save();
+        }
+        Session::instance()->delete(ORM::factory("Setting")->getValue("sso.token.key"));
+        
+        // Now, start a new one!
+        $newToken = ORM::factory("Sso_Token");
+        $newToken->token = $token;
+        $newToken->sso_key = $key;
+        $newToken->return_url = $returnURL;
+        $newToken->created = gmdate("Y-m-d H:i:s");
+        $newToken->expires = gmdate("Y-m-d H:i:s", strtotime("+".ORM::factory("Setting")->getValue("sso.token.expires")));
+        $newToken->save();
+        
+        // Store this token!
+        Session::instance()->set(ORM::factory("Setting")->getValue("sso.token.key"), $token);
+        
+        return $newToken;
+    }
 }
 
 ?>
