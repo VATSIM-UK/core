c2acd68f41e1a5b2fe166075e4e4cef0fd86be7f
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2013-09-29 02:08:00 +0100

    Final error checking of email allocation system. Addresses issue #33.

--- application/classes/Model/Account/Email.php
+++ application/classes/Model/Account/Email.php
@@ -63,10 +63,16 @@ class Model_Account_Email extends Model_Master {
      * Check whether the current email has been assigned to an SSO system.
      * 
      * @param string $system The system to check for an assignment for.
+     * @param integer $id The account ID to use.
      * @param boolean $returnEmail If set to TRUE the email will be returned, instead of true/false.
      * @return boolean TRUE if this system has an email assignment. FALSE otherwise.
      */
-    public function assigned_to_sso($system, $returnEmail=false){
+    public function assigned_to_sso($system, $id=null, $returnEmail=false){
+        // Use the ID?
+        if($id != null){
+            $this->where("account_id", "=", $id);
+        }
+        
         // Let's loop through ALL emails
         foreach($this->where("deleted", "IS", NULL)->find_all() as $email){
             $sso = $email->sso_email->find();
@@ -80,6 +86,10 @@ class Model_Account_Email extends Model_Master {
         }
 
         // If we still don't have a return by now, let's just get the primary!
+        // Use the ID?
+        if($id != null){
+            $this->where("account_id", "=", $id);
+        }
         return $this->get_active_primary(false)->email;
     }
     
