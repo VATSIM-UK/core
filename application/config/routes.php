198efcb42a58a412b91e9631670b1e89e6f2720d
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2013-11-09 20:14:38 +0000

    Setting up the training and theory routes + files.

--- application/config/routes.php
+++ application/config/routes.php
@@ -7,13 +7,19 @@ defined('SYSPATH') OR die('No direct access allowed.');
  * ************************************************************ */
 Route::set('frontend_system_route', '<directory>(/)(<controller>(/)(<action>(/)(<area>(/))))',
         array(
-            "directory" => "(sso)",
+            "directory" => "(sso|training)",
         ))
         ->defaults(array(
             'directory' => 'Sso',
             'controller' => 'Manage',
             'action' => 'display',
             'area' => NULL,
+        ));
+Route::set('default_training', 'training(/)(<controller>(/)(<action>(/)(<area>(/))))')
+        ->defaults(array(
+            'directory' => 'Training',
+            'controller' => 'Course',
+            'action' => 'dynamic_display'
         ));
 Route::set('default', '')
         ->defaults(array(
