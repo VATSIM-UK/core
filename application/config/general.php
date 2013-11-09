9f2a7bca1a0b5cb05a9d16f40b5324d1e1768da7
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2013-11-09 20:15:07 +0000

    Added the system version and release date to the general config file.

--- application/config/general.php
+++ application/config/general.php
@@ -6,4 +6,6 @@ return array(
     'session_name' => 'vuk_auth',
     'site_title' => 'VATSIM United Kingdom',
     'system_user' => 707070,
+    'system_version' => exec("git describe --abbrev=0 --tags"),
+    'system_version_date' => gmdate("d/m/y H:i \G\M\T", filemtime(realpath(APPPATH."../.git/"))),
 );
