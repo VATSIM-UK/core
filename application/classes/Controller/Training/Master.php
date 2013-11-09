ef2cc7e5a4dc39b38ca30261d9e783929e2b8e0d
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2013-11-09 00:35:50 +0000

    Created master training controller file.

---
+++ application/classes/Controller/Training/Master.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Training_Master extends Controller_Master {
+    protected $_templateDir = "Standalone"; // Override parent settings.
+    
+    
+}
