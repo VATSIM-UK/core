1345f0295dda9cef5a9756280bae7c69ce2857a7
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2013-11-10 13:04:28 +0000

    EMails now have the automatically generated footer on them.

--- application/views/Email/Global/Footer.php
+++ application/views/Email/Global/Footer.php
@@ -1,6 +1,6 @@
 <p align="left" style="border-top: 1px dashed black;">
     This email was automatically sent from our system at <?= gmdate("H:i:s D jS M Y") ?> GMT.<br />
-    Please <a href="mailto:web-support@vatsim-uk.co.uk?subject=POSTMASTER/EK-<?= $_key ?>/QID-<?= $_id ?>/TP-<?= $_template ?>/LO-<?= $_layout ?>">let us know</a>
+    Please <a href="mailto:web-support@vatsim-uk.co.uk?body=Error with POSTMASTER/EK-<?= $_key ?>/QID-<?= $_id ?>/TP-<?= $_template ?>/LO-<?= $_layout ?>\n\n">let us know</a>
     if it's not been sent to the correct person, or displays incorrectly.<br /><br />
     Reference: <strong>POSTMASTER/EK-<?= $_key ?>/QID-<?= $_id ?>/TP-<?= $_template ?>/LO-<?= $_layout ?></strong>
 </p>
