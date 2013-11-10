4596e97cca742db7b2e9d04bfd9567eff3d146f8
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2013-11-10 02:22:38 +0000

    Added a standard footer to all email templates

--- application/views/Email/Christmas/Default.php
+++ application/views/Email/Christmas/Default.php
@@ -24,10 +24,6 @@
                     <?php endif; ?>
                 </p>
             <?php endif; ?>
-            <p align="left" style="border-top: 1px dashed black;">
-                This email was automatically sent from our system at <?=gmdate("H:i:s D jS M Y")?> GMT.<br />
-                Please <a href="mailto:web-support@vatsim-uk.co.uk">let us know</a> if it's not been sent to the correct person, or displays incorrectly.<br />
-                Reference: <strong>POSTMASTER/EK-<?=$_key?>/QID-<?=$_id?>/TP-<?=$_template?>/LO-<?=$_layout?></strong>
-            </p>
+            <?php View::factory("Email/Global/Footer")->render() ?>
         </div>
 </html>
