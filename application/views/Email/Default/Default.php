4e7519ecdfb8bcc2e528ce1f4d012a6c76c7ad04
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2013-11-10 01:34:45 +0000

    Added the ability to differentiate between templates and layouts.  Also added the delayed flag for emails that aren't sent for whatever reason.  Queue is now updated when emails are sent. Resolves issue #59.

--- application/views/Email/Default/Default.php
+++ application/views/Email/Default/Default.php
@@ -18,6 +18,7 @@
             <p align="left" style="border-top: 1px dashed black;">
                 This email was automatically sent from our system at <?=gmdate("H:i:s D jS M Y")?> GMT.<br />
                 Please <a href="mailto:web-support@vatsim-uk.co.uk">let us know</a> if it's not been sent to the correct person, or displays incorrectly.
+                Reference: <strong>POSTMASTER/EK-<?=$_key?>/QID-<?=$_id?>/TP-<?=$_template?>/LO-<?=$_layout?></strong>
             </p>
         </div>
 </html>
