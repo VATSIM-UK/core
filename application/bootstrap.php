48c89ec5aeef322d36f3be0d15cb49278922c3a0
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2013-11-10 12:15:49 +0000

    Bootstrap now counts beta environment as development.  Resolves issue #73.

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -74,7 +74,7 @@ if (isset($_SERVER['KOHANA_ENV'])) {
 Kohana::$environment = isset($_SERVER['KOHANA_ENV']) ? constant('Kohana::' . strtoupper($_SERVER['KOHANA_ENV'])) : Kohana::PRODUCTION;
 
 if(isset($_SERVER["PWD"])){
-    $dev = preg_match("/(httpdocs|dev)/i", $_SERVER["PWD"]);
+    $dev = preg_match("/(httpdocs|dev|beta|staging)/i", $_SERVER["PWD"]);
 } else {
     $dev = false;
 }
