c013e3dccfae3fca1d285012e7692828dc3a8fd7
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2013-11-10 03:30:51 +0000

    Added the staging URL to bootstrap.php

--- application/classes/Controller/Sso/Master.php
+++ application/classes/Controller/Sso/Master.php
@@ -27,9 +27,11 @@ class Controller_Sso_Master extends Controller_Master {
         
         if(!is_object($this->_current_token) || !$this->_current_token->loaded() || strtotime($this->_current_token->expires) < strtotime(gmdate("Y-m-d H:i:s"))){
             $this->session()->delete("sso_security_grace");
+            die("HERE!");
             return false;
         }
         if($checkAccount === true && !is_object($this->_current_account) && !$this->_current_account->loaded()){
+            die("HERE!");
             return false;
         }
         return true;
