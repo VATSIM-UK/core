b8e15add415e013fc5c7178602acfd722411eb06
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2014-03-02 00:53:35 +0000

    Modified the theory results to allow for MARKING and REVIEW states for exams and questions.

--- application/classes/Enum/Training/Theory/Result.php
+++ application/classes/Enum/Training/Theory/Result.php
@@ -3,10 +3,12 @@
 defined('SYSPATH') or die('No direct script access.');
 
 class Enum_Training_Theory_Result extends Enum_Main {
-    const STARTED = 0;
-    const PASS = 1;
-    const FAIL = 2;
-    const CANCEL = 3;
+    const STARTED = 10;
+    const CANCEL = 20;
+    const MARKING = 40;
+    const REVIEW = 50;
+    const PASS = 80;
+    const FAIL = 90;
     
     public static function getResult($value){
          switch($value){
@@ -21,6 +23,12 @@ class Enum_Training_Theory_Result extends Enum_Main {
                    break;
               case self::STARTED:
                    return 'In Progress';
+                   break;
+              case self::MARKING:
+                   return 'Pending Marking';
+                   break;
+              case self::REVIEW:
+                   return 'Under Review';
                    break;
               default:
                    return parent::getDescription($value);
