4d8f1bb8616dacb84f98ce89d18adf16ed4d6b0e
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2014-03-21 23:26:32 +0000

    Renamed getResult -> getDescription.  Also added DEFAULT_TYPE to allow for changing of question types.

--- application/classes/Enum/Training/Theory/Question/Type.php
+++ application/classes/Enum/Training/Theory/Question/Type.php
@@ -3,12 +3,16 @@
 defined('SYSPATH') or die('No direct script access.');
 
 class Enum_Training_Theory_Question_Type extends Enum_Main {
-    const CHOICE = 10;
+    const MCHOICET = 10;
     
-    public static function getResult($value){
+    // MUST REMAIN AT BOTTOM OF LIST.
+    const DEFAULT_TYPE = 10;
+
+    
+    public static function getDescription($value){
          switch($value){
-              case self::CHOICE:
-                   return 'Multiple Choice';
+              case self::MCHOICET:
+                   return 'Multiple Choice (Text)';
                    break;
               default:
                    return parent::getDescription($value);
