28c925085f51a9e22eeb6395f6c140c7d1ace712
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2014-03-22 22:11:46 +0000

    Added the main menu to the standalone wrapper.

--- media/scripts/Standalone/global.js
+++ media/scripts/Standalone/global.js
@@ -17,6 +17,71 @@ function addTableRowDeleteLinks() {
     });
 }
 
+var allowMainMenu = true;
+function mainMenuOpen(ovrAllow) {
+    if (allowMainMenu || ovrAllow === true) {
+        if(ovrAllow === true){
+            $("#menuContain").show();
+            $("#menuToggle").hide();
+            $("#menuToggle").addClass("menuOpen");
+            $("#menuToggle").removeClass("menuClosed");
+        } else {
+            $("#menuToggle").toggle("fade");
+            $("#menuContain").toggle("blind", function() {
+                $("#menuToggle").addClass("menuOpen");
+                $("#menuToggle").removeClass("menuClosed");
+            });
+        }
+    }
+}
+
+function mainMenuClose() {
+    if (allowMainMenu) {
+        $("#menuContain").toggle("blind", function() {
+            $("#menuToggle").show();
+            $("#menuToggle").addClass("menuClosed");
+            $("#menuToggle").removeClass("menuOpen");
+        });
+    }
+}
+
+function toggleStaticMenu() {
+    if ($("#staticMenuToggle").is(":checked")) {
+        $.cookie("static_menu", "true");
+        $(".container-menu").css("position", "static");
+        allowMainMenu = false;
+        $(".container-menu").prependTo(".container-content > .content");
+        $(".container-menu").removeClass("container");
+    } else {
+        $.removeCookie("static_menu");
+        $(".container-menu").css("position", "absolute");
+        allowMainMenu = true;
+        $(".container-menu").prependTo("#mainContainer");
+        $(".container-menu").addClass("container");
+    }
+}
+
 $(document).ready(function() {
     addTableRowDeleteLinks();
+
+    // Open menu on click/hover
+    $("#menuToggle").mouseover(function() {
+        if (!$("#menuToggle").hasClass("menuOpen")) {
+            $("#menuToggle").addClass("menuOpen");
+            $("#menuToggle").removeClass("menuClosed");
+            mainMenuOpen();
+        }
+    });
+    // Close when no longer hovering.
+    $("#menuContain").hover(null, mainMenuClose);
+
+    // Toggle the static menu on/off
+    $("#staticMenuToggle").change(toggleStaticMenu);
+
+    // If a cookie has been set for the status of the static menu, use it!
+    if ($.cookie("static_menu") == "true") {
+        $("#staticMenuToggle").prop('checked', true);
+        toggleStaticMenu();
+        mainMenuOpen(true);
+    }
 });
