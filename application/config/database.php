477d10467bf6de35938776a63fa39afcdbc84937
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2014-04-05 14:01:39 +0100

    Modified the DB config to make use of the MySQLi changes.

--- application/config/database.php
+++ application/config/database.php
@@ -30,42 +30,42 @@ $dbOvr = array (
 // Default DB settings
 $dbDefs = array(
     'default' => array(
-        'type' => 'MySQL',
+        'type' => 'MySQLi',
         'connection' => $dbOvr[$useDB]['connection'],
         'table_prefix' => '',
         'charset' => 'utf8',
         'caching' => TRUE,
     ),
     'site' => array(
-        'type' => 'MySQL',
+        'type' => 'MySQLi',
         'connection' => $dbOvr[$useDB]['connection'],
         'table_prefix' => 'site_',
         'charset' => 'utf8',
         'caching' => TRUE,
     ),
     'mship' => array(
-        'type' => 'MySQL',
+        'type' => 'MySQLi',
         'connection' => $dbOvr[$useDB]['connection'],
         'table_prefix' => 'mship_',
         'charset' => 'utf8',
         'caching' => TRUE,
     ),
     'sso' => array(
-        'type' => 'MySQL',
+        'type' => 'MySQLi',
         'connection' => $dbOvr[$useDB]['connection'],
         'table_prefix' => 'sso_',
         'charset' => 'utf8',
         'caching' => TRUE,
     ),
     'train' => array(
-        'type' => 'MySQL',
+        'type' => 'MySQLi',
         'connection' => $dbOvr[$useDB]['connection'],
         'table_prefix' => 'train_',
         'charset' => 'utf8',
         'caching' => TRUE,
     ),
     'sys' => array(
-        'type' => 'MySQL',
+        'type' => 'MySQLi',
         'connection' => $dbOvr[$useDB]['connection'],
         'table_prefix' => 'sys_',
         'charset' => 'utf8',
