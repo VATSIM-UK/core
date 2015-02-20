fdf0fe46588f767ccb8fc7fee285df85fb12ec5f
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2015-02-20 14:28:42 +0000

    Changed name of the migration for issue #205 as it wasn't meaningful.

--- app/database/migrations/2015_02_20_140500_system_postmaster_webhooks.php
+++ app/database/migrations/2015_02_20_140500_system_postmaster_webhooks.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class SystemPostmasterQueueMessageId extends Migration {
+class SystemPostmasterWebhooks extends Migration {
 
     /**
      * Run the migrations.
