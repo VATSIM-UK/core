0c17bed27c5f7157f955dff1071b605c9b41563f
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2015-02-22 18:38:11 +0000

    More query optimisation and eager loading! Statistics page is now cached. Refs issue #227.

--- app/controllers/adm/Dashboard.php
+++ app/controllers/adm/Dashboard.php
@@ -10,66 +10,73 @@ use \Response;
 use \Redirect;
 use \View;
 use \Input;
+use \Cache;
 
 class Dashboard extends \Controllers\Adm\AdmController {
 
-    public function getIndex(){
-        // All Stats
-        $statistics = array();
-        $statistics['members_total'] = (\Models\Mship\Account::count());
-        $statistics['members_active'] = (\Models\Mship\Account::where("status", "=", 0)->count());
-        $statistics['members_division'] = (\Models\Mship\Account\State::where("state", "=", \Enums\Account\State::DIVISION)->count());
-        $statistics['members_nondivision'] = (\Models\Mship\Account\State::where("state", "!=", \Enums\Account\State::DIVISION)->count());
-        $statistics['members_pending_update'] = (\Models\Mship\Account::where("cert_checked_at", "<=", \Carbon\Carbon::now()->subHours(24)->toDateTimeString())->count());
-        $statistics['members_qualifications'] = (\Models\Mship\Account\Qualification::count());
+    public function getIndex() {
+        $statistics = Cache::tags((new \Models\Mship\Account())->getTable())->remember("statistics.mship", 60, function() {
+            // All Stats
+            $statistics = array();
+            $statistics['members_total'] = (\Models\Mship\Account::count());
+            $statistics['members_active'] = (\Models\Mship\Account::where("status", "=", 0)->count());
+            $statistics['members_division'] = (\Models\Mship\Account\State::where("state", "=", \Enums\Account\State::DIVISION)->count());
+            $statistics['members_nondivision'] = (\Models\Mship\Account\State::where("state", "!=", \Enums\Account\State::DIVISION)->count());
+            $statistics['members_pending_update'] = (\Models\Mship\Account::where("cert_checked_at", "<=", \Carbon\Carbon::now()->subHours(24)->toDateTimeString())->count());
+            $statistics['members_qualifications'] = (\Models\Mship\Account\Qualification::count());
+            return $statistics;
+        });
 
-        // API Requests
-        $membershipStats = array();
-        $membershipStatsKeys = ["members.division.current", "members.division.new", "members.new", "members.current"];
-        $date = \Carbon\Carbon::parse("45 days ago");
-        while($date->lt(\Carbon\Carbon::parse("today midnight"))) {
-            $counts = array();
-            foreach($membershipStatsKeys as $key){
-                $counts[$key] = Statistic::getStatistic($date->toDateString(), $key);
+        $membershipStats = Cache::tags((new Statistic())->getTable())->remember("statistics.membership.graph", 60 * 24, function() {
+            $membershipStats = array();
+            $membershipStatsKeys = ["members.division.current", "members.division.new", "members.new", "members.current"];
+            $date = \Carbon\Carbon::parse("45 days ago");
+            while ($date->lt(\Carbon\Carbon::parse("today midnight"))) {
+                $counts = array();
+                foreach ($membershipStatsKeys as $key) {
+                    $counts[$key] = Statistic::getStatistic($date->toDateString(), $key);
+                }
+                $membershipStats[$date->toDateString()] = $counts;
+                $date->addDay();
             }
-            $membershipStats[$date->toDateString()] = $counts;
-            $date->addDay();
-        }
+            return $membershipStats;
+        });
 
         return $this->viewMake("adm.dashboard")
-                    ->with("statistics", $statistics)
-                    ->with("membershipStats", $membershipStats);
+                        ->with("statistics", $statistics)
+                        ->with("membershipStats", $membershipStats);
     }
 
-    public function anySearch($searchQuery=null){
-        if($searchQuery == null){
+    public function anySearch($searchQuery = null) {
+        if ($searchQuery == null) {
             $searchQuery = Input::get("q", null);
         }
 
-        if(strlen($searchQuery) < 2 OR $searchQuery == null){
+        if (strlen($searchQuery) < 2 OR $searchQuery == null) {
             return Redirect::route("adm.dashboard");
         }
 
         // Direct member?
-        if(is_numeric($searchQuery) && Account::find($searchQuery)){
+        if (is_numeric($searchQuery) && Account::find($searchQuery)) {
             return Redirect::route("adm.mship.account.details", [$searchQuery]);
         }
 
         // Global searches!
-        $members = Account::where("account_id", "LIKE", "%".$searchQuery."%")
-                      ->orWhere(\DB::raw("CONCAT(`name_first`, ' ', `name_last`)"), "LIKE", "%".$searchQuery."%")
-                      ->remember(60)
-                      ->limit(25)
-                      ->get();
+        $members = Account::where("account_id", "LIKE", "%" . $searchQuery . "%")
+                ->orWhere(\DB::raw("CONCAT(`name_first`, ' ', `name_last`)"), "LIKE", "%" . $searchQuery . "%")
+                ->remember(60)
+                ->limit(25)
+                ->get();
         $emails = AccountEmail::withTrashed()
-                              ->where("email", "LIKE", "%".$searchQuery."%")
-                              ->remember(60)
-                              ->limit(25)
-                              ->get();
+                ->where("email", "LIKE", "%" . $searchQuery . "%")
+                ->remember(60)
+                ->limit(25)
+                ->get();
 
-        $this->_pageTitle = "Global Search Results: ".$searchQuery;
+        $this->_pageTitle = "Global Search Results: " . $searchQuery;
         return $this->viewMake("adm.search")
-                    ->with("members", $members)
-                    ->with("emails", $emails);
+                        ->with("members", $members)
+                        ->with("emails", $emails);
     }
+
 }
