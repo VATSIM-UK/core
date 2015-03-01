eb5b90e55385841e17074d9d4aa2653d9d060fc6
NFarrington <neil.farrington@vatsim-uk.co.uk>
2015-03-01 07:58:55 +0000

    Add TS API framework and database migration; resolves #237

---
+++ app/database/migrations/2015_02_28_224937_teamspeak_database_structure.php
@@ -0,0 +1,110 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TeamspeakDatabaseStructure extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// drop tables if they exist
+		
+		Schema::dropIfExists('teamspeak_alias');
+		Schema::dropIfExists('teamspeak_ban');
+		Schema::dropIfExists('teamspeak_confirmation');
+		Schema::dropIfExists('teamspeak_log');
+		Schema::dropIfExists('teamspeak_registration');
+
+		// create table structures
+
+		Schema::create('teamspeak_alias', function($table) {
+			$table->smallInteger('id')->primary()->unsigned()->increments();
+			$table->integer('account_id')->unique()->unsigned();
+			$table->string('display_name', 30);
+			$table->string('notes', 255)->nullable();
+			$table->timestamps();
+		});
+
+		Schema::create('teamspeak_ban', function($table) {
+			$table->smallInteger('id')->primary()->unsigned()->increments();
+			$table->integer('account_id')->unsigned()->index();
+			$table->mediumInteger('duration')->unsigned();
+			$table->string('reason', 255);
+			$table->timestamps();
+			$table->softDeletes();
+		});
+
+		Schema::create('teamspeak_confirmation', function($table) {
+			$table->mediumInteger('registration_id')->primary()->unsigned()->increments();
+			$table->string('privilege_key', 50);
+			$table->string('confirmation_string', 50)->nullable();
+			$table->timestamps();
+		});
+
+		Schema::create('teamspeak_log', function($table) {
+			$table->integer('id')->primary()->unsigned()->increments();
+			$table->mediumInteger('registration_id')->unsigned()->nullable();
+			$table->string('type', 20); // switch to enum?
+			$table->string('message', 255);
+			$table->timestamps();
+		});
+
+		Schema::create('teamspeak_registration', function($table) {
+			$table->mediumInteger('id')->primary()->unsigned()->increments();
+			$table->integer('account_id')->unsigned()->index();
+			$table->bigInteger('registration_ip')->unique();
+			$table->bigInteger('last_ip')->nullable();
+			$table->timestamp('last_login')->nullable();
+			$table->string('last_os', 15)->nullable();
+			$table->timestamp('last_bot_pm')->nullable();
+			$table->string('uuid', 50)->nullable();
+			$table->smallInteger('database_id')->unsigned()->nullable();
+			$table->enum('status', ['new', 'detected', 'active', 'deleted']);
+			$table->timestamps();
+			$table->softDeletes();
+		});
+
+		// define relationships (foreign keys)
+
+		Schema::table('teamspeak_alias', function($table) {
+			$table->foreign('account_id')->references('account_id')->on('mship_account');
+		});
+
+		Schema::table('teamspeak_ban', function($table) {
+			$table->foreign('account_id')->references('account_id')->on('mship_account');
+		});
+
+		Schema::table('teamspeak_confirmation', function($table) {
+			$table->foreign('registration_id')->references('id')->on('teamspeak_registration');
+		});
+
+		Schema::table('teamspeak_log', function($table) {
+			$table->foreign('registration_id')->references('id')->on('teamspeak_registration');
+		});
+
+		Schema::table('teamspeak_registration', function($table) {
+			$table->foreign('account_id')->references('account_id')->on('mship_account');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::dropIfExists('teamspeak_alias');
+		Schema::dropIfExists('teamspeak_ban');
+		Schema::dropIfExists('teamspeak_confirmation');
+		Schema::dropIfExists('teamspeak_log');
+		Schema::dropIfExists('teamspeak_registration');
+	}
+
+}
