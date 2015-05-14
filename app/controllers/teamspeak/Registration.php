c678ac7e9df177a0a3ce4a7c8b3d4a6e7676b18d
Neil Farrington <accounts@apache-portal.com>
2015-05-14 12:55:26 +0100

    Update TeamSpeak framework to recognise CloudFlare clients

--- app/controllers/teamspeak/Registration.php
+++ app/controllers/teamspeak/Registration.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace Controllers\Teamspeak;
+<?php namespace Controllers\Teamspeak;
 
 use Redirect;
 use Response;
@@ -9,24 +7,32 @@ use Models\Teamspeak\Registration as RegistrationModel;
 use Models\Teamspeak\Confirmation as ConfirmationModel;
 use Controllers\Teamspeak\TeamspeakAdapter;
 
-class Registration extends \Controllers\BaseController {
+class Registration extends \Controllers\BaseController
+{
 
     // create new registration process
-    public function getNew() {
-        if (count($this->_account->teamspeak_registrations) >= 3)
+    public function getNew()
+    {
+        if (count($this->_account->teamspeak_registrations) >= 3) {
             return Redirect::route("mship.manage.dashboard");
+        }
 
-        if (!$this->_account->new_registration)
-            $_registration = $this->createRegistration($this->_account->account_id,
-                                                                    $this->_account->last_login_ip);
-        else
+        if (!$this->_account->new_registration) {
+            $registration_ip = $_SERVER["HTTP_CF_CONNECTING_IP"] ?: $this->_account->last_login_ip;
+            $_registration = $this->createRegistration($this->_account->account_id, $registration_ip);
+        } else {
             $_registration = $this->_account->new_registration->load('confirmation');
+        }
 
-        if (!$_registration->confirmation)
-            $_confirmation = $this->createConfirmation($_registration->id,
-                md5($_registration->created_at->timestamp), $this->_account->account_id);
-        else
+        if (!$_registration->confirmation) {
+            $_confirmation = $this->createConfirmation(
+                $_registration->id,
+                md5($_registration->created_at->timestamp),
+                $this->_account->account_id
+            );
+        } else {
             $_confirmation = $_registration->confirmation;
+        }
 
         $this->_pageTitle = "New Registration";
         $view = $this->viewMake("teamspeak.new");
@@ -35,25 +41,33 @@ class Registration extends \Controllers\BaseController {
         return $view;
     }
 
-    public function getConfirmed() {
+    public function getConfirmed()
+    {
         return $this->viewMake("teamspeak.success");
     }
 
     // delete registration (if owned)
-    public function getDelete($registration) {
-        if ($this->_account->account_id == $registration->account_id) $registration->delete();
+    public function getDelete($registration)
+    {
+        if ($this->_account->account_id == $registration->account_id) {
+            $registration->delete();
+        }
         return Redirect::back();
     }
 
     // get status of registration
-    public function postStatus($registration) {
-        if ($this->_account->account_id == $registration->account_id)
+    public function postStatus($registration)
+    {
+        if ($this->_account->account_id == $registration->account_id) {
             return Response::make($registration->status);
-        else return Response::make("Cannot retrieve registration status.");
+        } else {
+            return Response::make("Cannot retrieve registration status.");
+        }
     }
 
     // create a new registration model
-    protected function createRegistration($accountID, $registrationIP) {
+    protected function createRegistration($accountID, $registrationIP)
+    {
         $_registration = new RegistrationModel();
         $_registration->account_id = $accountID;
         $_registration->registration_ip = $registrationIP;
@@ -63,7 +77,8 @@ class Registration extends \Controllers\BaseController {
     }
 
     // create a new confirmation model
-    protected function createConfirmation($registrationID, $confirmationString, $accountID) {
+    protected function createConfirmation($registrationID, $confirmationString, $accountID)
+    {
         $key_description = "CID:" . $accountID . " RegID:" . $registrationID;
         $key_custominfo = "ident=registration_id value=" . $registrationID;
         $_confirmation = new ConfirmationModel();
