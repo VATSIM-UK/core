cca34a3ae3a93d9da003c70b3e6cc27fb7df9861
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2015-05-30 02:55:53 +0100

    Resolved ErrorException: Exception 'ErrorException' with message 'Trying to get property of non-object' in /var/www/core/app/controllers/sso/Authentication.php:38

--- app/controllers/sso/Authentication.php
+++ app/controllers/sso/Authentication.php
@@ -35,7 +35,7 @@ class Authentication extends \Controllers\BaseController {
             return Redirect::to("/mship/auth/login?returnURL=".urlencode(url("/sso/auth/login?token=".Request::query("token")."&return=1"))."&force=".Request::query("force", 0));
         } else {
             // We're successfully authenticated it seems... We can now return the access token.
-            $ssoToken->account_id = Auth::user()->get()->account_id;
+            $ssoToken->account_id = $this->_account->account_id;
             $ssoToken->expires_at = \Carbon\Carbon::now("GMT")->addSeconds(30)->toDateTimeString();
             $ssoToken->save();
 
