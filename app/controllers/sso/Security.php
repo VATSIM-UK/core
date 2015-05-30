d75bbf40c8468022588b87e358a866b7aed8bc46
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2015-05-30 03:10:34 +0100

    Now return is_banned flag in SSO detail to make it easier to determine (rather than using the account_status flag).  @jpfox

--- app/controllers/sso/Security.php
+++ app/controllers/sso/Security.php
@@ -133,6 +133,7 @@ class Security extends \Controllers\BaseController {
         $return["account_state_current"] = $account->current_state->label;
         $return["account_status"] = $account->status;
         $return["is_invisible"] = boolval($account->is_invisible);
+        $return["is_banned"] = boolval($account->is_banned);
         $return["experience"] = $account->experience;
         $return["reg_date"] = $account->joined_at->toDateTimeString();
         $return["impersonation"] = Session::get("auth_override", false);
