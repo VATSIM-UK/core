f8458632e21f4201d49fe1132de81f2b0c2062f1
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2015-05-31 14:30:34 +0100

    Built migration to convert enum -> varchar

---
+++ app/database/migrations/2015_05_31_132800_upgrade_v2110_v2111.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpgradeV2110V2111 extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        // Modify the TS log enum type.
+        // This is a clunky way of doing it, but DBAL doesn't support changes to enums using the inbuilt methods.
+        // We have to use this to get rid of the enum and make it VC.
+        Schema::table("teamspeak_log", function($table){
+            DB::statement('ALTER TABLE teamspeak_log CHANGE slug url VARCHAR(75)');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+    }
+}
