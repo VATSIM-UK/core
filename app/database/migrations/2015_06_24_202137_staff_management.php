720362b3d93eb0475856f956fe57a57816df699f
NFarrington <neil.farrington@vatsim-uk.co.uk>
2015-06-24 21:43:45 +0000

    Update staff_management migration

--- app/database/migrations/2015_06_24_202137_staff_management.php
+++ app/database/migrations/2015_06_24_202137_staff_management.php
@@ -23,9 +23,15 @@ class StaffManagement extends Migration
         Schema::create('staff_positions', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('department_id')->unsigned();
-            $table->integer('account_id')->unsigned()->nullable();
             $table->integer('reports_to')->unsigned()->nullable();
             $table->string('name', 50);
+            $table->timestamps();
+        });
+
+        Schema::create('staff_account_position', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('account_id')->unsigned();
+            $table->integer('position_id')->unsigned();
             $table->timestamps();
         });
 
@@ -33,15 +39,13 @@ class StaffManagement extends Migration
             $table->increments('id');
             $table->integer('attribute_id')->unsigned();
             $table->integer('position_id')->unsigned();
-            $table->integer('assigned_by')->unsigned();
-            $table->boolean('assigned');
             $table->timestamps();
         });
 
         Schema::create('staff_departments', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('parent_id')->unsigned()->nullable();
-            $table->integer('position_id')->unsigned();
+            $table->integer('position_id')->unsigned()->nullable();
             $table->string('name', 50);
             $table->timestamps();
         });
@@ -58,14 +62,17 @@ class StaffManagement extends Migration
 
         Schema::table('staff_positions', function ($table) {
             $table->foreign('department_id')->references('id')->on('staff_departments');
-            $table->foreign('account_id')->references('account_id')->on('mship_account');
             $table->foreign('reports_to')->references('id')->on('staff_positions');
+        });
+
+        Schema::table('staff_account_position', function ($table) {
+            $table->foreign('account_id')->references('account_id')->on('mship_account');
+            $table->foreign('position_id')->references('id')->on('staff_positions');
         });
 
         Schema::table('staff_attribute_position', function ($table) {
             $table->foreign('attribute_id')->references('id')->on('staff_attributes');
             $table->foreign('position_id')->references('id')->on('staff_positions');
-            $table->foreign('assigned_by')->references('id')->on('staff_positions');
         });
 
         Schema::table('staff_departments', function ($table) {
@@ -91,14 +98,17 @@ class StaffManagement extends Migration
 
         Schema::table('staff_positions', function ($table) {
             $table->dropForeign('staff_positions_department_id_foreign');
-            $table->dropForeign('staff_positions_account_id_foreign');
             $table->dropForeign('staff_positions_reports_to_foreign');
+        });
+
+        Schema::table('staff_account_position', function ($table) {
+            $table->dropForeign('staff_account_position_account_id_foreign');
+            $table->dropForeign('staff_account_position_position_id_foreign');
         });
 
         Schema::table('staff_attribute_position', function ($table) {
             $table->dropForeign('staff_attribute_position_attribute_id_foreign');
             $table->dropForeign('staff_attribute_position_position_id_foreign');
-            $table->dropForeign('staff_attribute_position_assigned_by_foreign');
         });
 
         Schema::table('staff_departments', function ($table) {
