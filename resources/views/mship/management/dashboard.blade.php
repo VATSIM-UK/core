eafe4a331a0a119f502abbc40f5c2f7a5016c19f
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2015-08-22 00:13:33 +0100

    "No registrations found" was being disabled due wrong reference to a relationship rather than a collection.

--- resources/views/mship/management/dashboard.blade.php
+++ resources/views/mship/management/dashboard.blade.php
@@ -210,7 +210,7 @@
         <th class='hidden-xs hidden-sm'>TeamSpeak Registrations @if (count($_account->teamspeak_registrations) < 3)<br /><small>[ {!! link_to_route('teamspeak.new', 'New Registration') !!} ]</small>@endif</th>
         <td>
             <span class="hidden-md hidden-lg" style="border-bottom: dashed black 1px; padding-bottom: 2px; margin-bottom: 2px;"><strong>TeamSpeak Registrations @if (count($_account->teamspeak_registrations) < 3)<br /><small>[ {!! link_to_route('teamspeak.new', 'New Registration') !!} ]</small>@endif</strong></span>
-            @if (count($_account->teamspeak_registrations) == 0)
+            @if (count($_account->teamspeakRegistrations) == 0)
                     No registrations found.
             @endif
             @foreach ($_account->teamspeakRegistrations as $tsreg)
