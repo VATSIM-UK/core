74d80a75fc228d4a91c5e16c30fd850e8768d11e
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2015-08-22 22:49:27 +0100

    Basic tests added.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,21 +1,31 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
-
-$factory->define(App\User::class, function ($faker) {
+$factory->defineAs(App\Models\Mship\Account::class, "normal", function ($faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
-        'password' => str_random(10),
-        'remember_token' => str_random(10),
+        'account_id' => 980234,
+        'name_first' => $faker->name,
+        'name_last' => $faker->name,
+        'status' => App\Models\Account::STATUS_ACTIVE,
+        'is_invisible' => 0,
+    ];
+});
+
+$factory->defineAs(App\Models\Mship\Account::class, "suspended_network", function ($faker) {
+    return [
+        'account_id' => rand(900000, 1300000),
+        'name_first' => $faker->name,
+        'name_last' => $faker->name,
+        'status' => App\Models\Account::STATUS_NETWORK_SUSPENDED,
+        'is_invisible' => 0,
+    ];
+});
+
+$factory->defineAs(App\Models\Mship\Account::class, "suspended_local", function ($faker) {
+    return [
+        'account_id' => rand(900000, 1300000),
+        'name_first' => $faker->name,
+        'name_last' => $faker->name,
+        'status' => App\Models\Account::STATUS_SYSTEM_BANNED,
+        'is_invisible' => 0,
     ];
 });
--- tests/MShipTest.php
+++ tests/MShipTest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class MshipTest extends BaseTest
+class MshipTest extends TestCase
 {
 
     use DatabaseTransactions;
@@ -12,6 +12,14 @@ class MshipTest extends BaseTest
     public function testLandingPage(){
         $this->visit('/')
              ->see('This page will redirect you automatically');
+    }
+    public function testDashboard(){
+        $user = factory(App\Models\Mship\Account::class, "normal")->make();
+
+        $this->actingAs($user)
+             ->visit("/mship/manage/dashboard")
+             ->see("Below are the current details stored by the Single Sign-On (SSO) system.");
+        $this->dump();
     }
     public function testAddSecondaryEmailSuccess(){
         $this->visit("/mship/auth/login");
