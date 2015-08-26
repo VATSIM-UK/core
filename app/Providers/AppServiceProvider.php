b1fb705be0983d89aea09c583c0d169d746f88fc
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2015-08-26 15:34:23 +0100

    Created a VATSIM.net system account for use when logging network bans. Related to issue #322.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         define("VATUK_ACCOUNT_SYSTEM", "707070");
+        define("VATSIM_ACCOUNT_SYSTEM", "606060");
 
         // Ensure that the VATSIM UK System accounts are in existence.
         // If this is not protected, we cannot run any artisan commands if there's an issue with the database.
@@ -29,6 +30,23 @@ class AppServiceProvider extends ServiceProvider
 
                 // Add all required emails by this account.
                 $a->addEmail("no-reply@vatsim-uk.co.uk", true, true);
+            }
+        }
+
+        // Ensure that the VATSIM.NET System accounts are in existence.
+        // If this is not protected, we cannot run any artisan commands if there's an issue with the database.
+        if(!$this->app->runningInConsole()){
+            $check = \Models\Mship\Account::find(VATSIM_ACCOUNT_SYSTEM);
+            if(!is_object($check) || !$check->exists){
+                $a = new \Models\Mship\Account();
+                $a->account_id = VATSIM_ACCOUNT_SYSTEM;
+                $a->name_first = "VATSIM";
+                $a->name_last = "NET";
+                $a->is_system = true;
+                $a->save();
+
+                // Add all required emails by this account.
+                $a->addEmail("no-reply@vatsim.net", true, true);
             }
         }
     }
