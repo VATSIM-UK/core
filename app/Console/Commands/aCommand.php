17a2ed2b5a01b456e0c34c0aa474e4e57bc5ad12
Anthony Lawrence <freelancer@anthonylawrence.me.uk>
2015-08-29 17:50:54 +0100

    Spotted aCommand using an old session key for processing things. Switched to login via Auth.

--- app/Console/Commands/aCommand.php
+++ app/Console/Commands/aCommand.php
@@ -12,7 +12,7 @@ class aCommand extends Command {
      * @return void
      */
     public function __construct() {
-        Session::set("auth_adm_account", 707070);
+        \Auth::loginUsingId(707070);
 
         parent::__construct();
     }
