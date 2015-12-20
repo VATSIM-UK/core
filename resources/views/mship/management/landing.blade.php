e6198c0009ed642ca00738a3eef2ec18aa12a8f4
NFarrington <neil.farrington@vatsim-uk.co.uk>
2015-12-20 20:04:03 +0000

    Correct VATSIM-UK to VATSIM UK

--- resources/views/mship/management/landing.blade.php
+++ resources/views/mship/management/landing.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <p>
-        You are currently <strong>not</strong> logged in to our Core system.  VATSIM.net now have their own Single-Sign On system for use by divisions so you <strong>will not</strong> be presented with the standard VATSIM-UK login page.
+        You are currently <strong>not</strong> logged in to our Core system.  VATSIM.net now have their own Single-Sign On system for use by divisions so you <strong>will not</strong> be presented with the standard VATSIM UK login page.
     </p>
     <p>
         This page will redirect you automatically in <span id='timerCount'>10</span> seconds.  If you don't wish to wait, you can <?=HTML::link("/mship/auth/login", "start your login now")?>.
