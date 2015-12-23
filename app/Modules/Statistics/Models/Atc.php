2f82b1cd5a84f96925bbd52894bee1c564d74432
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2015-12-23 19:00:55 +0000

    Triggering events from the Atc model on create/update/delete.

--- app/Modules/Statistics/Models/Atc.php
+++ app/Modules/Statistics/Models/Atc.php
@@ -2,20 +2,36 @@
 
 namespace App\Modules\Statistics\Models;
 
+use App\Modules\Statistics\Events\AtcSessionStarted;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use \Event;
 use App\Modules\Statistics\Events\AtcSessionEnded;
+use App\Modules\Statistics\Events\AtcSessionDeleted;
 
 class Atc extends Model
 {
     use SoftDeletes;
 
-    protected $table      = "statistics_atc";
+    protected $table      = "statistic_atc";
     protected $primaryKey = "id";
     protected $fillable   = ["account_id", "qualification_id", "callsign", "connected_at", "disconnected_at"];
     public    $dates      = ["connected_at", "disocnnected_at", "created_at", "updated_at", "deleted_at"];
     public    $timestamps = true;
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($model) {
+            event(new AtcSessionStarted($model));
+        });
+
+        static::deleted(function($model){
+            event(new AtcSessionDeleted($model));
+        });
+
+    }
 
     public static function scopeForAccountId($query, $id)
     {
@@ -41,15 +57,15 @@ class Atc extends Model
     {
         return $query->where("disconnected_at", "IS NOT", null);
     }
-
-    public static function firstOrCreate($id, $attributes = [])
-    {
-        parent::firstOrCreate($id, $attributes);
-    }
+//
+//    public static function firstOrCreate($attributes = [])
+//    {
+//        parent::firstOrCreate($attributes);
+//    }
 
     public function setDisconnectedAtAttribute($timestamp)
     {
         $this->attributes['disconnected_at'] = $timestamp;
-        Event::fire(new AtcSessionEnded($this));
+        event(new AtcSessionEnded($this));
     }
 }
