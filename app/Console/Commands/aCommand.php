55f41cddba421b2f4cb1f326e12a8bd48291a82b
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2015-12-30 21:38:32 +0000

    Setup the slack integration and provided useful methods for all commands.

--- app/Console/Commands/aCommand.php
+++ app/Console/Commands/aCommand.php
@@ -2,10 +2,13 @@
 
 namespace App\Console\Commands;
 
+use Maknz\Slack\Client;
 use Session;
 use Illuminate\Console\Command;
 
 class aCommand extends Command {
+    protected $slack = null;
+
     /**
      * Create a new command instance.
      *
@@ -13,5 +16,30 @@ class aCommand extends Command {
      */
     public function __construct() {
         parent::__construct();
+
+        // Configure slack for Cron!
+        $settings = [
+            "channel" => "wscronjobs",
+            "link_names" => true,
+        ];
+        $this->slack = new Client("https://hooks.slack.com/services/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", $settings);
+    }
+
+    protected function sendSlackError($errorCode, $error){
+        $slackMessage = "@here :exclamation: *This is important*.";
+        $slackMessage.= " ".$this->getName(). " (ERROR_".$errorCode.") -> ";
+        $slackMessage.= $error;
+
+        $this->slack->send($slackMessage);
+    }
+
+    protected function sendSlackSuccess($message="Has run successfully."){
+        $slackMessage = "*".$this->getName(). "* -> ";
+
+        if($message){
+            $slackMessage.= "_".$message."_";
+        }
+
+        $this->slack->send($slackMessage);
     }
 }
