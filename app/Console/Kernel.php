c3a5a5db8bd799f85e6b5e087cccdcb31e2b15d0
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2015-12-30 22:27:51 +0000

    Forgot to remove testCommand in feature/slack_cron_commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\TestCommand::class,
         Commands\MembersCertImport::class,
         Commands\MembersCertUpdate::class,
         Commands\SysStatisticsDaily::class,
