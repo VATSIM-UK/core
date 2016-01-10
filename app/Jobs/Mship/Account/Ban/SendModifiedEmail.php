5a4c5e738bd3cba534008d1031b5da4b8d77846d
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2016-01-10 22:12:40 +0000

    Added useful jobs for bans.

---
+++ app/Jobs/Mship/Account/Ban/SendModifiedEmail.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Jobs\Mship\Account\Ban;
+
+use App\Jobs\Job;
+use Illuminate\Contracts\Bus\SelfHandling;
+
+class SendModifiedEmail extends Job implements SelfHandling
+{
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
