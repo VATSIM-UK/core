9bb9014ba3914ce437ed097c7c4a5fe09566e355
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2016-01-10 16:46:49 +0000

    Further email changes/improvements.

---
+++ app/Jobs/Sys/DispatchNotificationEmail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs\Sys;
+
+use App\Jobs\Job;
+use App\Models\Mship\Account;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class DispatchNotificationEmail extends Job implements SelfHandling, ShouldQueue {
+    use InteractsWithQueue, SerializesModels;
+
+    private $senderEmail = null;
+    private $displaySenderAs = null;
+    private $recipient = null;
+    private $recipientEmail = null;
+    private $subject = null;
+    private $body = null;
+
+    // TODO: Not happy with this many params.  Fix it, please.
+    public function __construct($senderName, $senderEmail, Account $recipient, Account\Email $recipientEmail = null, $subject, $body){
+        $this->$senderEmail = $senderEmail;
+        $this->displaySenderAs = $senderName;
+        $this->recipient = $recipient;
+        $this->recipientEmail = $recipientEmail;
+        $this->subject = $subject;
+        $this->body = $body;
+    }
+
+    public function handle(Mailer $mailer){
+        $mailer->send("emails.messages.post", ["recipient" => $this->recipient, "body" => $this->body], function($m) {
+            $m->subject($this->subject);
+            $m->to($this->recipientEmail->email, $this->recipient->name);
+        });
+    }
+}
