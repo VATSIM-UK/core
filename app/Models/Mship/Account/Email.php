1d9e20c3a785f49c6f833a9e6f01b9553c4cf300
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2016-02-20 03:41:16 +0000

    Fixed issue #376 - emails are now hard delete.

--- app/Models/Mship/Account/Email.php
+++ app/Models/Mship/Account/Email.php
@@ -4,8 +4,6 @@ namespace App\Models\Mship\Account;
 
 use App\Jobs\Mship\Email\TriggerNewEmailVerificationProcess;
 use App\Models\Sso\Email as SSOEmail;
-use App\Traits\RecordsActivity;
-use Illuminate\Database\Eloquent\SoftDeletes as SoftDeletingTrait;
 use Validator;
 
 /**
@@ -31,22 +29,10 @@ use Validator;
 class Email extends \Eloquent
 {
 
-    use SoftDeletingTrait, RecordsActivity;
-
     protected $table      = "mship_account_email";
     protected $dates      = ['created_at', 'updated_at', 'deleted_at'];
     protected $fillable   = ['email'];
     protected $touches    = ['account'];
-
-    public function account()
-    {
-        return $this->hasOne("\App\Models\Mship\Account", "account_id", "account_id");
-    }
-
-    public function tokens()
-    {
-        return $this->morphMany("\App\Models\Sys\Token", "related");
-    }
 
     public function scopeEmailMatches($query, $email){
         return $query->where("email", "LIKE", sanitize_email($email));
@@ -57,9 +43,19 @@ class Email extends \Eloquent
         return $query->whereNotNull("verified_at");
     }
 
+    public function account()
+    {
+        return $this->hasOne(\App\Models\Mship\Account::class, "account_id", "account_id");
+    }
+
+    public function tokens()
+    {
+        return $this->morphMany(\App\Models\Sys\Token::class, "related");
+    }
+
     public function ssoEmails()
     {
-        return $this->hasMany("\App\Models\Sso\Email", "account_email_id", "account_email_id");
+        return $this->hasMany(\App\Models\Sso\Email::class, "account_email_id");
     }
 
     public function assignToSso($ssoAccount)
@@ -106,15 +102,12 @@ class Email extends \Eloquent
     /**
      * Save the email's current state.
      *
-     * Prior to saving, check if this email already exists in the database.
      * If the email isn't verified, a verification email will be dispatched.
      *
      * @param array $options Additional options to use when saving this Email.
      * @return boolean
      */
     public function save(array $options=[]){
-
-
         $saveResult = parent::save($options);
 
         if(!$this->is_verified){
