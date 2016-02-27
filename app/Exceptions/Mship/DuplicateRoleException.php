81484959317645dcd14abf6fefafdc1a4eec3bee
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2016-02-27 21:56:35 +0000

    Duplicate role exception, to be thrown when adding a duplicate role to a members account.  Relates to issue #387.

---
+++ app/Exceptions/Mship/DuplicateRoleException.php
@@ -0,0 +1,14 @@
+<?php namespace App\Exceptions\Mship;
+
+class DuplicateRoleException extends \Exception {
+
+    private $role;
+
+    public function __construct($role){
+        $this->role = $role;
+    }
+
+    public function __toString(){
+        return "The role '" . $this->role->name . "' is already attached to this account.";
+    }
+}
