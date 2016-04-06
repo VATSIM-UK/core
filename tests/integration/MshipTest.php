bf597aff91660cf38c3e8b1079c31a77f8f27a83
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2016-04-06 15:26:04 +0100

    Outline for application testing Mship.

--- tests/integration/MshipTest.php
+++ tests/integration/MshipTest.php
@@ -6,12 +6,34 @@ class MshipTest extends TestCase
 {
     use DatabaseTransactions;
 
-    /** @test **/
+    /** @test * */
     public function it_redirects_to_the_landing_page_when_viewing_the_root_url()
     {
-        $this->visit("/");
+        $this->visit("/")
+             ->seePageIs(route("mship.manage.landing"));
+    }
+    
+    /** @test **/
+    public function it_authenticates_a_user_post_vatsim_cert_return()
+    {
+        
+    }
+    
+    /** @test **/
+    public function it_determines_that_cert_is_offline_and_offers_alternative_login()
+    {
+        
+    }
 
-        $this->assertRedirectedToRoute("mship.manage.landing");
+    /** @test **/
+    public function it_displays_the_members_dashboard()
+    {
+
+    }
+
+    /** @test **/
+    public function it_redirects_away_from_the_dashboard_if_the_member_isnt_logged_in()
+    {
 
     }
 }
