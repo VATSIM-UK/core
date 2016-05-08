b9aadf5bb2d0211999f7e9873b2e4c32682d0a39
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2016-05-08 21:52:09 +0100

    Added additional scopes to referee model.

--- app/Modules/Visittransfer/Models/Referee.php
+++ app/Modules/Visittransfer/Models/Referee.php
@@ -7,26 +7,41 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * App\Modules\Ais\Models\Fir
  *
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Modules\Ais\Models\Aerodrome[] $airfields
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Modules\Ais\Models\Aerodrome[]  $airfields
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Modules\Ais\Models\Fir\Sector[] $sectors
  */
-class Referee extends Model {
+class Referee extends Model
+{
 
-    protected $table = "vt_referee";
-    protected $fillable = [
-        "application_id", "account_id",
-        "email", "relationship",
+    protected $table      = "vt_referee";
+    protected $fillable   = [
+        "application_id",
+        "account_id",
+        "email",
+        "relationship",
     ];
-    protected $touches = ["application"];
-    public $timestamps = false;
+    protected $touches    = ["application"];
+    public    $timestamps = false;
 
-    const STATUS_DRAFT = 10;
-    const STATUS_REQUESTED = 30;
-    const STATUS_COMPLETED = 35;
+    const STATUS_DRAFT        = 10;
+    const STATUS_REQUESTED    = 30;
+    const STATUS_COMPLETED    = 35;
     const STATUS_UNDER_REVIEW = 50;
-    const STATUS_ACCEPTED = 90;
-    const STATUS_REJECTED = 95;
+    const STATUS_ACCEPTED     = 90;
+    const STATUS_REJECTED     = 95;
+
+    public static function scopePending($query)
+    {
+        return $query->whereNull("submitted_at");
+    }
+
+    public static function scopeStatus($query, $status){
+        return $query->where("status", "=", $status);
+    }
+
+    public static function scopeStatusIn($query, Array $stati){
+        return $query->whereIn("status", $stati);
+    }
 
     public function account()
     {
