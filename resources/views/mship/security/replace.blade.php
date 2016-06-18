e6d93a5634d708ef39bdb013ceb10f8cbac1f3f2
Neil Farrington <accounts@apache-portal.com>
2016-06-18 17:23:12 +0100

    Add password reset link to security replacement; fixes #324

--- resources/views/mship/security/replace.blade.php
+++ resources/views/mship/security/replace.blade.php
@@ -58,9 +58,35 @@
             <div class="form-group">
                 <div class="col-sm-offset-5 col-sm-7">
                     <button type="submit" class="btn btn-default" name="processextra_security_replace" value="extra_security_replace">Proceed</button>
+                    <a class="btn btn-default" href='#' onclick='javascript: checkResetPassword();'>Forgotten Password</a>
                 </div>
             </div>
         </form>
     </div>
 </div>
+
+<div class="modal fade" id="resetConfirmModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Reset Secondary Password</h4>
+            </div>
+            <div class="modal-body">
+                <p>Once you click this button, your old secondary password will be gone forever.  We'll then start the password recovery process for you - are you sure you wish to continue?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-danger" id="confirm" onclick="javascript: window.location.href ='{{ URL::route("mship.security.forgotten") }}';">Confirm</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<!-- Dialog show event handler -->
+<script type="text/javascript">
+    function checkResetPassword(){
+        $('#resetConfirmModal').modal()
+    }
+</script>
 @stop
