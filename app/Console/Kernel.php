7d184078d91185b4b4c6d775dbd95a3b8e95922f
Neil Farrington <accounts@apache-portal.com>
2016-06-29 16:46:35 -0700

    Load module commands from their respective directories

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,7 +3,11 @@
 namespace App\Console;
 
 use App\Console\Commands\TestCommand;
+use Caffeinated\Modules\Facades\Module;
+use File;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -29,6 +33,24 @@ class Kernel extends ConsoleKernel
 
         \App\Modules\NetworkData\Console\Commands\DownloadAndParse::class,
     ];
+
+    /**
+     * Get the Artisan application instance.
+     *
+     * @return \Illuminate\Console\Application
+     */
+    protected function getArtisan()
+    {
+        foreach (Module::enabled() as $module) {
+            $moduleCommandsFile = config('modules.path').'/'.$module['namespace'].'/Console/commands.php';
+            if (File::exists($moduleCommandsFile)) {
+                $moduleCommands = require $moduleCommandsFile;
+                $this->commands = array_merge($this->commands, $moduleCommands);
+            }
+        }
+
+        return parent::getArtisan();
+    }
 
     /**
      * Define the application's command schedule.
