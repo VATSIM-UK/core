30a1707b2cd6674e96dd4dfc2a3ad72918da4c67
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2016-08-21 00:31:15 +0100

    Handling the event for statusChanged on Application.

--- app/Modules/Visittransfer/Providers/EventServiceProvider.php
+++ app/Modules/Visittransfer/Providers/EventServiceProvider.php
@@ -13,6 +13,7 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         \App\Modules\Visittransfer\Events\ApplicationUnderReview::class => [
             \App\Modules\Visittransfer\Listeners\NotifyApplicantOfStatusChange::class,
+            \App\Modules\Visittransfer\Listeners\NotifyCommunityOfUnderReviewApplication::class,
         ],
 
         \App\Modules\Visittransfer\Events\ApplicationSubmitted::class => [
@@ -20,13 +21,13 @@ class EventServiceProvider extends ServiceProvider
             \App\Modules\Visittransfer\Listeners\NotifyAllReferees::class,
         ],
 
+        \App\Modules\Visittransfer\Events\ApplicationStatusChanged::class => [
+            \App\Modules\Visittransfer\Listeners\NotifyApplicantOfStatusChange::class,
+        ],
+
         \App\Modules\Visittransfer\Events\ReferenceUnderReview::class => [
             \App\Modules\Visittransfer\Listeners\NotifyRefereeOnReferenceCompletion::class,
             \App\Modules\Visittransfer\Listeners\NotifyApplicantOfReferenceCompletion::class,
-        ],
-
-        \App\Modules\Visittransfer\Events\ApplicationStatusChanged::class => [
-            \App\Modules\Visittransfer\Listeners\NotifyApplicantOfStatusChange::class,
         ],
     ];
 
