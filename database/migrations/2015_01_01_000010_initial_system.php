79527a0501ea563936a33af5e58561a634a12b02
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2016-09-02 20:55:42 +0100

    Fixes #548 - Exiled consolidation migration (@Nfarrington will be very happy).

--- database/migrations/2015_01_01_000010_initial_system.php
+++ database/migrations/2015_01_01_000010_initial_system.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class VanillaSystemV221 extends Migration
+class InitialSystem extends Migration
 {
     /**
      * Run the migrations.
