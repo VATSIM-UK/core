07c708c7184a83ec6ba6af65b4f27462114b11e3
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2016-09-02 21:51:48 +0100

    Changed the type hinting on the paremeter from ReferenceUnderReview to ReferenceDeleted to fit with the event itself.

--- app/Modules/Visittransfer/Listeners/NotifyRefereeOfReferenceDeletion.php
+++ app/Modules/Visittransfer/Listeners/NotifyRefereeOfReferenceDeletion.php
@@ -1,7 +1,6 @@
 <?php namespace App\Modules\Visittransfer\Listeners;
 
-use App\Modules\Visittransfer\Events\ReferenceUnderReview;
-use App\Modules\Visittransfer\Jobs\SendApplicantReferenceSubmissionEmail;
+use App\Modules\Visittransfer\Events\ReferenceDeleted;
 use App\Modules\Visittransfer\Jobs\SendRefereeConfirmationEmail;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -13,7 +12,7 @@ class NotifyRefereeOnReferenceCompletion implements ShouldQueue
         //
     }
 
-    public function handle(ReferenceUnderReview $event)
+    public function handle(ReferenceDeleted $event)
     {
         $confirmationEmailJob = new SendRefereeConfirmationEmail($event->reference);
 
