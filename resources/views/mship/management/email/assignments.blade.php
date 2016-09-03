1cec8f87f0c3495afda417bfc31b3cada71ebb21
Alex Toff <admin@cobaltgrid.com>
2016-09-03 14:14:17 +0100

    Fixes #444 - Added white-box blue-header to pages in the issue.

--- resources/views/mship/management/email/assignments.blade.php
+++ resources/views/mship/management/email/assignments.blade.php
@@ -1,74 +1,79 @@
 @extends('layout')
 
 @section('content')
+<div class="panel panel-ukblue">
+	<div class="panel-heading">	Email Assignments</div>
+	<div class="panel-body">
+			<!-- Content Of Panel [START] -->
+		<p>
+			When you are logged into systems within VATSIM UK, as standard we use your <strong>primary</strong> email address
+			when sending you important emails from that service.
+		</p>
+		<p>
+			If you wish for <em>some</em> services to dispatch emails to an alternative address <strong>instead of your primary email</strong>
+			you can specify that using the matrix below.
+		</p>
+		<p>
+			<strong>Please note:</strong> you may only have <em>one</em> email address assigned to a specific system.  Once that email address
+			expires (or becomes unverified after a period of time) we will default back to your primary email.
+		</p>
 
-    <p>
-        When you are logged into systems within VATSIM UK, as standard we use your <strong>primary</strong> email address
-        when sending you important emails from that service.
-    </p>
-    <p>
-        If you wish for <em>some</em> services to dispatch emails to an alternative address <strong>instead of your primary email</strong>
-        you can specify that using the matrix below.
-    </p>
-    <p>
-        <strong>Please note:</strong> you may only have <em>one</em> email address assigned to a specific system.  Once that email address
-        expires (or becomes unverified after a period of time) we will default back to your primary email.
-    </p>
+		{!! Form::open(["route" => ["mship.manage.email.assignments"], "class" => "form-horizontal"]) !!}
+		<table class="table table-striped table-bordered">
+			<thead>
+				<tr>
+					<th class="col-md-2">
+						Email &#8594;<br />
+						System &#8595;
+					</th>
+					<th>PRIMARY<br />{{ $userPrimaryEmail }}</th>
+					@foreach($userSecondaryVerified as $email)
+						<th>SECONDARY<br />{{ $email->email }}</th>
+					@endforeach
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($userMatrix as $um)
+					<tr>
+						<th>{{ $um['sso_system']->name }}</th>
+						<td>
+							@if($um['assigned_email_id'] == $userPrimaryEmail)
+								<em>Default</em>
+							@else
+								<div class="radio">
+									<label>
+										<input type="radio" name="assign_{{ $um['sso_system']->id }}" value="pri">
+										Return to default
+									</label>
+								</div>
+							@endif
+						</td>
+						@foreach($userSecondaryVerified as $email)
+							<td>
+								<div class="radio">
+									<label>
+										<input type="radio" name="assign_{{ $um['sso_system']->id }}" value="{{ $email->id }}" {{ $email->id == $um['assigned_email_id'] ? "checked='checked'" : "" }}>
+										Assign
+									</label>
+								</div>
+							</td>
+						@endforeach
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
 
-    {!! Form::open(["route" => ["mship.manage.email.assignments"], "class" => "form-horizontal"]) !!}
-    <table class="table table-striped table-bordered">
-        <thead>
-            <tr>
-                <th class="col-md-2">
-                    Email &#8594;<br />
-                    System &#8595;
-                </th>
-                <th>PRIMARY<br />{{ $userPrimaryEmail }}</th>
-                @foreach($userSecondaryVerified as $email)
-                    <th>SECONDARY<br />{{ $email->email }}</th>
-                @endforeach
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($userMatrix as $um)
-                <tr>
-                    <th>{{ $um['sso_system']->name }}</th>
-                    <td>
-                        @if($um['assigned_email_id'] == $userPrimaryEmail)
-                            <em>Default</em>
-                        @else
-                            <div class="radio">
-                                <label>
-                                    <input type="radio" name="assign_{{ $um['sso_system']->id }}" value="pri">
-                                    Return to default
-                                </label>
-                            </div>
-                        @endif
-                    </td>
-                    @foreach($userSecondaryVerified as $email)
-                        <td>
-                            <div class="radio">
-                                <label>
-                                    <input type="radio" name="assign_{{ $um['sso_system']->id }}" value="{{ $email->id }}" {{ $email->id == $um['assigned_email_id'] ? "checked='checked'" : "" }}>
-                                    Assign
-                                </label>
-                            </div>
-                        </td>
-                    @endforeach
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+		<div class="form-group">
+			<div class="col-sm-offset-5 col-sm-7">
+				<button type="submit" class="btn btn-default" name="processassignments"
+						value="new_email_add">Save Assignments
+				</button>
+			</div>
+		</div>
 
-    <div class="form-group">
-        <div class="col-sm-offset-5 col-sm-7">
-            <button type="submit" class="btn btn-default" name="processassignments"
-                    value="new_email_add">Save Assignments
-            </button>
-        </div>
-    </div>
-
-    {!! Form::close() !!}
-
+		{!! Form::close() !!}
+		<!-- Content Of Panel [END] -->
+	</div>
+</div>
 @stop
 --}}
--- resources/views/mship/security/replace.blade.php
+++ resources/views/mship/security/replace.blade.php
@@ -1,92 +1,97 @@
 @extends('layout')
 
 @section('content')
+<div class="panel panel-ukblue">
+	<div class="panel-heading">  Secondary Password</div>
+	<div class="panel-body">
+		<?php if($disable): ?>
+			<p>To disable your secondary password, please enter your current password below.</p>
+		<?php else: ?>
+			<p>
+				<?php if (isset($sls_type) && $sls_type == "forced"): ?>
+					An administrator has requested that you create a second password before continuing.  You will not be able to use any services until this action has been completed.
+				<?php elseif (isset($sls_type) && $sls_type == "requested"): ?>
+					You have requested to set a secondary password on your account.  Please complete the form below to complete this request.
+				<?php elseif (isset($sls_type) && $sls_type == "expired"): ?>
+					Your previous secondary password has expired, please enter your old password along with a new one below.  Remember though, they must be different!
+				<?php else: ?>
+					To change your secondary password, complete the form below.
+				<?php endif; ?>
+			</p>
 
-<?php if($disable): ?>
-    <p>To disable your secondary password, please enter your current password below.</p>
-<?php else: ?>
-    <p>
-        <?php if (isset($sls_type) && $sls_type == "forced"): ?>
-            An administrator has requested that you create a second password before continuing.  You will not be able to use any services until this action has been completed.
-        <?php elseif (isset($sls_type) && $sls_type == "requested"): ?>
-            You have requested to set a secondary password on your account.  Please complete the form below to complete this request.
-        <?php elseif (isset($sls_type) && $sls_type == "expired"): ?>
-            Your previous secondary password has expired, please enter your old password along with a new one below.  Remember though, they must be different!
-        <?php else: ?>
-            To change your secondary password, complete the form below.
-        <?php endif; ?>
-    </p>
+			<?php if (isset($requirements)): ?>
+				<div class="alert alert-warning">
+					<strong>Please note, your password must contain:</strong>
+					<ul>
+						<?php foreach ($requirements as $r): ?>
+							<li>&nbsp;&nbsp;<?= $r ?></li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+			<?php endif; ?>
+		<?php endif; ?>
 
-    <?php if (isset($requirements)): ?>
-        <div class="alert alert-warning">
-            <strong>Please note, your password must contain:</strong>
-            <ul>
-                <?php foreach ($requirements as $r): ?>
-                    <li>&nbsp;&nbsp;<?= $r ?></li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
-    <?php endif; ?>
-<?php endif; ?>
+		<div class="row">
+			<div class="col-md-7 col-md-offset-2">
+				{!! Form::open(["route" => ["mship.security.replace", $disable], "class" => "form-horizontal"]) !!}
+					<?php if ((isset($sls_type) && ($sls_type == "replace" OR $sls_type == "expired" OR $sls_type == "forced")) OR $disable): ?>
+						<div class="form-group">
+							<label class="col-sm-5 control-label" for="old_password">Old Password</label>
+							<div class="col-sm-7">
+								<input class="form-control" type="password" id="old_password" name="old_password" placeholder="Old Password">
+								<span class="help-block col-md-offset-1">May be a temporary password</span>
+							</div>
+						</div>
+					<?php endif; ?>
+					<?php if(!$disable): ?>
+						<div class="form-group">
+							<label class="col-sm-5 control-label" for="new_password">New Password</label>
+							<div class="col-sm-7">
+								<input class="form-control" type="password" id="new_password" name="new_password" placeholder="New Password">
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-5 control-label" for="new_password2">Confirm New Password</label>
+							<div class="col-sm-7">
+								<input class="form-control" type="password" id="new_password2" name="new_password2" placeholder="Confirm New Password">
+							</div>
+						</div>
+					<?php endif; ?>
+					<div class="form-group">
+						<div class="col-sm-offset-5 col-sm-7">
+							<button type="submit" class="btn btn-default" name="processextra_security_replace" value="extra_security_replace">Proceed</button>
+							<a class="btn btn-default" href='#' onclick='javascript: checkResetPassword();'>Forgotten Password</a>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
 
-<div class="row">
-    <div class="col-md-7 col-md-offset-2">
-        {!! Form::open(["route" => ["mship.security.replace", $disable], "class" => "form-horizontal"]) !!}
-            <?php if ((isset($sls_type) && ($sls_type == "replace" OR $sls_type == "expired" OR $sls_type == "forced")) OR $disable): ?>
-                <div class="form-group">
-                    <label class="col-sm-5 control-label" for="old_password">Old Password</label>
-                    <div class="col-sm-7">
-                        <input class="form-control" type="password" id="old_password" name="old_password" placeholder="Old Password">
-                        <span class="help-block col-md-offset-1">May be a temporary password</span>
-                    </div>
-                </div>
-            <?php endif; ?>
-            <?php if(!$disable): ?>
-                <div class="form-group">
-                    <label class="col-sm-5 control-label" for="new_password">New Password</label>
-                    <div class="col-sm-7">
-                        <input class="form-control" type="password" id="new_password" name="new_password" placeholder="New Password">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-5 control-label" for="new_password2">Confirm New Password</label>
-                    <div class="col-sm-7">
-                        <input class="form-control" type="password" id="new_password2" name="new_password2" placeholder="Confirm New Password">
-                    </div>
-                </div>
-            <?php endif; ?>
-            <div class="form-group">
-                <div class="col-sm-offset-5 col-sm-7">
-                    <button type="submit" class="btn btn-default" name="processextra_security_replace" value="extra_security_replace">Proceed</button>
-                    <a class="btn btn-default" href='#' onclick='javascript: checkResetPassword();'>Forgotten Password</a>
-                </div>
-            </div>
-        </form>
-    </div>
+		<div class="modal fade" id="resetConfirmModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						<h4 class="modal-title">Reset Secondary Password</h4>
+					</div>
+					<div class="modal-body">
+						<p>Once you click this button, your old secondary password will be gone forever.  We'll then start the password recovery process for you - are you sure you wish to continue?</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+						<button type="button" class="btn btn-danger" id="confirm" onclick="javascript: window.location.href ='{{ URL::route("mship.security.forgotten") }}';">Confirm</button>
+					</div>
+				</div><!-- /.modal-content -->
+			</div><!-- /.modal-dialog -->
+		</div><!-- /.modal -->
+
+		<!-- Dialog show event handler -->
+		<script type="text/javascript">
+			function checkResetPassword(){
+				$('#resetConfirmModal').modal()
+			}
+		</script>
+		<!-- Content Of Panel [END] -->
+	</div>
 </div>
-
-<div class="modal fade" id="resetConfirmModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Reset Secondary Password</h4>
-            </div>
-            <div class="modal-body">
-                <p>Once you click this button, your old secondary password will be gone forever.  We'll then start the password recovery process for you - are you sure you wish to continue?</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-danger" id="confirm" onclick="javascript: window.location.href ='{{ URL::route("mship.security.forgotten") }}';">Confirm</button>
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
-<!-- Dialog show event handler -->
-<script type="text/javascript">
-    function checkResetPassword(){
-        $('#resetConfirmModal').modal()
-    }
-</script>
 @stop
