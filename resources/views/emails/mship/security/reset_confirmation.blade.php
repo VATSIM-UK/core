3b4c607f0151841832253f57eb41da50264beadf
NFarrington <neil.farrington@vatsim-uk.co.uk>
2016-09-25 23:21:54 +0000

    Fix password reset email links

--- resources/views/emails/mship/security/reset_confirmation.blade.php
+++ resources/views/emails/mship/security/reset_confirmation.blade.php
@@ -6,8 +6,8 @@
     In order to verify this request, you need to click the link in this email.  This request will expire in 12 hours.
 </p>
 
-<p>To authorise this request, please {!! link_to(url("/mship/security/forgotten-link", ["code" => $token->code]), "click here") !!}.  Alternatively, copy the link below into your browser:</p>
+<p>To authorise this request, please {!! link_to(route("mship.security.forgotten.link", ["code" => $token->code]), "click here") !!}.  Alternatively, copy the link below into your browser:</p>
 
 <p>
-    {{{ url("/mship/security/forgotten-link", ["code" => $token->code]) }}}
+    {{ route("mship.security.forgotten.link", ["code" => $token->code]) }}
 </p>
