38d09bb2e15299cb2b96980269ca7df645527eeb
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2016-12-19 17:00:19 +0000

    Further improvements to ATC Stats Tracking.

--- app/Modules/NetworkData/Database/Migrations/2015_12_23_160400_create_atc_statistics_tables.php
+++ app/Modules/NetworkData/Database/Migrations/2015_12_23_160400_create_atc_statistics_tables.php
@@ -11,25 +11,17 @@ class CreateAtcStatisticsTables extends Migration
      */
     public function up()
     {
-        // This check is required as this table was accidentally added to a very early migration.
-        // It's now in use on production environments.
-        // Old migration updated, so that the modules make sense on their own.
-
-        if(!Schema::hasTable("statistic_atc")){
-            Schema::create('networkdata_atc', function ($table) {
-                $table->bigIncrements('id')->unsigned();
-                $table->integer('account_id')->unsigned();
-                $table->string('callsign', 10);
-                $table->smallInteger('qualification_id')->unsigned();
-                $table->tinyInteger('facility_type')->unsigned();
-                $table->timestamp('connected_at')->nullable();
-                $table->timestamp('disconnected_at')->nullable();
-                $table->timestamps();
-                $table->softDeletes();
-            });
-        } else {
-            Schema::rename("statistic_atc", "networkdata_atc");
-        }
+        Schema::create('networkdata_atc', function ($table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->integer('account_id')->unsigned();
+            $table->string('callsign', 10);
+            $table->smallInteger('qualification_id')->unsigned();
+            $table->tinyInteger('facility_type')->unsigned();
+            $table->timestamp('connected_at')->nullable();
+            $table->timestamp('disconnected_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
     }
 
     /**
@@ -39,6 +31,6 @@ class CreateAtcStatisticsTables extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('networkdata_atc');
+        Schema::drop("networkdata_atc");
     }
 }
