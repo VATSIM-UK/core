8823139adfac953dd0088f9cf9dd859e088ff51d
Neil Farrington <me@neilfarrington.com>
2016-12-28 19:27:58 +0000

    Ignore CommandNotFoundException - stops BugSnag reporting issue

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -21,6 +21,7 @@ class Handler extends ExceptionHandler
         \Illuminate\Auth\AuthenticationException::class,
         \Illuminate\Auth\Access\AuthorizationException::class,
         \Symfony\Component\HttpKernel\Exception\HttpException::class,
+        \Symfony\Component\Console\Exception\CommandNotFoundException::class,
         \Illuminate\Database\Eloquent\ModelNotFoundException::class,
         \Illuminate\Session\TokenMismatchException::class,
         \Illuminate\Validation\ValidationException::class,
