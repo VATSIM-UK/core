c21991ca93d49ac01b61c20292a73cfb4e1fd993
Alex Toff <alex.toff@vatsim.uk>
2017-03-10 22:03:13 +0000

    Add inline css to increase chances of email looking right

--- resources/views/emails/default.blade.php
+++ resources/views/emails/default.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="font-family: sans-serif;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;width: 100%;">
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
     <style type="text/css">
       /* Bootstrap Styling */
       html {
@@ -118,24 +118,97 @@
       }
     </style>
   </head>
-  <body style="margin-top: 2%">
-    <div class="container" >
+  <body style="margin-top: 2%;margin: 0;font-family: 'Roboto', sans-serif;">
+    <div class="container" style="padding-right: 15px;padding-left: 15px;margin-right: auto;margin-left: auto;">
       <div class="row" style="background-color: #17375e; border-bottom: 5px solid #00b0f0;">
         <div class="col-xs-8" style="padding:25px;">
-          <img width="200px" style="padding-bottom:5px;" src="{!! asset("assets/images/vatsim_uk_logo.png") !!}" /></br>
-          <i style="color:rgb(205, 205, 205);"><small>{{ $subject }}</small></i>
+          <img width="200px" style="padding-bottom:5px;" src="http://core.vatsim.uk/assets/images/vatsim_uk_logo.png">
+          <i style="color:rgb(205, 205, 205);"><small>Welcome to VATSIM UK</small></i>
         </div>
       </div>
       <div class="row" style="background-color: #e3e3e3;">
         <div class="col-xs-12">
-          <div class="panel">
-            <div class="panel-heading">{{ $subject }}</div>
-            <div class="panel-body">
+          <div class="panel" style="padding-top: 10px;padding-left: 8px;padding-right: 8px;border: none !important;">
+            <div class="panel-heading" style="background-color: #17375e;border-color: #17375e;color: #fff;border-top-left-radius: 3px;border-top-right-radius: 3px;padding: 10px 15px;border-bottom: 1px solid transparent;">Welcome to VATSIM UK</div>
+            <div class="panel-body" style="padding: 15px;background-color: white;">
               <div class="row">
                 <div class="col-xs-12">
-                  @yield('content', "No content to display")
+                      <p>
+        Dear Alex Toff,
+    </p>
+
+    <p>
+    This email serves as confirmation that your central account with VATSIM UK has been created. We have received the
+    following details about you:
+</p>
+
+<p>
+    CID: 1294298<br>
+    Full Name: Alex Toff<br>
+    Primary Email: alex.toff@vatsim.uk<br>
+    Secondary Emails:<br>
+            -- test@testing.com<br>
+            -- my_secondary_email@emails.com<br>
+        </p>
+
+<p>
+    Status: Active<br>
+    State: [DIVISION] Division<br>
+</p>
+
+<p>
+    ATC Qualification: S2<br>
+    Pilot Qualification(s): P1<br>
+</p>
+
+<p>
+    Now that your account has been created, you can login to any of our web services and these details will be
+    transferred automatically.
+</p>
+
+<p>
+    Furthermore, any updates you make to your details via the vatsim.net membership portal will be synchronised with our
+    database in a timely manner, to ensure all our systems remain up to date.
+</p>
+
+<p>
+    If any details are incorrect, or you have any concerns, please
+    contact <a href="mailto:community@vatsim-uk.co.uk" style="color: #00b0f0;">our community department</a> who will be able to help you
+    further.
+</p>
+
+<h2>What next?</h2>
+
+<p>
+    If you're interested in flying, there are a number of 'How to....' guides and training available:
+    <a href="https://www.vatsim.uk/pilot-info/" style="color: #00b0f0;">https://www.vatsim.uk/pilot-info/</a>
+</p>
+<p>
+    Pilot Training
+    <a href="https://www.vatsim.uk/pilot-training/" style="color: #00b0f0;">https://www.vatsim.uk/pilot-training/</a>
+</p>
+
+<p>
+    If you're interested in providing ATC:
+    <a href="https://www.vatsim.uk/becoming-a-controller/" style="color: #00b0f0;">https://www.vatsim.uk/becoming-a-controller/</a>
+</p>
+
+<p>
+    For both Pilot and ATC training our online system will allow you to book mentoring (once you are enrolled as per the above links):
+    <a href="https://cts.vatsim.uk/" style="color: #00b0f0;">https://cts.vatsim.uk/</a>
+</p>
+
+<p>
+    The UK TeamSpeak Server, Slack and Forum - these are used for voice and written communications for the entire UK community. Feel free to join the community and ask questions and meet others:
+    <li> TeamSpeak & Slack - <a href="https://www.vatsim.uk/tsreg/" style="color: #00b0f0;">https://www.vatsim.uk/tsreg/</a></li>
+    <li> Forum - <a href="https://community.vatsim.uk/" style="color: #00b0f0;">https://community.vatsim.uk/</a></li>
+</p>
+
+
+On behalf of the entire team, welcome to the United Kingdom.
+
                   <p>
-                    Kind regards,<br />
+                    Kind regards,<br>
                     <strong>VATSIM UK</strong>
                   </p>
                 </div>
@@ -146,23 +219,23 @@
           <div class="col-xs-offset-1 col-xs-10 col-md-offset-3 col-md-6">
             <div class="row">
               <div class="col-xs-12 text-center" style="text-align: center">
-                <a class="social-link" href="https://twitter.com/vatsimuk">
+                <a class="social-link" href="https://twitter.com/vatsimuk" style="color: black;border-radius: 3px;border: 1px solid;text-align: center;background-color: rgb(216, 214, 214);padding: 3px;word-wrap: break-word;text-decoration: none;margin-right: 10px;">
                   @vatsimuk
                 </a>
-                <a class="social-link" href="https://vatsim.uk">
+                <a class="social-link" href="https://vatsim.uk" style="color: black;border-radius: 3px;border: 1px solid;text-align: center;background-color: rgb(216, 214, 214);padding: 3px;word-wrap: break-word;text-decoration: none;margin-right: 10px;">
                   <i class="ion ion-earth"></i>
                   vatsim.uk
                 </a>
               </div>
             </div>
           </div>
-          <div class="panel">
-            <div class="panel-heading panel-info">Notice</div>
-            <div class="panel-body">
+          <div class="panel" style="padding-bottom: 10px;padding-top: 10px;padding-left: 8px;padding-right: 8px;border: none !important;">
+            <div class="panel-heading panel-info" style="background-color: #a6a6a6;border-color: #17375e;color: #fff;border-top-left-radius: 3px;border-top-right-radius: 3px;padding: 10px 15px;border-bottom: 1px solid transparent;">Notice</div>
+            <div class="panel-body" style="padding: 15px;background-color: white;">
               <div class="row">
                 <div class="col-xs-12">
-                  This email was automatically sent from our system at <?= gmdate("H:i:s D jS M Y") ?> GMT. Do not reply directly to this email, as the address is not monitored.<br />
-                  Please <a href="mailto:web-support@vatsim-uk.co.uk?subject=Erroneous automatic email"  data-toggle="tooltip" title="Report Error">let us know</a>
+                  This email was automatically sent from our system at 21:46:09 Fri 10th Mar 2017 GMT. Do not reply directly to this email, as the address is not monitored.<br>
+                  Please <a href="mailto:web-support@vatsim-uk.co.uk?subject=Erroneous automatic email" data-toggle="tooltip" title="Report Error" style="color: #00b0f0;">let us know</a>
                   if it's not been sent to the correct person, or displays incorrectly.
                 </div>
               </div>
