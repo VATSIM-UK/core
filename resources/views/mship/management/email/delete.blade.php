dbcf6d26d6dceaaef52832672b15092d02ba09f8
Alex Toff <alex.toff@vatsim.uk>
2017-03-13 21:35:28 +0000

    Add new delete button to secondary emails on dashboard, and add the template for the deleting page

---
+++ resources/views/mship/management/email/delete.blade.php
@@ -0,0 +1,36 @@
+@extends('layout')
+
+@section('content')
+  <div class="panel panel-ukblue">
+    <div class="panel-heading"> Delete Secondary Email</div>
+    <div class="panel-body text-center">
+      <!-- Content Of Panel [START] -->
+      <p>
+        Are you sure you would like to delete <b>{{ $email->email }}</b> from your secondary emails?
+      </p>
+
+      @if ($assignments->count() > 0)
+        <p>
+          Deleteing this email will reset the following SSO assignments to their default value ({{ \Auth::user()->email }})
+          <div class="row">
+            <div class="col-xs-2 col-xs-offset-5">
+              <ul>
+                @foreach ($assignments as $assignment)
+                  <li>{{ $assignment->ssoAccount->name }}</li>
+                @endforeach
+              </ul>
+            </div>
+          </div>
+        </p>
+      @endif
+      {!! Form::open(["route" => ["mship.manage.email.delete.post", $email->id], "class" => "form-horizontal"]) !!}
+      <div class="form-group">
+        <button type="submit" class="btn btn-danger" name=""
+        value="delete_email">Delete</button>
+      </div>
+      {!! Form::close() !!}
+      <!-- Content Of Panel [END] -->
+    </div>
+  </div>
+
+@stop
