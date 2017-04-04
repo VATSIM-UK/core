22c2c4d228fe86d3afdf813b55f93c3de9679467
Alex Toff <alex.toff@vatsim.uk>
2017-04-04 18:39:20 +0100

    Add new functions to the network data trait

--- app/Modules/NetworkData/Traits/NetworkDataAccount.php
+++ app/Modules/NetworkData/Traits/NetworkDataAccount.php
@@ -14,6 +14,28 @@ trait NetworkDataAccount
         return $this->hasMany(\App\Modules\NetworkData\Models\Atc::class, 'account_id', 'id');
     }
 
+    /**
+     * Get the member's current ATC session, if connected.
+     *
+     * @return \App\Modules\NetworkData\Models\Atc
+     */
+    public function networkDataAtcCurrent()
+    {
+        return $this->hasOne(\App\Modules\NetworkData\Models\Atc::class, 'account_id', 'id')
+            ->whereNull('disconnected_at')
+            ->limit(1);
+    }
+
+    /**
+     * Determine if the user is on the network.
+     *
+     * @return bool
+     */
+    public function getIsOnNetworkAttribute()
+    {
+        return $this->networkDataAtcCurrent->exists;
+    }
+
     /*
      * Fetch all Pilot Sessions
      *
