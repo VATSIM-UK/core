d09e1a485cd94ec3f54745818c6d83dcd2f4efd4
Alex Toff <alex.toff@vatsim.uk>
2017-04-18 19:50:40 +0100

    Remove unused contruct method on exception

--- app/Exceptions/Mship/InvalidCIDException.php
+++ app/Exceptions/Mship/InvalidCIDException.php
@@ -4,11 +4,6 @@ namespace App\Exceptions\Mship;
 
 class InvalidCIDException extends \Exception
 {
-    public function __construct()
-    {
-        //
-    }
-
     public function __toString()
     {
         return 'CID is invalid.';
