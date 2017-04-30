aa80efe71ca7b88c32d25e5cabe8df9b50f00aaa
Neil Farrington <me@neilfarrington.com>
2017-04-30 12:27:03 +0100

    Update model docs

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -8,7 +8,15 @@ use Illuminate\Notifications\Notifiable;
 /**
  * App\Models\Contact
  *
+ * @property int $id
+ * @property string $key
+ * @property string $name
+ * @property string $email
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Contact whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Contact whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Contact whereKey($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Contact whereName($value)
  * @mixin \Eloquent
  */
 class Contact extends Model
