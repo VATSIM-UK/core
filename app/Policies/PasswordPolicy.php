680d4e0193687d8621ba64b36637161c1efb6269
Neil Farrington <me@neilfarrington.com>
2017-05-01 16:33:56 +0100

    Add new password policy

---
+++ app/Policies/PasswordPolicy.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Mship\Account;
+
+class PasswordPolicy extends Policy
+{
+    /**
+     * Determine whether the user can create a new password.
+     *
+     * @param Account $user
+     * @return mixed
+     */
+    public function create(Account $user)
+    {
+        if ($user->hasPassword()) {
+            return $this->deny('You already have a password set.');
+        }
+
+        return $this->allow();
+    }
+
+    /**
+     * Determine whether the user can update their password.
+     *
+     * @param Account $user
+     * @return mixed
+     */
+    public function update(Account $user)
+    {
+        if (!$user->hasPassword()) {
+            return $this->deny('You do not have a password set.');
+        }
+
+        return $this->allow();
+    }
+
+
+    /**
+     * Determine whether the user can delete their password.
+     *
+     * @param Account $user
+     * @return mixed
+     */
+    public function delete(Account $user)
+    {
+        if ($user->mandatory_password) {
+            return $this->deny('You are not allowed to disable your secondary password.');
+        } elseif (!$user->hasPassword()) {
+            return $this->deny('You do not have a password set.');
+        }
+
+        return $this->allow();
+    }
+}
