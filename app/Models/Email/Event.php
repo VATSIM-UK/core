49f4a3eb8447972e2b84f5a6a27741c62ede08fc
Neil Farrington <me@neilfarrington.com>
2017-05-13 15:43:42 +0100

    Cast mail event data to array

--- app/Models/Email/Event.php
+++ app/Models/Email/Event.php
@@ -9,5 +9,8 @@ class Event extends Model
     protected $table = 'email_events';
     protected $dates = ['triggered_at'];
     protected $guarded = ['id'];
+    protected $casts = [
+        'data' => 'array',
+    ];
     public $timestamps = false;
 }
