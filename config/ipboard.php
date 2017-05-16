ce73edd91b2dfefec7eb04c7669695db814a0bf7
CalumTowers <calum.towers@vatsim-uk.co.uk>
2017-05-16 22:01:49 +0100

    Add HTTPS

--- config/ipboard.php
+++ config/ipboard.php
@@ -7,7 +7,7 @@
  */
 
 return [
-    'api_url' => 'http://community.vatsim.uk/api/',
+    'api_url' => 'https://community.vatsim.uk/api/',
     'api_key' => '6119994a92722cd19b6fc7e625902d9b',
     'api_reference_name' => 'ANTHONY_TEST',
 ];
