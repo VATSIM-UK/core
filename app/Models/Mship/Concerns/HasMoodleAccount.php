611362f8657ff7f10fcc00c0870d6153660a3dc1
Neil Farrington <me@neilfarrington.com>
2017-08-20 20:10:11 -0400

    Search Moodle accounts using string ID

--- app/Models/Mship/Concerns/HasMoodleAccount.php
+++ app/Models/Mship/Concerns/HasMoodleAccount.php
@@ -84,7 +84,7 @@ trait HasMoodleAccount
 
         $dirty = array_keys(array_diff_assoc($old, $new));
         if (!empty($dirty)) {
-            DB::table('vatuk_moodle.mdl_user')->where('username', $this->id)->update($new);
+            DB::table('vatuk_moodle.mdl_user')->where('username', (string) $this->id)->update($new);
         } else {
             // do nothing - account is up to date
         }
