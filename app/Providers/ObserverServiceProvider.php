09646585484323e83bd2c4c60f99de314497d1ca
Neil Farrington <me@neilfarrington.com>
2017-08-31 16:02:35 -0400

    Update activity-tracked models

--- app/Providers/ObserverServiceProvider.php
+++ app/Providers/ObserverServiceProvider.php
@@ -3,8 +3,8 @@
 namespace App\Providers;
 
 use App\Models\Mship;
-use App\Models\TeamSpeak;
-use App\Models\Sso;
+use App\Models\Sys;
+use App\Models\VisitTransfer;
 use App\Observers\ModelActivityObserver;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,15 +20,11 @@ class ObserverServiceProvider extends ServiceProvider
         Mship\Account::observe(ModelActivityObserver::class);
         Mship\Account\Ban::observe(ModelActivityObserver::class);
         Mship\Account\Note::observe(ModelActivityObserver::class);
-        Mship\Ban\Reason::observe(ModelActivityObserver::class);
-        Mship\Note\Type::observe(ModelActivityObserver::class);
         Mship\Permission::observe(ModelActivityObserver::class);
-        Mship\Qualification::observe(ModelActivityObserver::class);
         Mship\Role::observe(ModelActivityObserver::class);
-        Mship\State::observe(ModelActivityObserver::class);
-        TeamSpeak\Confirmation::observe(ModelActivityObserver::class);
-        TeamSpeak\Registration::observe(ModelActivityObserver::class);
-        Sso\Email::observe(ModelActivityObserver::class);
+        Sys\Notification::observe(ModelActivityObserver::class);
+        VisitTransfer\Application::observe(ModelActivityObserver::class);
+        VisitTransfer\Reference::observe(ModelActivityObserver::class);
     }
 
     /**
