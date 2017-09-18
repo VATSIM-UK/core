4fe5201ce31179097ca7f9a6d2848db98616257c
Neil Farrington <me@neilfarrington.com>
2017-09-18 18:13:53 -0400

    Remove unused GET 'password/reset' route

--- routes/web-main.php
+++ routes/web-main.php
@@ -12,7 +12,6 @@ Route::get('login-vatsim', 'Auth\LoginController@vatsimSsoReturn')->name('auth-v
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Password Reset
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
