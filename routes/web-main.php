14feafce810514047a39831f7ea9ade9867d9d3d
Neil Farrington <me@neilfarrington.com>
2017-09-28 23:00:50 +0100

    Refactor main routes

--- routes/web-main.php
+++ routes/web-main.php
@@ -38,48 +38,30 @@ Route::group(['prefix' => 'webhook', 'namespace' => 'Webhook'], function () {
 });
 
 // Members
+Route::group(['prefix' => 'mship', 'namespace' => 'Mship', 'middleware' => 'auth_full_group'], function () {
+    Route::post('auth/invisibility', ['as' => 'mship.auth.invisibility', 'uses' => 'Management@postInvisibility']);
 
-Route::group(['prefix' => 'mship', 'namespace' => 'Mship'], function () {
-    // Guest access
-    Route::group(['prefix' => 'auth'], function () {
-        Route::get('login-alternative', ['as' => 'mship.auth.loginAlternative', 'uses' => 'Authentication@getLoginAlternative']);
-        Route::post('login-alternative', ['as' => 'mship.auth.loginAlternative.post', 'uses' => 'Authentication@postLoginAlternative']);
+    Route::get('notification/list', ['as' => 'mship.notification.list', 'uses' => 'Notification@getList']);
+    Route::post('notification/acknowledge/{sysNotification}', ['as' => 'mship.notification.acknowledge', 'uses' => 'Notification@postAcknowledge']);
 
-        Route::group(['middleware' => ['auth_full_group']], function () {
-            Route::post('invisibility', ['as' => 'mship.auth.invisibility', 'uses' => 'Management@postInvisibility']);
-        });
-    });
+    Route::get('manage/dashboard', ['as' => 'mship.manage.dashboard', 'uses' => 'Management@getDashboard']);
+    Route::get('manage/email/verify/{code}', ['as' => 'mship.manage.email.verify', 'uses' => 'Management@getVerifyEmail']);
+    Route::get('manage/email/add', ['as' => 'mship.manage.email.add', 'uses' => 'Management@getEmailAdd']);
+    Route::post('manage/email/add', ['as' => 'mship.manage.email.add.post', 'uses' => 'Management@postEmailAdd']);
+    Route::get('manage/email/delete/{email}', ['as' => 'mship.manage.email.delete', 'uses' => 'Management@getEmailDelete']);
+    Route::post('manage/email/delete/{email}', ['as' => 'mship.manage.email.delete.post', 'uses' => 'Management@postEmailDelete']);
+    Route::get('manage/email/assignments', ['as' => 'mship.manage.email.assignments', 'uses' => 'Management@getEmailAssignments']);
+    Route::post('manage/email/assignments', ['as' => 'mship.manage.email.assignments.post', 'uses' => 'Management@postEmailAssignments']);
 
-    Route::group(['middleware' => 'auth_full_group', 'prefix' => 'notification'], function () {
-        Route::get('/list', ['as' => 'mship.notification.list', 'uses' => 'Notification@getList']);
-        Route::post('/acknowledge/{sysNotification}', ['as' => 'mship.notification.acknowledge', 'uses' => 'Notification@postAcknowledge']);
-    });
+    Route::get('feedback/new', ['as' => 'mship.feedback.new', 'uses' => 'Feedback@getFeedbackFormSelect']);
+    Route::post('feedback/new', ['as' => 'mship.feedback.new.post', 'uses' => 'Feedback@postFeedbackFormSelect']);
+    Route::get('feedback/new/{form}', ['as' => 'mship.feedback.new.form', 'uses' => 'Feedback@getFeedback']);
+    Route::post('feedback/new/{form}', ['as' => 'mship.feedback.new.form.post', 'uses' => 'Feedback@postFeedback']);
+    Route::get('feedback/users/search/{name}', ['as' => 'mship.feedback.usersearch', 'uses' => 'Feedback@getUserSearch']);
 
-    Route::group(['prefix' => 'manage', 'middleware' => ['auth_full_group']], function () {
-        Route::get('dashboard', ['as' => 'mship.manage.dashboard', 'uses' => 'Management@getDashboard']);
-        Route::get('email/verify/{code}', ['as' => 'mship.manage.email.verify', 'uses' => 'Management@getVerifyEmail']);
-        Route::get('email/add', ['as' => 'mship.manage.email.add', 'uses' => 'Management@getEmailAdd']);
-        Route::post('email/add', ['as' => 'mship.manage.email.add.post', 'uses' => 'Management@postEmailAdd']);
-        Route::get('email/delete/{email}', ['as' => 'mship.manage.email.delete', 'uses' => 'Management@getEmailDelete']);
-        Route::post('email/delete/{email}', ['as' => 'mship.manage.email.delete.post', 'uses' => 'Management@postEmailDelete']);
-        Route::get('email/assignments', ['as' => 'mship.manage.email.assignments', 'uses' => 'Management@getEmailAssignments']);
-        Route::post('email/assignments', ['as' => 'mship.manage.email.assignments.post', 'uses' => 'Management@postEmailAssignments']);
-    });
-
-    Route::group(['middleware' => ['auth_full_group'], 'prefix' => 'feedback'], function () {
-        Route::get('/new', ['as' => 'mship.feedback.new', 'uses' => 'Feedback@getFeedbackFormSelect']);
-        Route::post('/new', ['as' => 'mship.feedback.new.post', 'uses' => 'Feedback@postFeedbackFormSelect']);
-        Route::get('/new/{form}', ['as' => 'mship.feedback.new.form', 'uses' => 'Feedback@getFeedback']);
-        Route::post('/new/{form}', ['as' => 'mship.feedback.new.form.post', 'uses' => 'Feedback@postFeedback']);
-
-        Route::get('/users/search/{name}', ['as' => 'mship.feedback.usersearch', 'uses' => 'Feedback@getUserSearch']);
-    });
-
-    Route::group(['middleware' => ['auth_full_group']], function () {
-        Route::get('/email', ['as' => 'mship.email', 'uses' => 'Email@getEmail']);
-        Route::post('/email', ['as' => 'mship.email.post', 'uses' => 'Email@postEmail']);
-        Route::get('/email/recipient-search', ['as' => 'mship.email.recipient-search', 'uses' => 'Email@getRecipientSearch']);
-    });
+    Route::get('/email', ['as' => 'mship.email', 'uses' => 'Email@getEmail']);
+    Route::post('/email', ['as' => 'mship.email.post', 'uses' => 'Email@postEmail']);
+    Route::get('/email/recipient-search', ['as' => 'mship.email.recipient-search', 'uses' => 'Email@getRecipientSearch']);
 });
 
 // TeamSpeak
@@ -100,53 +82,31 @@ Route::group(['prefix' => 'mship/manage/slack', 'namespace' => 'Slack', 'middlew
 });
 
 // Community
-Route::get('/community', function () {
-    return redirect()->route('membership.deploy');
-});
+Route::group(['as' => 'community.membership.', 'namespace' => 'Community', 'middleware' => 'auth_full_group'], function () {
+    Route::get('community/membership/deploy', [
+        'as' => 'deploy',
+        'uses' => 'Membership@getDeploy',
+    ]);
 
-Route::group([
-    'as' => 'community.',
-    'namespace' => 'Community',
-    'prefix' => 'community',
-    'middleware' => ['auth_full_group'],
-], function () {
-    Route::group(['as' => 'membership.', 'prefix' => 'membership'], function () {
-        Route::get('/deploy', [
-            'as' => 'deploy',
-            'uses' => 'Membership@getDeploy',
-        ]);
-
-        Route::post('/deploy/{default?}', [
-            'as' => 'deploy.post',
-            'uses' => 'Membership@postDeploy',
-        ])->where('default', '[default|true]');
-    });
+    Route::post('community/membership/deploy/{default?}', [
+        'as' => 'deploy.post',
+        'uses' => 'Membership@postDeploy',
+    ])->where('default', '[default|true]');
 });
 
 // Network data
-
-Route::get('/network-data', function () {
-    return Redirect::route('networkdata.landing');
-});
-
-Route::group([
-    'as' => 'networkdata.',
-    'namespace' => 'NetworkData',
-    'prefix' => 'network-data',
-    'middleware' => ['auth_full_group'],
-], function () {
-    Route::get('/', function () {
+Route::group(['as' => 'networkdata.', 'namespace' => 'NetworkData', 'middleware' => 'auth_full_group'], function () {
+    Route::get('network-data', function () {
         return redirect()->route('networkdata.online');
     })->name('landing');
 
-    Route::get('/dashboard', 'MainController@getDashboard')->name('dashboard');
-    Route::get('/online', ['as' => 'online', 'uses' => 'Online@getOnline']);
+    Route::get('network-data/dashboard', 'MainController@getDashboard')->name('dashboard');
+    Route::get('network-data/online', ['as' => 'online', 'uses' => 'Online@getOnline']);
 });
 
 // Visit/Transfer
-
-Route::get('/visiting-transferring', function () {
-    return Redirect::route('visiting.landing');
+Route::get('visiting-transferring', function () {
+    return redirect()->route('visiting.landing');
 });
 
 Route::group([
@@ -158,107 +118,34 @@ Route::group([
     Route::get('/', ['as' => 'landing', 'uses' => 'Dashboard@getDashboard']);
 
     Route::group(['as' => 'application.', 'prefix' => 'application'], function () {
-        Route::get('', function () {
-            return Redirect::route('visiting.landing');
+        Route::get('/', function () {
+            return redirect()->route('visiting.landing');
         });
 
-        Route::get('/start/{type}/{team}', [
-            'as' => 'start',
-            'uses' => 'Application@getStart',
-        ])->where('type', "\d+");
+        Route::get('start/{type}/{team}', 'Application@getStart')->where('type', "\d+")->name('start');
+        Route::post('start/{type}/{team}', 'Application@postStart')->where('type', "\d+")->name('start.post');
 
-        Route::post('/start/{type}/{team}', [
-            'as' => 'start.post',
-            'uses' => 'Application@postStart',
-        ])->where('type', "\d+");
-
-        Route::group(['prefix' => '/{applicationByPublicId}'], function () {
-            Route::get('/continue', [
-                'as' => 'continue',
-                'uses' => 'Application@getContinue',
-            ]);
-
-            Route::get('/facility', [
-                'as' => 'facility',
-                'uses' => 'Application@getFacility',
-            ]);
-
-            Route::post('/facility', [
-                'as' => 'facility.post',
-                'uses' => 'Application@postFacility',
-            ]);
-
-            Route::post('/facility/manual', [
-                'as' => 'facility.manual.post',
-                'uses' => 'Application@postManualFacility',
-            ]);
-
-            Route::get('/statement', [
-                'as' => 'statement',
-                'uses' => 'Application@getStatement',
-            ]);
-
-            Route::post('/statement', [
-                'as' => 'statement.post',
-                'uses' => 'Application@postStatement',
-            ]);
-
-            Route::get('/referees', [
-                'as' => 'referees',
-                'uses' => 'Application@getReferees',
-            ]);
-
-            Route::post('/referees', [
-                'as' => 'referees.post',
-                'uses' => 'Application@postReferees',
-            ]);
-
-            Route::post('/referees/{reference}/delete', [
-                'as' => 'referees.delete.post',
-                'uses' => 'Application@postRefereeDelete',
-            ]);
-
-            Route::get('/submit', [
-                'as' => 'submit',
-                'uses' => 'Application@getSubmit',
-            ]);
-
-            Route::post('/submit', [
-                'as' => 'submit.post',
-                'uses' => 'Application@postSubmit',
-            ]);
-
-            Route::get('/withdraw', [
-                'as' => 'withdraw',
-                'uses' => 'Application@getWithdraw',
-            ]);
-
-            Route::post('/withdraw', [
-                'as' => 'withdraw.post',
-                'uses' => 'Application@postWithdraw',
-            ]);
-
-            Route::get('', [
-                'as' => 'view',
-                'uses' => 'Application@getView',
-            ]);
+        Route::group(['prefix' => '{applicationByPublicId}'], function () {
+            Route::get('/', 'Application@getView')->name('view');
+            Route::get('continue', 'Application@getContinue')->name('continue');
+            Route::get('facility', 'Application@getFacility')->name('facility');
+            Route::post('facility', 'Application@postFacility')->name('facility.post');
+            Route::post('facility/manual', 'Application@postManualFacility')->name('facility.manual.post');
+            Route::get('statement', 'Application@getStatement')->name('statement');
+            Route::post('statement', 'Application@postStatement')->name('statement.post');
+            Route::get('referees', 'Application@getReferees')->name('referees');
+            Route::post('referees', 'Application@postReferees')->name('referees.post');
+            Route::post('referees/{reference}/delete', 'Application@postRefereeDelete')->name('referees.delete.post');
+            Route::get('submit', 'Application@getSubmit')->name('submit');
+            Route::post('submit', 'Application@postSubmit')->name('submit.post');
+            Route::get('withdraw', 'Application@getWithdraw')->name('withdraw');
+            Route::post('withdraw', 'Application@postWithdraw')->name('withdraw.post');
         });
     });
 
     Route::group(['as' => 'reference.', 'prefix' => 'reference'], function () {
-        Route::get('/complete/{token}', [
-            'as' => 'complete',
-            'uses' => 'Reference@getComplete',
-        ]);
-
-        Route::post('/complete/{token}', [
-            'as' => 'complete.post',
-            'uses' => 'Reference@postComplete',
-        ]);
-
-        Route::post('/complete/{token}/cancel', [
-            'as' => 'complete.cancel',
-            'uses' => 'Reference@postCancel',
-        ]);
+        Route::get('complete/{token}', 'Reference@getComplete')->name('complete');
+        Route::post('complete/{token}', 'Reference@postComplete')->name('complete.post');
+        Route::post('complete/{token}/cancel', 'Reference@postCancel')->name('complete.cancel');
     });
 });
