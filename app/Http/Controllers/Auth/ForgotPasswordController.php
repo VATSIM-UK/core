690da415efd4c118ddc10c25ad948fcea7cb5784
Neil Farrington <me@neilfarrington.com>
2017-11-12 12:41:47 +0000

    Fix resetting passwords in different sessions

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -18,16 +18,6 @@ class ForgotPasswordController extends BaseController
     use SendsPasswordResetEmails;
 
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
-
-    /**
      * Send a reset link to the given user.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -38,7 +28,7 @@ class ForgotPasswordController extends BaseController
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
-        $response = $this->broker()->sendResetLink(['email' => Auth::guard('vatsim-sso')->user()->email]);
+        $response = $this->broker()->sendResetLink(['id' => Auth::guard('vatsim-sso')->user()->id]);
 
         return $response == Password::RESET_LINK_SENT
             ? $this->sendResetLinkResponse($response)
