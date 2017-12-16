6d46bf69783d8c10f48a3f2e0b5459d332284be7
Neil Farrington <me@neilfarrington.com>
2017-12-16 16:57:37 -0500

    Add containsPoint()

--- app/Models/Smartcars/FlightCriteria.php
+++ app/Models/Smartcars/FlightCriteria.php
@@ -48,4 +48,40 @@ class FlightCriteria extends Model
     ];
 
     public $timestamps = false;
+
+    /**
+     * Calculates whether the criteria contains the given point.
+     *
+     * Based on https://github.com/substack/point-in-polygon/blob/master/index.js
+     *
+     * @param float $latitude   The latitude of the point (vertical, y)
+     * @param float $longitude  The longitude of the point (horizontal, x)
+     * @return bool
+     */
+    public function containsPoint($latitude, $longitude)
+    {
+        $x = $longitude;
+        $y = $latitude;
+        $vs = [
+            [$this->p1_longitude, $this->p1_latitude],
+            [$this->p2_longitude, $this->p2_latitude],
+            [$this->p3_longitude, $this->p3_latitude],
+            [$this->p4_longitude, $this->p4_latitude],
+        ];
+
+        $inside = false;
+        for ($i = 0, $j = sizeof($vs) - 1; $i < sizeof($vs); $j = $i++) {
+            $xi = $vs[$i][0];
+            $yi = $vs[$i][1];
+            $xj = $vs[$j][0];
+            $yj = $vs[$j][1];
+
+            $intersect = (($yi > $y) != ($yj > $y)) && ($x < ($xj - $xi) * ($y - $yi) / ($yj - $yi) + $xi);
+            if ($intersect) {
+                $inside = !$inside;
+            }
+        }
+
+        return $inside;
+    }
 }
