26f1866da7d26e52c4047c9e13fd117a9feacad0
Calum Towers <me@calumtowers.com>
2017-12-30 21:18:19 +0000

    Address NF comments
    Update dates, column names and pagnation

--- resources/views/network-data/dashboard.blade.php
+++ resources/views/network-data/dashboard.blade.php
@@ -6,26 +6,26 @@
             {!! HTML::panelOpen('My ATC Sessions', ['type' => 'vuk', 'key' => 'letter-a']) !!}
             <div class="row">
                 <div class="col-md-12" style="margin-bottom: 15px;">
-                    <span style="display: flex; justify-content: center;">{{ $atcSessions->appends('pilotSessions', Input::get('pilotSessions'))->links() }}</span>
+                    <span style="display: flex; justify-content: center;">{{ $atcSessions->links() }}</span>
                     <table class="table table-striped tabled-bordered table-hover">
                         <tr>
+                            <th>Date</th>
                             <th>Callsign</th>
-                            <th>Type</th>
+                            <th>Position</th>
                             <th>Frequency</th>
                             <th>Duration</th>
-                            <th>Logged On</th>
                         </tr>
                         @foreach($atcSessions as $atc)
                             <tr>
+                                <td>{{ $atc->connected_at->format('d/m/Y') }}</td>
                                 <td>{{ $atc->callsign }}</td>
                                 <td>{{ $atc->type }}</td>
                                 <td>{{ $atc->frequency }}</td>
                                 <td>{{ $atc->human_duration }}</td>
-                                <td>{{ HTML::fuzzyDate($atc->connected_at) }}</td>
                             </tr>
                         @endforeach
                     </table>
-                    <span style="display: flex; justify-content: center;">{{ $atcSessions->appends('pilotSessions', Input::get('pilotSessions'))->links() }}</span>
+                    <span style="display: flex; justify-content: center;">{{ $atcSessions->links() }}</span>
                 </div>
 
             </div>
@@ -36,26 +36,26 @@
             {!! HTML::panelOpen('My Pilot Sessions', ['type' => 'vuk', 'key' => 'letter-p']) !!}
             <div class="row">
                 <div class="col-md-12" style="margin-bottom: 15px;">
-                    <span style="display: flex; justify-content: center;">{{ $pilotSessions->appends('atcSessions', Input::get('atcSessions'))->links() }}</span>
+                    <span style="display: flex; justify-content: center;">{{ $pilotSessions->links() }}</span>
                     <table class="table table-striped tabled-bordered table-hover">
                         <tr>
+                            <th>Date</th>
                             <th>Callsign</th>
                             <th>Departure</th>
                             <th>Arrival</th>
                             <th>Duration</th>
-                            <th>Logged On</th>
                         </tr>
                         @foreach($pilotSessions as $pilot)
                             <tr>
+                                <td>{{ $pilot->connected_at->format('d/m/Y') }}</td>
                                 <td>{{ $pilot->callsign }}</td>
                                 <td>{{ $pilot->departure_airport }}</td>
                                 <td>{{ $pilot->arrival_airport }}</td>
                                 <td>{{ $pilot->human_duration }}</td>
-                                <td>{{ HTML::fuzzyDate($pilot->connected_at) }}</td>
                             </tr>
                         @endforeach
                     </table>
-                    <span style="display: flex; justify-content: center;">{{ $pilotSessions->appends('atcSessions', Input::get('atcSessions'))->links() }}</span>
+                    <span style="display: flex; justify-content: center;">{{ $pilotSessions->links() }}</span>
                 </div>
 
             </div>
