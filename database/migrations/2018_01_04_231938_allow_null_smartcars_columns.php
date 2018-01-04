419d4fa8eb102ddd9e7b381e5d4f87be68c2ac28
Calum Towers <me@calumtowers.com>
2018-01-04 23:34:15 +0000

    Allow some NULL columns
    Resolves #1084

---
+++ database/migrations/2018_01_04_231938_allow_null_smartcars_columns.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AllowNullSmartcarsColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('ALTER TABLE `smartcars_pirep`
+	CHANGE COLUMN `comments` `comments` MEDIUMTEXT NULL COLLATE \'utf8mb4_unicode_ci\' AFTER `landing_rate`');
+        DB::statement('ALTER TABLE `smartcars_posrep`
+	CHANGE COLUMN `time_departure` `time_departure` TIME NULL AFTER `phase`');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('ALTER TABLE `smartcars_pirep`
+	CHANGE COLUMN `comments` `comments` MEDIUMTEXT NOT NULL COLLATE \'utf8mb4_unicode_ci\' AFTER `landing_rate`');
+        DB::statement('ALTER TABLE `smartcars_posrep`
+	CHANGE COLUMN `time_departure` `time_departure` TIME NOT NULL AFTER `phase`');
+    }
+}
