2b2629ba6a225c782fa3b070fa986be78d2b2c6f
Calum Towers <me@calumtowers.com>
2018-01-12 17:50:18 +0000

    Tawk.To on prod only (#1093)
    * Tawk.To on prod only
    
    Only show Tawk.To chat script if on production
    
    * Amend as per NF comments

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -272,6 +272,7 @@
 
 </script>
 
+@if(App::environment('production'))
 <script type="text/javascript">
     var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
     (function () {
@@ -297,6 +298,7 @@
         });
     };
 </script>
+@endif
 
 @yield('scripts')
 
