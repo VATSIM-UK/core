e8af0800f5be254230b4e5f7f609470e21a905c5
Neil Farrington <me@neilfarrington.com>
2018-02-25 12:31:05 +0000

    Add additional permissions checks to admin routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ Route::group(['domain' => config('app.url')], function () {
     require base_path('routes/web-main.php');
 });
 
-Route::group(['domain' => config('app.url')], function () {
+Route::group(['domain' => config('app.url'), 'middleware' => ['admin']], function () {
     require base_path('routes/web-admin.php');
 });
 
