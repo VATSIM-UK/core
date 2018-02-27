3ae49e58f9a79f18527010c0a948bd369548cf69
Alex Toff <a.toff@virgin.net>
2018-02-27 09:45:18 +0000

    StyleCI

--- app/Models/Mship/Role.php
+++ app/Models/Mship/Role.php
@@ -139,7 +139,7 @@ class Role extends Model
             $perm_has = str_replace('*', '[A-Za-z0-9]+', $perm_has);
             $perm_has = '/^'.$perm_has.'$/';
 
-            return (boolean) preg_match($perm_has, $permission);
+            return (bool) preg_match($perm_has, $permission);
         })->isEmpty();
     }
 
