4db5ee913e6d216e39a2eb53acb1d7cf82d3d603
Neil Farrington <me@neilfarrington.com>
2018-02-27 10:37:04 +0000

    Avoid double-protecting smartCARS routes

--- app/Http/Middleware/CheckAdminPermissions.php
+++ app/Http/Middleware/CheckAdminPermissions.php
@@ -6,6 +6,10 @@ use Closure;
 
 class CheckAdminPermissions
 {
+    protected $except = [
+        'adm/smartcars',
+    ];
+
     /**
      * Handle an incoming request.
      *
@@ -15,10 +19,35 @@ class CheckAdminPermissions
      */
     public function handle($request, Closure $next)
     {
+        if ($this->shouldPassThrough($request)) {
+            return $next($request);
+        }
+
         if (!$request->user()->hasPermission($request->decodedPath())) {
             abort(403);
         }
 
         return $next($request);
+    }
+
+    /**
+     * Determine if the request has a URI that should pass through.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function shouldPassThrough($request)
+    {
+        foreach ($this->except as $except) {
+            if ($except !== '/') {
+                $except = trim($except, '/');
+            }
+
+            if (starts_with($request->decodedPath(), $except)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
