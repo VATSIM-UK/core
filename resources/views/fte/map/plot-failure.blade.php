1b4ba35c0905fd7c914ec2821725736d452153a3
CalumTowers <me@calumtowers.com>
2018-04-20 22:15:26 +0100

    Add icon on map at point of failure

---
+++ resources/views/fte/map/plot-failure.blade.php
@@ -0,0 +1,13 @@
+var failMarker = new google.maps.Marker({
+position: new google.maps.LatLng({{ $pirep->failedAt->latitude }}, {{ $pirep->failedAt->longitude }}),
+title: 'Failure Point',
+map: map,
+});
+
+var failMarkerwindow = new google.maps.InfoWindow({
+content: '{{ $pirep->pass_reason }}',
+});
+
+failMarker.addListener('click', function() {
+failMarkerwindow.open(map, failMarker);
+});
