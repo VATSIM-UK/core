771d3a436a476461c61614f36e1bf9c721fbb93c
CalumTowers <me@calumtowers.com>
2018-04-22 15:16:50 +0100

    Implement network check on FTE

--- app/Listeners/Smartcars/EvaluateFlightCriteria.php
+++ app/Listeners/Smartcars/EvaluateFlightCriteria.php
@@ -4,6 +4,7 @@ namespace App\Listeners\Smartcars;
 
 use App\Events\Smartcars\BidCompleted;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\DB;
 
 class EvaluateFlightCriteria implements ShouldQueue
 {
@@ -57,7 +58,27 @@ class EvaluateFlightCriteria implements ShouldQueue
             return;
         }
 
+        $pirepTime = $this->minutes($pirep->flight_time);
+
+        $networkTime = DB::table('networkdata_pilots')
+            ->whereBetween('connected_at', [$posreps->first()->created_at, $posreps->last()->created_at])
+            ->where('account_id', '=', $bid->account_id)
+            ->sum('minutes_online');
+
+        if ((($networkTime / $pirepTime) * 100 ) < 90) {
+            $pirep->markFailed('You were not connected to the VATSIM network.', null);
+            $pirep->save();
+
+            return;
+        }
+
         $pirep->markPassed('Success: Flight passed all required checks');
         $pirep->save();
+    }
+
+    public function minutes($time)
+    {
+        $time = explode(':', $time);
+        return ($time[0]*60) + ($time[1]) + ($time[2]/60);
     }
 }
