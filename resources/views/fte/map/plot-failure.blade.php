f09b50b53a22c564696de953b8cbbe9b89a9d08d
CalumTowers <me@calumtowers.com>
2018-04-28 15:40:55 +0100

    Add 'F' label to marker on map

--- resources/views/fte/map/plot-failure.blade.php
+++ resources/views/fte/map/plot-failure.blade.php
@@ -2,6 +2,7 @@ var failMarker = new google.maps.Marker({
 position: new google.maps.LatLng({{ $pirep->failedAt->latitude }}, {{ $pirep->failedAt->longitude }}),
 title: 'Failure Point',
 map: map,
+label: 'F',
 });
 
 var failMarkerwindow = new google.maps.InfoWindow({
