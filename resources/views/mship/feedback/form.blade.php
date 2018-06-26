d2a4d3c5ae5f4b564b39ffc14b3006270abb0dd9
CalumTowers <me@calumtowers.com>
2018-06-26 19:35:09 +0100

    Refactor feedback form front end
    Remove submit button usage and style

--- resources/views/mship/feedback/form.blade.php
+++ resources/views/mship/feedback/form.blade.php
@@ -74,7 +74,7 @@
         <div class="row">
             <div class="col-md-12">
               {{ Form::label('name', 'Member\'s Name') }}
-              {{ Form::text('name', null, ['id' => 'member-search-name']) }}
+              {{ Form::text('name', null, ['id' => 'member-search-name', 'class' => 'form-control']) }}
             </div>
         </div>
         <hr>
@@ -110,8 +110,8 @@
         @if (!isset($form))
           {!! Form::open(["route" => ["mship.feedback.new"]]) !!}
             {{Form::label('feedback_type', 'What kind of feedback would you like to leave?')}}
-            {{Form::select('feedback_type', $feedbackForms)}}
-            {{Form::submit("Next")}}
+            {{Form::select('feedback_type', $feedbackForms, [], ['class' => 'form-control']) }}
+            <button type="submit" class="btn btn-default">Next</button>
           {!! Form::close() !!}
         @else
           {!! Form::open(["route" => ["mship.feedback.new.form.post", $form->slug]]) !!}
@@ -151,7 +151,7 @@
             </div>
   				@endforeach
           <div class="form-group">
-            {{ Form::submit() }}
+            <button type="submit" class="btn btn-default">Submit</button>
           </div>
           {!! Form::close() !!}
         @endif
