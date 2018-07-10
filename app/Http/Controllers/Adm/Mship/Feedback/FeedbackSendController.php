f102560824e601473f71fc3eedb435df7ed0d458
CalumTowers <me@calumtowers.com>
2018-07-10 22:16:30 +0100

    Add route and controller

---
+++ app/Http/Controllers/Adm/Mship/Feedback/FeedbackSendController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Adm\Mship\Feedback;
+
+use Illuminate\Http\Request;
+use App\Models\Mship\Feedback\Feedback as FeedbackModel;
+use Illuminate\Support\Facades\Redirect;
+
+
+class FeedbackSendController extends \App\Http\Controllers\BaseController
+{
+    public function store(FeedbackModel $feedback, Request $request) {
+        // dd("Hit Controller");
+
+        $conditions = [];
+        $conditions[] = $this->account->hasChildPermission('adm/mship/feedback/list') || $this->account->hasChildPermission('adm/mship/feedback/list/*');
+        $conditions[] = $this->account->hasPermission('adm/mship/feedback/list/'.$feedback->form->slug);
+
+        foreach ($conditions as $condition) {
+            if ($condition && !$feedback->sent_at) {
+                $feedback->markSent(\Auth::user(), $request->input('comment'));
+
+                return Redirect::back()
+                    ->withSuccess('Feedback sent to user!');
+            }
+        }
+
+        abort(403, 'Unauthorized action.');
+    }
+}
