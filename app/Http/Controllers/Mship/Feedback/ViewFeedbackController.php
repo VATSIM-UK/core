c4071d636fd7e5aec207ee4982783e4a4fb72240
CalumTowers <me@calumtowers.com>
2018-07-10 23:54:14 +0100

    Re-order list

--- app/Http/Controllers/Mship/Feedback/ViewFeedbackController.php
+++ app/Http/Controllers/Mship/Feedback/ViewFeedbackController.php
@@ -8,7 +8,10 @@ class ViewFeedbackController extends \App\Http\Controllers\BaseController
 {
     public function show()
     {
-        $feedback = $this->account->feedback()->sent()->get();
+        $feedback = $this->account->feedback()
+            ->sent()
+            ->get()
+            ->reverse();
 
         if (!isset($feedback)) {
             return Redirect::route('mship.manage.dashboard')->withError('You have no feedback available to view at this time.');
