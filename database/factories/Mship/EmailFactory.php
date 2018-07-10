fa992907c0259a93e606d0f052aa0890ac1aa402
Callum Axon <callumaxon@gmail.com>
2018-07-10 20:21:44 +0100

    Email Assignments Tests (#1186)

---
+++ database/factories/Mship/EmailFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Mship\Account\Email::class, function (Faker $faker) {
+    return [
+        'id' => $faker->numberBetween(1, 100000),
+        'account_id' => function () {
+            return factory(\App\Models\Mship\Account::class)->create()->id;
+        },
+        'email' => $faker->email,
+        'verified_at' => $faker->dateTime(),
+        'created_at' => $faker->dateTime(),
+        'updated_at' => $faker->dateTime(),
+    ];
+});
+
+$factory->state(\App\Models\Mship\Account\Email::class, 'unverified', function (Faker $faker) {
+    return [
+        'verified_at' => null,
+    ];
+});
+
+$factory->state(\App\Models\Mship\Account\Email::class, 'verified', function () {
+    return [
+        'verified_at' => now(),
+    ];
+});
