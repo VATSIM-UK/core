2950057ce57ae2ac4f05111c65061e4e57b1ddde
CalumTowers <me@calumtowers.com>
2018-07-14 17:04:43 +0100

    Incomplete tests

---
+++ tests/Feature/Feedback/FeedbackSendTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
+
+class FeedbackSendTest extends TestCase
+{
+    //
+}
