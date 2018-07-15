0f999d2942ca7d21b90a88f5b4ce62acc9f13c80
Calum Towers <me@calumtowers.com>
2018-07-15 18:01:37 +0000

    Apply fixes from StyleCI

--- database/factories/FeedbackFactory.php
+++ database/factories/FeedbackFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Models\Mship\Feedback\Form::class, function (Faker $faker) 
     return [
         'name' => $faker->words(2, true),
         'slug' => strtolower($faker->word),
-        'contact_id' => $faker->numberBetween(1,3),
+        'contact_id' => $faker->numberBetween(1, 3),
         'enabled' => 1,
         'targeted' => 1,
         'public' => 1,
@@ -15,19 +15,19 @@ $factory->define(App\Models\Mship\Feedback\Form::class, function (Faker $faker) 
 
 $factory->define(App\Models\Mship\Feedback\Question\Type::class, function (Faker $faker) {
     return [
-        "name" => $faker->word,
-        "code" => $faker->words(10, true),
+        'name' => $faker->word,
+        'code' => $faker->words(10, true),
     ];
 });
 
 $factory->define(App\Models\Mship\Feedback\Question::class, function (Faker $faker) {
     return [
-        "type_id" => factory(App\Models\Mship\Feedback\Question\Type::class)->create(),
-        "form_id" => factory(App\Models\Mship\Feedback\Form::class)->create(),
-        "slug" => $faker->word,
-        "question" => $faker->sentence,
-        "required" => 0,
-        "sequence" => 1,
-        "permanent" => 1,
+        'type_id' => factory(App\Models\Mship\Feedback\Question\Type::class)->create(),
+        'form_id' => factory(App\Models\Mship\Feedback\Form::class)->create(),
+        'slug' => $faker->word,
+        'question' => $faker->sentence,
+        'required' => 0,
+        'sequence' => 1,
+        'permanent' => 1,
     ];
 });
--- tests/Feature/FeedbackTest.php
+++ tests/Feature/FeedbackTest.php
@@ -2,12 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Mship\Feedback\Form;
+use App\Models\Mship\Feedback\Question;
+use App\Models\Mship\Feedback\Question\Type;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
-use App\Models\Mship\Account;
-use App\Models\Mship\Feedback\Form;
-use App\Models\Mship\Feedback\Question\Type;
-use App\Models\Mship\Feedback\Question;
 
 class FeedbackTest extends TestCase
 {
@@ -22,52 +21,52 @@ class FeedbackTest extends TestCase
     public function itCreatesAFeedbackForm()
     {
         $form = [
-            "name" => "My New Feedback Form",
-            "slug" => "mnff"
+            'name' => 'My New Feedback Form',
+            'slug' => 'mnff',
         ];
 
         $newForm = factory(Form::class)->create($form);
 
-        $this->assertEquals("My New Feedback Form", $newForm->name);
-        $this->assertEquals("mnff", $newForm->slug);
+        $this->assertEquals('My New Feedback Form', $newForm->name);
+        $this->assertEquals('mnff', $newForm->slug);
         $this->assertDatabaseHas('mship_feedback_forms', $form);
     }
 
     /** @test * */
     public function itCreatesAQuestionType()
     {
-        $code = "<input name=\"%1\" type=\"radio\" style=\"margin-left: 20px;\" value=\"%4\" id=\"%1\" %5> %3";
+        $code = '<input name="%1" type="radio" style="margin-left: 20px;" value="%4" id="%1" %5> %3';
 
         $questionType = [
-            "name" => "radio",
-            "code" => $code,
+            'name' => 'radio',
+            'code' => $code,
         ];
 
         $newQuestionType = factory(Type::class)->create($questionType);
 
-        $this->assertEquals("radio", $newQuestionType->name);
+        $this->assertEquals('radio', $newQuestionType->name);
         $this->assertEquals($code, $newQuestionType->code);
-        $this->assertDatabaseHas("mship_feedback_question_types", $questionType);
+        $this->assertDatabaseHas('mship_feedback_question_types', $questionType);
     }
 
     /** @test * */
     public function itCreatesAQuestion()
     {
         $question = [
-            "type_id" => factory(Type::class)->create()->id,
-            "form_id" => factory(Form::class)->create()->id,
-            "slug" => "myquestion",
-            "question" => "This is a sample question.",
-            "required" => 0,
-            "sequence" => 1,
-            "permanent" => 1,
+            'type_id' => factory(Type::class)->create()->id,
+            'form_id' => factory(Form::class)->create()->id,
+            'slug' => 'myquestion',
+            'question' => 'This is a sample question.',
+            'required' => 0,
+            'sequence' => 1,
+            'permanent' => 1,
         ];
 
         $newQuestion = factory(Question::class)->create($question);
 
-        $this->assertEquals("myquestion", $newQuestion->slug);
-        $this->assertEquals("This is a sample question.", $newQuestion->question);
-        $this->assertDatabaseHas("mship_feedback_questions", $question);
+        $this->assertEquals('myquestion', $newQuestion->slug);
+        $this->assertEquals('This is a sample question.', $newQuestion->question);
+        $this->assertDatabaseHas('mship_feedback_questions', $question);
     }
 
     /** @test * */
