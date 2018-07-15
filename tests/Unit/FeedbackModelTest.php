6e980a339976b256329584a5a9d8b6a2df199cba
CalumTowers <me@calumtowers.com>
2018-07-15 19:50:57 +0100

    Actually remember how PHP works

--- tests/Unit/FeedbackModelTest.php
+++ tests/Unit/FeedbackModelTest.php
@@ -5,14 +5,24 @@ namespace Tests\Unit;
 use App\Models\Mship\Feedback\Form;
 use App\Models\Mship\Feedback\Question;
 
-/** @test * */
-public function itSavesTheQuestionsForAForm()
+class FeedbackTest extends TestCase
 {
-    $form = factory(Form::class)->create();
+    use DatabaseTransactions;
 
-    factory(Question::class, 3)->create([
-        'form_id' => $form->id,
-    ]);
+    public function setUp()
+    {
+        parent::setUp();
+    }
 
-    $this->assertEquals(3, $form->questions->count());
+    /** @test * */
+    public function itSavesTheQuestionsForAForm()
+    {
+        $form = factory(Form::class)->create();
+
+        factory(Question::class, 3)->create([
+            'form_id' => $form->id,
+        ]);
+
+        $this->assertEquals(3, $form->questions->count());
+    }
 }
