c8e0399f75a7e7ff0273d2016ddc8534b627013f
CalumTowers <me@calumtowers.com>
2018-07-16 20:15:35 +0100

    Add sending tests

--- tests/Feature/FeedbackTest.php
+++ tests/Feature/FeedbackTest.php
@@ -2,19 +2,36 @@
 
 namespace Tests\Feature;
 
+use App\Models\Mship\Feedback\Feedback;
 use App\Models\Mship\Feedback\Form;
 use App\Models\Mship\Feedback\Question;
 use App\Models\Mship\Feedback\Question\Type;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
+use App\Models\Mship\Role;
+use App\Models\Mship\Account;
+use App\Models\Mship\State;
 
 class FeedbackTest extends TestCase
 {
     use DatabaseTransactions;
 
+    private $admin;
+    private $member;
+    private $feedback;
+
     public function setUp()
     {
         parent::setUp();
+
+        $this->admin = factory(Account::class)->create();
+        $this->admin->roles()->attach(Role::find(1));
+        $this->admin->addState(State::findByCode('DIVISION'));
+
+        $this->member = factory(Account::class)->create();
+        $this->member->addState(State::findByCode('DIVISION'));
+
+        $this->feedback = factory(Feedback::class)->create();
     }
 
     /** @test * */
@@ -96,7 +113,16 @@ class FeedbackTest extends TestCase
     /** @test * */
     public function itAllowsSendingWithPermission()
     {
-        //
+        $this->actingAs($this->admin)->post(route('adm.mship.feedback.send', $this->feedback->id))
+            ->assertRedirect()
+            ->assertSessionHas('success');
+    }
+
+    /** @test * */
+    public function itDoesNotAllowSendingWithoutPermission()
+    {
+        $this->actingAs($this->member)->post(route('adm.mship.feedback.send', $this->feedback->id))
+            ->assertStatus(403);
     }
 
     /** @test * */
