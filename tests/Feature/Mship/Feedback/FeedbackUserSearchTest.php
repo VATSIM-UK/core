4ff008324e30a7dddfb40801771d43d4a5737b43
Calum Towers <me@calumtowers.com>
2018-07-19 18:50:46 +0100

    Add tests for user lookup return

---
+++ tests/Feature/Mship/Feedback/FeedbackUserSearchTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
+use App\Models\Mship\Account;
+
+class FeedbackUserSearchTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    private $member;
+    private $otherMember;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->member = factory(Account::class)->create([
+            'name_first' => 'John',
+            'name_last' => 'Doe'
+        ]);
+
+        $this->otherMember = factory(Account::class)->create([
+            'name_first' => 'Brad',
+            'name_last' => 'Pitt'
+        ]);
+    }
+
+    /** @test * */
+    public function testItDoesNotReturnCurrentUser()
+    {
+        $searchQuery = $this->actingAs($this->member)->get(route('mship.feedback.usersearch', $this->member->real_name))->getContent();
+
+        $this->assertNotContains($this->member->real_name, $searchQuery);
+    }
+
+    /** @test * */
+    public function testItReturnsAnotherUser()
+    {
+        $searchQuery = $this->actingAs($this->member)->get(route('mship.feedback.usersearch', $this->otherMember->real_name))->getContent();
+
+        $this->assertContains($this->otherMember->real_name, $searchQuery);
+    }
+}
