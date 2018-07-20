6878d9827de9a580e7b1b7c8273b9729e0b19047
Calum Towers <me@calumtowers.com>
2018-07-20 21:53:55 +0100

    Reformat and remove line breaks

--- tests/Feature/AdminMiddlewareTest.php
+++ tests/Feature/AdminMiddlewareTest.php
@@ -21,12 +21,7 @@ class AdminMiddlewareTest extends TestCase
 
         $this->normalAccount = factory(\App\Models\Mship\Account::class)->create();
 
-        $this->normalAccount->addState(State::findByCode('DIVISION'));
-
-        $this->nothingAccount = factory(\App\Models\Mship\Account::class)->create();
-
         $this->superUser = factory(\App\Models\Mship\Account::class)->create();
-
         $this->superUser->roles()->attach(\App\Models\Mship\Role::find(1));
     }
 
@@ -34,55 +29,37 @@ class AdminMiddlewareTest extends TestCase
     public function testAUserWithPermissionCanAccessAnExplicitEndPoint()
     {
         $role = factory(\App\Models\Mship\Role::class)->create();
-
-        // permission pre-populated via migrations: GET adm/dashboard
-        $role->permissions()->attach(Permission::find(2));
-
+        $role->permissions()->attach(Permission::find(2)); // GET adm/dashboard
         $this->normalAccount->roles()->attach($role);
 
         $this->actingAs($this->normalAccount, 'web')->get(route('adm.dashboard'))->assertSuccessful();
-
         $this->actingAs($this->superUser, 'web')->get(route('adm.dashboard'))->assertSuccessful();
-
-        $this->actingAs($this->nothingAccount, 'web')->get(route('adm.dashboard'))->assertForbidden();
     }
 
     /** @test * */
     public function testAUserWithPermissionCanAccessAWildcardEndpoint()
     {
         $role = factory(\App\Models\Mship\Role::class)->create();
-
-        // permission pre-populated via migrations: GET adm/mship/account/*
-        $role->permissions()->attach(Permission::find(6));
-
+        $role->permissions()->attach(Permission::find(6)); // GET adm/mship/account/*
         $this->normalAccount->roles()->attach($role);
 
         $this->actingAs($this->normalAccount, 'web')->get(route('adm.mship.account.details',
             $this->normalAccount))->assertSuccessful();
-
         $this->actingAs($this->superUser, 'web')->get(route('adm.mship.account.details',
             $this->normalAccount))->assertSuccessful();
-
-        $this->actingAs($this->nothingAccount, 'web')->get(route('adm.mship.account.details',
-            $this->normalAccount))->assertForbidden();
     }
 
     /** @test * */
     public function testAUserWithAnExplicitPermissionCanAccessEndpoint()
     {
         $testAccount = factory(\App\Models\Mship\Account::class)->create();
-
         $permission = factory(\App\Models\Mship\Permission::class)->create(['name' => "adm/mship/account/{$testAccount->id}/"]);
-
         $role = factory(\App\Models\Mship\Role::class)->create();
-
         $role->permissions()->attach($permission->first());
-
         $this->normalAccount->roles()->attach($role);
 
         $this->actingAs($this->normalAccount, 'web')->get(route('adm.mship.account.details',
             $testAccount))->assertSuccessful();
-
         $this->actingAs($this->superUser, 'web')->get(route('adm.mship.account.details',
             $testAccount))->assertSuccessful();
     }
