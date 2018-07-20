a24db50e2b5f907f0a521f97db2b28652e498c21
Calum Towers <me@calumtowers.com>
2018-07-20 23:04:17 +0100

    Restyle feedback view into table

--- resources/views/mship/feedback/view.blade.php
+++ resources/views/mship/feedback/view.blade.php
@@ -5,16 +5,29 @@
 	<div class="panel-heading"> My Feedback</div>
 	<div class="panel-body">
 		<p>Here, you can view feedback that has been sent to you by the VATSIM UK training departments.</p>
-        @foreach($feedback as $item)
-            <b>Type: </b> {{ $item->form->name }}<br />
-			<b>Feedback Left: </b> {{ $item->created_at->format('d M Y') }}<br />
-			@if($item->sent_comment)
-			<b>Comment: </b> {{ $item->sent_comment }}<br />
-			<b>Comment By: </b> {{ $item->sender->real_name }} ({{ $item->sender->id }})<br /><br />
-			@else
-			<b>Comment: </b> The training department wanted to make you aware of some positive feedback that was left for you.<br /><br />
-			@endif
-        @endforeach
+
+		<table class="table">
+			<tr>
+				<th>Type</th>
+				<th>Feedback Left At</th>
+				<th>Message</th>
+				<th>Message From</th>
+			</tr>
+			@foreach($feedback as $item)
+				<tr>
+					<td>{{ $item->form->name }}</td>
+					<td>{{ $item->created_at->format('d M Y') }}</td>
+					@if($item->sent_comment)
+						<td>{{ $item->sent_comment }}</td>
+						<td>{{ $item->sender->real_name }} ({{ $item->sender->id }})</td>
+					@else
+						<td>The training department wanted to make you aware of some positive feedback that was left for you.</td>
+						<td>Automatic</td>
+				</tr>
+				@endif
+			@endforeach
+		</table>
+
     </div>
 </div>
 
