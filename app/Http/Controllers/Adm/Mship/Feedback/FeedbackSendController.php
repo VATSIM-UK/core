f5c39909908d5aedafba1a2b2e03d448ddf60084
Calum Towers <me@calumtowers.com>
2018-07-20 22:44:07 +0100

    Use Auth facade

--- app/Http/Controllers/Adm/Mship/Feedback/FeedbackSendController.php
+++ app/Http/Controllers/Adm/Mship/Feedback/FeedbackSendController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Adm\Mship\Feedback;
 use App\Models\Mship\Feedback\Feedback as FeedbackModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Auth;
 
 class FeedbackSendController extends \App\Http\Controllers\BaseController
 {
     public function store(FeedbackModel $feedback, Request $request)
     {
-        $feedback->markSent(\Auth::user(), $request->input('comment'));
+        $feedback->markSent(Auth::user(), $request->input('comment'));
 
         return Redirect::back()
             ->withSuccess('Feedback sent to user!');
