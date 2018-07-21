bb47c46e5c05827f9e1f550fd35be04f92e42d5c
vatsim-uk-pa <privileged-access@vatsim-uk.co.uk>
2018-07-21 16:04:48 +0000

    Apply fixes from StyleCI

--- database/factories/WaitingListFactory.php
+++ database/factories/WaitingListFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Training\WaitingList::class, function (Faker $faker) {
     $name = $faker->name;
+
     return [
         'name' => $name,
         'slug' => str_slug($name),
