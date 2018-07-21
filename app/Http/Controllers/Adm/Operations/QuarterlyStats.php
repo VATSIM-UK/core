a80560d1b6b08dcea2a533aae5c5aefb9994259c
Calum Towers <me@calumtowers.com>
2018-07-21 20:26:11 +0100

    Quarterly Stats (#1179)

---
+++ app/Http/Controllers/Adm/Operations/QuarterlyStats.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Adm\Operations;
+
+use App\Http\Controllers\Adm\AdmController;
+use App\Models\Mship\Account;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Validation\Rule;
+
+class QuarterlyStats extends AdmController
+{
+    public function get()
+    {
+        return $this->viewMake('adm.ops.qstats');
+    }
+
+    public function generate(Request $request)
+    {
+        $this->generateValidation($request);
+
+        $startDate = Carbon::parse($request->get('year').'-'.$request->get('quarter'));
+        $endDate = Carbon::parse($request->get('year').'-'.$request->get('quarter'))->addMonths(4);
+
+        $stats = collect([
+            ['name' => 'Left Division', 'value' => $this->membersLeftDivision($startDate, $endDate)],
+            ['name' => 'Pilots Visiting', 'value' => $this->pilotsVisiting($startDate, $endDate)],
+            ['name' => 'New Joiners as First Division', 'value' => $this->newJoinersAsFirstDivision($startDate, $endDate)],
+            ['name' => 'Members Becoming Inactive', 'value' => $this->membersBecomingInactive($startDate, $endDate)],
+            ['name' => 'Visiting Controllers Above S1', 'value' => $this->visitingControllersAboveS1($startDate, $endDate)],
+            ['name' => 'Completed Transfer (Ex OBS)', 'value' => $this->completedTransfersExObs($startDate, $endDate)],
+        ]);
+
+        return $this->viewMake('adm.ops.qstats')
+                ->with('stats', $stats)
+                ->with('quarter', $startDate->quarter)
+                ->with('year', $startDate->year);
+    }
+
+    private function generateValidation(Request $request)
+    {
+        return $request->validate([
+            'quarter' => [
+                'required',
+                Rule::in(['01-01', '04-01', '07-01', '10-01']),
+            ],
+            'year' => 'required|numeric|min:2016|max:'.Carbon::now()->year,
+        ]);
+    }
+
+    private function membersLeftDivision($startDate, $endDate)
+    {
+        return Account::whereHas('statesHistory', function ($q) use ($startDate, $endDate) {
+            $q->where('code', 'DIVISION')->whereBetween('end_at', [$startDate, $endDate]);
+        })->count();
+    }
+
+    private function pilotsVisiting($startDate, $endDate)
+    {
+        return Account::whereHas('notes', function ($q) use ($startDate, $endDate) {
+            $q->where('content', 'like', '% Pilot Training was accepted%')->whereBetween('created_at', [$startDate, $endDate]);
+        })->count();
+    }
+
+    private function newJoinersAsFirstDivision($startDate, $endDate)
+    {
+        return Account::whereHas('statesHistory', function ($q) use ($startDate, $endDate) {
+            $q->whereBetween('start_at', [$startDate, $endDate])->whereColumn('mship_account.created_at', 'start_at')->where('mship_state.id', 3);
+        })->whereBetween('joined_at', [$startDate, $endDate])->count();
+    }
+
+    private function membersBecomingInactive($startDate, $endDate)
+    {
+        return Account::whereHas('statesHistory', function ($q) use ($startDate, $endDate) {
+            $q->where('mship_state.id', 3)->where(function ($query) use ($startDate, $endDate) {
+                $query->whereNotBetween('end_at', [$startDate, $endDate])->orWhere('end_at', null);
+            });
+        })->whereHas('datachanges', function ($q) use ($startDate, $endDate) {
+            $q->where('data_key', 'inactive')->where('data_new', 1)->whereBetween('created_at', [$startDate, $endDate]);
+        })->count();
+    }
+
+    private function completedTransfersExObs($startDate, $endDate)
+    {
+        return Account::whereHas('statesHistory', function ($q) use ($startDate, $endDate) {
+            $q->where('mship_state.id', 3)->whereNull('end_at')->whereBetween('start_at', [$startDate, $endDate]);
+        })->whereHas('statesHistory', function ($q) use ($startDate, $endDate) {
+            $q->where('mship_state.id', '!=', 3)->whereBetween('end_at', [$startDate, $endDate]);
+        })->whereHas('qualifications', function ($q) {
+            $q->whereBetween('mship_qualification.id', [3, 10]);
+        })->count();
+    }
+
+    private function visitingControllersAboveS1($startDate, $endDate)
+    {
+        return Account::whereHas('statesHistory', function ($q) use ($startDate, $endDate) {
+            $q->where('mship_state.id', 2)->whereBetween('start_at', [$startDate, $endDate]);
+        })->whereHas('qualifications', function ($q) {
+            $q->whereBetween('mship_qualification.id', [3, 10]);
+        })->count();
+    }
+}
---
+++ resources/views/adm/ops/qstats.blade.php
@@ -0,0 +1,87 @@
+@extends('adm.layout')
+
+@section('content')
+    <section class="content">
+        <div class="row">
+            <div class="col xs-12">
+                <div class="box box-warning">
+                    <div class="box-header">
+                        <div class="box-title">Quarterly Stats</div>
+                        <div class="box-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <p>Here, you can generate statistics that are used for the quarterly report to
+                                        VATEUR.</p>
+                                </div>
+                            </div>
+                            {!! Form::open(['route' => 'adm.ops.qstats.generate', 'method' => 'post', 'autocomplete' => 'off']) !!}
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        {!! Form::label("quarter" ,"Quarter",['class' => 'control-label']),
+                                        Form::select("quarter", ['01-01' => 'Q1', '04-01' => 'Q2', '07-01' => 'Q3', '10-01' => 'Q4'], '', ['class' => 'form-control', 'id' => 'quarter', 'placeholder' => 'Please Select Quarter']) !!}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        {!! Form::label("year","Year",['class' => 'control-label']),
+                                        Form::number("year", '', ['class' => 'form-control', 'id' => 'year']) !!}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <div class="form-group">
+                                        {!! Form::submit('Generate', ['class' => 'btn btn-primary']) !!}
+                                    </div>
+                                </div>
+                            </div>
+
+                            {!! Form::close() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        @isset($stats)
+            <div class="row">
+                <div class="col xs-12">
+                    <div class="box box-primary">
+                        <div class="box-header">
+                            <div class="box-title">Results</div>
+                        </div>
+                        <div class="box-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <p>These are the stastics for <strong>Q{{ $quarter }}</strong> of <strong>{{ $year }}</strong>.</p>
+                                </div>
+                            </div>
+                            <table id="quarterly-stats" class="table table-bordered">
+                                <thead>
+                                <tr>
+                                    <th class="col-md-1">Statistic</th>
+                                    <th class="col-md-2">Value</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+
+                                @foreach($stats as $stat)
+                                    <tr>
+                                        <td>{{ $stat['name'] }}</td>
+                                        <td>{{ $stat['value'] }}</td>
+                                    </tr>
+                                @endforeach
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+
+    </section>
+@stop
