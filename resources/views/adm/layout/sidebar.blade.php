1e725299fa01c0064e7e6926d7544ad30f649f14
Calum Towers <me@calumtowers.com>
2018-07-22 20:53:29 +0100

    Remove commented out pages

--- resources/views/adm/layout/sidebar.blade.php
+++ resources/views/adm/layout/sidebar.blade.php
@@ -31,54 +31,6 @@
                 </a>
             </li>
             @endif
-                    {{-- </li>
-        <li>
-            <a href="pages/mailbox.html">
-                <i class="fa fa-envelope"></i> <span>Mailbox</span>
-                <small class="badge pull-right bg-yellow">12</small>
-                <small class="badge pull-right bg-red">SOON</small>
-            </a>
-        </li>
-        <li class="treeview">
-            <a href="#">
-                <i class="fa fa-table"></i> <span>vData</span>
-                <i class="fa fa-angle-left pull-right"></i>
-                <small class="badge pull-right bg-red">SOON</small>
-            </a>
-            <ul class="treeview-menu">
-                <li><a href="#"><i class="fa fa-angle-double-right"></i> Airline Database</a></li>
-                <li><a href="#"><i class="fa fa-angle-double-right"></i> Aircraft Database</a></li>
-                <li><a href="{{ URL::to('/adm/navdata/airport') }}"><i class="fa fa-angle-double-right"></i> Airport Database</a></li>
-                <li><a href="#"><i class="fa fa-angle-double-right"></i> ATC Database</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="#">
-                <i class="ion ion-android-microphone"></i> <span>ATC Bookings</span>
-                <small class="badge pull-right bg-blue">1721</small>
-                <small class="badge pull-right bg-red">SOON</small>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <i class="ion ion-plane"></i> <span>Pilot Bookings</span>
-                <small class="badge pull-right bg-green">3,456</small>
-                <small class="badge pull-right bg-red">SOON</small>
-            </a>
-        </li>
-        <li class="treeview">
-            <a href="#">
-                <i class="fa fa-bar-chart-o"></i>
-                <span>Statistics</span>
-                <i class="fa fa-angle-left pull-right"></i>
-                <small class="badge pull-right bg-red">SOON</small>
-            </a>
-            <ul class="treeview-menu">
-                <li><a href="#"><i class="fa fa-angle-double-right"></i> API Calls</a></li>
-                <li><a href="#"><i class="fa fa-angle-double-right"></i> Pilot Bookings</a></li>
-                <li><a href="#"><i class="fa fa-angle-double-right"></i> ATC Bookings</a></li>
-            </ul>
-        </li>--> --}}
             @if($_account->hasChildPermission("adm/mship"))
                 <li class="treeview {{ ((\Request::is('adm/mship*') && !\Request::is('adm/mship/feedback*')) ? 'active' : '') }}">
                     <a href="#">
