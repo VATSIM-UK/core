fb6d8c74b7ee8bf5fd27b145af13dcdce453e453
Calum Towers <me@calumtowers.com>
2018-07-22 20:55:16 +0100

    Reformat

--- resources/views/adm/layout/sidebar.blade.php
+++ resources/views/adm/layout/sidebar.blade.php
@@ -12,16 +12,14 @@
         </div>
     </div>
 
-    <!-- search form -->
     {!! Form::open(["url" => URL::route("adm.search"), "method" => "GET", "class" => "sidebar-form"]) !!}
-    <div class="input-group">
-        <input type="text" name="q" class="form-control" placeholder="Search..."/>
-        <span class="input-group-btn">
-            <button type='submit' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
-        </span>
-    </div>
-{!! Form::close() !!}
-<!-- /.search form -->
+        <div class="input-group">
+            <input type="text" name="q" class="form-control" placeholder="Search..."/>
+            <span class="input-group-btn">
+                <button type='submit' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
+            </span>
+        </div>
+    {!! Form::close() !!}
 
     <ul class="sidebar-menu">
 
