ef78133bdb714e83a35bb22c8faedba5ff889e25
Callum Axon <callumaxon@gmail.com>
2018-07-26 18:10:33 +0100

    Controller for front-end views

---
+++ app/Http/Controllers/Adm/Training/WaitingListManagementController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Adm\Training;
+
+use App\Http\Controllers\Adm\AdmController;
+use App\Models\Training\WaitingList;
+
+class WaitingListManagementController extends AdmController
+{
+    protected $waitingList;
+
+    public function __construct(WaitingList $waitingList)
+    {
+        parent::__construct();
+
+        $this->waitingList = $waitingList;
+    }
+
+    public function index()
+    {
+        $lists = $this->waitingList::with(['accounts'])->get();
+
+        return $this->viewMake('adm.training.index')->with('lists', $lists);
+    }
+
+    public function show(WaitingList $waitingList)
+    {
+        return $this->viewMake('adm.training.manage')
+            ->with('waitingList', $waitingList->with([
+                'accounts' => function ($query) {
+                    $query->orderBy('position');
+                }
+            ])->first());
+    }
+}
