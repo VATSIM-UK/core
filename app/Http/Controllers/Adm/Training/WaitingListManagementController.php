f6a399ace03302dd4f37f31c425e0e94f214462d
vatsim-uk-pa <privileged-access@vatsim-uk.co.uk>
2018-07-26 17:10:46 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Adm/Training/WaitingListManagementController.php
+++ app/Http/Controllers/Adm/Training/WaitingListManagementController.php
@@ -29,7 +29,7 @@ class WaitingListManagementController extends AdmController
             ->with('waitingList', $waitingList->with([
                 'accounts' => function ($query) {
                     $query->orderBy('position');
-                }
+                },
             ])->first());
     }
 }
