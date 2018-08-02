55f3fee2a2e0496431db8f539f1ca2cea07de618
vatsim-uk-pa <privileged-access@vatsim-uk.co.uk>
2018-08-02 13:36:48 +0000

    Apply fixes from StyleCI

--- tests/Unit/VisitTransfer/VisitTransferCleanupTest.php
+++ tests/Unit/VisitTransfer/VisitTransferCleanupTest.php
@@ -3,104 +3,104 @@
 namespace Tests\Unit\VisitTransfer;
 
 use Artisan;
-    use Carbon\Carbon;
-    use Illuminate\Foundation\Testing\DatabaseTransactions;
-    use Tests\TestCase;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
 
-    class VisitTransferCleanupTest extends TestCase
+class VisitTransferCleanupTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public $newApplication;
+    public $oldApplication;
+    public $facility;
+
+    public function setUp()
     {
-        use DatabaseTransactions;
+        parent::setUp();
 
-        public $newApplication;
-        public $oldApplication;
-        public $facility;
-
-        public function setUp()
-        {
-            parent::setUp();
-
-            $account = factory(\App\Models\Mship\Account::class)->create();
-            $this->facility = factory(\App\Models\VisitTransfer\Facility::class, 'atc_visit')->create([
+        $account = factory(\App\Models\Mship\Account::class)->create();
+        $this->facility = factory(\App\Models\VisitTransfer\Facility::class, 'atc_visit')->create([
                 'stage_reference_enabled' => 1,
                 'stage_reference_quantity' => 1,
             ]);
-            $application = $account->createVisitingTransferApplication([
+        $application = $account->createVisitingTransferApplication([
                 'type' => \App\Models\VisitTransfer\Application::TYPE_VISIT,
                 'facility_id' => $this->facility->id,
             ]);
-            $this->newApplication = $application->fresh();
+        $this->newApplication = $application->fresh();
 
-            Carbon::setTestNow(Carbon::now()->subHours(3));
-            $account = factory(\App\Models\Mship\Account::class)->create();
-            $application = $account->createVisitingTransferApplication([
+        Carbon::setTestNow(Carbon::now()->subHours(3));
+        $account = factory(\App\Models\Mship\Account::class)->create();
+        $application = $account->createVisitingTransferApplication([
                 'type' => \App\Models\VisitTransfer\Application::TYPE_VISIT,
                 'facility_id' => $this->facility->id,
             ]);
-            Carbon::setTestNow();
-            $this->oldApplication = $application->fresh();
-        }
+        Carbon::setTestNow();
+        $this->oldApplication = $application->fresh();
+    }
 
-        /** @test */
-        public function testItOnlyCancelsOldApplications()
-        {
-            Artisan::call('visit-transfer:cleanup');
+    /** @test */
+    public function testItOnlyCancelsOldApplications()
+    {
+        Artisan::call('visit-transfer:cleanup');
 
-            $this->assertEquals(\App\Models\VisitTransfer\Application::STATUS_EXPIRED, $this->oldApplication->fresh()->status);
-            $this->assertEquals(\App\Models\VisitTransfer\Application::STATUS_IN_PROGRESS, $this->newApplication->fresh()->status);
-        }
+        $this->assertEquals(\App\Models\VisitTransfer\Application::STATUS_EXPIRED, $this->oldApplication->fresh()->status);
+        $this->assertEquals(\App\Models\VisitTransfer\Application::STATUS_IN_PROGRESS, $this->newApplication->fresh()->status);
+    }
 
-        public function testItLapsesApplicationsForOldContactedReferees()
-        {
-            $application = factory(\App\Models\VisitTransfer\Application::class)->create([
+    public function testItLapsesApplicationsForOldContactedReferees()
+    {
+        $application = factory(\App\Models\VisitTransfer\Application::class)->create([
                 'type' => \App\Models\VisitTransfer\Application::TYPE_VISIT,
                 'facility_id' => $this->facility->id,
                 'status' => \App\Models\VisitTransfer\Application::STATUS_SUBMITTED,
                 'references_required' => 1,
                 'submitted_at' => Carbon::now(),
             ]);
-            Carbon::setTestNow(Carbon::now()->subDays(15));
-            factory(\App\Models\VisitTransfer\Reference::class)->create([
+        Carbon::setTestNow(Carbon::now()->subDays(15));
+        factory(\App\Models\VisitTransfer\Reference::class)->create([
                 'status' => \App\Models\VisitTransfer\Reference::STATUS_REQUESTED,
                 'contacted_at' => Carbon::now(),
                 'application_id' => $application->id,
             ]);
-            Carbon::setTestNow();
+        Carbon::setTestNow();
 
-            Artisan::call('visit-transfer:cleanup');
-            $this->assertEquals(\App\Models\VisitTransfer\Application::STATUS_LAPSED, $application->fresh()->status);
-        }
+        Artisan::call('visit-transfer:cleanup');
+        $this->assertEquals(\App\Models\VisitTransfer\Application::STATUS_LAPSED, $application->fresh()->status);
+    }
 
-        public function testItWontLapseIncorrectApplications()
-        {
-            $application1 = factory(\App\Models\VisitTransfer\Application::class)->create([
+    public function testItWontLapseIncorrectApplications()
+    {
+        $application1 = factory(\App\Models\VisitTransfer\Application::class)->create([
                 'type' => \App\Models\VisitTransfer\Application::TYPE_VISIT,
                 'facility_id' => $this->facility->id,
                 'status' => \App\Models\VisitTransfer\Application::STATUS_SUBMITTED,
                 'references_required' => 1,
                 'submitted_at' => Carbon::now(),
             ]);
-            Carbon::setTestNow(Carbon::now()->subDays(12));
-            factory(\App\Models\VisitTransfer\Reference::class)->create([
+        Carbon::setTestNow(Carbon::now()->subDays(12));
+        factory(\App\Models\VisitTransfer\Reference::class)->create([
                 'status' => \App\Models\VisitTransfer\Reference::STATUS_REQUESTED,
                 'contacted_at' => Carbon::now(),
                 'application_id' => $application1->id,
             ]);
-            Carbon::setTestNow();
+        Carbon::setTestNow();
 
-            $application2 = factory(\App\Models\VisitTransfer\Application::class)->create([
+        $application2 = factory(\App\Models\VisitTransfer\Application::class)->create([
                 'type' => \App\Models\VisitTransfer\Application::TYPE_VISIT,
                 'facility_id' => $this->facility->id,
                 'status' => \App\Models\VisitTransfer\Application::STATUS_SUBMITTED,
                 'references_required' => 1,
                 'submitted_at' => Carbon::now(),
             ]);
-            factory(\App\Models\VisitTransfer\Reference::class)->create([
+        factory(\App\Models\VisitTransfer\Reference::class)->create([
                 'status' => \App\Models\VisitTransfer\Reference::STATUS_REQUESTED,
                 'application_id' => $application2->id,
             ]);
 
-            Artisan::call('visit-transfer:cleanup');
-            $this->assertEquals(\App\Models\VisitTransfer\Application::STATUS_SUBMITTED, $application1->fresh()->status);
-            $this->assertEquals(\App\Models\VisitTransfer\Application::STATUS_SUBMITTED, $application2->fresh()->status);
-        }
+        Artisan::call('visit-transfer:cleanup');
+        $this->assertEquals(\App\Models\VisitTransfer\Application::STATUS_SUBMITTED, $application1->fresh()->status);
+        $this->assertEquals(\App\Models\VisitTransfer\Application::STATUS_SUBMITTED, $application2->fresh()->status);
     }
+}
