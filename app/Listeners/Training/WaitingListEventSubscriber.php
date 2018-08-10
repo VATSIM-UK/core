0989d71bd0858483c894a4cbf4a34e0bae0acd6b
Callum Axon <callumaxon@gmail.com>
2018-08-10 23:41:09 +0100

    Add dummy events for waiting list management

--- app/Listeners/Training/WaitingListEventSubscriber.php
+++ app/Listeners/Training/WaitingListEventSubscriber.php
@@ -6,16 +6,36 @@ use Illuminate\Support\Facades\Log;
 
 class WaitingListEventSubscriber
 {
-    public function userAdded($event)
+    public function accountAdded($event)
     {
         return Log::stack(['training'], "Account {$event->account} ({$event->account->id}) was added to {$event->waitingList}");
+    }
+
+    public function accountPromoted()
+    {
+        // TODO: Implement proper logging.
+    }
+
+    public function accountDemoted()
+    {
+        // TODO: Implement proper logging.
     }
 
     public function subscribe($events)
     {
         $events->listen(
             'App\Events\Training\AccountAddedToWaitingList',
-            'App\Listeners\WaitingListEventSubscriber@userAdded'
+            'App\Listeners\WaitingListEventSubscriber@accountAdded'
+        );
+
+        $events->listen(
+            'App\Events\Training\AccountPromotedInWaitingList',
+            'App\Listeners\WaitingListEventSubscriber@accountPromoted'
+        );
+
+        $events->listen(
+            'App\Events\Training\AccountDemotedInWaitingList',
+            'App\Listeners\WaitingListEventSubscriber@accountDemoted'
         );
     }
 }
