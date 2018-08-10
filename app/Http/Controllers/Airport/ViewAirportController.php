4422ba2806c2f72721718973f376104eeb142294
Alex Toff <me@alextoff.uk>
2018-08-10 17:13:40 +0100

    Help StyleCI Out

--- app/Http/Controllers/Airport/ViewAirportController.php
+++ app/Http/Controllers/Airport/ViewAirportController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\Airport;
 
 use App\Http\Controllers\BaseController;
-    use App\Models\Airport;
+use App\Models\Airport;
 
-    class ViewAirportController extends BaseController
+class ViewAirportController extends BaseController
+{
+    public function show(Airport $airport)
     {
-        public function show(Airport $airport)
-        {
-            $airport->load(['navaids', 'runways', 'procedures', 'stations']);
+        $airport->load(['navaids', 'runways', 'procedures', 'stations']);
 
-            return view('airport.view')->with('airport', $airport);
-        }
+        return view('airport.view')->with('airport', $airport);
     }
+}
