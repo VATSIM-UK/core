c304d255961680c9b517d941826bd8e5cb09730f
Callum Axon <callumaxon@gmail.com>
2018-08-10 23:41:49 +0100

    Actually add the event files for dummy events

---
+++ app/Events/Training/AccountDemotedInWaitingList.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events\Training;
+
+use App\Models\Mship\Account;
+use App\Models\Training\WaitingList;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class AccountDemotedInWaitingList
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    protected $account;
+
+    protected $waitingList;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Account $account
+     * @param WaitingList $waitingList
+     */
+    public function __construct(Account $account, WaitingList $waitingList)
+    {
+        $this->account = $account;
+        $this->waitingList = $waitingList;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
