379b1a1d18cd49a343c7dc8072c8585d80318588
CalumTowers <me@calumtowers.com>
2018-08-11 18:24:02 +0100

    For merge

--- tests/Unit/ApplicationTest.php
+++ tests/Unit/ApplicationTest.php
@@ -2,12 +2,12 @@
 
 namespace Tests\Unit\VisitTransfer;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class ApplicationTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
     /** Unit Testing */
 
