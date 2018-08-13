834a2ac2d612a4a130f416ed60efa6beac307c71
vatsim-uk-pa <privileged-access@vatsim-uk.co.uk>
2018-08-13 07:27:40 +0000

    Apply fixes from StyleCI

--- app/Events/Training/AccountRemovedFromWaitingList.php
+++ app/Events/Training/AccountRemovedFromWaitingList.php
@@ -5,12 +5,10 @@ namespace App\Events\Training;
 use App\Models\Mship\Account;
 use App\Models\Training\WaitingList;
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
 class AccountRemovedFromWaitingList
 {
