3104a23d3daf26d8165ddd9d8a6cf253dbb148bc
Calum Towers <me@calumtowers.com>
2018-08-24 23:43:30 +0100

    CORE-173 / CORE-175 Dynamic Homepage/External Services (#1234)

--- app/Http/Controllers/Site/HomePageController.php
+++ app/Http/Controllers/Site/HomePageController.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Controllers\Site;
 
+use App\Models\Mship\State as State;
+use Illuminate\Support\Facades\Cache as Cache;
+use Illuminate\Support\Facades\DB as DB;
+
 class HomePageController extends \App\Http\Controllers\BaseController
 {
     public function __invoke()
     {
-        return view('site.home');
+        return $this->viewMake('site.home')
+            ->with('stats', $this->stats());
+    }
+
+    private function stats()
+    {
+        $divisionMembers = Cache::remember('home.mship.stats', 1440, function () {
+            $stat['members_division'] = DB::table('mship_account_state')
+                ->leftJoin('mship_account', 'mship_account_state.account_id', '=', 'mship_account.id')
+                ->where('inactive', '=', 0)
+                ->whereNull('mship_account.deleted_at')
+                ->where('state_id', '=', State::findByCode('DIVISION')->id)
+                ->count();
+
+            return $stat;
+        });
+
+        return $divisionMembers;
     }
 }
