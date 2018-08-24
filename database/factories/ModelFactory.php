2903dd6c0b035c3fd5351df8ad3438221199e0a2
vatsim-uk-pa <privileged-access@vatsim-uk.co.uk>
2018-08-24 16:29:22 +0000

    Apply fixes from StyleCI

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -4,7 +4,7 @@ $factory->define(App\Models\Mship\Account::class, function (Faker\Generator $fak
     return [
         'id' => rand(10000000, 99999999),
         'name_first' => $faker->firstName,
-        'name_last' => $faker->lastName                                  ,
+        'name_last' => $faker->lastName,
         'email' => $faker->email,
         'is_invisible' => 0,
     ];
