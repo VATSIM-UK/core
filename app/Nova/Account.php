05cb3b53aeec95f4f7dd4f7c6d62ce2ffde2a8f7
vatsim-uk-pa <privileged-access@vatsim-uk.co.uk>
2018-08-24 23:29:37 +0000

    Apply fixes from StyleCI

--- app/Nova/Account.php
+++ app/Nova/Account.php
@@ -55,14 +55,14 @@ class Account extends Resource
                 ->creationRules('unique:users,email')
                 ->updateRules('unique:users,email,{{resourceId}}'),
 
-            Text::make('ATC Rating', function (){
+            Text::make('ATC Rating', function () {
                 return $this->qualificationAtc->code;
             })->exceptOnForms(),
 
             Text::make('State', function () {
                 $state = $this->states()->first();
 
-                return sprintf("%s (%s / %s)", ucwords(strtolower($state->code)), $state->pivot->region, $state->pivot->division);
+                return sprintf('%s (%s / %s)', ucwords(strtolower($state->code)), $state->pivot->region, $state->pivot->division);
             }),
 
             Password::make('Password')
