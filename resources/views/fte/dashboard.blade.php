01ebacc02c6ee19cab5e9131d18c0633a01445f8
Alex Toff <me@alextoff.uk>
2018-09-01 18:43:05 +0100

    Remove none-avail notice

--- resources/views/fte/dashboard.blade.php
+++ resources/views/fte/dashboard.blade.php
@@ -73,11 +73,7 @@
                 <div class="panel" id="allExercises">
                     <div class="panel-body">
                         <div class="text-center">
-                            @if(\App\Models\Smartcars\Flight::enabled()->count() > 0)
-                                <a href="{{ route('fte.exercises') }}" class="btn btn-primary">View All Exercises &gt;&gt;</a>
-                            @else
-                                <strong>No Flight Training Exercises are available at the moment.</strong>
-                            @endif
+                            <a href="{{ route('fte.exercises') }}" class="btn btn-primary">View All Exercises &gt;&gt;</a>
                         </div>
                     </div>
                 </div>
