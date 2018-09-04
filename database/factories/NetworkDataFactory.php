8e28f2e9cc11f642455b6e05501922e4023f6bc0
Alex Toff <me@alextoff.uk>
2018-09-04 22:30:20 +0100

    Fix odd StyleCI Fix

--- database/factories/NetworkDataFactory.php
+++ database/factories/NetworkDataFactory.php
@@ -7,28 +7,28 @@
         $facility = $faker->randomElement([[4, 'TWR'], [3, 'GND'], [2, 'DEL'], [5, 'APP']]);
 
         return [
-        'account_id' => function () {
-            return factory(\App\Models\Mship\Account::class)->create()->id;
-        },
-        'callsign' => $faker->randomElement(['EGLL', 'EGKK', 'EGCC', 'EGBB']).'_'.$faker->randomElement(['N', 'S', 'F', '']).'_'.$facility[1],
-        'frequency' => $faker->randomFloat(3, 118, 134),
-        'connected_at' => $faker->dateTimeBetween('6 hours ago'),
-        'facility_type' => $facility[0],
-        'qualification_id' => function () {
-            return factory(\App\Models\Mship\Qualification::class)->create()->id;
-        },
-    ];
+            'account_id' => function () {
+                return factory(\App\Models\Mship\Account::class)->create()->id;
+            },
+            'callsign' => $faker->randomElement(['EGLL', 'EGKK', 'EGCC', 'EGBB']) . '_' . $faker->randomElement(['N', 'S', 'F', '']) . '_' . $facility[1],
+            'frequency' => $faker->randomFloat(3, 118, 134),
+            'connected_at' => $faker->dateTimeBetween('6 hours ago'),
+            'facility_type' => $facility[0],
+            'qualification_id' => function () {
+                return factory(\App\Models\Mship\Qualification::class)->create()->id;
+            },
+        ];
     });
-$factory->defineAs(\App\Models\NetworkData\Atc::class, 'offline', function (Faker $faker) {
-    $start = $faker->dateTimeBetween('6 hours ago');
-    $end = $faker->dateTimeBetween($start);
+    $factory->defineAs(\App\Models\NetworkData\Atc::class, 'offline', function (Faker $faker) {
+        $start = $faker->dateTimeBetween('6 hours ago');
+        $end = $faker->dateTimeBetween($start);
 
-    return array_merge(
-        factory(\App\Models\NetworkData\Atc::class)->raw(),
-        [
-            'connected_at' => $start,
-            'disconnected_at' => $faker->dateTimeBetween($start),
-            'minutes_online' => Carbon::instance($start)->diffInMinutes(Carbon::instance($end)),
-        ]
-    );
-});
+        return array_merge(
+            factory(\App\Models\NetworkData\Atc::class)->raw(),
+            [
+                'connected_at' => $start,
+                'disconnected_at' => $faker->dateTimeBetween($start),
+                'minutes_online' => Carbon::instance($start)->diffInMinutes(Carbon::instance($end)),
+            ]
+        );
+    });
