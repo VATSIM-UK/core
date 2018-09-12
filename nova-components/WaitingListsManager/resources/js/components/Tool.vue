1274e41451e2f339e94a059c9267b17936268f50
Callum Axon <callumaxon@gmail.com>
2018-09-12 21:12:25 +0100

    Make api call re-usable

--- nova-components/WaitingListsManager/resources/js/components/Tool.vue
+++ nova-components/WaitingListsManager/resources/js/components/Tool.vue
@@ -1,31 +1,36 @@
 <template>
     <div>
-        <loading-view :loading="!loaded">
-            <p class="flex flex-col justify-center text-center p-2" v-if="numberOfAccounts < 1">
-                There are no accounts assigned to this waiting list.
-            </p>
+        <div class="relative">
+            <loading-view :loading="!loaded">
+                <div class="relative">
+                    <p class="flex flex-col justify-center text-center p-2" v-if="numberOfAccounts < 1">
+                        There are no accounts assigned to this waiting list.
+                    </p>
 
-            <div class="relative -py-3 -px-6" v-if="loaded && numberOfAccounts >= 1">
-                <div class="overflow-hidden overflow-x-auto relative">
-                    <table cellpadding="0" cellspacing="0" data-testid="resource-table" class="table w-full">
-                        <thead>
-                        <tr>
-                            <th class="text-left">Name</th>
-                            <th class="text-left">CID</th>
-                            <th class="text-left">Added On</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr v-for="account in accounts">
-                            <td>{{ account.name }}</td>
-                            <td>{{ account.id }}</td>
-                            <td>{{ account.created_at }}</td>
-                        </tr>
-                        </tbody>
-                    </table>
+                    <div class="card" v-if="loaded && numberOfAccounts >= 1">
+                        <div class="overflow-hidden overflow-x-auto">
+                            <table cellpadding="0" cellspacing="0" data-testid="resource-table" class="table w-full">
+                                <thead>
+                                <tr>
+                                    <th class="text-left">Name</th>
+                                    <th class="text-left">CID</th>
+                                    <th class="text-left">Added On</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr v-for="account in accounts">
+                                    <td>{{ account.name }}</td>
+                                    <td>{{ account.id }}</td>
+                                    <td>{{ this.moment(account.created_at).format("MMMM Do YYYY") }}</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </loading-view>
+            </loading-view>
+        </div>
+
     </div>
 </template>
 
@@ -41,16 +46,23 @@
         },
 
         mounted() {
-            axios.get(`/nova-vendor/waiting-lists-manager/accounts/${this.resourceId}`)
-                .then(response => {
-                    this.accounts = response.data;
-                    this.loaded = true;
-                });
+            this.loadAccounts()
         },
 
         computed: {
             numberOfAccounts() {
                 if (this.loaded) return Object.keys(this.accounts).length
+            }
+        },
+
+        methods: {
+            loadAccounts() {
+                axios.get(`/nova-vendor/waiting-lists-manager/accounts/${this.resourceId}`)
+                    .then(response => {
+                        this.accounts = response.data;
+                        this.loaded = true;
+                    }
+                );
             }
         }
     }
