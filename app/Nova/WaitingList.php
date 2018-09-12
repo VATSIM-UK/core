5b60ab423d07ec7e1ed308680cbc93b73647830f
Callum Axon <callumaxon@gmail.com>
2018-09-12 19:11:57 +0100

    CORE-192 - Waiting List Creation (#1259)
    * Fix error with composer.json file
    
    * Implement nova policy methods for Nova
    
    * Create form fields for a WaitingList
    
    * Apply fixes from StyleCI

---
+++ app/Nova/WaitingList.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Text;
+
+class WaitingList extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\\Models\\Training\\WaitingList';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'name',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            Text::make('Name')
+                ->rules(['required'])
+                ->creationRules('unique:training_waiting_list,name'),
+
+            Select::make('Department')->options([
+                1 => 'ATC Training',
+                2 => 'Pilot Training',
+            ])->displayUsingLabels()->rules(['required'])->sortable(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
--- app/Policies/Training/WaitingListPolicy.php
+++ app/Policies/Training/WaitingListPolicy.php
@@ -5,9 +5,10 @@ namespace App\Policies\Training;
 use App\Models\Mship\Account;
 use App\Models\Mship\Role;
 use App\Models\Training\WaitingList;
+use App\Policies\BasePolicy;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class WaitingListPolicy
+class WaitingListPolicy extends BasePolicy
 {
     use HandlesAuthorization;
 
@@ -46,5 +47,43 @@ class WaitingListPolicy
     private function basePermission(Account $account, WaitingList $waitingList)
     {
         return $waitingList->staff->contains($account);
+    }
+
+    /**
+     * Nova Specific Policies
+     */
+    public function viewAny(Account $account)
+    {
+        // TODO: Implement viewAny() method.
+    }
+
+    public function view(Account $account)
+    {
+        // TODO: Implement view() method.
+    }
+
+    public function create(Account $account)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function update(Account $account)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete(Account $account)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function restore(Account $account)
+    {
+        // TODO: Implement restore() method.
+    }
+
+    public function forceDelete(Account $account)
+    {
+        // TODO: Implement forceDelete() method.
     }
 }
