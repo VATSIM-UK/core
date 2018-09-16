ffd23ec0f0e13da1802766b4e7a0afd1e730d90a
Calum Towers <me@calumtowers.com>
2018-09-16 17:16:46 +0100

    CORE-123 Community Pages (#1263)

--- app/Http/Controllers/Site/StaffPageController.php
+++ app/Http/Controllers/Site/StaffPageController.php
@@ -12,6 +12,33 @@ class StaffPageController extends \App\Http\Controllers\BaseController
         $this->addBreadcrumb('Staff', route('site.staff'));
 
         return $this->viewMake('site.staff')
-                    ->with('ipboard', new Ipboard());
+            ->with('teamPhotos', $this->getStaffPhotos());
+    }
+
+    private function getStaffPhotos()
+    {
+        $teamPhotos = collect(
+            [
+                54 => null,
+                91 => null,
+                2311 => null,
+                3580 => null,
+                4366 => null,
+                5125 => null,
+                6037 => null,
+                6286 => null,
+            ]
+        );
+
+        try {
+            $ipboard = new Ipboard();
+
+            foreach ($teamPhotos->keys()->toArray() as $staff) {
+                $teamPhotos->put($staff, $ipboard->getMemberById($staff)->photoUrl);
+            }
+        } catch (\Exception $e) {
+        }
+
+        return $teamPhotos;
     }
 }
