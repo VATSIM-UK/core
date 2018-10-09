3372c9a9f6485708b1df550732c5d542a39f46aa
Callum Axon <callumaxon@gmail.com>
2018-10-09 19:02:01 +0100

    CORE-193 - Superman User Command (#1282)

---
+++ tests/Unit/SupermanCommandTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Mship\Account;
+use App\Models\Mship\Role;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SupermanCommandTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $account;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->account = factory(Account::class)->create();
+    }
+
+    /** @test **/
+    public function itReportsToConsoleWhenSuccessful()
+    {
+        $this->artisan('grant:superman', ['cid' => $this->account->id])
+            ->expectsOutput('Account added to the superman role!');
+    }
+
+    /** @test **/
+    public function itReportsToConsoleWhenRoleAlreadyFound()
+    {
+        $this->account->roles()->attach(Role::find(1));
+
+        $this->artisan('grant:superman', ['cid' => $this->account->id])
+            ->expectsOutput('The specified account already has the "superman" role.');
+    }
+
+    /** @test **/
+    public function itReportsToConsoleWhenTheCIDIsNotFound()
+    {
+        $this->artisan('grant:superman', ['cid' => 0000000])
+            ->expectsOutput('The specific CID was not found.');
+    }
+
+    /** @test **/
+    public function itAttachesRoleSuccessfully()
+    {
+        $this->artisan('grant:superman', ['cid' => $this->account->id])
+            ->expectsOutput('Account added to the superman role!');
+
+        $this->assertDatabaseHas('mship_account_role', [
+            'account_id' => $this->account->id,
+            'role_id' => 1,
+        ]);
+    }
+}
