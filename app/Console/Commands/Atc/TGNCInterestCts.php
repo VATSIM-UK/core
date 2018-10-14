28dce69ddb676b9bbba031d8c57dad0a89000976
Calum Towers <me@calumtowers.com>
2018-10-14 19:12:54 +0000

    Apply fixes from StyleCI

--- app/Console/Commands/Atc/TGNCInterestCts.php
+++ app/Console/Commands/Atc/TGNCInterestCts.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Console\Commands\Atc;
+
 use Illuminate\Support\Facades\DB;
 
 class TGNCInterestCts
 {
     public static function getUsers()
     {
-        $results = DB::connection('cts')->select("CALL TGNCInterest()");
+        $results = DB::connection('cts')->select('CALL TGNCInterest()');
 
         $results = collect($results)->each(function ($item, $key) {
             // flatten the collection
