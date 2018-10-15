034f0c1e98e4a6457691caed30549970e814f3e3
Anbthony Lawrence <artisan@anthonylawrence.me.uk>
2018-10-15 18:17:49 +0100

    Fix some wayward local env errors.

--- config/database.php
+++ config/database.php
@@ -34,48 +34,48 @@ return [
     'connections' => [
 
         'sqlite' => [
-            'driver' => 'sqlite',
+            'driver'   => 'sqlite',
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
+            'prefix'   => '',
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_MYSQL_HOST'),
-            'port' => env('DB_MYSQL_PORT'),
-            'database' => env('DB_MYSQL_NAME'),
-            'username' => env('DB_MYSQL_USER'),
-            'password' => env('DB_MYSQL_PASS'),
+            'driver'      => 'mysql',
+            'host'        => env('DB_MYSQL_HOST'),
+            'port'        => env('DB_MYSQL_PORT'),
+            'database'    => env('DB_MYSQL_NAME'),
+            'username'    => env('DB_MYSQL_USER'),
+            'password'    => env('DB_MYSQL_PASS'),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_MYSQL_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_MYSQL_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => env('DB_MYSQL_PREFIX', ''),
-            'strict' => true,
-            'engine' => null,
+            'charset'     => env('DB_MYSQL_CHARSET', 'utf8mb4'),
+            'collation'   => env('DB_MYSQL_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix'      => env('DB_MYSQL_PREFIX', ''),
+            'strict'      => true,
+            'engine'      => null,
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
+            'driver'   => 'pgsql',
+            'host'     => env('DB_HOST', '127.0.0.1'),
+            'port'     => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+            'sslmode'  => 'prefer',
         ],
 
         'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
+            'driver'   => 'sqlsrv',
+            'host'     => env('DB_HOST', 'localhost'),
+            'port'     => env('DB_PORT', '1433'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
+            'charset'  => 'utf8',
+            'prefix'   => '',
         ],
 
     ],
@@ -106,12 +106,20 @@ return [
 
     'redis' => [
 
-        'cluster' => false,
+        'client' => 'predis',
 
         'default' => [
-            'host' => '127.0.0.1',
-            'port' => 6379,
-            'database' => 0,
+            'host'     => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port'     => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DB', 0),
+        ],
+
+        'cache' => [
+            'host'     => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port'     => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_CACHE_DB', 1),
         ],
 
     ],
