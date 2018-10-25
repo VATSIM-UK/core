432036303de93bf2ae0740d18f647b7c47592c78
Callum Axon <callumaxon@gmail.com>
2018-10-25 18:21:28 +0100

    CORE-207 - Existing Slack Registration Error Message (#1289)

---
+++ tests/Feature/Mship/SlackFeatureTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature\Mship;
+
+use App\Models\Mship\Account;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SlackFeatureTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $account;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->account = factory(Account::class)->create(['slack_id' => '123456789']);
+    }
+
+    /** @test **/
+    public function testRedirectIfRegistrationAlreadyExists()
+    {
+        $this->actingAs($this->account)->get(route('slack.new'))->assertRedirect(route('mship.manage.dashboard'))
+            ->assertSessionHas('error',
+                'You already have a Slack registration with this account. Please contact the Web Services Department if you believe this to be an error.');
+    }
+}
