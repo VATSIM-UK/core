bdab585d0732ab2cee99e19d515fff2dbdf040ba
Calum Towers <me@calumtowers.com>
2018-11-02 21:18:15 +0000

    Update tests/Feature/Mship/ResetPasswordTest.php

--- tests/Feature/Mship/ResetPasswordTest.php
+++ tests/Feature/Mship/ResetPasswordTest.php
@@ -13,8 +13,6 @@ class ResetPasswordTest extends TestCase
     /** @test **/
     public function a_user_can_reset_their_password()
     {
-        $this->withoutExceptionHandling();
-
         $user = factory(Account::class)->create();
 
         $this->actingAs($user, 'vatsim-sso')
