a0c589b2716cd8916792284b0e89742dfe588487
Calum Towers <me@calumtowers.com>
2018-11-02 21:35:40 +0000

    Restore test in full

--- tests/Feature/Mship/ResetPasswordTest.php
+++ tests/Feature/Mship/ResetPasswordTest.php
@@ -13,13 +13,14 @@ class ResetPasswordTest extends TestCase
     /** @test **/
     public function a_user_can_reset_their_password()
     {
-        $this->withoutExceptionHandling();
-
         $user = factory(Account::class)->create();
 
         $this->actingAs($user, 'vatsim-sso')
+            ->get(route('auth-secondary'));
+
+        $this->actingAs($user, 'vatsim-sso')
             ->post(route('password.email'))
-            ->assertRedirect()
+            ->assertRedirect(route('auth-secondary'))
             ->assertSessionHas('status');
     }
 }
