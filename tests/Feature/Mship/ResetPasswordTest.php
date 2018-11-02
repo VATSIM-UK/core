a983d6ee69ec3dec2f7ed09e91a4e4cc66fc7e40
Calum Towers <me@calumtowers.com>
2018-11-02 20:05:24 +0000

    Apply fixes from StyleCI

--- tests/Feature/Mship/ResetPasswordTest.php
+++ tests/Feature/Mship/ResetPasswordTest.php
@@ -4,16 +4,16 @@ namespace Tests\Feature\Mship;
 
 use App\Models\Mship\Account;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Tests\TestCase;
 use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
 
 class ResetPasswordTest extends TestCase
 {
-
     use DatabaseTransactions;
 
     /** @test **/
-    public function a_user_can_reset_their_password() {
+    public function a_user_can_reset_their_password()
+    {
         $user = factory(Account::class)->create();
 
         $this->actingAs($user, 'vatsim-sso')
