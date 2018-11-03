75b9f8c92dc9967c0d7a704aa551e2ecce0727b7
Calum Towers <me@calumtowers.com>
2018-11-03 18:39:53 +0000

    Remove old permission system

---
+++ database/migrations/2018_11_03_172113_delete_old_permissions_system.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class DeleteOldPermissionsSystem extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Schema::dropIfExists('mship_account_role');
+        Schema::dropIfExists('mship_permission_role');
+        Schema::dropIfExists('mship_role');
+        Schema::dropIfExists('mship_permission');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // TODO: Implement down method.
+    }
+}
