9c2bc2fbc38cdc313cd2e54caba4bb76b02bc290
Calum Towers <me@calumtowers.com>
2018-11-05 21:11:38 +0000

    Refactor to remove creation of privacc

--- tests/Feature/VisitorStatsTest.php
+++ tests/Feature/VisitorStatsTest.php
@@ -2,33 +2,18 @@
 
 namespace Tests\Feature;
 
-use App\Models\Mship\Account;
-use App\Models\Mship\State;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Spatie\Permission\Models\Role;
 use Tests\TestCase;
 
 class VisitorStatsTest extends TestCase
 {
     use DatabaseTransactions;
 
-    private $account;
-
-    public function setUp()
-    {
-        parent::setUp();
-
-        $this->account = factory(Account::class)->create();
-
-        $this->account->assignRole(Role::findByName('privacc'));
-
-        $this->account->addState(State::findByCode('DIVISION'));
-    }
-
     /** @test * */
     public function testNoHoursArePulledOnGet()
     {
-        $this->actingAs($this->account)->get(route('adm.visiting.hours.create'))
+        $this->actingAs($this->privacc)
+            ->get(route('adm.visiting.hours.create'))
             ->assertDontSee('accounts');
     }
 
@@ -36,8 +21,10 @@ class VisitorStatsTest extends TestCase
     public function testOnlyVisitingControllersAreSelected()
     {
         $this->withoutMiddleware('auth_full_group')
-            ->actingAs($this->account->fresh())
+            ->actingAs($this->privacc)
             ->get(route('adm.visiting.hours.search'))
-            ->assertRedirect();
+            ->assertSee('accounts')
+            ->assertSee('startDate')
+            ->assertSee('endDate');
     }
 }
