c248c3646096030f95614ddd20121356794ce042
Calum Towers <me@calumtowers.com>
2018-11-10 13:44:54 +0000

    Update tests/Feature/AdminMiddlewareTest.php

--- tests/Feature/AdminMiddlewareTest.php
+++ tests/Feature/AdminMiddlewareTest.php
@@ -64,7 +64,6 @@ class AdminMiddlewareTest extends TestCase
 
         $this->actingAs($staff->fresh())
             ->get(route('adm.dashboard'))
-            ->assertSuccessful()
             ->assertSee('Administration Control Panel');
 
         $this->actingAs($staff->fresh())
