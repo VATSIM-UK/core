6a71032451fab20a2f25117d4716f1c5f5becacc
Calum Towers <me@calumtowers.com>
2018-11-10 17:56:18 +0000

    Implement down method to old permissions system removal

--- database/migrations/2018_11_03_172113_delete_old_permissions_system.php
+++ database/migrations/2018_11_03_172113_delete_old_permissions_system.php
@@ -28,6 +28,45 @@ class DeleteOldPermissionsSystem extends Migration
      */
     public function down()
     {
-        // TODO: Implement down method.
+        Schema::create('mship_role', function ($table) {
+            $table->increments('id')->unsigned();
+            $table->string('name', 75);
+            $table->boolean('default')->default(0);
+            $table->smallInteger('session_timeout')->unsigned()->nullable();
+            $table->boolean('password_mandatory')->default(0);
+            $table->integer('password_lifetime')->default(0);
+            $table->timestamps();
+        });
+
+        Schema::create('mship_permission', function ($table) {
+            $table->increments('id')->unsigned();
+            $table->string('name');
+            $table->string('display_name', 100);
+            $table->timestamps();
+        });
+
+        Schema::create('mship_account_role', function ($table) {
+            $table->increments('id')->unsigned();
+            $table->integer('account_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::create('mship_permission_role', function ($table) {
+            $table->increments('id')->unsigned();
+            $table->integer('permission_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('mship_permission_role', function ($table) {
+            $table->foreign('permission_id')->references('id')->on('mship_permission')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('mship_role')->onDelete('cascade');
+        });
+
+        Schema::table('mship_account_role', function ($table) {
+            $table->foreign('account_id')->references('id')->on('mship_account');
+            $table->foreign('role_id')->references('id')->on('mship_role')->onDelete('cascade');
+        });
     }
 }
