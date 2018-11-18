b764afd4b5a761493f108050d73f477d07acea20
Callum Axon <callumaxon@gmail.com>
2018-11-18 09:50:22 +0000

    Add ability to add student via Nova actioN

--- app/Nova/Actions/Training/AddStudentToWaitingList.php
+++ app/Nova/Actions/Training/AddStudentToWaitingList.php
@@ -2,16 +2,23 @@
 
 namespace App\Nova\Actions\Training;
 
+use App\Events\Training\AccountAddedToWaitingList;
+use App\Models\Mship\Account;
 use Illuminate\Bus\Queueable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Collection;
 use Laravel\Nova\Actions\Action;
 use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Fields\Text;
 
 class AddStudentToWaitingList extends Action
 {
     use InteractsWithQueue, Queueable, SerializesModels;
+
+
+    public $name = 'Add Student';
 
     /**
      * Perform the action on the given models.
@@ -22,7 +29,22 @@ class AddStudentToWaitingList extends Action
      */
     public function handle(ActionFields $fields, Collection $models)
     {
-        //
+        /** @var \App\Models\Training\WaitingList $waitingList */
+        $waitingList = $models->first();
+
+        $cid = $fields->cid;
+
+        try {
+            $account = Account::findOrFail($cid);
+        } catch (ModelNotFoundException $e) {
+            return Action::danger('The specified CID was not found.');
+        }
+
+        $waitingList->addToWaitingList($cid, auth()->user());
+
+        event(new AccountAddedToWaitingList($account, $waitingList, auth()->user()));
+
+        return Action::message('Student added to Waiting List.');
     }
 
     /**
@@ -33,7 +55,7 @@ class AddStudentToWaitingList extends Action
     public function fields()
     {
         return [
-            //
+            Text::make('CID')->rules('required'),
         ];
     }
 }
