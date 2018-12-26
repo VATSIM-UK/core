63c2d631f07436fdc73f55298d0b3a0417952379
Alex Toff <me@alextoff.uk>
2018-12-26 20:27:47 +0000

    Address Comments

--- database/factories/NotificationFactory.php
+++ database/factories/NotificationFactory.php
@@ -1,14 +1,14 @@
 <?php
 
-    use Faker\Generator as Faker;
+use Faker\Generator as Faker;
 
-    $factory->define(\App\Models\Sys\Notification::class, function (Faker $faker) {
-        return [
-            'title' => $faker->sentence,
-            'content' => $faker->text,
-            'status' => \App\Models\Sys\Notification::STATUS_GENERAL,
-            'created_at' => \Carbon\Carbon::now()->subMinute(),
-            'updated_at' => \Carbon\Carbon::now()->subMinute(),
-            'effective_at' => \Carbon\Carbon::now()->subMinute(),
-        ];
-    });
+$factory->define(\App\Models\Sys\Notification::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'content' => $faker->text,
+        'status' => \App\Models\Sys\Notification::STATUS_GENERAL,
+        'created_at' => \Carbon\Carbon::now()->subMinute(),
+        'updated_at' => \Carbon\Carbon::now()->subMinute(),
+        'effective_at' => \Carbon\Carbon::now()->subMinute(),
+    ];
+});
