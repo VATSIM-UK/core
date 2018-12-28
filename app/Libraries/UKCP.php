051769ece779f3aa4f4bbee2aa4bfac29a6c2f5d
Calum Towers <me@calumtowers.com>
2018-12-28 23:38:32 +0000

    View tokens on dashboard
    Will break due to route for delete not being defined

--- app/Libraries/UKCP.php
+++ app/Libraries/UKCP.php
@@ -2,6 +2,10 @@
 
 namespace App\Libraries;
 
+use App\Models\Mship\Account;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+
 class UKCP
 {
 
@@ -14,5 +18,28 @@ class UKCP
     public function __construct()
     {
         $this->apiKey = env('UKCP_KEY');
+    }
+
+    /**
+     * @param Account $account
+     * @return array|\Illuminate\Support\Collection|mixed|\Psr\Http\Message\ResponseInterface
+     */
+    public function getValidTokens(Account $account)
+    {
+        try {
+            $client = new Client;
+            $result = $client->get(env('UKCP_BASE_URL') . '/user/' . $account->id, ['headers' => [
+                'Authorization' => 'Bearer ' . $this->apiKey
+            ]]);
+        } catch (ClientException $e) {
+            return collect();
+        }
+
+        $result = json_decode($result->getBody()->getContents());
+        $result = collect($result->tokens)->filter(function ($item) {
+            return $item->revoked === false;
+        })->all();
+
+        return $result;
     }
 }
