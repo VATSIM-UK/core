a8d248096eda4903c8a045dafc6a6a1579c3a928
Calum Towers <me@calumtowers.com>
2018-12-31 12:39:37 +0000

    Correct typo

--- database/migrations/2018_12_31_122052_add_network_data_indices.php
+++ database/migrations/2018_12_31_122052_add_network_data_indices.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddNetworkDataIndicies extends Migration
+class AddNetworkDataIndices extends Migration
 {
     /**
      * Run the migrations.
