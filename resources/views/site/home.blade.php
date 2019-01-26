4d3b6c7f3f3425e1eae191d5f6926c1389391a1b
Calum Towers <me@calumtowers.com>
2019-01-26 21:39:40 +0000

    feat: Add Tawk.To To Homepage (#1415)

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -360,6 +360,26 @@
     </div>
 </section>
 
+@if(App::environment('production'))
+<script type="text/javascript">
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/57bb3bfca767d83b45e79605/1aqq3gev7';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+
+    @if(Auth::check())
+        Tawk_API.visitor = {
+        name: "{{ Auth::user()->name }} ({{ Auth::user()->id }})",
+        email: "{{ Auth::user()->email }}"
+    };
+    @endif
+</script>
+@endif
 
 <!-- Scripts -->
 
