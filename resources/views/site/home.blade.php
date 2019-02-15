19f4a4eff3dba5d66b07caccaea107878605f8ea
Calum Towers <me@calumtowers.com>
2019-02-15 18:19:49 +0000

    Remove about us
    Not yet implemented

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -110,9 +110,6 @@
                     <a class="nav-link active" href="#">Home <span class="arrow"></span></a>
                     <ul class="nav">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">About VATSIM UK</a>
-                        </li>
-                        <li class="nav-item">
                             <a class="nav-link" href="{{ route('site.staff') }}">Staff</a>
                         </li>
                         <li class="nav-item">
