8e51bf42f6423ebdada54e51097b802aa51ab568
Alex Toff <me@alextoff.uk>
2019-04-03 19:11:19 +0100

    Make travis generate CTS events table

--- app/Models/Cts/MockCtsDatabase.php
+++ app/Models/Cts/MockCtsDatabase.php
@@ -76,6 +76,26 @@ class MockCtsDatabase
               KEY `date` (`date`)
             ) ENGINE=InnoDB AUTO_INCREMENT=96258 DEFAULT CHARSET=utf8mb4;"
         );
+
+        DB::connection('cts')->statement(
+            "create table events
+            (
+                id smallint(5) unsigned auto_increment
+                    primary key,
+                event varchar(100) default '' not null,
+                date date default '0000-00-00' not null,
+                `from` time default '00:00:00' not null,
+                `to` time default '00:00:00' not null,
+                image enum('0', 'jpeg', 'jpg', 'gif', 'png') default '0' null,
+                text longtext not null,
+                tagline varchar(100) not null,
+                thread varchar(150) null,
+                add_by int(7) unsigned default 0 not null,
+                add_date datetime default '0000-00-00 00:00:00' not null,
+                gone int(1) default 0 not null,
+                priority int(1) default 1 not null
+            ) charset=utf8mb4;"
+        );
     }
 
     public static function destroy()
