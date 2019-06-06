99ddd6fc7d817ae15ea21056e255465347fb616d
Alex Toff <atoff@users.noreply.github.com>
2019-06-06 19:18:15 +0100

    feat: Admin Member Sync (#1457)

--- tests/Feature/Adm/AccountTest.php
+++ tests/Feature/Adm/AccountTest.php
@@ -2,8 +2,14 @@
 
 namespace Tests\Feature\Adm;
 
+use App\Jobs\Mship\SyncToCTS;
+use App\Jobs\Mship\SyncToHelpdesk;
+use App\Jobs\Mship\SyncToMoodle;
 use App\Models\Mship\Account;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
 
 class AccountTest extends TestCase
@@ -11,12 +17,14 @@ class AccountTest extends TestCase
     use DatabaseTransactions;
 
     protected $admin;
+    protected $user;
 
     public function setUp()
     {
         parent::setUp();
 
         $admin = factory(Account::class)->create();
+        $this->user = factory(Account::class)->create();
         $admin->assignRole('privacc');
         $this->admin = $admin->fresh();
     }
@@ -27,5 +35,22 @@ class AccountTest extends TestCase
         $this->actingAs($this->admin)
                 ->get(route('adm.mship.account.details', '12345'))
                 ->assertNotFound();
+    }
+
+    /** @test * */
+    public function testCanQueueUserToSync()
+    {
+        Cache::flush(); // Remove time lockout cache
+        Queue::fake();
+
+        $this->actingAs($this->admin)
+            ->get(route('adm.mship.account.sync', $this->user->id))
+            ->assertRedirect()
+            ->assertSessionHas('success', 'User queued to sync to external services!');
+
+        Queue::assertPushed(SyncToCTS::class);
+        Queue::assertPushed(SyncToMoodle::class);
+        Queue::assertPushed(SyncToHelpdesk::class);
+        //Queue::assertPushed(SyncToForums::class);
     }
 }
