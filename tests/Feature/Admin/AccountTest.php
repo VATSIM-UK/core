c6f323c78ef678178c7d1edd51ccdd71088f28a1
Alex Toff <me@alextoff.uk>
2019-07-21 21:42:05 +0100

    Test fixes

--- tests/Feature/Admin/AccountTest.php
+++ tests/Feature/Admin/AccountTest.php
@@ -5,7 +5,9 @@ namespace Tests\Feature\Admin;
 use App\Jobs\Mship\SyncToCTS;
 use App\Jobs\Mship\SyncToHelpdesk;
 use App\Jobs\Mship\SyncToMoodle;
+use App\Models\Mship\Account;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
@@ -38,5 +40,18 @@ class AccountTest extends TestCase
         Queue::assertPushed(SyncToMoodle::class);
         Queue::assertPushed(SyncToHelpdesk::class);
         //Queue::assertPushed(SyncToForums::class);
+    }
+
+    /** @test */
+    public function testCanImpersonateUser()
+    {
+        $this->assertNull(Auth::user());
+
+        $this->actingAs($this->privacc)
+            ->post(route('adm.mship.account.impersonate', $this->user->id), ['reason' => 'Lorem Ipsum Dorum'])
+            ->assertRedirect()
+            ->assertSessionHas('success', 'You are now impersonating this user - your reason has been logged. Be good!');
+
+        $this->assertEquals($this->user->id, Auth::user()->id);
     }
 }
