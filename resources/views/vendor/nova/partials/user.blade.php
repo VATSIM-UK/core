b0888dfb3f2f6d515e13641a13265746a05b8069
Callum Axon <callumaxon@gmail.com>
2020-01-04 20:28:35 +0000

    remove dropdown

--- resources/views/vendor/nova/partials/user.blade.php
+++ resources/views/vendor/nova/partials/user.blade.php
@@ -10,13 +10,3 @@
         {{ $user->name ?? $user->email ?? __('Nova User') }}
     </span>
 </dropdown-trigger>
-
-<dropdown-menu slot="menu" width="200" direction="rtl">
-    <ul class="list-reset">
-        <li>
-            <a href="{{ route('nova.logout') }}" class="block no-underline text-90 hover:bg-30 p-3">
-                {{ __('Logout') }}
-            </a>
-        </li>
-    </ul>
-</dropdown-menu>
