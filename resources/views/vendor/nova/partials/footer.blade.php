610cf013b77e3985efb58c5277aca2906367265f
Callum Axon <callumaxon@gmail.com>
2020-01-04 20:15:17 +0000

    remove nova link from footer

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="https://nova.laravel.com" class="text-primary dim no-underline">Administration Panel</a>
+    <span>Administration Panel</span>
     <span class="px-1">&middot;</span>
     &copy; {{ date('Y') }} VATSIM United Kingdom Division
     <span class="px-1">&middot;</span>
