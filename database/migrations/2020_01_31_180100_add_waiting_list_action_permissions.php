418e039f55d9eba2a4fc3a4ffe7db528574054b8
Calum Towers <me@calumtowers.com>
2020-01-31 18:03:59 +0000

    Add new permissions

---
+++ database/migrations/2020_01_31_180100_add_waiting_list_action_permissions.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddWaitingListActionPermissions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->createPermission('waitingLists/addAccounts');
+        $this->createPermission('waitingLists/addFlags');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+
+    private function createPermission(string $name, $guard = 'web')
+    {
+        return \Spatie\Permission\Models\Permission::create([
+            'name' => $name,
+            'guard_name' => $guard
+        ]);
+    }
+}
