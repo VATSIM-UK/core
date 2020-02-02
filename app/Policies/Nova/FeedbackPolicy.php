78cacc0396b34cf605197c0925a6759a20652585
Calum Towers <me@calumtowers.com>
2020-02-02 19:28:00 +0000

    Account for result returning array in conditional

--- app/Policies/Nova/FeedbackPolicy.php
+++ app/Policies/Nova/FeedbackPolicy.php
@@ -5,6 +5,7 @@ namespace App\Policies\Nova;
 use App\Models\Mship\Account;
 use App\Models\Mship\Feedback\Feedback;
 use App\Policies\BasePolicy;
+use Illuminate\Support\Facades\Log;
 
 class FeedbackPolicy extends BasePolicy
 {
@@ -20,7 +21,7 @@ class FeedbackPolicy extends BasePolicy
     public function view(Account $account, Feedback $feedback)
     {
         return $account->checkPermissionTo("feedback/view/{$feedback->form->slug}", self::GUARD)
-            && $feedback->account_id != $account->hiddenFeedbackUsers();
+            && !in_array($feedback->account_id, $account->hiddenFeedbackUsers());
     }
 
     public function actionFeedback(Account $account)
