92818fe0020275f2b9deb80368e00e9a05642e68
Calum Towers <me@calumtowers.com>
2020-03-20 12:45:37 +0000

    Real fix

--- app/Http/Controllers/UKCP/Token.php
+++ app/Http/Controllers/UKCP/Token.php
@@ -42,10 +42,10 @@ class Token extends BaseController
         $latestId = $this->ukcp->getValidTokensFor(auth()->user());
 
         if ($latestId->isEmpty()) {
-            return Redirect::route('ukcp.guide');
+            return Redirect::route('ukcp.token.refresh');
         }
 
-        return $this->viewMake('ukcp.guide')->with('newToken', $latestId->first()->id);
+        return $this->viewMake('ukcp.token.guide')->with('newToken', $latestId->first()->id);
     }
 
     public function download($tokenId)
