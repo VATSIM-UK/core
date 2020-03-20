6e0dd6c9fc90e7c2cc9d9fd2535dd3cf6ae524ab
Calum Towers <me@calumtowers.com>
2020-03-20 12:28:49 +0000

    ukcp.guide is correct

--- app/Http/Controllers/UKCP/Token.php
+++ app/Http/Controllers/UKCP/Token.php
@@ -34,7 +34,7 @@ class Token extends BaseController
             return Redirect::route('mship.manage.dashboard')
                 ->withError('An unknown error occured, please contact Web Services.');
         }
-        return redirect()->route('ukcp.token.guide')->withSuccess('Tokens Updated!');
+        return redirect()->route('ukcp.guide')->withSuccess('Tokens Updated!');
     }
 
     public function show()
@@ -42,10 +42,10 @@ class Token extends BaseController
         $latestId = $this->ukcp->getValidTokensFor(auth()->user());
 
         if ($latestId->isEmpty()) {
-            return Redirect::route('ukcp.token.guide');
+            return Redirect::route('ukcp.guide');
         }
 
-        return $this->viewMake('ukcp.token.guide')->with('newToken', $latestId->first()->id);
+        return $this->viewMake('ukcp.guide')->with('newToken', $latestId->first()->id);
     }
 
     public function download($tokenId)
