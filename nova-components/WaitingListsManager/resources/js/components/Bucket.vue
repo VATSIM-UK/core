a5fb6c74833e9b71c4795cba6f4e060a9f30858f
Callum Axon <callumaxon@gmail.com>
2020-03-29 16:57:24 +0100

    fix moment issue

--- nova-components/WaitingListsManager/resources/js/components/Bucket.vue
+++ nova-components/WaitingListsManager/resources/js/components/Bucket.vue
@@ -29,7 +29,7 @@
                         </div>
                     </td>
                     <td>{{ account.id }}</td>
-                    <td>{{ account.created_at }}</td>
+                    <td>{{ createdFormatted(account.created_at) }}</td>
                     <td>
                         <note-indicator
                             :account="account"
@@ -111,6 +111,10 @@
                 if (!flag.endorsement_id) {
                     this.$emit('changeFlag', flag)
                 }
+            },
+
+            createdFormatted (created_at) {
+                return moment(created_at).format("MMMM Do YYYY")
             }
         },
 
@@ -121,7 +125,9 @@
 
             showHourChecker () {
                 return this.type === 'atc'
-            }
+            },
+
+
         },
     }
 </script>
