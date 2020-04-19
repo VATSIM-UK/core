4f0ee5d682bed92c7379c4ed3a8e650cbeac7ec7
Callum Axon <callumaxon@gmail.com>
2020-04-19 12:01:06 +0100

    fix: Correct Wrap Feedback Responses (#1744)

---
+++ resources/views/vendor/nova/partials/meta.blade.php
@@ -0,0 +1,7 @@
+{{-- <link rel="icon" type="image/png" href="{{ asset('/img/favicon.png') }} "> --}}
+<style>
+    .flex-no-shrink {
+        flex-shrink: 1;
+        white-space: pre-line;
+    }
+</style>
