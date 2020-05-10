fe948f94466fb2eeccd39b28d25e956cc5e483ab
Calum Towers <me@calumtowers.com>
2020-05-10 17:18:45 +0100

    ci: Migrate PR Testing From Travis To GitHub Actions (#1767)

--- config/services.php
+++ config/services.php
@@ -36,8 +36,8 @@ return [
     ],
 
     'slack' => [
-        'token' => env('SLACK_SECRET'),
-        'token_register' => env('SLACK_TOKEN_REGISTER')
+        'token' => env('SLACK_SECRET', 'secret'),
+        'token_register' => env('SLACK_TOKEN_REGISTER', 'token')
     ],
 
     'teamspeak' => [
@@ -55,7 +55,7 @@ return [
     ],
 
     'ukcp' => [
-        'url' => env('UKCP_URL'),
+        'url' => env('UKCP_URL', 'https://ukcp.vatsim.uk'),
         'key' => env('UKCP_KEY'),
     ],
 
