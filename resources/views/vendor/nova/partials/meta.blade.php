4d3c5655164ee7a30ea9e2229ac5a37cb0e7f88d
Calum Towers <me@calumtowers.com>
2020-05-11 19:42:59 +0100

    Remove custom CSS causing button formatting issues

--- resources/views/vendor/nova/partials/meta.blade.php
+++ resources/views/vendor/nova/partials/meta.blade.php
@@ -1,7 +1,1 @@
 {{-- <link rel="icon" type="image/png" href="{{ asset('/img/favicon.png') }} "> --}}
-<style>
-    .flex-no-shrink {
-        flex-shrink: 1;
-        white-space: pre-line;
-    }
-</style>
