58c23bb37373c62864d749867db36571a5ef5659
Calum Towers <me@calumtowers.com>
2020-05-14 18:07:18 +0100

    Revert "Test coverage with random change to Waiting Lists Manager"
    This reverts commit 6da5d3863c5ab0b45632c0400ad21ddbe8f4d9b3.

--- nova-components/WaitingListsManager/src/Http/WaitingListAccountResource.php
+++ nova-components/WaitingListsManager/src/Http/WaitingListAccountResource.php
@@ -12,10 +12,6 @@ class WaitingListAccountResource extends JsonResource
      */
     public function toArray($request)
     {
-        if ($request = 'test') {
-            return [];
-        }
-
         return [
             'id' => $this->id,
             'pivot_id' => $this->pivot->id,
