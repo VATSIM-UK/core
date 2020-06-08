f4a8f3c99bafb2cb7518905febb4b17f9b5b3a5e
Calum Towers <me@calumtowers.com>
2020-06-08 14:23:33 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
-use App\Libraries\VatsimApi;
-use GuzzleHttp\Client;
-use Vatsim\Xml\Facades\XML as VatsimXML;
+// use App\Libraries\VatsimApi;
+// use GuzzleHttp\Client;
+// use Vatsim\Xml\Facades\XML as VatsimXML;
 
-$client = (new Client);
+// $client = (new Client);
 
-$new_r = (new VatsimApi($client))->ratingsFor(1258635);
-$old_r = VatsimXML::getData(1258635, 'idstatusint');
+// $new_r = (new VatsimApi($client))->ratingsFor(1258635);
+// $old_r = VatsimXML::getData(1258635, 'idstatusint');
 
 //$new_r = (new VatsimApi($client))->previousRatingFor(1258635);
 //$old_r = VatsimXML::getData(1258635, 'idstatusprat');
 
-dd($old_r, $new_r);
+// dd($old_r, $new_r);
 
 Route::group(['domain' => config('app.url')], function () {
     require base_path('routes/web-public.php');
