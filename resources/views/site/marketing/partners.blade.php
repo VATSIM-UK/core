1a27ac7d82f89b4f07e241d5124d19e788574fdf
Daniel Plumb <me@danielplumb.co.uk>
2020-07-19 19:31:54 +0100

    Adds vRAF & BAV as partners

--- resources/views/site/marketing/partners.blade.php
+++ resources/views/site/marketing/partners.blade.php
@@ -5,41 +5,10 @@
         <!-- starts corporate partners panel -->
         <div class="col-md-6">
             <div class="panel panel-ukblue">
-                <div class="panel-heading"><i class="glyphicon glyphicon-pencil"></i> &thinsp; Corporate Partners
+                <div class="panel-heading">Corporate Partners
                 </div>
-                <div class="panel-body">
-                    VATSIM UK employs a range of partners and for this section we can write some text. I'm not too sure
-                    what text you would like to be placed here, but I'll leave this in place to pad it out a little bit.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultricies at libero quis dictum.
-                    Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed et
-                    suscipit magna. Cras est enim, porttitor ac neque a, fermentum cursus massa. Etiam quis eros sed
-                    arcu mattis mollis in eu mauris.
-
-                    <div class="row mt-4">
-                        <div class="col-md-6">
-                            <img class="img-responsive center-block" src="/images/branding/different_colour.png">
-                            <p class="text-center">BA Virtual</p>
-                            <p class="text-center"><small>Duis ac faucibus erat. Pellentesque rhoncus sollicitudin efficitur. Sed semper varius facilisis.</small></p>
-                        </div>
-                        <div class="col-md-6">
-                            <img class="img-responsive center-block" src="/images/branding/different_colour.png">
-                            <p class="text-center">Do not rotate or flip our logo</p>
-                            <p class="text-center"><small>Duis ac faucibus erat. Pellentesque rhoncus sollicitudin efficitur. Sed semper varius facilisis.</small></p>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-6">
-                            <img class="img-responsive center-block" src="/images/branding/different_colour.png">
-                            <p class="text-center">Do not change the colours of our logo</p>
-                            <p class="text-center"><small>Duis ac faucibus erat. Pellentesque rhoncus sollicitudin efficitur. Sed semper varius facilisis.</small></p>
-                        </div>
-                        <div class="col-md-6">
-                            <img class="img-responsive center-block" src="/images/branding/different_colour.png">
-                            <p class="text-center">Do not rotate or flip our logo</p>
-                            <p class="text-center"><small>Duis ac faucibus erat. Pellentesque rhoncus sollicitudin efficitur. Sed semper varius facilisis.</small></p>
-                        </div>
-                    </div>
+                <div class="panel-body text-center font-italic">
+                    There are currently no corporate partners to display.
                 </div>
             </div>
         </div>
@@ -48,46 +17,27 @@
         <!-- starts community partners panel -->
         <div class="col-md-6">
             <div class="panel panel-ukblue">
-                <div class="panel-heading"><i class="glyphicon glyphicon-pencil"></i> &thinsp; Community Partners
+                <div class="panel-heading">Community Partners
                 </div>
                 <div class="panel-body">
-                    VATSIM UK employs a range of partners and for this section we can write some text. I'm not too sure
-                    what text you would like to be placed here, but I'll leave this in place to pad it out a little bit.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultricies at libero quis dictum.
-                    Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed et
-                    suscipit magna. Cras est enim, porttitor ac neque a, fermentum cursus massa. Etiam quis eros sed
-                    arcu mattis mollis in eu mauris.
-
                     <div class="row mt-4">
                         <div class="col-md-4">
-                            <img class="img-responsive align-middle" src="/images/branding/different_colour.png">
+                            <img class="img-responsive align-middle" src="/images/partners/vraf.png">
                         </div>
                         <div class="col-md-8">
-                            <p>VATSIM UK</p>
-                            <p><small>Duis ac faucibus erat. Pellentesque rhoncus sollicitudin efficitur. Sed semper varius facilisis. Morbi at massa ac quam aliquam pulvinar. Vivamus posuere congue auctor. Sed quis dictum tortor, ac accumsan enim.</small></p>
+                            <p>Virtual Royal Air Force (vRAF)</p>
+                            <p><small>vRAF simulates the Royal Air Force's daily activities taking part in a variety of training and operational exercises including; air-to-air refueling, carrier operations, low-level flying and more.</small></p>
                         </div>
                     </div>
-
                     <div class="row mt-4">
                         <div class="col-md-4">
-                            <img class="img-responsive align-middle" src="/images/branding/different_colour.png">
+                            <img class="img-responsive align-middle" src="/images/partners/bavirtual.png">
                         </div>
                         <div class="col-md-8">
-                            <p>VATSIM UK</p>
-                            <p><small>Duis ac faucibus erat. Pellentesque rhoncus sollicitudin efficitur. Sed semper varius facilisis. Morbi at massa ac quam aliquam pulvinar. Vivamus posuere congue auctor. Sed quis dictum tortor, ac accumsan enim.</small></p>
+                            <p>BAVirtual</p>
+                            <p><small>Established in 2000, BAVirtual is Virtual Airline that fulfills a role to provide a structured, hands-on educational environment for aspiring pilots that accurately mimic the operations of British Airways.</small></p>
                         </div>
                     </div>
-
-                    <div class="row mt-4">
-                        <div class="col-md-4">
-                            <img class="img-responsive align-middle" src="/images/branding/different_colour.png">
-                        </div>
-                        <div class="col-md-8">
-                            <p>VATSIM UK</p>
-                            <p><small>Duis ac faucibus erat. Pellentesque rhoncus sollicitudin efficitur. Sed semper varius facilisis. Morbi at massa ac quam aliquam pulvinar. Vivamus posuere congue auctor. Sed quis dictum tortor, ac accumsan enim.</small></p>
-                        </div>
-                    </div>
-
                 </div>
             </div>
         </div>
