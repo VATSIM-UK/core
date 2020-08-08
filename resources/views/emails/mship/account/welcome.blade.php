637d4d993be3280e96e88014d2ef72f0350d6fb9
Alex Toff <me@alextoff.uk>
2020-08-08 19:36:47 +0100

    Update email content

--- resources/views/emails/mship/account/welcome.blade.php
+++ resources/views/emails/mship/account/welcome.blade.php
@@ -2,69 +2,15 @@
 
 @section('body')
 
+<h2>Welcome to VATSIM United Kingdom!</h2>
 <p>
-    This email serves as confirmation that your central account with VATSIM UK has been created. We have received the
-    following details about you:
+    We are glad to have you on board, and by picking VATSIM UK as your home division,
+    you are now part of one of busiest divisions on the network. Whether you are here to learn about Air Traffic Control,
+    to improve your pilot skills or just to meet new people, you are sure to find something new.
 </p>
-<hr>
-<table>
-    <caption style="display: none">Table showing member's details</caption>
-    <tr>
-        <th scope="row">CID</th>
-        <td>{!! $account->id !!}</td>
-    </tr>
-    <tr>
-        <th scope="row">Full Name</th>
-        <td>{!! $account->name !!}</td>
-    </tr>
-    <tr>
-        <th scope="row">Primary Email</th>
-        <td>{!! $account->email !!}</td>
-    </tr>
-    <tr>
-        <th scope="row">Secondary Emails</th>
-        <td>
-            @forelse($account->secondaryEmails as $e)
-                {!! $e->email !!}<br/>
-            @empty
-
-                No secondary emails registered.
-            @endforelse
-        </td>
-    </tr>
-    <tr>
-        <th scope="row">Status</th>
-        <td>{!! $account->status_string !!}</td>
-    </tr>
-    <tr>
-        <th scope="row">State</th>
-        <td>{!! $account->primary_state !!}</td>
-    </tr>
-    <tr>
-        <th scope="row">ATC Qualification</th>
-        <td>{!! $account->qualification_atc !!}</td>
-    </tr>
-    <tr>
-        <th scope="row">Pilot Qualification(s)</th>
-        <td>{!! $account->qualifications_pilot_string !!}</td>
-    </tr>
-</table>
-
-<hr>
 <p>
-    Now that your account has been created, you can login to any of our web services and these details will be
-    transferred automatically.
-</p>
-
-<p>
-    Furthermore, any updates you make to your details via the vatsim.net membership portal will be synchronised with our
-    database in a timely manner, to ensure all our systems remain up to date.
-</p>
-
-<p>
-    If any details are incorrect, or you have any concerns, please
-    contact {!! link_to('mailto:member-services@vatsim.uk', 'our Member Services team') !!} who will be able to help you
-    further.
+    Now that your account has been created and we have received your details, you have access to a whole range of optional
+    training courses, events and activities to make the most of your time on the network.
 </p>
 
 
@@ -80,24 +26,30 @@ VATSIM UK provides many different ways to get to know and keep in touch with mem
 <p>Discord - Our Discord server provides an opportunity for more instant communication with members. Come here to introduce yourself, find someone to help you with a software problem, and get the latest news</p>
 <div style="margin-left:2em; margin-bottom: 2em;"><a href="{{route('discord.show')}}" class="btn btn-primary">Register for Discord</a></div>
 
+<p>Division Website - Our division website hosts all the information you need, including our policies, staff team and information on how to enrol in our training courses. It is also where you should go to configure settings such as secondary emails and passwords for your VATSIM UK account</p>
+<div style="margin-left:2em; margin-bottom: 0.5em;"><a href="{{route('discord.show')}}" class="btn btn-primary">Visit our Site</a></div>
+<div style="margin-left:2em; margin-bottom: 2em;"><a href="{{route('dashboard')}}" class="btn btn-primary">Edit your details</a></div>
 
-<h2>What next?</h2>
 
+<h2>Training</h2>
 <p>
-    If you're interested in flying, VATSIM UK offers multiple pilot training courses:
+    If you're interested in flying, VATSIM UK offers multiple pilot training courses. Browse our current offering at
     {!! link_to_route('site.pilots.landing') !!}
 </p>
 
 <p>
-    If you're interested in providing ATC:
+    If you're interested in learning about Air Traffic Control, with the goal of controlling on the VATSIM network, check out
     {!! link_to_route('site.atc.newController') !!}
 </p>
 
+<h2>Getting help</h2>
 <p>
-    For both Pilot and ATC training our online system will allow you to book mentoring (once you are enrolled as per the above links):
-    {!! link_to("https://cts.vatsim.uk/", "https://cts.vatsim.uk/") !!}
+    We recommend making use of our Discord, community forum, or TeamSpeak to get instant help from members and division staff.
+</p>
+<p>
+    If you have any concerns or difficulties getting setup, {!! link_to('mailto:member-services@vatsim.uk', 'our Member Services team') !!} would be happy to help you.
 </p>
 
 
-On behalf of the entire team, welcome to the United Kingdom.
+On behalf of the entire staff team and our members, welcome to the United Kingdom.
 @stop
