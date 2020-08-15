2240a6bd4132b004a82ee9b4cfe9c340a505095b
Alex Toff <me@alextoff.uk>
2020-08-15 11:41:23 +0100

    Make sync-sub classes extend Job

--- app/Jobs/Mship/SyncToMoodle.php
+++ app/Jobs/Mship/SyncToMoodle.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs\Mship;
 
+use App\Jobs\Job;
 use App\Models\Mship\Account;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -9,7 +10,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class SyncToMoodle implements ShouldQueue
+class SyncToMoodle extends Job implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
