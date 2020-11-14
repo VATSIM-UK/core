f43843a93a762c5a57d83f9186ce4af5303cee2f
Calum Towers <towers@hey.com>
2020-11-14 17:40:59 +0000

    Remove deprecated function
    Method was previously (from framework) just returning the actual key anyway

--- app/Models/Concerns/TracksChanges.php
+++ app/Models/Concerns/TracksChanges.php
@@ -70,7 +70,6 @@ trait TracksChanges
         $attributes = $this->originalTracked;
 
         foreach ($this->trackableFromArray($attributes) as $key => $value) {
-            $key = $this->removeTableFromKey($key);
 
             if ($this->isTrackable($key) && $value != $this->$key) {
                 $dataChange = new \App\Models\Sys\Data\Change();
