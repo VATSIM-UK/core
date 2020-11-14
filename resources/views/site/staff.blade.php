f5b04781fdcea65f8c2ac997bf3496546147647a
Calum Towers <me@calumtowers.com>
2020-11-14 10:21:33 +0000

    style: Update Web Staff

--- resources/views/site/staff.blade.php
+++ resources/views/site/staff.blade.php
@@ -171,10 +171,6 @@
                                 <td>Developer</td>
                                 <td>Andy Ford</td>
                             </tr>
-                            <tr>
-                                <td>Developer</td>
-                                <td>Daniel Plumb</td>
-                            </tr>
                         </table>
                     </div>
                 </div>
