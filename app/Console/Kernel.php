e72ebc05ba6715f6d56d69d7c7d7e7d60ba60b19
Calum Towers <towers@hey.com>
2020-12-07 18:00:05 +0000

    perf: Stop TeamSpeak Runner Running in Background

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,7 +37,6 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('teaman:runner', ['-v'])
             ->everyMinute()
-            ->runInBackground()
             ->withoutOverlapping();
 
         $schedule->command('networkdata:download')
