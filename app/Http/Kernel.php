4f16bf9a7a2439f32fa5bc07fefc3f87dc08b66c
Alex Toff <me@alextoff.uk>
2021-01-09 17:19:52 +0000

    Fix unauthenticated redirect to go to dashboard first

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,7 +68,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         // native
-        'auth'            => \Illuminate\Auth\Middleware\Authenticate::class,
+        'auth'            => \App\Http\Middleware\Authenticate::class,
         'auth.basic'      => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings'        => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can'             => \Illuminate\Auth\Middleware\Authorize::class,
