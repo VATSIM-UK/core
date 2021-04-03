24b2d4abd5f72ca02c8fba0dc65582b20f8701cb
Calum Towers <towers@hey.com>
2021-04-03 17:12:29 +0100

    first pass at de-dupe

--- app/Console/Commands/Members/SyncCtsRoles.php
+++ app/Console/Commands/Members/SyncCtsRoles.php
@@ -2,12 +2,13 @@
 
 namespace App\Console\Commands\Members;
 
-use App\Console\Commands\Command;
 use App\Models\Mship\Account;
-use App\Repositories\Cts\ExaminerRepository;
+use App\Console\Commands\Command;
+use Illuminate\Support\Collection;
+use Spatie\Permission\Models\Role;
 use App\Repositories\Cts\MentorRepository;
 use App\Repositories\Cts\StudentRepository;
-use Spatie\Permission\Models\Role;
+use App\Repositories\Cts\ExaminerRepository;
 
 class SyncCtsRoles extends Command
 {
@@ -41,26 +42,57 @@ class SyncCtsRoles extends Command
         $this->syncMentorsByCallsign('OBS', 32); // OBS Mentors
         $this->syncMentorsByCallsign('EGKK_GND', 53); // Gatwick Mentors
 
-        $this->syncPilotStudents(); // Pilot TG Members ????
+        $this->syncPilotStudents(63); // Pilot Students
 
         $this->syncAtcExaminers(31);
         $this->syncPilotExaminers(40);
     }
 
-    private function syncMentorsByRts($rtsId, $roleId)
+    private function syncMentorsByRts(int $rtsId, int $roleId): void
     {
-        $mentors = (new MentorRepository)->getMentorsWithin($rtsId);
+        $hasRole = $this->getAccountsWithRoleId($roleId);
+        $shouldHaveRole = (new MentorRepository)->getMentorsWithin($rtsId);
+        $this->syncRoles($hasRole, $shouldHaveRole, $roleId);
+    }
 
-        $role = Role::findById($roleId)->users()->pluck('id');
+    private function syncMentorsByCallsign(string $search, int $roleId): void
+    {
+        $hasRole = $this->getAccountsWithRoleId($roleId);
+        $shouldHaveRole = (new MentorRepository)->getMentorsFor($search);
+        $this->syncRoles($hasRole, $shouldHaveRole, $roleId);
+    }
 
+    private function syncAtcExaminers(int $roleId): void
+    {
+        $hasRole = $this->getAccountsWithRoleId($roleId);
+        $shouldHaveRole = (new ExaminerRepository)->getAtcExaminers();
+        $this->syncRoles($hasRole, $shouldHaveRole, $roleId);
+    }
+
+    private function syncPilotExaminers(int $roleId): void
+    {
+        $hasRole = $this->getAccountsWithRoleId($roleId);
+        $shouldHaveRole = (new ExaminerRepository)->getPilotExaminers();
+        $this->syncRoles($hasRole, $shouldHaveRole, $roleId);
+    }
+
+    private function syncPilotStudents(int $roleId): void
+    {
+        $hasRole = $this->getAccountsWithRoleId($roleId);
+        $shouldHaveRole = (new StudentRepository)->getStudentsWithin(13);
+        $this->syncRoles($hasRole, $shouldHaveRole, $roleId);
+    }
+
+    private function syncRoles(Collection $hasRole, Collection $shouldHaveRole, $roleId): void
+    {
         // Users that have the role, but are not mentors
-        $removeRole = $role->filter(function ($value) use ($mentors) {
-            return ! $mentors->contains($value);
+        $removeRole = $hasRole->filter(function ($value) use ($shouldHaveRole) {
+            return ! $shouldHaveRole->contains($value);
         })->all();
 
         // Users that are mentors, but do not have the role
-        $assignRole = $mentors->filter(function ($value) use ($role) {
-            return ! $role->contains($value);
+        $assignRole = $shouldHaveRole->filter(function ($value) use ($hasRole) {
+            return ! $hasRole->contains($value);
         })->all();
 
         foreach ($assignRole as $account) {
@@ -72,103 +104,8 @@ class SyncCtsRoles extends Command
         }
     }
 
-    private function syncMentorsByCallsign($search, $roleId)
+    private function getAccountsWithRoleId(int $roleId): array
     {
-        $mentors = (new MentorRepository)->getMentorsFor($search);
-
-        $role = Role::findById($roleId)->users()->pluck('id');
-
-        // Users that have the role, but are not mentors
-        $removeRole = $role->filter(function ($value) use ($mentors) {
-            return ! $mentors->contains($value);
-        })->all();
-
-        // Users that are mentors, but do not have the role
-        $assignRole = $mentors->filter(function ($value) use ($role) {
-            return ! $role->contains($value);
-        })->all();
-
-        foreach ($assignRole as $account) {
-            Account::find($account)->assignRole($roleId);
-        }
-
-        foreach ($removeRole as $account) {
-            Account::find($account)->removeRole($roleId);
-        }
-    }
-
-    private function syncAtcExaminers($roleId)
-    {
-        $examiners = (new ExaminerRepository)->getAtcExaminers();
-
-        $role = Role::findById($roleId)->users()->pluck('id');
-
-        // Users that have the role, but are not examiners
-        $removeRole = $role->filter(function ($value) use ($examiners) {
-            return ! $examiners->contains($value);
-        })->all();
-
-        // Users that are examiners, but do not have the role
-        $assignRole = $examiners->filter(function ($value) use ($role) {
-            return ! $role->contains($value);
-        })->all();
-
-        foreach ($assignRole as $account) {
-            Account::find($account)->assignRole($roleId);
-        }
-
-        foreach ($removeRole as $account) {
-            Account::find($account)->removeRole($roleId);
-        }
-    }
-
-    private function syncPilotExaminers($roleId)
-    {
-        $examiners = (new ExaminerRepository)->getPilotExaminers();
-
-        $role = Role::findById($roleId)->users()->pluck('id');
-
-        // Users that have the role, but are not examiners
-        $removeRole = $role->filter(function ($value) use ($examiners) {
-            return ! $examiners->contains($value);
-        })->all();
-
-        // Users that are examiners, but do not have the role
-        $assignRole = $examiners->filter(function ($value) use ($role) {
-            return ! $role->contains($value);
-        })->all();
-
-        foreach ($assignRole as $account) {
-            Account::find($account)->assignRole($roleId);
-        }
-
-        foreach ($removeRole as $account) {
-            Account::find($account)->removeRole($roleId);
-        }
-    }
-
-    private function syncPilotStudents()
-    {
-        $students = (new StudentRepository)->getStudentsWithin(13);
-
-        $role = Role::findById(63)->users()->pluck('id');
-
-        // Users that have the role, but are not members
-        $removeRole = $role->filter(function ($value) use ($students) {
-            return ! $students->contains($value);
-        })->all();
-
-        // Users that are members, but do not have the role
-        $assignRole = $students->filter(function ($value) use ($role) {
-            return ! $role->contains($value);
-        })->all();
-
-        foreach ($assignRole as $account) {
-            Account::find($account)->assignRole(63);
-        }
-
-        foreach ($removeRole as $account) {
-            Account::find($account)->removeRole(63);
-        }
+        return Role::findById($roleId)->users()->pluck('id');
     }
 }
