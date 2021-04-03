b939910b3b27258b1dcf7fa484fccf2d1227187b
Calum Towers <towers@hey.com>
2021-04-03 16:59:02 +0100

    do not sync trainee examiners

--- app/Repositories/Cts/ExaminerRepository.php
+++ app/Repositories/Cts/ExaminerRepository.php
@@ -13,10 +13,6 @@ class ExaminerRepository
                                     ->orWhere('S1', '=', 1)
                                     ->orWhere('S2', '=', 1)
                                     ->orWhere('S3', '=', 1)
-                                    ->orWhere('OBStrain', '=', 1)
-                                    ->orWhere('S1train', '=', 1)
-                                    ->orWhere('S2train', '=', 1)
-                                    ->orWhere('S3train', '=', 1)
                                     ->get();
 
         return $examiners->map(function ($examiner) {
@@ -32,11 +28,6 @@ class ExaminerRepository
                                     ->orWhere('P3', '=', 1)
                                     ->orWhere('P4', '=', 1)
                                     ->orWhere('P5', '=', 1)
-                                    ->orWhere('P1train', '=', 1)
-                                    ->orWhere('P2train', '=', 1)
-                                    ->orWhere('P3train', '=', 1)
-                                    ->orWhere('P4train', '=', 1)
-                                    ->orWhere('P5train', '=', 1)
                                     ->get();
 
         return $examiners->map(function ($examiner) {
