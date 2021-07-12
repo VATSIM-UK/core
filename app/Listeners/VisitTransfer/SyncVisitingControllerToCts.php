8032150c295b61c1519363a268eae2459e49fa69
Callum Axon <git@callumaxon.uk>
2021-07-12 20:31:00 +0100

    missing files

---
+++ app/Listeners/VisitTransfer/SyncVisitingControllerToCts.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Listeners\VisitTransfer;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Events\VisitTransfer\ApplicationAccepted;
+
+class SyncVisitingControllerToCts
+{
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(ApplicationAccepted $event)
+    {
+        $event->application->account->syncToCTS();
+    }
+}
