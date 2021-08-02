b7165e2267550369bec1912f4754b61a120f072d
Callum Axon <git@callumaxon.uk>
2021-08-02 20:36:19 +0100

    feat: Add Area hour checker

--- app/Http/Controllers/Atc/EndorsementController.php
+++ app/Http/Controllers/Atc/EndorsementController.php
@@ -26,4 +26,41 @@ class EndorsementController extends BaseController
             ->with('conditions', $endorsement->conditions)
             ->with('hours', $hours->all());
     }
+
+    public function getAreaIndex()
+    {
+        $endorsements = Endorsement::whereIn('name', ['LON_S_CTR', 'LON_C_CTR', 'LON_N_CTR','SCO_CTR'])->get();
+
+        if ($endorsements->count() < 1) {
+            return Redirect::route('mship.manage.dashboard')
+                ->withError('Endorsements improperly configured');
+        }
+
+        if (!$this->account->qualificationAtc->isS3) {
+            return Redirect::route('mship.manage.dashboard')
+                ->withError('Only S3 rated controllers can see their C1 Training Place eligibility.');
+        }
+
+        $endorsements = $endorsements->load('conditions')->map(function ($endorsement) {
+            $conditions = $endorsement->conditions->map(function ($condition) use ($endorsement) {
+                return [
+                    'endorsement_id' => $endorsement->id,
+                    // extract the likely position name from the criterion loaded into the database.
+                    'position' => str_replace('%', '_', $condition->positions[0]),
+                    'required_hours' => $condition->required_hours,
+                    'within_months' => $condition->within_months,
+                    'progress' => round($condition->progressForUser($this->account)->sum(), 1),
+                    'complete' => $condition->isMetForUser($this->account)
+                ];
+            });
+
+            return [
+                'name' => $endorsement->name,
+                'conditions' => $conditions
+            ];
+        });
+
+        return $this->viewMake('controllers.endorsements.area')
+            ->with('endorsements', $endorsements);
+    }
 }
---
+++ tests/Feature/Atc/AreaHourCheckTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature\Atc;
+
+use Tests\TestCase;
+use App\Models\Mship\Account;
+use App\Models\Atc\Endorsement;
+use App\Models\Mship\Qualification;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AreaHourCheckTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    protected $hourCheckRoute = 'controllers.hour_check.area';
+
+    /** @test */
+    public function testRedirectsAwayIfUserNotS3()
+    {
+        $account = factory(Account::class)->create();
+
+        $qualification = Qualification::code('S2')->first();
+        $account->addQualification($qualification);
+        $account->save();
+
+        $this->actingAs($account->fresh())
+            ->get(route($this->hourCheckRoute))
+            ->assertRedirect(route('mship.manage.dashboard'));
+    }
+
+    /** @test */
+    public function testSuccessfulNavigationIfUserIsS3()
+    {
+        // create relevant endorsement.
+        factory(Endorsement::class)->create(['name' => 'LON_S_CTR']);
+        $account = factory(Account::class)->create();
+
+        $qualification = Qualification::code('S3')->first();
+        $account->addQualification($qualification);
+        $account->save();
+
+        $this->actingAs($account->fresh())
+            ->get(route($this->hourCheckRoute))
+            ->assertOk();
+    }
+
+    /** @test */
+    public function testRedirectsAwayIfNoRelevantEndorsementsCreated()
+    {
+        $account = factory(Account::class)->create();
+
+        $qualification = Qualification::code('S3')->first();
+        $account->addQualification($qualification);
+        $account->save();
+
+        $this->actingAs($account->fresh())
+            ->get(route($this->hourCheckRoute))
+            ->assertRedirect(route('mship.manage.dashboard'));
+    }
+}
