878b98810f90d1d723da688a0e57fcec21529402
Callum Axon <AxonC@users.noreply.github.com>
2021-08-30 15:02:59 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- nova-components/WaitingListsManager/src/Http/WaitingListsManagerController.php
+++ nova-components/WaitingListsManager/src/Http/WaitingListsManagerController.php
@@ -2,19 +2,19 @@
 
 namespace Vatsimuk\WaitingListsManager\Http;
 
-use Illuminate\Http\Request;
-use App\Models\Mship\Account;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Routing\Controller;
-use App\Models\Training\WaitingList;
-use Illuminate\Support\Facades\Auth;
-use App\Services\Training\OfferTrainingPlace;
-use App\Models\Training\WaitingList\WaitingListStatus;
-use App\Models\Training\TrainingPlace\TrainingPosition;
-use App\Models\Training\WaitingList\WaitingListAccount;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Events\Training\AccountChangedStatusInWaitingList;
+use App\Models\Mship\Account;
+use App\Models\Training\TrainingPlace\TrainingPosition;
+use App\Models\Training\WaitingList;
+use App\Models\Training\WaitingList\WaitingListAccount;
+use App\Models\Training\WaitingList\WaitingListStatus;
+use App\Services\Training\OfferTrainingPlace;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class WaitingListsManagerController extends Controller
 {
@@ -73,11 +73,11 @@ class WaitingListsManagerController extends Controller
     public function getAvailablePlaces(WaitingList &$waitingList)
     {
         return response()->json([
-            'places' => TrainingPosition::availablePlacesForWaitingList($waitingList)
+            'places' => TrainingPosition::availablePlacesForWaitingList($waitingList),
         ]);
     }
 
-    public function offerTrainingPlace(WaitingList &$waitingList, TrainingPosition $trainingPosition, Request $request) : JsonResponse
+    public function offerTrainingPlace(WaitingList &$waitingList, TrainingPosition $trainingPosition, Request $request): JsonResponse
     {
         try {
             $account = Account::findOrFail($request->get('account_id'));
@@ -85,7 +85,7 @@ class WaitingListsManagerController extends Controller
             return response()->json(['message' => 'Account not found'], 400);
         }
 
-        if (!$this->getWaitingListAccounts($waitingList, true)->pluck('id')->contains($account->id)) {
+        if (! $this->getWaitingListAccounts($waitingList, true)->pluck('id')->contains($account->id)) {
             return response()->json(['message' => 'Account not eligible for training place.'], 403);
         }
 
