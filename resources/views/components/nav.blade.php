cc65c466d05dffd5a89d2e9ed6fa61ba44510e14
Alex Toff <atoff@users.noreply.github.com>
2022-02-06 17:14:36 +0000

    feat: User Waiting List Inspection (#2358)
    * First progress
    
    * Apply fixes from StyleCI
    
    [ci skip] [skip ci]
    
    * Fix gitignore to cover horizon, fix test
    
    * fix not assessing requirement properly
    
    * More tweaks and testing
    
    * Apply fixes from StyleCI
    
    [ci skip] [skip ci]
    
    * Clarity change
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,14 +1,13 @@
 <nav id="nav" class="navbar navbar-uk navbar-fixed-top">
     @if (is_local_environment())
-        <div class="dev_environment_notification">
-            You are in a <b>NON-PRODUCTION</b> environment
-        </div>
+    <div class="dev_environment_notification">
+        You are in a <b>NON-PRODUCTION</b> environment
+    </div>
     @endif
     @include('components.top-notification')
     <div class="nav_upper_container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle nav nav-collapsed" data-toggle="collapse"
-                    data-target="#nav-inner">
+            <button type="button" class="navbar-toggle nav nav-collapsed" data-toggle="collapse" data-target="#nav-inner">
                 <span class="nav-collapsed-icon"></span>
                 <span class="nav-collapsed-icon"></span>
                 <span class="nav-collapsed-icon"></span>
@@ -67,7 +66,7 @@
                                 <li>{!! link_to_route("site.atc.mentor", "Becoming a Mentor") !!}</li>
                                 <li>{!! link_to_route("site.atc.bookings", "Bookings") !!}</li>
                                 @if(currentUserHasAuth())
-                                        <li>{!! link_to_route("ukcp.guide", "UK Controller Plugin") !!}</li>
+                                <li>{!! link_to_route("ukcp.guide", "UK Controller Plugin") !!}</li>
                                 @endif
                             </ul>
                         </li>
@@ -83,6 +82,13 @@
                                 <li class="divider"></li>
                                 <li class="dropdown-header">Hour Checker</li>
                                 <li>{!! link_to_route("controllers.hour_check.area", "C1 Training Place") !!}</li>
+                            </ul>
+                        </li>
+                        <li class="col-sm-12">
+                            <ul>
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Waiting Lists</li>
+                                <li>{!! link_to_route("mship.waiting-lists.index", "My Waiting Lists") !!}</li>
                             </ul>
                         </li>
                     </ul>
@@ -154,77 +160,75 @@
             </ul>
 
             @if(currentUserHasAuth())
-                <ul class="nav navbar-nav navbar-right navcustom">
-                    <li class="dropdown dropdown-large navbar-notification hidden-xs">
-                        <a href="{{ route("mship.notification.list") }}" title="Notifications">
-                            @if(Auth::user()->has_unread_notifications)
-                                <i class="fa fa-bell unread-notifications"></i>
-                            @else
-                                <i class="fa fa-bell"></i>
-                            @endif
-                        </a>
-                    </li>
-                    @if(Auth::user()->can('use-permission', 'adm'))
-                        <li class="dropdown dropdown-large">
-                            <a href="{{ route("adm.index") }}" title="Admin Dashboard">
-                                <i class="fa fa-briefcase"></i>
-                            </a>
+            <ul class="nav navbar-nav navbar-right navcustom">
+                <li class="dropdown dropdown-large navbar-notification hidden-xs">
+                    <a href="{{ route("mship.notification.list") }}" title="Notifications">
+                        @if(Auth::user()->has_unread_notifications)
+                        <i class="fa fa-bell unread-notifications"></i>
+                        @else
+                        <i class="fa fa-bell"></i>
+                        @endif
+                    </a>
+                </li>
+                @if(Auth::user()->can('use-permission', 'adm'))
+                <li class="dropdown dropdown-large">
+                    <a href="{{ route("adm.index") }}" title="Admin Dashboard">
+                        <i class="fa fa-briefcase"></i>
+                    </a>
+                </li>
+                @endif
+                <li class="dropdown dropdown-large">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <span class="hidden-md">{{Auth::user()->name.' (' .Auth::user()->id.')'}} <b class="caret"></b></span>
+                        <i class="fa fa-sliders visible-md-inline-block"><b class="caret"></b></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-logout dropdown-menu-large row mainmenu_dropdown">
+                        <li class="col-sm-12">
+                            <ul>
+                                <li>{!! link_to_route("landing", "Dashboard") !!}</li>
+                                <li class="divider"></li>
+                                <li><a>ATC Rating: <b>
+                                            @if(Auth::user()->qualification_atc == "")
+                                            OBS
+                                            @else
+                                            {{ Auth::user()->qualification_atc }}
+                                            @endif
+                                        </b></a></li>
+                                <li><a>Pilot Rating(s): <b>
+                                            {{ (Auth::user()->toArray())['pilot_rating'] }}
+                                            @if((Auth::user()->toArray())['pilot_rating'] == "")
+                                            P0
+                                            @endif
+                                        </b></a></li>
+                                <li class="visible-xs visible-sm">{{ link_to_route("mship.notification.list","Notifications") }}</li>
+                                <li class="divider"></li>
+                                <li>{!! link_to_route('password.change', "Modify Password") !!}</li>
+                                @if(!Auth::user()->mandatory_password)
+                                <li>{!! link_to_route("password.delete", "Disable Password") !!}</li>
+                                @endif
+                                <li class="divider"></li>
+                                <li>{!! link_to_route("mship.manage.email.add", "Add Email Address") !!}</li>
+                                <li>{!! link_to_route("mship.manage.email.assignments", "Email Assignments") !!}</li>
+                                <li class="divider"></li>
+                                <li>
+                                    {!! Form::open(['route' => 'logout', 'id' => 'logout-form']) !!}
+                                    {!! Form::close() !!}
+                                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log
+                                        Out</a>
+                                </li>
+                            </ul>
                         </li>
-                    @endif
-                    <li class="dropdown dropdown-large">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <span class="hidden-md">{{Auth::user()->name.' (' .Auth::user()->id.')'}} <b
-                                            class="caret"></b></span>
-                            <i class="fa fa-sliders visible-md-inline-block"><b class="caret"></b></i>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-logout dropdown-menu-large row mainmenu_dropdown">
-                            <li class="col-sm-12">
-                                <ul>
-                                    <li>{!! link_to_route("landing", "Dashboard") !!}</li>
-                                    <li class="divider"></li>
-                                    <li><a>ATC Rating: <b>
-                                                @if(Auth::user()->qualification_atc == "")
-                                                    OBS
-                                                @else
-                                                    {{ Auth::user()->qualification_atc }}
-                                                @endif
-                                            </b></a></li>
-                                    <li><a>Pilot Rating(s): <b>
-                                                {{ (Auth::user()->toArray())['pilot_rating'] }}
-                                                @if((Auth::user()->toArray())['pilot_rating'] == "")
-                                                    P0
-                                                @endif
-                                            </b></a></li>
-                                    <li class="visible-xs visible-sm">{{ link_to_route("mship.notification.list","Notifications") }}</li>
-                                    <li class="divider"></li>
-                                    <li>{!! link_to_route('password.change', "Modify Password") !!}</li>
-                                    @if(!Auth::user()->mandatory_password)
-                                        <li>{!! link_to_route("password.delete", "Disable Password") !!}</li>
-                                    @endif
-                                    <li class="divider"></li>
-                                    <li>{!! link_to_route("mship.manage.email.add", "Add Email Address") !!}</li>
-                                    <li>{!! link_to_route("mship.manage.email.assignments", "Email Assignments") !!}</li>
-                                    <li class="divider"></li>
-                                    <li>
-                                        {!! Form::open(['route' => 'logout', 'id' => 'logout-form']) !!}
-                                        {!! Form::close() !!}
-                                        <a href="{{ route('logout') }}"
-                                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log
-                                            Out</a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
+                    </ul>
+                </li>
+            </ul>
             @else
-                <ul class="nav navbar-nav navbar-right navcustom">
-                    <li class="dropdown dropdown-large">
-                        <a href="{{ route("login") }}" title="Login">
-                            Login &thinsp;<i class="fa fa-arrow-right"></i>
-                        </a>
-                    </li>
-                </ul>
+            <ul class="nav navbar-nav navbar-right navcustom">
+                <li class="dropdown dropdown-large">
+                    <a href="{{ route("login") }}" title="Login">
+                        Login &thinsp;<i class="fa fa-arrow-right"></i>
+                    </a>
+                </li>
+            </ul>
             @endif
         </div>
     </div>
