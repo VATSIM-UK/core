af62b3a1b913ac91254f8abdc69eda44664141f4
hsugden <harry.sugden@me.com>
2022-02-28 18:11:43 +0000

    Fixed TC NE top-down responsibilities

--- resources/views/site/operations/sectors.blade.php
+++ resources/views/site/operations/sectors.blade.php
@@ -145,7 +145,7 @@
                         LON_M_CTR (120.025) &#8594; LON_C_CTR (127.100) &#8594; LON_SC_CTR (132.600) &#8594; LON_CTR (127.825)
                     </p>
                     <p style="margin-left: 40px">
-                        <strong>London/Stansted (EGSS), London/Luton (EGGW), Cambridge (EGSC)</strong><br>
+                        <strong>London/Stansted (EGSS), Cambridge (EGSC)</strong><br>
                         LTC_NE_CTR (118.825) &#8594; LTC_N_CTR (119.775) &#8594; LTC_CTR (135.800) &#8594; LTC_E_CTR (121.225) &#8594; LON_E_CTR (118.475) &#8594; LON_C_CTR (127.100) &#8594; LON_SC_CTR (132.600) &#8594; LON_CTR (127.825)
                     </p>
                     <p style="margin-left: 40px">
