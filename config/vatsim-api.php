ef781dae196a865196a8ae3938386c3051ab2719
Ryan Bentley <42263358+ryan1336@users.noreply.github.com>
2022-09-02 21:44:48 +0100

    update vatsim api url

--- config/vatsim-api.php
+++ config/vatsim-api.php
@@ -5,7 +5,7 @@ return [
     /*
      * The location of the VATSIM API
      */
-    'base' => env('VATSIM_API_BASE', 'https://apiv2.vatsim.dev/v2/'),
+    'base' => env('VATSIM_API_BASE', 'https://apiv2-dev.vatsim.net/v2/'),
 
     /*
      * The API key for the VATSIM API
