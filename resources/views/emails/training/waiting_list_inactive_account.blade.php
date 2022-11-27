addd351b84e1dd67f125241bf1a90e086a6be92d
Callum Axon <git@callumaxon.uk>
2022-11-27 12:31:51 +0000

    Remove atc references in email template

--- resources/views/emails/training/waiting_list_inactive_account.blade.php
+++ resources/views/emails/training/waiting_list_inactive_account.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
 
-<p>Your VATSIM account was marked as inactive. You were on a waiting list for ATC training in VATSIM United Kingdom. You have now been removed from this waiting list(s).</p>
+<p>Your VATSIM account was marked as inactive. You were on a waiting list for training in VATSIM United Kingdom. You have now been removed from this waiting list(s).</p>
 
 <p>If you believe this to be in error, then please get in contact with the VATSIM UK Member Services team via the helpdesk.</p>
 
