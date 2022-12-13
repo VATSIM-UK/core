69c03e720e766324e59f5dfcea9a5b7426aa0a16
Callum Axon <git@callumaxon.uk>
2022-12-13 13:40:41 +0000

    refactor: add names to indexes to stop mysql squawking

--- database/migrations/2022_12_13_131128_index_networkdata_for_account_disconnected_and_callsign.php
+++ database/migrations/2022_12_13_131128_index_networkdata_for_account_disconnected_and_callsign.php
@@ -16,18 +16,19 @@ return new class extends Migration
         Schema::table('networkdata_atc', function (Blueprint $table) {
             // index to allow query pattern which retrieves an accounts ATC sessions by their disconnected timestamp
             // and then callsign.
-            $table->index(['account_id', 'disconnected_at', 'callsign']);
+            $table->index(['account_id', 'disconnected_at', 'callsign'], 'networkdata_atc_by_date_callsign');
 
             // index to allow query pattern which retrieves an accounts ATC sessions first by the callsign
-            // and then by the disconnected timestamp
-            $table->index(['account_id', 'callsign', 'disconnected_at']);
+            // and then by the disconnected timestamp. (Name specified for index due to name length limit)
+            $table->index(['account_id', 'callsign', 'disconnected_at'], 'networkdata_atc_by_callsign_date');
 
             // index to allow endorsement conditions to retrieve network data by account, callsign, connected_at,
             // qualification_id and deleted_at which helps boost performance
             // when a condition has a requirement for ATC sessions to be completed with a particular qualification
             // and within a particular number of months.
             // Deleted_at is included as the Networkdata/Atc model has soft deletes.
-            $table->index(['account_id', 'callsign', 'connected_at', 'qualification_id', 'deleted_at']);
+            // (Name specified for index due to name length limit)
+            $table->index(['account_id', 'callsign', 'connected_at', 'qualification_id', 'deleted_at'], 'networkdata_endorsement_check_query');
         });
     }
 
