d0f3898e3d12eba639b7e3b5bf4ac551cfa2900f
Alex Toff <me@alextoff.uk>
2023-05-02 11:57:54 +0000

    more test cases

--- tests/Unit/Mship/HasDiscordAccountTest.php
+++ tests/Unit/Mship/HasDiscordAccountTest.php
@@ -20,14 +20,18 @@ class HasDiscordAccountTest extends TestCase
 
         $this->mock(Discord::class, function (MockInterface $mock) {
             $mock->shouldReceive('setNickname')->once();
-            $mock->shouldReceive('getUserRoles')->andReturn(collect(['5678']))->once();
-            $mock->shouldReceive('grantRoleById')->with($this->user, '1234')->once();
-            $mock->shouldReceive('removeRoleById')->with($this->user, '5678')->once();
+            $mock->shouldReceive('getUserRoles')->andReturn(collect(['2']))->once();
+            $mock->shouldReceive('grantRoleById')->with($this->user, '1')->once();
+            $mock->shouldReceive('removeRoleById')->with($this->user, '2')->once();
         });
 
         $permissionHas = factory(Permission::class)->create();
-        DiscordRoleRule::factory()->create(['discord_id' => '1234', 'permission_id' => $permissionHas]); // Role rule means user should have
-        DiscordRoleRule::factory()->create(['discord_id' => '5678', 'permission_id' => factory(Permission::class)->create()]); // Role rule means user shouldn't have role
+        DiscordRoleRule::factory()->create(['discord_id' => '1', 'permission_id' => $permissionHas]); // Role rule means user should have
+        DiscordRoleRule::factory()->create(['discord_id' => '1', 'permission_id' => factory(Permission::class)->create()]); // Role rule means user shouldn't have (but granted by one above)
+
+        DiscordRoleRule::factory()->create(['discord_id' => '2', 'permission_id' => factory(Permission::class)->create()]); // Role rule means user shouldn't have role. Should be removed
+
+        DiscordRoleRule::factory()->create(['discord_id' => '3', 'permission_id' => factory(Permission::class)->create()]); // Role rule means user shouldn't have role
 
         $this->user->givePermissionTo($permissionHas);
 
