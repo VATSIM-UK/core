a214fd96ef3e4686f355d4ecdf45f623ea52d4e2
Callum Axon <git@callumaxon.uk>
2023-05-14 11:54:14 +0100

    feat: support FI and FE pilot ratings

---
+++ database/migrations/2023_05_14_102302_add_pilot_instructor_ratings.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table("mship_qualification")->insert(
+            [
+                'code' => 'FI',
+                'type' => 'pilot',
+                'name_small' => 'FI',
+                'name_long' => 'Flight Instructor',
+                'name_grp' => 'Flight Instructor',
+                'vatsim' => 31,
+            ]
+        );
+
+        DB::table("mship_qualification")->insert(
+            [
+                'code' => 'FE',
+                'type' => 'pilot',
+                'name_small' => 'FE',
+                'name_long' => 'Flight Examiner',
+                'name_grp' => 'Flight Examiner',
+                'vatsim' => 63,
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table("mship_qualification")->where('code', 'FI')->delete();
+        DB::table("mship_qualification")->where('code', 'FE')->delete();
+    }
+};
