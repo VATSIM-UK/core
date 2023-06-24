07a7b9aacee50d2413e3e5d2e16f702254ebd6ae
Callum Axon <git@callumaxon.uk>
2023-06-24 14:02:16 +0100

    feat: add new cookie consent library

---
+++ resources/lang/vendor/cookieConsent/en/cookies.php
@@ -0,0 +1,31 @@
+<?php
+return [
+    'title' => 'We use cookies',
+    'intro' => 'This website uses cookies in order to enhance the overall user experience.',
+    'link' => 'Take a look at our <a href=":url">Cookies Policy</a> for more information.',
+
+    'essentials' => 'Only essentials',
+    'all' => 'Accept all',
+    'customize' => 'Customize',
+    'manage' => 'Manage cookies',
+    'details' => [
+        'more' => 'More details',
+        'less' => 'Less details',
+    ],
+    'save' => 'Save settings',
+
+    'categories' => [
+        'essentials' => [
+            'title' => 'Essential cookies',
+            'description' => 'There are some cookies that we have to include in order for certain web pages to function. For this reason, they do not require your consent.',
+        ],
+        'analytics' => [
+            'title' => 'Analytics cookies',
+            'description' => 'We use these for internal research on how we can improve the service we provide for all our users. These cookies assess how you interact with our website – as an anonymous user (the gathered data does not identify you personally).',
+        ],
+        'optional' => [
+            'title' => 'Optional cookies',
+            'description' => 'These cookies enable features that could improve your user experience, but their absence will not impact your ability to browse our website.',
+        ],
+    ],
+];
