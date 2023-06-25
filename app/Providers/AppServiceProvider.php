569b40b881bbbe4e8677e38c119be249b63d42dd
Callum Axon <git@callumaxon.uk>
2023-06-25 09:27:21 +0100

    revert: discord rate limiting changes rollback (#3124)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,10 +8,8 @@ use App\Libraries\Forum;
 use App\Libraries\UKCP;
 use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
 use HTML;
-use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Validator;
@@ -51,10 +49,6 @@ class AppServiceProvider extends ServiceProvider
 
         View::composer('layout*', function ($view) {
             $view->with('_bannerUrl', BaseController::generateBannerUrl());
-        });
-
-        RateLimiter::for('discord-sync', function (object $job) {
-            return Limit::perHour(1)->by($job->getAccountId());
         });
     }
 
