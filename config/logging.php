88c2dab5ae99e90579967c179cb1d668df452258
Alex Toff <me@alextoff.uk>
2023-09-14 15:57:13 +0100

    Imports

--- config/logging.php
+++ config/logging.php
@@ -1,6 +1,9 @@
 <?php
 
+use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\SyslogUdpHandler;
+use Monolog\Processor\PsrLogMessageProcessor;
 
 return [
     /*
