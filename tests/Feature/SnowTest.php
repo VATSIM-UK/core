9a85c76c6d341f9af77b24ab3be4ef144dc6be2b
CalumTowers <towers@hey.com>
2023-10-31 21:16:33 +0000

    wip

--- tests/Feature/SnowTest.php
+++ tests/Feature/SnowTest.php
@@ -4,41 +4,39 @@ use Carbon\Carbon;
 
 class SnowTest extends \Tests\TestCase
 {
-    private $script;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-        $this->script = '<script src="'.mix('js/snow.js').'"></script>';
-    }
-
     public function testSnowDuringPeriod()
     {
-        $this->markTestSkipped('Figure out a way to sort for Vite...');
-
         \Carbon\Carbon::setTestNow(new Carbon('1st December 2019'));
-        $this->get(route('site.home'))
+
+        $home = $this->get(route('site.home'))
             ->assertOk()
-            ->assertSee($this->script, false);
-        $this->get(route('site.join'))->assertOk()
-            ->assertSee($this->script, false);
+            ->getContent();
+
+        $this->assertTrue((bool) preg_match('/snow-.*\.js/', $home));
 
         \Carbon\Carbon::setTestNow(new Carbon('5th January 2019'));
-        $this->get(route('site.home'))
+
+        $main = $this->get(route('site.join'))
             ->assertOk()
-            ->assertSee($this->script, false);
+            ->getContent();
+
+        $this->assertTrue((bool) preg_match('/snow-.*\.js/', $main));
     }
 
     public function testNoSnowOutsidePeriod()
     {
-        $this->markTestSkipped('Figure out a way to sort for Vite...');
-        
         \Carbon\Carbon::setTestNow(new Carbon('10th January 2020'));
-        $this->get(route('site.home'))
+
+        $main = $this->get(route('site.home'))
             ->assertOk()
-            ->assertDontSee($this->script, false);
-        $this->get(route('site.join'))
+            ->getContent();
+
+        $this->assertFalse((bool) preg_match('/snow-.*\.js/', $main));
+
+        $main = $this->get(route('site.join'))
             ->assertOk()
-            ->assertDontSee($this->script, false);
+            ->getContent();
+
+        $this->assertFalse((bool) preg_match('/snow-.*\.js/', $main));
     }
 }
