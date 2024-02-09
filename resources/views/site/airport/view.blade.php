f979acbcc8e30470c65ccfc787520a3cb6c0e1c5
Alex Toff <atoff@users.noreply.github.com>
2024-02-09 23:41:11 +0000

    fix: ChartFox Airport Interface

--- resources/views/site/airport/view.blade.php
+++ resources/views/site/airport/view.blade.php
@@ -267,7 +267,7 @@
                 <div class="panel panel-ukblue">
                     <div class="panel-heading"><i class="fa fa-map"></i> Charts</div>
                     <div class="text-summary panel-body">
-                        <iframe class="w-100" style="min-height:80vh;width:100%" src="https://api.chartfox.org/v2/interfaces/airports/{{ $airport->icao }}?token={{ config('services.chartfox.public_token') }}"></iframe>
+                        <iframe class="w-100" style="min-height:80vh;width:100%" src="https://api.chartfox.org/v2/interfaces/airport/{{ $airport->icao }}?token={{ config('services.chartfox.public_token') }}"></iframe>
                     </div>
                 </div>
 
