4e6dbc591567f0943ef172f5ef9439d8c8c4c8de
Calum Towers <towers@hey.com>
2024-02-26 16:56:17 +0000

    roster homepage improvement

--- app/Livewire/Roster/Index.php
+++ app/Livewire/Roster/Index.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire\Roster;
 
+use App\Models\Roster;
 use Livewire\Component;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.roster.index');
+        return view('livewire.roster.index', [
+            'roster' => Roster::where('account_id', auth()->user()->id)->exists()
+        ]);
     }
 }
--- resources/views/livewire/roster/index.blade.php
+++ resources/views/livewire/roster/index.blade.php
@@ -3,15 +3,42 @@
     <div class="bg-white px-6 py-12 shadow space-y-6 sm:rounded-lg sm:px-12">
         <div class="flex flex-col items-center space-y-8">
             <div class="flex flex-col items-center space-y-4">
-                <p>Something about whether the logged in user is on the roster.</p>
+                <span class="text-2xl font-bold">👋 Hello, {{ auth()->user()->name_first }}!</span>
                 <div>
-                    <a wire:navigate href="{{ route('site.roster.renew') }}"
-                        class="text-bold text-blue-500 hover:cursor-pointer">Renew my currency</a>
-                     |
-                    <a wire:navigate href="{{ route('site.roster.search') }}"
-                        class="text-bold text-blue-500 hover:cursor-pointer">Search the roster</a>
+                    @if($roster)
+                        <span
+                            class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-md font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Active on Roster</span>
+                    @else
+                        <span
+                            class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-md font-medium text-red-700 ring-1 ring-inset ring-red-600/20">Inactive on Roster</span>
+                    @endif
                 </div>
+                @if($roster)
+                    <span>
+                        You are currently <span class="font-bold">active</span> on the VATSIM UK roster and can control any positions
+                        <a class="text-blue-500 hover:cursor-pointer" href="{{ route('site.roster.show', ['account' => auth()->user()]) }}">listed on your roster page</a>.
+                    </span>
+                @elseif(auth()->user()->hasState('DIVISION'))
+                    <span>
+                        You are currently <span class="font-bold">inactive</span> on the VATSIM UK roster, and cannot control any UK positions until you
+                        <a class="text-blue-500 hover:cursor-pointer" href="{{ route('site.roster.renew') }}">renew your currency</a>.
+                    </span>
+                @else
+                    <span>
+                        You are currently <span class="font-bold">inactive</span> on the VATSIM UK roster, and cannot control any UK positions.
+                        <br>
+                        Please <a class="text-blue-500 hover:cursor-pointer" href="mailto:member-services@vatsim.uk">contact Member Services</a> if you believe this is incorrect.
+                    </span>
+                @endif
             </div>
+            <div class="flex flex-col">
+                @if(!$roster && auth()->user()->hasState('DIVISION'))
+                    <a wire:navigate href="{{ route('site.roster.renew') }}"
+                       class="text-bold text-blue-500 hover:cursor-pointer">Renew my currency</a>
+                @endif
+                <a wire:navigate href="{{ route('site.roster.search') }}"
+                   class="text-bold text-blue-500 hover:cursor-pointer">Search the roster</a>
+                </div>
         </div>
     </div>
 </div>
