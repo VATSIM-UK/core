bef51a989c334a5dc914a1c8cf6b713f3d013698
Calum Towers <towers@hey.com>
2024-02-28 16:17:50 +0000

    disable endorsement pages

--- app/Http/Controllers/Atc/EndorsementController.php
+++ app/Http/Controllers/Atc/EndorsementController.php
@@ -10,57 +10,63 @@ class EndorsementController extends BaseController
 {
     public function getGatwickGroundIndex()
     {
-        $endorsement = PositionGroup::with('conditions')->where('name', 'Gatwick S1 (DEL/GND)')->first();
+        return Redirect::route('mship.manage.dashboard')
+            ->withError("We're making some changes to this page, please check back later.");
 
-        $hours = $endorsement->conditions->map(function ($condition) {
-            return $condition->progressForUser($this->account);
-        });
-
-        if (! $this->account->fully_defined || ! $this->account->qualificationAtc->isS1) {
-            return Redirect::route('mship.manage.dashboard')
-                ->withError('Only S1 rated controllers are eligible for a Gatwick Ground endorsement.');
-        }
-
-        return $this->viewMake('controllers.endorsements.gatwick_ground')
-            ->with('endorsment', $endorsement)
-            ->with('conditions', $endorsement->conditions)
-            ->with('hours', $hours->all());
+//        $endorsement = PositionGroup::with('conditions')->where('name', 'Gatwick S1 (DEL/GND)')->first();
+//
+//        $hours = $endorsement->conditions->map(function ($condition) {
+//            return $condition->progressForUser($this->account);
+//        });
+//
+//        if (! $this->account->fully_defined || ! $this->account->qualificationAtc->isS1) {
+//            return Redirect::route('mship.manage.dashboard')
+//                ->withError('Only S1 rated controllers are eligible for a Gatwick Ground endorsement.');
+//        }
+//
+//        return $this->viewMake('controllers.endorsements.gatwick_ground')
+//            ->with('endorsment', $endorsement)
+//            ->with('conditions', $endorsement->conditions)
+//            ->with('hours', $hours->all());
     }
 
     public function getAreaIndex()
     {
-        $positionGroups = PositionGroup::whereIn('name', ['LON_S_CTR', 'LON_C_CTR', 'LON_N_CTR', 'SCO_CTR'])->get();
+        return Redirect::route('mship.manage.dashboard')
+            ->withError("We're making some changes to this page, please check back later.");
 
-        if ($positionGroups->count() < 1) {
-            return Redirect::route('mship.manage.dashboard')
-                ->withError('Endorsements improperly configured');
-        }
-
-        if (! $this->account->fully_defined || ! $this->account->qualificationAtc->isS3) {
-            return Redirect::route('mship.manage.dashboard')
-                ->withError('Only S3 rated controllers can see their C1 Training Place eligibility.');
-        }
-
-        $positionGroups = $positionGroups->load('conditions')->map(function ($positionGroup) {
-            $conditions = $positionGroup->conditions->map(function ($condition) use ($positionGroup) {
-                return [
-                    'position_group_id' => $positionGroup->id,
-                    // extract the likely position name from the criterion loaded into the database.
-                    'position' => str_replace('%', '_', $condition->positions[0]),
-                    'required_hours' => $condition->required_hours,
-                    'within_months' => $condition->within_months,
-                    'progress' => round($condition->progressForUser($this->account)->sum(), 1),
-                    'complete' => $condition->isMetForUser($this->account),
-                ];
-            });
-
-            return [
-                'name' => $positionGroup->name,
-                'conditions' => $conditions,
-            ];
-        });
-
-        return $this->viewMake('controllers.endorsements.area')
-            ->with('positionGroups', $positionGroups);
+//        $positionGroups = PositionGroup::whereIn('name', ['LON_S_CTR', 'LON_C_CTR', 'LON_N_CTR', 'SCO_CTR'])->get();
+//
+//        if ($positionGroups->count() < 1) {
+//            return Redirect::route('mship.manage.dashboard')
+//                ->withError('Endorsements improperly configured');
+//        }
+//
+//        if (! $this->account->fully_defined || ! $this->account->qualificationAtc->isS3) {
+//            return Redirect::route('mship.manage.dashboard')
+//                ->withError('Only S3 rated controllers can see their C1 Training Place eligibility.');
+//        }
+//
+//        $positionGroups = $positionGroups->load('conditions')->map(function ($positionGroup) {
+//            $conditions = $positionGroup->conditions->map(function ($condition) use ($positionGroup) {
+//                return [
+//                    'position_group_id' => $positionGroup->id,
+//                    // extract the likely position name from the criterion loaded into the database.
+//                    'position' => str_replace('%', '_', $condition->positions[0]),
+//                    'required_hours' => $condition->required_hours,
+//                    'within_months' => $condition->within_months,
+//                    'progress' => round($condition->progressForUser($this->account)->sum(), 1),
+//                    'complete' => $condition->isMetForUser($this->account),
+//                ];
+//            });
+//
+//            return [
+//                'name' => $positionGroup->name,
+//                'conditions' => $conditions,
+//            ];
+//        });
+//
+//        return $this->viewMake('controllers.endorsements.area')
+//            ->with('positionGroups', $positionGroups);
     }
 }
