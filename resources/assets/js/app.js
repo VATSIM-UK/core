645b4371ce94add9d996e25bc1a68b483a772ebd
Calum Towers <towers@hey.com>
2024-02-29 22:03:36 +0000

    fix: Import AlpineJS from Repo

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,7 +8,7 @@ $("body").scrollspy({
 
 $(".tooltip_displays").tooltip();
 
-import { Alpine } from "../../../vendor/livewire/livewire/dist/livewire.esm";
+import Alpine from 'alpinejs';
 import collapse from "@alpinejs/collapse";
 
 Alpine.plugin(collapse);
