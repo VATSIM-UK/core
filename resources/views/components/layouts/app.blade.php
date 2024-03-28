78a54851703846f90d3c379290ce0b10a8765c1d
Callum Axon <git@callumaxon.uk>
2024-03-28 10:32:22 +0000

    wip: notifications not got the current styling for some reason on the front end - fine in admin

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,23 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
 
-    @vite('resources/assets/css/tailwind.css')
     @filamentStyles
+    @vite('resources/assets/css/tailwind.css')
 
     <title>{{ isset($title) ? "VATSIM UK | $title" : 'VATSIM UK' }}</title>
 </head>
 <body>
-<div class="flex w-screen h-screen items-center justify-center text-center bg-gray-100">
-    <div class="flex min-h-full w-full md:w-2/3 flex-col justify-center py-12 px-6 lg:px-8">
-        <div class="flex justify-center mb-2">
-            <a href="{{ route('site.home') }}">
-                <img width="200" src="{{ secure_asset('images/branding/vatsimuk_blackblue.png') }}" unselectable="on">
-            </a>
+    <div class="flex w-screen h-screen items-center justify-center text-center bg-gray-100">
+        <div class="flex min-h-full w-full md:w-2/3 flex-col justify-center py-12 px-6 lg:px-8">
+            <div class="flex justify-center mb-2">
+                <a href="{{ route('site.home') }}">
+                    <img width="200" src="{{ secure_asset('images/branding/vatsimuk_blackblue.png') }}" unselectable="on">
+                </a>
+            </div>
+            {{ $slot }}
         </div>
-        {{ $slot }}
     </div>
-</div>
+    @filamentScripts
+    @livewire('notifications')
 </body>
-@filamentScripts
-@livewire('notifications')
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-import preset from './vendor/filament/support/tailwind.config.preset';
-const colors = require('tailwindcss/colors');
+import preset from "./vendor/filament/support/tailwind.config.preset";
+const colors = require("tailwindcss/colors");
 
 module.exports = {
+    darkMode: "class",
+    preset: [preset],
     content: [
-        './resources/**/*.blade.php',
-        './vendor/filament/**/*.blade.php',
+        "./app/Filament/**/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/views/livewire/**/*.blade.php",
+        "./resources/views/filament/**/*.blade.php",
+        "./vendor/filament/**/*.blade.php",
     ],
     theme: {
         extend: {
             colors: {
-                brand: '#25ADE3'
-            }
+                brand: "#25ADE3",
+            },
         },
     },
-    plugins: [
-        require('@tailwindcss/forms'),
-    ],
-}
+    plugins: [require("@tailwindcss/forms")],
+};
