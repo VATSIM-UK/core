f16009d98f2def1211463936c035b8e7408601d3
Callum Axon <git@callumaxon.uk>
2024-04-08 20:51:29 +0100

    pr comments

--- app/Events/Mship/Endorsement/TierEndorsementAdded.php
+++ app/Events/Mship/Endorsement/TierEndorsementAdded.php
@@ -5,7 +5,6 @@ namespace App\Events\Mship\Endorsement;
 use App\Models\Mship\Account;
 use App\Models\Mship\Account\Endorsement;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -18,18 +17,6 @@ class TierEndorsementAdded
      */
     public function __construct(private Endorsement $endorsement, private Account $account)
     {
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
     }
 
     public function getAccount()
--- resources/views/emails/mship/endorsement/tier_endorsement_granted.blade.php
+++ resources/views/emails/mship/endorsement/tier_endorsement_granted.blade.php
@@ -9,7 +9,7 @@
 		<li>{{ $position }}</li>
 	@endforeach
 </ul>
-<p>The listed positions are inn addition to the positions you are already entitled to control via your controller rating and other Tier or Solo endorsements.
+<p>The listed positions are in addition to the positions you are already entitled to control via your controller rating and other Tier or Solo endorsements.
 This has been reflected on your controller roster and is effective from the reciept of this email.</p>
 
 <p>Congratulations!</p>
