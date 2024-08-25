c27092592088370b0e07c2f028d115b49944a08e
Callum Axon <git@callumaxon.uk>
2024-08-25 20:31:49 +0100

    linting

--- app/Models/TVCP/VisitTransferApplication.php
+++ app/Models/TVCP/VisitTransferApplication.php
@@ -12,12 +12,17 @@ class VisitTransferApplication extends Model
     protected $table = 'tvcp_visit_transfer_applications';
 
     public const TYPE_TRANSFER = 'transfer';
+
     public const TYPE_VISIT = 'visit';
 
     public const STATUS_SUBMITTED = 'submitted';
+
     public const STATUS_ACCEPTED = 'accepted';
+
     public const STATUS_COMPLETED = 'completed';
+
     public const STATUS_FAILED = 'failed';
+
     public const STATUS_WITHDRAWN = 'withdrawn';
 
     public const STATUSES = [
--- app/Policies/VisitTransferApplicationPolicy.php
+++ app/Policies/VisitTransferApplicationPolicy.php
@@ -4,7 +4,6 @@ namespace App\Policies;
 
 use App\Models\Mship\Account;
 use App\Models\TVCP\VisitTransferApplication;
-use Illuminate\Auth\Access\Response;
 
 class VisitTransferApplicationPolicy
 {
