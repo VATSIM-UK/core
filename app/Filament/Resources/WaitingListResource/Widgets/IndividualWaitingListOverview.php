00f5ec14682531e03b75c055c80caec74e11e15d
Max Brokman <max.brokman@gmail.com>
2024-11-24 16:46:07 +0000

    Waiting list overview widget fix (#3886)
    * fix: Overview widget was still using WaitingListAccount as pivot, missed in big refactor
    
    * fix: Accidental committed this

--- app/Filament/Resources/WaitingListResource/Widgets/IndividualWaitingListOverview.php
+++ app/Filament/Resources/WaitingListResource/Widgets/IndividualWaitingListOverview.php
@@ -2,24 +2,26 @@
 
 namespace App\Filament\Resources\WaitingListResource\Widgets;
 
+use App\Models\Training\WaitingList;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
-use Illuminate\Database\Eloquent\Model;
 
 class IndividualWaitingListOverview extends BaseWidget
 {
-    public ?Model $record = null;
+    public ?WaitingList $record = null;
 
     protected static ?string $pollingInterval = null;
 
     protected function getStats(): array
     {
-        $totalAccounts = $this->record->accounts()->count();
+        $totalAccounts = $this->record->waitingListAccounts()->count();
 
-        $averageWaitTime = $this->record->accounts->average(fn ($account) => $account->pivot->created_at->diffInDays(now()));
+        $averageWaitTime = $this->record->waitingListAccounts->average(fn ($listAccount) => $listAccount->created_at->diffInDays(now()));
+        $longestWaitTime = $this->record->waitingListAccounts->max(fn ($listAccount) => $listAccount->created_at->diffInDays(now()));
 
         return [
             Stat::make('Total accounts', $totalAccounts),
+            Stat::make('Longest wait time', $longestWaitTime ? round($longestWaitTime).'days' : 'N/A'),
             Stat::make('Average wait time', $averageWaitTime ? round($averageWaitTime).' days' : 'N/A'),
         ];
     }
