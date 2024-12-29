4983be6237a22a4fe938827e289de2ef22d2e2c2
CJA <git@callumaxon.uk>
2024-12-29 11:17:42 +0000

    feat: add Solo Endorsement overview on admin panel (#3939)

---
+++ app/Filament/Resources/SoloEndorsementResource/Pages/ListSoloEndorsements.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\SoloEndorsementResource\Pages;
+
+use App\Filament\Resources\SoloEndorsementResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListSoloEndorsements extends ListRecords
+{
+    protected static string $resource = SoloEndorsementResource::class;
+}
