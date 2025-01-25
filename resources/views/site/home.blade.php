ae7199b3a782e9c545139244b85e9a28a601a20b
Alex Toff <atoff@users.noreply.github.com>
2025-01-25 14:20:37 +0000

    feat: Homepage Nav Update (#3959)
    * fix: Devcontainer first start errors
    
    * add nav update suggestions

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -141,14 +141,10 @@
                             <a class="nav-link" href="{{ route('site.atc.newController') }}">Become a Controller</a>
                         </li>
                         <li class="nav-item">
+                            <a class="nav-link" href="{{ config('services.docs.url') }}">Documentation</a>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link" href="{{ route('site.roster.index') }}">Controller Roster</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://cts.vatsim.uk/home/solo.php">Solo Endorsements</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://cts.vatsim.uk/home/validations.php">Special
-                                Endorsements</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('site.atc.heathrow') }}">Heathrow Endorsements</a>
