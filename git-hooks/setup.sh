548e9ba63fe6ed5d43b24e5a3c86253c23e23623
Kristián Kunc <kristian.kunc@gmail.com>
2025-03-06 17:15:39 +0100

    add comments to hooks

--- git-hooks/setup.sh
+++ git-hooks/setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This script will initialize all the hooks in the git-hooks directory
+# Running: chmod +x git-hooks/setup.sh && ./git-hooks/setup.sh
+
 for file in $(ls ./git-hooks | grep -v '\.sh$'); do
     chmod +x ./git-hooks/$file
     ln -sf ../../git-hooks/$file .git/hooks/$file
