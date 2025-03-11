cdf6ce6c1d2bccba83664d9f4457ac66c08746de
Kristián Kunc <kristian.kunc@gmail.com>
2025-03-11 22:26:01 +0100

    add ratelimiting

--- app/Http/Controllers/Api/CtsController.php
+++ app/Http/Controllers/Api/CtsController.php
@@ -4,16 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Repositories\Cts\BookingRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Http\Response;
 
 class CtsController
 {
-    private $bookingRepository;
-
     public function __construct(private BookingRepository $bookingRepository)
     {}
 
     public function getBookings(Request $request)
     {
+        if (app()->environment() !== 'development' && RateLimiter::tooManyAttempts('get-bookings:'.$request->ip(), 1)) {
+            $seconds = RateLimiter::availableIn('get-bookings:'.$request->ip());
+            return response()->json([
+            'message' => 'Too many requests. Please try again after '.$seconds.' seconds.'
+            ], Response::HTTP_TOO_MANY_REQUESTS)
+            ->header('Retry-After', $seconds)
+            ->header('X-RateLimit-Reset', now()->addSeconds($seconds)->getTimestamp());
+        }
+
+        RateLimiter::hit('get-bookings:'.$request->ip(), 300); // 5 minutes
+
         $bookings = $this->bookingRepository->getBookings(50);
 
         if ($bookings->isEmpty()) {
