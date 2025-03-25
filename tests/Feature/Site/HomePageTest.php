2518039da4cfbebd3bddf42f09c97800c4d1f9c4
Callum Axon <git@callumaxon.uk>
2025-03-25 18:39:07 +0000

    feat: hide booking names from main site sidebar

--- tests/Feature/Site/HomePageTest.php
+++ tests/Feature/Site/HomePageTest.php
@@ -34,7 +34,7 @@ class HomePageTest extends TestCase
             ->assertSee($booking->position)
             ->assertSee(Carbon::parse($booking->from)->format('H:i'))
             ->assertSee(Carbon::parse($booking->to)->format('H:i'))
-            ->assertSee(e($booking->member->name))
+            ->assertDontSee(e($booking->member->name))
             ->assertSee($booking->member->cid);
     }
 
