78cc42468a1db3e79b9ff1c4ddc3e76caf294cd7
Daniel 'MrAdder' Green <dgreen03@gmail.com>
2025-04-30 17:01:18 +0100

    Convert OPS/Sectors to Markdown
    - Created Markdown Folder within resources/site/operations
    - Converted all of the sections to Markdown

---
+++ resources/views/site/operations/markdown/ukareasectors.blade.php
@@ -0,0 +1,19 @@
+<x-markdown>
+### Introduction
+
+This page explains the basics of the En Route (CTR) sectors in the UK. The information is split into sections for different stages of flight.
+
+If you are ever unsure who to call, please message one of our controllers. They will be more than happy to help!
+
+**Do not call** `EGTT_I_CTR` or `EGPX_I_CTR` as civilian ‘airliner’ traffic – these positions are for UK Flight Information Services (FIS) outside of controlled airspace only.
+
+**Do not call** `EGVV_CTR` as a civilian ‘airliner’ – these positions are for Military operations. UK FIS may be available on request (normally only above FL100).
+
+### Radio Callsigns
+
+The two main radio callsigns for Area controllers in the UK are:
+
+1. **"London Control"** – all LON and LTC callsigns  
+2. **"Scottish Control"** – all SCO, STC and MAN callsigns
+
+</x-markdown>
