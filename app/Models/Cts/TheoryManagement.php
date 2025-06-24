3dd59a4a73d5298d984048d2f14dd4a8d1519a89
William Shaw <williamrockstar110@gmail.com>
2025-06-24 15:48:35 +0100

    lint

--- app/Models/Cts/TheoryManagement.php
+++ app/Models/Cts/TheoryManagement.php
@@ -2,13 +2,8 @@
 
 namespace App\Models\Cts;
 
-use App\Models\Mship\Account;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\Log;
-use App\Models\Cts\Member;
 
 class TheoryManagement extends Model
 {
@@ -19,18 +14,11 @@ class TheoryManagement extends Model
     protected $table = 'theory_settings';
 
     public $timestamps = false;
-    
+
     protected $fillable = ['item', 'setting'];
 
-    /**
-     * Get result for the internal Core account_id, also their CId
-     * and handle the logic where a member_id in CTS is different to that
-     * of the account_id in Core.
-     */
-    
-
-
-     public function questions() {
+    public function questions()
+    {
         return $this->hasMany(\App\Models\Cts\TheoryQuestion::class, 'level', 'level');
-     }
+    }
 }
