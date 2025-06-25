3fc8d86612632c001f2b52e65e225de210ab296e
Callum Axon <git@callumaxon.uk>
2025-06-25 19:37:20 +0100

    Minimum functionality to administer roster restrictions

--- app/Models/Roster.php
+++ app/Models/Roster.php
@@ -7,6 +7,7 @@ use App\Models\Atc\PositionGroup;
 use App\Models\Atc\PositionGroupPosition;
 use App\Models\Mship\Account;
 use App\Models\Mship\Account\Endorsement;
+use App\Models\Mship\Account\Note;
 use App\Models\Mship\Qualification;
 use App\Notifications\Roster\RemovedFromRoster;
 use Illuminate\Database\Eloquent\Builder;
@@ -52,6 +53,11 @@ class Roster extends Model
     public function account()
     {
         return $this->belongsTo(Account::class);
+    }
+
+    public function restrictionNote()
+    {
+        return $this->belongsTo(Note::class, 'restriction_note_id');
     }
 
     public function remove(?RosterUpdate $update = null)
