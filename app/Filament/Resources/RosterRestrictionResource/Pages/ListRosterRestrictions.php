766f112a1cc353d7616103722dcf21cf89411c71
Callum Axon <git@callumaxon.uk>
2025-06-29 12:47:41 +0100

    Remove roster restrictions create button from List resource

--- app/Filament/Resources/RosterRestrictionResource/Pages/ListRosterRestrictions.php
+++ app/Filament/Resources/RosterRestrictionResource/Pages/ListRosterRestrictions.php
@@ -3,17 +3,9 @@
 namespace App\Filament\Resources\RosterRestrictionResource\Pages;
 
 use App\Filament\Resources\RosterRestrictionResource;
-use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListRosterRestrictions extends ListRecords
 {
     protected static string $resource = RosterRestrictionResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
 }
