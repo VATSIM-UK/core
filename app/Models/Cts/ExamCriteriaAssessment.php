f5754a78db0ea071be5379a335d8e225697ba36c
Max Brokman <max.brokman@gmail.com>
2025-07-05 21:23:51 +0100

    fix: linter

--- app/Models/Cts/ExamCriteriaAssessment.php
+++ app/Models/Cts/ExamCriteriaAssessment.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ExamCriteriaAssessment extends Model
 {
     protected $table = 'practical_criteria_assess';
+
     protected $connection = 'cts';
+
     public $timestamps = false;
 
     public $guarded = [];
