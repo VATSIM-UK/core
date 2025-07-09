d7c9f9123ec16513b9e614564dfde6daf59fc9d7
Coby Chapman <cobylc.121@gmail.com>
2025-07-09 09:24:34 +0100

    Update ListAccounts.php

--- app/Filament/Admin/Resources/AccountResource/Pages/ListAccounts.php
+++ app/Filament/Admin/Resources/AccountResource/Pages/ListAccounts.php
@@ -4,7 +4,6 @@ namespace App\Filament\Admin\Resources\AccountResource\Pages;
 
 use App\Filament\Admin\Helpers\Pages\BaseListRecordsPage;
 use App\Filament\Admin\Resources\AccountResource;
-use App\Models\Mship\Account;
 
 class ListAccounts extends BaseListRecordsPage
 {
