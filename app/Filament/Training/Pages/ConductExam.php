867a1ccc958a75008ca28b548719b307ed0940cc
Callum Axon <git@callumaxon.uk>
2025-07-10 19:47:10 +0100

    wip

--- app/Filament/Training/Pages/ConductExam.php
+++ app/Filament/Training/Pages/ConductExam.php
@@ -37,7 +37,7 @@ class ConductExam extends Page implements HasForms, HasInfolists
 
     public ?int $examId = null;
 
-    private ExamBooking $examBooking;
+    public ExamBooking $examBooking;
 
     public function mount(): void
     {
@@ -45,21 +45,39 @@ class ConductExam extends Page implements HasForms, HasInfolists
 
         $this->examBooking = ExamBooking::findOrFail($this->examId);
 
-        $existingAssessmentData = ExamCriteriaAssessment::where('examid', $this->examId)
-            ->get()
+        $examCriteriaAssessmentById = ExamCriteriaAssessment::where('examid', $this->examId)->get()
             ->mapWithKeys(
                 function ($item) {
                     return [
                         $item->criteria_id => [
-                            'grade' => $item->result,
+                            'grade' => $item->result ?? 'N',
                             'comments' => $item->notes,
                         ],
                     ];
                 }
-            )
-            ->toArray();
+            );
+
+        $existingAssessmentData = ExamCriteria::byType($this->examBooking->exam)
+            ->get()
+            ->mapWithKeys(
+                function ($item) use ($examCriteriaAssessmentById) {
+                    $existingAssessment = $examCriteriaAssessmentById->get($item->id);
+                    return [
+                        $item->critera_id => [
+                            'grade' => $existingAssessment['grade'] ?? 'N',
+                            'comments' => $examCriteriaAssessmentById->get($item->criteria_id)['comments'] ?? '',
+                        ],
+                    ];
+                }
+            );
 
         $this->form->fill(['form' => $existingAssessmentData]);
+        dd($this->form->getState());
+//        if (!$existingAssessmentData){
+//            $this->form->fill();
+//        } else {
+//            $this->form->fill(['form' => $existingAssessmentData]);
+//        }
     }
 
     protected function getHeaderActions(): array
@@ -83,6 +101,7 @@ class ConductExam extends Page implements HasForms, HasInfolists
                     TextEntry::make('Exam Start')->getStateUsing(fn () => $this->examBooking->startDate),
                     TextEntry::make('Exam End')->getStateUsing(fn () => $this->examBooking->endDate),
                     TextEntry::make('Exam Accepted At')->getStateUsing(fn () => $this->examBooking->time_taken),
+                    TextEntry::make('data')->getStateUsing(fn () => json_encode($this->data)),
                 ])
                 ->columns(3),
                 Section::make('Examiner Details')->schema([
@@ -96,14 +115,19 @@ class ConductExam extends Page implements HasForms, HasInfolists
                             $examiner = $this->examBooking->examiners->secondaryExaminer;
                             return $examiner ? "{$examiner->account->name} ({$examiner->account->id})" : 'N/A';
                         }),
+                    TextEntry::make('Trainee Examiner')
+                        ->getStateUsing(function ()  {
+                            $examiner = $this->examBooking->examiners->traineeExaminer;
+                            return $examiner ? "{$examiner->account->name} ({$examiner->account->id})" : 'N/A';
+                        }),
                 ])
-                ->columns(2),
+                ->columns(3),
             ]);
     }
 
     public function form(Form $form): Form
     {
-        $criteria = ExamCriteria::byType('TWR')->get();
+        $criteria = ExamCriteria::byType($this->examBooking->exam)->get();
 
         $criteriaComponents = $criteria->map(
             function (ExamCriteria $criteria) {
@@ -135,9 +159,39 @@ class ConductExam extends Page implements HasForms, HasInfolists
             }
         );
 
+        $additionalCommentsComponents = Fieldset::make('form.additional_comments')
+            ->label('Additional Comments')
+            ->schema([
+                RichEditor::make('form.additional_comments.comments')
+                    ->label('Comments')
+                    ->default('')
+                    ->disableToolbarButtons(['attachFiles', 'blockquote'])
+                    ->live(debounce: 1000)
+                    ->columnSpan(12)
+                    ->afterStateUpdated(fn () => $this->save()),
+            ])->columns(12);
+
+        $examResultComponents = Fieldset::make('form.exam_result')
+            ->label('Exam Result')
+            ->schema([
+                Select::make('form.exam_result.result')
+                    ->label('Result')
+                    ->options([
+                        'P' => 'Pass',
+                        'F' => 'Fail',
+                        'N' => 'Incomplete',
+                    ])
+                    ->default('N')
+                    ->required()
+                    ->columnSpan(3)
+                    ->live()
+            ])->columns(12);
+
         return $form
             ->schema([
                 ...$criteriaComponents,
+                $additionalCommentsComponents,
+                $examResultComponents,
             ])
             ->statePath('data');
     }
@@ -146,7 +200,7 @@ class ConductExam extends Page implements HasForms, HasInfolists
     {
         $formData = collect($this->form->getState())['form'];
 
-        $flattenedFormData = collect($formData)->map(
+        $flattenedFormData = collect($formData)->except(['additional_comments', 'exam_result'])->map(
             fn ($item, $key) => [
                 'criteria_id' => $key,
                 'grade' => $item['grade'],
@@ -167,7 +221,7 @@ class ConductExam extends Page implements HasForms, HasInfolists
                         'examid' => $this->examId,
                         'criteria_id' => $item['criteria_id'],
                         'result' => $item['grade'],
-                        'notes' => $item['comments'],
+                        'notes' => $item['comments'] ?? "",
                         'addnotes' => $item['comments'] ? true : false,
                     ],
                 );
