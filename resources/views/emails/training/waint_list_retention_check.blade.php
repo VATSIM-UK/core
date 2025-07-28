2da788aed2b468cd0abf4e09e438ea17ddd7de1b
Kristián Kunc <kristian.kunc@gmail.com>
2025-07-28 17:56:24 +0200

    update retention emails per requests

--- resources/views/emails/training/waint_list_retention_check.blade.php
+++ resources/views/emails/training/waint_list_retention_check.blade.php
@@ -2,9 +2,14 @@
 
 @section('body')
 
-    <p>In order to maintain your place on the {{ $waiting_list_name }} waiting list. You must confirm your spot by clicking
-        the link below. </p>
+    <p>Owing to high demand, VATSIM UK periodically checks that members on our waiting lists are still engaged and willing
+        to continue to wait for training. Should you fail to respond to this message within 7 days, you will be removed
+        from the {{ $waiting_list_name }} waiting list.</p>
 
-    <p><a href="{{ $retention_check_url }}">Confirm my place on the waiting list</a></p>
+    <p>In order to maintain your place on the {{ $waiting_list_name }} waiting list, you must click the link below.
+        Alternatively, if you have any questions, please raise a ticket for the attention of ATC Training at the VATSIM UK
+        Helpdesk: <a href="https://helpdesk.vatsim.uk">https://helpdesk.vatsim.uk</a></p>
+
+    <p><a href="{{ $retention_check_url }}">Confirm my place on the waiting list.</a></p>
 
 @stop
