e2076804aadec038808b46f81c894b5dfcbea2ca
CLC0609 <cobylc.121@gmail.com>
2025-07-31 19:57:26 +0100

    Removed navblade link

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -76,13 +76,6 @@
                                 <li><a href="{{ route('site.atc.heathrow') }}">Heathrow</a></li>
                             </ul>
                         </li>
-                        <li class="col-sm-12">
-                            <ul>
-                                <li class="divider"></li>
-                                <li class="dropdown-header">Hour Checker</li>
-                                <li><a href="{{ route('controllers.hour_check.area') }}">C1 Training Place</a></li>
-                            </ul>
-                        </li>
                     </ul>
                 </li>
 
