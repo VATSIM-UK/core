875e57a499732d1613059bdbc8dc7c866708015b
Liaely <170049417+Liaely@users.noreply.github.com>
2025-10-23 05:25:05 +0100

    Convert to markdown

---
+++ resources/views/site/training/S1-syllabus-body.php
@@ -0,0 +1,346 @@
+<x-markdown>
+# S1 Syllabus and Lesson Plans
+
+**Version 1.0**
+**Effective Date:** 01st November 2025
+
+---
+
+## Section 1 | Scope
+
+This document outlines the syllabus and lesson structure for VATSIM United Kingdom Division Observer to Student 1 controller training (OBS-S1).
+
+## Section 2 | Grading
+
+In a mentoring report, a student will be graded in accordance with the rubric in this section.
+The rubric approximately follows the first five objectives from the cognitive domain of Bloom's Taxonomy:
+
+- Remember
+- Understand
+- Apply
+- Analyse
+- Evaluate
+
+Not every student will precisely fit the definition for a single grade. The mentor's judgement and experience will be needed to decide which grade to award for a given competency.
+
+Before submission for practical examination, a student must have passed the S1 theory exam, have reached 'Good' in all competencies, and should have reached 'Test Standard' in most competencies.
+
+Alongside the guidance in other sections of this syllabus, mentors and instructors will need to use their judgement and experience to decide when it is appropriate to submit a student for practical examination.
+
+| Grade | Description |
+| --- | --- |
+| Not covered | The student has not covered this competency. |
+| Covered | Key facts and concepts can be recalled, with some understanding shown. If the student has demonstrated pracical application, it will be with significant mentor input. |
+| Developing | Knowledge is satisfactorily applied to practical situations with some mentor input. A strong understanding is shown, either explicity, or implicitly. |
+| Good | Routine situations are handled with little mentor input; more uncommon situations are handled with some mentor input. Understanding of multiple competencies is integrated either explicity or implicitly. |
+| Test standard | Most situations are handled with minimal mentor input. Appropriate consideration is given to the long term effects of actions. Decisions can be justified with an explaination as to why they chose to take it rather than an alternative. |
+
+## Section 3 | Theoretical Knowledge Syllabus
+
+The following list contains the theoretical content covered in the [S1 | Aerodrome Control](https://moodle.vatsim.uk/course/view.php?id=15) e-learning course.
+
+- Introduction to VATSIM
+    - VATSIM organisational structure, rules, and regulations
+    - ATC training and assessment
+    - Systems and software
+    - Connecting to VATSIM
+- Air Traffic Management
+    - Air law, procedures, and airspace classification
+    - Charts
+    - Flight rules and flight plans
+    - The aerodrome
+    - Air traffic control units
+- Meteorology
+    - Meteorological phenomena and reporting
+    - Altimetry
+    - Low visibility procedures
+- Equipment and System
+    - Radio communications
+    - Radiotelephony
+    - Text communications
+    - Coordination
+    - Data communications
+    - Surveillance
+    - Datablocks and the Departure/Arrival lists
+    - VATSIM callsign structure
+- Aircraft
+    - Principles of flight types of aircraft
+- Provision of Aerodrome Services
+    - Control of surface traffic
+    - Runway in use
+    - Provision of an Aerodrome Control Service
+    - VFR traffic
+
+## Section 4 | Course Aims
+
+### 4.1 Stand Allocation
+
+The student should be able to use the EuroScope arrival list and the UK Controller Plugin (UKCP) to assign stands.
+
+### 4.2 Aerodrome Movement Area
+
+The student should be able to:
+
+- Describe and state the uses of aprons, taxiways, and runways
+- Consider local taxiways and apron restrictions (width, runway infringement, etc) when issuing ground movement instructions
+
+### 4.3 Nomenclature
+
+The student should be able to identify the difference between an ICAO and IATA code.
+
+### 4.4 Aeronautical Charts
+
+The student should be able to:
+
+- Identify and find the following types of charts:
+    - Aerodrome chart
+    - Aircraft ground movement/parking/docking chart
+    - Standard Instrument Departure chats
+- Identify the following features on the above charts:
+    - Taxiways
+    - Location signs
+    - Mandatory signs
+    - CAT I holding points
+    - CAT II/III holding points
+    - Aerodrome Reference Point (ARP)
+    - Runway lengths
+    - Marked obstacles
+    - Taxiway hotspots
+    - Aerodrome elevation
+    - Communication frequencies
+
+### 4.5 Flight Planning
+
+The student should be able to:
+
+- State the purpose of a flight plan and how pilots file them
+- Amend a flight plan using EuroScope
+- Use the UK Standard Route Document and/or its associated UKCP function to identify and correct invalid flight plan routings up to the EGTT/EGPX FIR boundary
+- Allocate initial altitudes and levels for a given departure instruction (clearance from APC/TC/AC or as per a SID)
+- Verify and correct RFLs in accordance with the 'semi-circular' rule
+- State where the 'semi-circular' rule is used in a 'north-south' or 'east-west' configuration
+- Verify and correct incorrect RFLs which exceed inter-TMA and bordering FIR cruise level restrictions
+
+### 4.6 Standard Instrument Departures (SIDs)
+
+The student should be able to:
+
+- State the basic definition of a SID
+- Understand the difference between non-RNAV (conventional) and RNAV1 SIDs
+- Assign the correct SID for the filed route
+- Identify and manage out-of-date and incorrect SIDs
+
+### 4.7 Meteorology
+
+The student should be able to:
+
+- State a basic defition of:
+    - Wind
+    - Temperature
+    - Humidity
+    - Visibility
+    - Pressure
+- Comment on the change in wind velocity (speed and direction) from the surface to the 2000 ft wind (free winds)
+
+### 4.8 Altimetry
+
+The student should be able to:
+
+- State the definition of QNH
+- State the definition of QFE
+- State the difference between QNH and QFE
+- Issue QNH and QFE correctly to aircraft, noting the phraseology change with pressures below 1000 hPa.
+
+### 4.9 Meteorological Reporting - ATIS, METAR, and TAF
+
+The student should be able to:
+
+- State the function and purpose of an Automated Terminal Information System (ATIS)
+- State the function and purpose of a Meteorological Aerodrome Report (METAR)
+- State the function and purpose of a Terminal Aerodrome Forecast (TAF)
+- Set-up and ATIS with the correct 'vatatis' URL, and broadcast it via text and voice modalities
+- Decode basic METARs and TAFs
+
+### 4.10 Low Visibility Procedures (LVPs)
+
+The student should be able to:
+
+- State the purpose of Low Visibility Procedures, and the minima for their enforcement at their training aerodrome
+- Describe and practically demonstrate an understanding of the effect of LVPs on aerodrome operations
+- Modify and ATIS broadcast to show that LVPs are in force
+
+### 4.11 Phraseology and Radiotelephony (RT)
+
+The student should be able to:
+
+- Use the phonetic alphabet where appropriate
+- Correctly use the readability scale (1-5)
+- Issue instructions with phraseology detailed in the [S1 | Aerodrome Control](https://moodle.vatsim.uk/course/view.php?id=15) e-learning course and [CAP413](https://www.caa.co.uk/our-work/publications/documents/content/cap-413), including:
+    - Taxi instructions
+    - Level instructions
+    - Heading instructions
+    - Speed instructions
+    - Departure clearances (SIDs and non-standard departure instructions)
+    - Instructions to hold short of an active runway
+    - SSR operating instructions (squawk codes)
+    - Altimeter settings (QNH/QFE)
+    - Frequency changes
+- Recognise where the use of plain English may be more appropriate than standard RT
+
+### 4.12 Text communications
+
+The student should be able to:
+
+- Communicate with pilots via text
+- Contact a supervisor using the `.wallop` command, and provide them with appropriate information to resolve an issue
+- Use the private messaging function in EuroScope
+- Use text aliases
+
+### 4.13 Squawk Codes
+
+The student should be able to:
+
+- Explain the purpose of a squawk code
+- State the purpose of conspicuity, special purpose (`7500, 7600, 7700, 7010...`) and discrete squawk codes
+
+### 4.14 Controller Clients
+
+The student should be able to:
+
+- Install and configure EuroScope, the UK Controller Pack, and the UK Controller Plugin
+- Update the UK Sector File and the UK Controller Pack with each AIRAC cycle
+- Install and configure the TrackAudio client
+- Connect to the VATSIM network and to the SweatBox server
+- Record information appropriately in the departure and arrival list datablocks
+
+### 4.15 Areas of Responsibility
+
+The student should be able to:
+
+- Describe in detail the areas of responsibilities (as per the [S1 | Aerodrome Control](https://moodle.vatsim.uk/course/view.php?id=15) e-learning course and [CAP413](https://www.caa.co.uk/our-work/publications/documents/content/cap-413)) of:
+    - Ground Movement Planner (GMP)
+    - Ground Movement Control (GMC)
+    - Air Control (AIR)
+- Describe in basic terms the areas of responsibilities (as per the [S1 | Aerodrome Control](https://moodle.vatsim.uk/course/view.php?id=15) e-learning course and [CAP413](https://www.caa.co.uk/our-work/publications/documents/content/cap-413)) of:
+    - Approach and Intermediate/Final Director Controller (APC and INT/FIN)
+    - Terminal Control (TC)
+    - Area Control (AC)
+
+### 4.16 Runway in Use
+
+The student should be able to:
+
+- Select an appropriate runway for use - in the absence of a controller covering the duties of the AIR controller - based on:
+    - The surface wind
+    - The 2000 ft wind (free wind)
+    - Local restrictions (e.g. nearby aerodrome operations, preferential wind limits)
+- Describe and practically demonstrate the series of events associated with a runway change, including:
+    - Determining the last aircraft assigned to the previous runway
+    - Determining the first aircraft assigned to the new runway
+    - Re-clearing aircraft
+
+### 4.17 Safety
+
+The student should be able to use the 'red carpet rule' when issuing ground movement instructions to identify and manage potential conflicts
+
+### 4.18 VFR Traffic
+
+The student should be able to:
+
+- Request a VFR circuit and CTR exit/entry clearance from the controller covering the duties of the AIR controller
+- State the purpose of Visual Reference Points (VRPs) and CTR entry/exit lanes, and correctly includ these in clearances where appropriate
+
+### 4.19 Coordination
+
+The student should be able to:
+
+- State the purpose of coordination
+- Coordinate with the relevant controllers to facilitate the functions of GMP and GMC, including but not limited to:
+    - Prenotes
+    - Clearances
+    - Flow restrictions (MDIs, CTOTs, EOBTs)
+    - Runway changes
+- Handover to, and take-over from, another controller
+
+### 4.20 Workload Management and Temperament
+
+The student should be able to:
+
+- Prioritise communications
+- Maintain effective control during busy periods
+- Remain calm and professional on all communication media
+
+### 4.21 Flow Management Techniques
+
+The student should be able to:
+
+- State the purpose of a Calculated Take-Off Time (CTOT)
+- Describe the purpose of a Minimum Departure Interval (MDI)
+- Sequence aircraft on the ground to minimise departure delays
+- Understand the benefits of imposing flow control restrictions at aerodromes, such as:
+    - Reducing congestion on taxiways and at holding points
+    - Reducing the time that aircraft spend with their engines running and burning fuel
+    - The wider implications for en-route airspace
+
+## Section 5 | Lesson Plans
+
+The lesson plans presented below should be taught to students in the order that they are presented in this document. At the end of each lesson, if a learning objective/aim has not been covered or met, mentors are requested to note this in the 'additional comments' box of the mentoring report.
+
+There are times where it may be beneficial to repeat a specific lesson or set of lessons, or combine lessons. Mentors are encouraged to do this where it is beneficial to do so.
+
+At the beginning of each session, whether it is one of the lessons in this document, or an ad-hoc lesson, outline to the student what the objectives of the lesson are, and how we intend to achive them. This may include running though the 'Aims' section of the lesson plan. This may also be a simple recognition that the purpose of the lesson is to get a feel for where the student is, or to see what issue arise in the session. Nevertheless, the importance of a clear brief cannot be stressed enough.
+
+After the completion of lesson 4, a student should be in a good position to pass their exam. If further lessons are required after the completion of lesson 4, mentors should list the aims of any further ad-hoc mentoring sessions in the 'additional comments' box.
+
+These lessons have been designed to be used in conjunction with a specific SweatBox file, listed at the top of each lesson. Each lesson has different objectives that must be met in order to complete the lesson.
+
+The SweatBox files contain deliberate mistakes that can be used to demonstrate selected areas of the syllabus, and ultimately achieve the objectives set out for each lesson. For example, some aircraft in lesson 2 have flight planning errors in their routing, RFL, and initial fix.
+
+As a mentor, you can use the aircraft in the SweatBoxes to help you demonstrate the objectives of the lesson to the student. Some objectives may not be met practically, and therefore should be taught in the theory section of the session.
+
+### 5.1 Runway Configurations
+
+| Lesson Number | Manchester (EGCC) | London Stansted (EGSS) | Edinburgh (EGPH) |
+| --- | --- | --- | --- |
+| 1 | 23 singles | 22 | 24 |
+| 2 | 23 duals   | 22 | 24 |
+| 3 | 05 singles | 04 | 06 |
+| 4 | 23 singles | 22 | 24 |
+
+### 5.2 Lesson 1
+
+| | Basic Aerodrome Control 1 |
+| --- | --- |
+| Environment | SweatBox |
+| Duration | 1 - 1.5 hours |
+| Objectives | 1. Connect to SweatBox</br>2. Discuss the roles and aeras of responsibilities of the GMP, GMC, and AIR controllers</br>3. Select a runway for use</br>4. Talk though the aerodrome layout and 'standard' taxi routings</br>5. Practise coordinating an initial logon to the network</br>6. Generate an ATIS</br>7. Explain the use of status blocks and scratch pads. Give a brief introduction to how scratchpads can be used, but emphasiese that the student can use them however they want</br>8. Explain and practise issing IFR clearances and mandatory readback items</br>9. Explain and practise using push-back and start-up instructions</br>10. Explain and practise issuing taxi instructions</br>11. Define and practise using the correct handoff procedure |
+| Student notes and resources | [S1 - Aerodrome Control e-learning course](https://moodle.vatsim.uk/course/view.php?id=15)</br>[Aeronautical Chart Symbols](https://www.aurora.nats.co.uk/htmlAIP/Publications/2021-01-28-AIRAC/html/eAIP/EG-GEN-2.3-en-GB.html#GEN-2.3) |
+
+### 5.3 Lesson 2
+
+| | Basic Aerodrome Control 2 |
+| --- | --- |
+| Environment | SweatBox |
+| Duration | 1 - 1.5 hours |
+| Objectives | Navigate to the eAIP and discuss the different chart types and their features.</br>2. Introduce basic altimetry - definitions of QNH and QFE and where they are used</br>3. Discuss the basic area of responsibilities of adjacent control positions (AIR, APC, FIN, INT, TC, and AC), and what they do, how they appear in EuroScope, and examples of when you might co-ordinate with them</br>4. Highlight any lateral and vertical flight plan restrictions specific to the student's training aerodrome</br>5. Recognise errors in flight plans such as initial routings for a SID, RFL for direction of flight, RFL for standing agreements</br>6. Explain and practise using conditional clearances</br>7. Introduce VFR clearances for the circuit and CTR entry/exit</br>8. Consolidate objectives from previous lessons |
+| Student notes and resources | [S1 - Aerodrome Control e-learning course](https://moodle.vatsim.uk/course/view.php?id=15)</br>[Standard Route Document (SRD)](https://nats-uk.ead-it.com/cms-nats/opencms/en/Publications/digital-datasets/)</br>[UKCP SRD search](https://ukcp.vatsim.uk/srd-routes)</br>[eAIP](https://nats-uk.ead-it.com/cms-nats/opencms/en/Publications/AIP/) |
+
+### 5.4 Lesson 3
+
+| | Advanced Aerodrome Control |
+| --- | --- |
+| Environment | SweatBox |
+| Duration | 1 - 1.5 hours |
+| Objectives | 1. Introduce low visibility procedures and talk though the local procedures associated with them</br>2. Introduce METARs and TAFs, and practise decoding simple examples</br>3. Introduce non-standard IFR clearances (airway departures, vectored departure) and practise issuing them.</br>4. Introduce the use of the command line - aliases, private messaging, function keys</br>5. Consolidate objectives from previous lessons |
+| Student notes and resources | [S1 - Aerodrome Control e-learning course](https://moodle.vatsim.uk/course/view.php?id=15)</br>[Standard Route Document (SRD)](https://nats-uk.ead-it.com/cms-nats/opencms/en/Publications/digital-datasets/)</br>[UKCP SRD search](https://ukcp.vatsim.uk/srd-routes)</br>[eAIP](https://nats-uk.ead-it.com/cms-nats/opencms/en/Publications/AIP/)</br>[METAR bank](https://docs.google.com/spreadsheets/d/1WfveAfGmAg_qFpZjx5k55XKTnVqtxlJTN_Vf9XwPtNM/edit?usp=sharing) |
+
+### 5.5 Lesson 4
+
+| | Aerodrome Control Consolidation |
+| --- | --- |
+| Environment | SweatBox |
+| Duration | 1 - 1.5 hours |
+| Objectives | 1. Discuss the purpose of flow management techniques including the use of CTOTs and MDIs</br>2. Discuss the benefits of introducing flow control</br>3. Introduce the use of flow management techniques during high traffic scenarios</br>4. Review the course aims and consolidate knowledge in preparation for the exam</br>5. In most situations forward to exam, even if you are unsure as to whether they will pass (if you think they will more likely fail then do one further consolidation session) |
+| Student notes and resources | [S1 - Aerodrome Control e-learning course](https://moodle.vatsim.uk/course/view.php?id=15)</br>[Aeronautical Chart Symbols](https://www.aurora.nats.co.uk/htmlAIP/Publications/2021-01-28-AIRAC/html/eAIP/EG-GEN-2.3-en-GB.html#GEN-2.3)</br>[Standard Route Document (SRD)](https://nats-uk.ead-it.com/cms-nats/opencms/en/Publications/digital-datasets/)</br>[UKCP SRD search](https://ukcp.vatsim.uk/srd-routes)</br>[eAIP](https://nats-uk.ead-it.com/cms-nats/opencms/en/Publications/AIP/) |
+
+</x-markdown>
