b556af1d5e2bfd9c454a174aca615d9b5226d0e4
CLC0609 <cobylc.121@gmail.com>
2025-11-04 09:41:34 +0000

    Start of the syllabus

---
+++ resources/views/site/policy/training-process/s3-syllabus-body.blade.php
@@ -0,0 +1,243 @@
+<x-markdown>
+# S3 Syllabus and Lesson Plans
+
+**Version 1.0**
+**Effective Date:** 01st November 2025
+
+---
+
+## Scope
+This document details the training syllabus for VATSIM UK Student 3 controller training (S2->S3).
+
+---
+
+## Section 1 | Introduction
+This syllabus details the competencies and content covered by the S3 training course in VATSIM UK. Students
+will meet the learning objectives through independent study of material provided in eLearning courses and
+traditional documentation, supported by mentors and instructors in one-to-one and group teaching
+sessions. Students are encouraged to ask questions in the [#app_students channel](https://discord.com/channels/705488311346790471/1044164318285004830/1044587766383648881) on [Discord](https://www.vatsim.uk/discord)
+
+---
+
+## Section 2 | Theoretical Knowledge Course
+The [S3 | Aerodrome Control](https://moodle.vatsim.uk/course/view.php?id=54) e-Learning course covers the theoretical content of the S3 course, which
+includes the following areas of knowledge:
+
+### 2.1 Introductory Concepts
+- The role of an Approach controller
+- Procedural vs Radar Separation
+- Airspace structure and management
+
+### 2.2 Altimetry and Meteorology
+- Basic Altimetry
+- Effects on Winds on aircraft
+- Meteorological Hazards
+
+### 2.3 Navigation
+- SIDs
+- STARs
+- Navigation aids
+- Holding fixes
+
+### 2.4 Approach Control Principles
+- Air Traffic Control Services
+- Types of Approach
+
+### 2.5 Equipment and Systems
+- Understanding Radar
+- Transponders and Squawks
+
+### 2.6 Identification, Validation and Verification
+- Identification
+- Validation
+- Verification
+
+### 2.7 Appreciation of Aircraft Performance
+- Aircraft Types and ‘average’ performance
+- Turning Aircraft
+- Climbing and Descending Aircraft
+- Wake Turbulence and Approach Spacing Considerations
+
+### 2.8 Separation Standards
+- Speed Separation
+- Lateral/Vertical Separation
+
+### 2.9 Applying and Maintaining Separation
+- Use of Headings
+- Paralleling
+- Streaming/Speed Control
+- Vertical Separation
+- Ensuring Separation
+
+### 2.10 Coordination
+- Coordination of releases
+- Providing departure instructions
+- Coordinating Aerodrome Control
+- Coordination with other Approach controllers
+
+### 2.11 Holding
+- Holding Patterns
+- Hold Separation
+- Descending aircraft in holds
+- Exiting the Hold
+
+### 2.12 Sequencing and Vectoring Traffic
+- Establishing a sequence
+- Use of Speed Control
+- Continuous Descent Arrivals
+- Establishing Aircraft on Approaches
+
+### 2.13 Missed Approach Procedures
+- Missed Approaches
+
+### 2.14 UK Flight Information Services (UKFIS)
+- UKFIS General
+- Basic Service
+- Traffic Service
+- Deconfliction Service
+- Crossers, Joiners and Leavers
+- SVFR
+
+### 2.15 Non-Compliance
+- Dealing With Non-Compliant Aircraft
+
+---
+
+## Section 3 | Block Training
+S3 Training now incorporates block training as a way to standardise the Approach/Radar training process
+and ensure all items are covered.
+
+Students should complete all blocks before being forwarded for a practical exam unless an exception is
+required (e.g., fast tracks). The block training system comprises of three blocks; students are only taught
+competencies in their block until completing the content of each and then they complete the end of block
+exam.
+
+DIAGRAM GOES HERE
+
+### 3.1 End of Block Exams
+
+The End of Block Exam for Block 1 shall be carried out on the Sweatbox server to avoid having traffic that is
+not in the syllabus for that block. For Block 2 and 3 the exam can be performed either on the network
+(preferred) or the Sweatbox server. The decision of network or Sweatbox mock exam should take into
+consideration traffic levels at a particular aerodrome at a particular time of day.
+
+The instructor running end of block exams shall attempt to ensure that the majority if not all the criteria in
+each block is assessed and should note criteria that has not been assessed and use their judgement and
+experience to decide whether it is necessary to do training sessions on a topic that has not been assessed.
+
+As normal if a student is deemed ready for the formal S3 Examination then they will be forwarded by the
+relevant training staff and the real exam will be conducted as stipulated in the ATC Training Handbook.
+
+### 3.1.1 CTS Permissions
+- End of Block Exam 1 - APP_B1
+- End of Block Exam 1 - APP_B2
+- End of Block Exam 1 - APP_B3
+
+### 3.1.2 First Session Guidance
+It is widely regarded that the learning curve from S2 to S3 is the steepest that students will undergo
+throughout their ATC Training tenure in VATSIM UK. Thus, mentors must ensure that students are
+theoretically prepared for their training and set realistic practical expectations for the first few mentoring
+sessions. Below is a list of do’s and don’ts for the first S3 mentoring session:
+
+### **DO:**
+- Ensure student’s Euroscope is correctly set up for the position they are training on. It is highly
+recommended to have turned on:
+    - RMA, where applicable
+    - SMAA
+    - Airspace bases
+    - Holding fixes/release points
+    - Any other symbology that aids learning (e.g. EDI/GLA Buffer)
+- Discuss and check baseline theoretical knowledge from Block 1; A student that is knowledgeable and prepared will find the step-up considerably easier.
+- Set realistic goals for students. Block 1 is a large chunk of the training so it will likely take a good number of sessions to master the competencies in the block.
+- Pause the session and fix mistakes early. The quicker a student understand why they have made a mistake, the more likely they are to grasp trickier concepts quicker in the future.
+
+### **DONT:**
+
+- Simply put the student straight into a Sweatbox session without having prepared them for it. Their confidence will be damaged when mistakes are made; this precious time to repair.
+- Bring in more advanced competencies from other blocks. Stick to teaching Block 1 competencies until the student has mastered them.
+- Expect the student to know everything. The theoretical training resources are a great tool for preparation, but they cannot prepare students for handling the real thing. Guide students on where to find information and discuss **why** mistakes are made.
+
+---
+
+## Section 4 | Syllabus
+
+### 4.1 Block 1
+Block 1 focuses on the core of approach/radar controlling and a list of items covered along with useful links are found below:
+- EuroScope Use
+    - Mode A+C (Not Easy mode)
+    - Tag Management
+    - Contacting aircraft from unstaffed airspace and assigning a squawk using UKCP
+    - Aircraft Select with keyboard (ASEL/Numpad +)
+    - Knows how to enable and disable features not normally displayed (SMAA, airways, etc.)
+- Airspace Classification
+- R/T
+    - Phraseology
+    - Frequency Prescence and attitude
+    - Frequency Management
+- Identification, Validation, Verification (IVV)
+- Vectoring and Descents and establishing on an ILS approach
+    - Multiple streams of traffic and creative vectoring and use of airspace
+    - Levels selected and lowest available and reasonable
+- Speed Control
+- Forward Planning
+- Ensuring separation
+- Local airfield knowledge (vMATS Part 2 GEN, APC & LOW Sections)
+- Handovers (PRAWNS)
+Where the concept of an item is based heavily in theory, links to sources will be made available.
+
+### 4.1.1 Euroscope use
+
+#### 4.1.1.1 Euroscope use
+During S3 Training, for students, Mode A+C Correlation (C-Mode) is **mandatory**. Mentors should check this is enabled.
+This can be checked in Other Set > General settings > Correlation mode > C-Mode
+
+#### 4.1.1.2 Tag Management 
+Approach Tags are NODE and NOVA 9000. Students should be able to use these tags and update them with every transmission.
+By default, training aerodromes use:
+- NODE for EGKK/EGSS
+- NOVA 9000 for all other aerodromes
+
+#### 4.1.1.3 Distance and Separation tools
+EuroScope distance and separation tools can be accessed by pressing F1 then d/s or by typing. distance or .sep. They can also be accessed by left clicking the buttons below.
+
+#### 4.1.1.4 Contacting and identifying aircraft from unstaffed airspace
+Students should be able to send “contact me’s” to aircraft from unstaffed airspace and complete IVV, if a squawk is not assigned, they should know how to assign squawks using UKCP.
+Aircraft not on the sector inbound list that are uncorrelated can be assumed two ways:
+1. They can be manually correlated by clicking on the target and typing their callsign and then clicking assume.
+2. They can be assumed while uncorrelated by typing a part of their callsign and pressing ASEL (Numpad +) and pressing F3 then ASEL again.
+
+#### 4.1.1.5 Aircraft Select (ASEL)
+EuroScope allows for tag manipulation with keyboard commands which also allows tags to
+be manipulated while uncorrelated.
+
+ASEL key (as default the numeric PLUS keyboard) - This is the aircraft selector key. To use
+there should be some letters typed from the callsign. Pressing this button, the first callsign
+that matches the letters typed before will be selected. You can press the ASEL key again to
+select the next match. You can do it in a loop to see all matching callsigns.
+
+To assume an uncorrelated target:
+1. Ensure chat bar is empty and type in their callsign
+2. Press ASEL (Numpad +)
+3. Press F3
+4. Press ASEL (Numpad +)
+
+This then populates the sector exit list as the target is assumed, which allows for squawk generation.
+
+If you do not have a numpad you may change the key to something else in: Other SET > General settings > Aircraft select key
+
+#### 4.1.1.6 Other Useful Features
+There are many things that can be enabled on the display settings.
+- SMAA can be enabled in SIDS
+- SMAA levels can be enabled for some airports in free text
+- RMA can be changed in STARS
+- Low and High Airways can be enabled for reference and to familiarise with different restrictions
+
+Most of the time relevant information is enabled by default, pressing F1 and then number 1-9 will cycle between different radars.
+
+Radar Screens:
+1. Normal radar map, no fixes
+2. Fixes, no labels
+3. Bases of CAS
+4. Fixes and VRPs with labels
+
+</x-markdown>
