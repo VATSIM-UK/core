8ebf991ef0801eae8eade77e9b837cd3ab9e2bd8
CLC0609 <cobylc.121@gmail.com>
2025-12-12 10:23:54 +0000

    Initial Setup

---
+++ resources/views/site/policy/training-process/c1-syllabus-body.blade.php
@@ -0,0 +1,12 @@
+<x-markdown>
+# Syllabus and Lesson Plans
+
+**Version 1.0**  
+**Effective Date:** 12th December 2025
+
+---
+
+## Scope
+This document details the training syllabus for VATSIM UK Controller 1 training (S3->C1)
+
+---
--- tests/Feature/Site/PolicyPagesTest.php
+++ tests/Feature/Site/PolicyPagesTest.php
@@ -60,4 +60,9 @@ class PolicyPagesTest extends TestCase
     {
         $this->get(route('site.policy.training.s2-syllabus'))->assertOk();
     }
+    #[Test]
+    public function test_it_loads_the_c1_syllabus()
+    {
+        $this->get(route('site.policy.training.c1-syllabus'))->assertOk();
+    }
 }
