61a5e8b9ae723fb9eb71681543ceec0ca621ab3e
Coby Chapman <cobylc.121@gmail.com>
2025-12-13 15:16:14 +0000

    Fixes #4396 - Convert C1 Training Syllabus to markdown in Core. (#4433)
    * Initial Setup
    
    * Added syllabus text
    
    * Fix styling issue
    
    * Added blank lines around all the headings

---
+++ resources/views/site/policy/training-process/c1-syllabus-body.blade.php
@@ -0,0 +1,299 @@
+<x-markdown>
+# Syllabus and Lesson Plans
+
+**Version 1.0**  
+**Effective Date:** 12th December 2025
+
+---
+
+## Scope
+
+This document details the training syllabus for VATSIM UK Controller 1 training (S3->C1)
+
+---
+
+## Section 1 | Introduction
+
+The syllabus details the competencies and content covered by the C1 training course in VATSIM UK. Students
+will meet the learning objectives through independent study of material provided in eLearning courses and
+traditional documentation, supported by mentors and instructors in one-to-one and group teaching sessions.
+
+## Section 2 | Grading
+
+In a mentoring report, a student will be graded in accordance with the rubric in this section.
+The rubric approximately follows the first five objectives from the cognitive domain of Bloom's Taxonomy:
+
+- Remember
+- Understand
+- Apply
+- Analyse
+- Evaluate
+
+Not every student will precisely fit the definition for a single grade. The mentor's judgement and experience will be needed to decide which grade to award for a given competency.
+
+Before submission for practical examination, a student must have passed the S1 theory exam, have reached 'Good' in all competencies, and should have reached 'Test Standard' in most competencies.
+
+Alongside the guidance in other sections of this syllabus, mentors and instructors will need to use their judgement and experience to decide when it is appropriate to submit a student for practical examination.
+
+| Grade | Description |
+| :--- | :--- |
+| Not covered | The student has not covered this competency. |
+| Covered | Key facts and concepts can be recalled, with some understanding shown. If the student has demonstrated pracical application, it will be with significant mentor input. |
+| Developing | Knowledge is satisfactorily applied to practical situations with some mentor input. A strong understanding is shown, either explicity, or implicitly. |
+| Good | Routine situations are handled with little mentor input; more uncommon situations are handled with some mentor input. Understanding of multiple competencies is integrated either explicity or implicitly. |
+| Test standard | Most situations are handled with minimal mentor input. Appropriate consideration is given to the long term effects of actions. Decisions can be justified with an explaination as to why they chose to take it rather than an alternative. |
+
+---
+
+## Section 3 | Syllabus
+
+### 3.1 Self-taught Competencies
+
+#### 3.1.1 Releases
+
+**Source: Examples of Coordination Document**
+
+The student should understand the following releases:
+- Radar Release
+- Full Release
+- Abbreviated Release
+- Radar Handover
+
+#### 3.1.2 Standing Agreements
+
+**Source: Agreed levels diagram**
+
+The student should know the agreed levels relevant to their sector.
+
+#### 3.1.3 Standard Routings
+
+**Source: Charts**
+
+The student should know the SIDs and STARs relevant to their sector, including any restrictions and where they
+are appropriate.
+
+#### 3.1.4 Enroute Holding
+
+**Source: Charts**
+**Source: ENR 3.6**
+
+The student should be able to describe any published enroute hold.
+
+#### 3.1.5 Altimetry
+
+**Source: CAP493**
+
+Students should be able to define the following terms:
+- Minimum Stack Level
+- Transition Level
+- Transition Altitude
+Students should be able to give the minimum stack level for the airfields and/or TMAs under their control.
+Given a pressure setting, they should also be able to convert between altitudes and flight levels.
+
+#### 3.1.6 Identification, Validation and Verification (Theory)
+
+**Source: CAP493, Section 1, Chapter 6 – 1, 2, 3, 4, 9**
+
+Students should be able to:
+- Identify the methods for identification by primary surveillance radar and how to achieve them
+- Identify the methods for identification by secondary surveillance radar and how to achieve them
+- When to inform aircraft that they are identified
+- Name the surveillance services and the conditions under which they are used
+- Explain the validation of Mode A codes
+- Explain the verification of Mode C codes
+- Identify the purpose of conspicuity squawks, listening squawks and where to find them
+
+#### 3.1.7 Flight Information Services
+
+**Source: CAP774**
+
+Students should be able to explain the provisions for each of: Basic Service, Traffic Service and Deconfliction
+Service. They also should be able to explain: applicable flight rules, surveillance or non-surveillance, terrain
+clearance, responsibility for maintaining separation. For the Basic Service, they should be able to explain the
+principle of “Duty of Care”. They should be aware that the procedural service exists but do not need to explain
+it any detail.
+
+### 3.2 Learning Objectives (LO)
+
+#### 3.2.1 Professionalism and Attitude
+
+The student remains calm and courteous at all times, even when a pilot or adjacent controller makes a
+mistake.
+
+#### 3.2.2 Use of Standard Radiotelephony
+
+Instructions are given using standard RT. They are unambiguous, decisive and do not contain superfluous
+words that add nothing to the value of the transmission.
+
+#### 3.2.3 Pace and Clarity
+
+The pace of transmission is consistent, and the student clearly demonstrates that they plan what they’re going
+to say before the say it. The student makes good use of alias files for prompt transmissions.
+
+#### 3.2.4 Use of Datablocks
+
+If the student elects not to use certain aspects of the datablock with a justified reason, they should be able to
+explain the conventions for using them. Students are, however, encouraged to use scratchpads and other
+aspects to aid their own situational awareness.
+
+The student utilises the heading, altitude and speed blocks and keeps them promptly up to date. When using
+soft speeds, they follow the standard conventions.
+
+### 3.2.5 Identification, Validation and Verification (SSR)
+
+This criterion does not consider aircraft that are deemed identified by the validation of previously assigned
+squawk code (i.e. transfer from a previous controller).
+
+The student identifies and validates aircraft, consistently and correctly verifying its Mode C readout.
+
+#### 3.2.6 Climbs and Descents with respect to Local Airspace
+
+The student keeps aircraft within controlled airspace and their sector and does not issue any instructions that
+could lead to this not being the case.
+
+#### 3.2.7 Presentation of Traffic
+
+The student demonstrates an understanding of the next controller’s requirements and manoeuvres aircraft
+such that they are in an optimal position for onward travel. For example, they may position aircraft in parallel,
+positioned in accordance with the direction that they will eventually turn further down route. An alternative
+example would be the controller “starting off” headings to place the aircraft in an optimal position for the next
+control - i.e. the beginning of a parallel stream.
+
+#### 3.2.8 Standing Agreements
+
+The student demonstrates an understanding of uncommon standing agreements. Examples of uncommon
+agreements are those that are rarely used, those that vary depending on the situation (e.g. aircraft type
+specific agreements into Paris) or involve multiple components (e.g. headings towards a certain fix or side of
+an airway).
+
+#### 3.2.9 Ensuring Separation
+
+The student consistently ensures separation, especially in regard to making sure headings or speeds have
+worked before removing vertical separation. In the event that they give an unsafe instruction, it is immediately
+corrected. If challenged over whether a particular situation is a failure to ensure separation (whether it is or
+not), the student gives a confident, justified answer.
+
+#### 3.2.10 Issuing of STARs
+
+The student re-assigns a STAR on the occasion that the pilot has filed a routing for a STAR that is inappropriate
+for their flight. For example, filing for a low-level STAR from high level. The student also understands and
+demonstrates how to use “stack-swap” STARs.
+
+#### 3.2.11 Coordination with Approach Units
+
+Examples of coordination: Releases, radar handovers, non-standard flights.
+All coordination is done where appropriate and is clear and concise.
+
+#### 3.2.12 Coordination with Enroute Units
+
+Examples of coordination: Releases, non-standard flights, coordinating a higher level when handoff is delayed,
+coordinating shortcuts.
+
+All coordination is done where appropriate and is clear and concise. In cases where verbal coordination is
+inappropriate, the student makes use of their controller clients inbuilt coordination functionality.
+
+#### 3.2.13 Provision of Top-down Control
+
+The student demonstrates the ability to reduce their level of top-down control in accordance with GRP in
+order to manage high workload.
+
+#### 3.2.14 Use of Vectors for Sequencing and Reduced Separation
+
+The student demonstrates parallel streaming, ensuring that horizontal separation is ensured before removing
+vertical separation.
+
+#### 3.2.15 Use of Speed Control
+
+The student applies appropriate speeds, reducing them in sufficient time for a holding facility or cancelling
+them promptly when no longer relevant.
+
+#### 3.2.16 Use of Enroute Holding
+
+The student descends aircraft in the stack promptly, keeping periods of gaps to a minimum. The student
+demonstrates good situational awareness by judging the next controller’s workload and presenting them with
+new aircraft in good time at appropriate levels.
+
+#### 3.2.17 Presentation of Aircraft into Holding Facilities
+
+Aircraft are presented to the approach units safely, with a suitable distance between them (no less than 6nm,
+where specific distance agreements are not in force).
+
+#### 3.2.18 Contacting Aircraft from Unmanned Airspace
+
+Aircraft are contacted before entering the students sector. Aircraft that have been sent a contactme are
+assumed by the student so that adjacent controllers are aware of who is trying to contact the aircraft.
+
+#### 3.2.19 Efficiency
+
+The student keeps aircraft climbing and descending wherever possible, reducing periods of level flight as far as
+practical.
+
+#### 3.2.20 Proactivity
+
+The student notices potential conflicts well in advance and uses techniques such as headings to resolve the
+conflict as quickly as possible, thus maximising expedition.
+Examples of measures to expedite resolution include, but are not limited to, use of headings to reduce
+separation and direct routings to avoid conflicts.
+“Conflicts” here are not defined as safety related but are broadly defined as an aircraft being prevented from
+reaching an objective (for example, a climb target) because another aircraft is blocking their way.
+
+#### 3.2.21 Prioritisation of Traffic
+
+The student identifies time critical instructions (such establishing on the localiser when providing top-down)
+and prioritises them above others. The student also makes use of “when ready” clearances in gaps between
+time-critical instructions, to avoid the workload later.
+
+#### 3.2.22 Appreciation of Aircraft Performance
+
+The student appreciates aircraft performance by giving descent instructions at appropriate times and applying
+speeds appropriate to the descent being attempted.
+
+### 3.3 Mentoring Report Structure
+
+#### 3.3.1 Frequency Presence
+
+- Professionalism and Attitude
+- Use of Standard Radiotelephony
+- Pace and Clarity
+
+#### 3.3.2 Setup and Configuration
+
+- Use of Datablocks
+
+#### 3.3.3 Identification, Validation and Verification
+
+- Identification, Validation and Verification (SSR)
+
+#### 3.3.4 Basic Enroute Control
+
+- Climbs and descents with respect to local airspace
+- Presentation of traffic
+- Standing agreements
+- Ensuring separation
+- Issuing of STARs
+
+#### 3.3.5 Coordination
+
+- Coordination with approach units
+- Coordination with enroute units
+
+#### 3.3.6 Top down
+
+- Provision of top-down control
+
+#### 3.3.7 Streaming and Holding
+
+- Use of vectors for sequencing and reduced separation
+- Use of speed control
+- Use of enroute holding
+- Presentation of aircraft into holding facilities
+
+#### 3.3.8 Situational Awareness
+
+- Contacting Aircraft from unmanned airspace
+- Efficiency
+- Proactivity
+- Prioritisation of traffic
+- Appreciation of aircraft performance
+
+</x-markdown>
