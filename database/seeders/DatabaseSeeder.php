a8e8751279cabdadbdec176bda2c51c2f63ecea0
CLC0609 <cobylc.121@gmail.com>
2026-01-11 13:40:57 +0000

    Also rollback another change I made for the seeders to work

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(RolesAndPermissionsSeeder::class);
-        $this->call(CtsExamSeeder::class);
-        $this->call(PositionsAndEndorsementsSeeder::class);
+        $this->call(RolesAndPermissionsSeeder::class);
     }
 }
