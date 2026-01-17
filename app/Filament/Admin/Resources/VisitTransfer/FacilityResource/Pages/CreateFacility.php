2a0a488b59bc8efae00d1557b72a7f7ea2940585
William Shaw <williamrockstar110@gmail.com>
2026-01-17 20:17:05 +0000

    remove unused import

--- app/Filament/Admin/Resources/VisitTransfer/FacilityResource/Pages/CreateFacility.php
+++ app/Filament/Admin/Resources/VisitTransfer/FacilityResource/Pages/CreateFacility.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Admin\Resources\VisitTransfer\FacilityResource\Pages;
 
 use App\Filament\Admin\Resources\VisitTransfer\FacilityResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateFacility extends CreateRecord
