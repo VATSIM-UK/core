bc10d3298a5e79df1baa47851386fecc349b2e4c
CLC0609 <cobylc.121@gmail.com>
2026-01-25 19:50:20 +0000

    chore: spelling error

--- resources/views/mship/waiting-lists/index.blade.php
+++ resources/views/mship/waiting-lists/index.blade.php
@@ -4,7 +4,7 @@
 <div>
     <p>This page shows the waiting lists you are currently in. Please note that any flags or criteria for being eligible on a waiting list are only calculated every 24 hours.</p>
     <p>You will only show on a waiting list if you meet the activity and experience hour requirements as set out in ATC Training Handbook section 5.12.1, 5.13.1 and 5.14.1. If your position shows this does NOT guarantee eligibility for a training place. You must have completed the relevant CTS Theory exam in order to be offered a training place. The full criteria can be found in the ATC Training Handbook sections linked above.</p>
-    <p>You can self-enrol onto waiting lists you are eligible for, with the exception of our Heathrow endorsement lists. If you cannot join a waiting list and believe you should be able to do so, please contact the appropriate department on the <a href="https://helpdesk.vatsim.uk/">helpdesk</a>.</p>
+    <p>You can self-enrol onto waiting lists you are eligible for, with the exception of our Heathrow endorsement list. If you cannot join a waiting list and believe you should be able to do so, please contact the appropriate department on the <a href="https://helpdesk.vatsim.uk/">helpdesk</a>.</p>
     <p>Please note that some waiting lists have capacity constraints and may not be available to join.
 </div>
 
