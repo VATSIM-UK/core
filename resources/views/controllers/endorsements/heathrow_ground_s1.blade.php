88ce63e8a2cd82ca2715c34d9850e18a9748ab20
CLC0609 <cobylc.121@gmail.com>
2026-01-25 19:49:31 +0000

    chore: add UK to restriction

--- resources/views/controllers/endorsements/heathrow_ground_s1.blade.php
+++ resources/views/controllers/endorsements/heathrow_ground_s1.blade.php
@@ -33,7 +33,7 @@
                             You must be active on the controller roster
                         </li>
                         <li>
-                            You must have controlled for at least 50 hours on any Ground or Delivery position
+                            You must have controlled for at least 50 hours on any UK Ground or Delivery position
                         </li>
                     </ul>
                     <h4>Step Two</h4>
