2728630333ff6c0d48d66dcd55e0be3b6f832b09
CLC0609 <cobylc.121@gmail.com>
2026-01-26 17:09:50 +0000

    chore: add migration to delete gatwick ground student and mentor role

---
+++ database/migrations/2026_01_25_193141_remove_gatwick_gnd_mentor_and_student_roles.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $roles = [
+            'Gatwick GND Students',
+            'ATC Mentor (Gatwick GND)',
+        ];
+
+        foreach ($roles as $roleName) {
+            $roleId = DB::table('mship_role')
+                ->where('name', $roleName)
+                ->value('id');
+
+            DB::table('mship_account_role')
+                ->where('role_id', $roleId)
+                ->where('model_type', 'App\\Models\\Mship\\Account')
+                ->delete();
+
+            DB::table('mship_role_permission')
+                ->where('role_id', $roleId)
+                ->delete();
+
+            DB::table('mship_role')
+                ->where('id', $roleId)
+                ->delete();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
