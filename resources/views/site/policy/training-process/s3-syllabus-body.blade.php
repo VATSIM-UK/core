8305f703829f6f53c7bb3b76589fae4e8cd8c828
Coby Chapman <cobylc.121@gmail.com>
2026-02-02 14:15:30 +0000

    chore: add initial text to the syllabus

--- resources/views/site/policy/training-process/s3-syllabus-body.blade.php
+++ resources/views/site/policy/training-process/s3-syllabus-body.blade.php
@@ -4,4 +4,161 @@
 **Version 1.0**  
 **Effective Date:** 12th December 2025
 
+---
+
+## Scope
+
+This document outlines the Senior Student (S3) training syllabus for students training in VATSIM UK.
+
+---
+
+## Scope
+
+This document is only to be used in conjunction with the S3 training scheme of the VATSIM UK Training Department and should never be adopted for real world use.
+
+The information published by VATSIM UK within this document is made available without warranty of any kind; the Organization accepts no responsibility or liability whether direct or indirect, 
+as to the currency, accuracy or quality of the information, nor for any consequence of its use.
+
+---
+
+## Introduction
+
+The syllabus details the competencies and content covered by the S3 training course in VATSIM UK. Students will meet the learning objectives through independent study of material provided in eLearning courses and traditional documentation, supported by mentors and instructors in one-to-one and group teaching sessions. Students are encouraged to ask questions in the #app_students channel on Discord.
+
+## Pre-Course Learning
+
+In advance of students beginning a course of practical training, they are expected to have completed the relevant theory examination and attended a pre-course seminar, which will serve to provide the basis for theoretical knowledge to be developed upon during a course of practical training. VATSIM UK’s S3 Moodle course should form the basis for much of the theoretical knowledge required for the rating.
+
+As is outlined in the relevant ATC Training Policy, completion of the S3 theory exam is a condition of the award of a training place. Should a prospective student fail to attend a pre-course seminar, they will be expected to recover their own theoretical knowledge before starting a practical training course.
+
+The pre-course seminar will cover the following content:
+- Course overview
+    - Block structure
+    - Course progression
+    - Theoretical Requirements
+- Needs, Hopes and Expectations
+    - Expectations of students
+    - Requirements to stay current
+- Supporting Documentation
+    - MATS Part 1
+    - vMATS
+    - CAP413
+    - Discord Training Fora     
+    - S3 Moodle Course
+    - Mentoring Reports
+- Equipment and Systems
+    - Euroscope Radar Display
+    - Topsky
+    - Tags and Lists
+    - Leader Lines
+- Standing Agreements
+    - Introduction to Standing Agreements
+    - Inbound Releases
+
+## Syllabus
+This section serves to outline the competencies required of an S3 controller within VATSIM UK and aims to explain the requirements of each competency. Insofar as is possible, this section should be considered exhaustive; it should not be required that a student receiving training towards an S3 rating in VATSIM UK demonstrate any skills beyond those which are listed in this section of this document.     
+
+### Block Training
+
+S3 Training now incorporates block training as a way to standardise the Approach Radar training process and ensure a graduated approach to student training.  This ensures that the syllabus is covered appropriately whilst minimising the risk of student overwhelm.
+
+Students should complete all blocks before being forwarded for a practical exam unless otherwise agreed with the training administrators (TGI, ATC TM, ATC TD). The block training system comprises of two blocks; students are only taught competencies in their block until completing the content of each and then they complete the end of block exam.
+
+#### Block One
+
+Block one aims to introduce student controllers to the basic concept of approach radar control. A student controller should, at the end of this block, be expected to demonstrate:
+- An understanding of the role of an approach radar controller.
+    - How to plan, at a basic level, a sequence for arrivals.
+    - The responsibilities of an approach radar controller.
+    - How to contact aircraft from unmanned airspace.
+    - How to manage their workload, including how to prioritise aircraft and manage their workload on a top-down basis.
+- Radar and Communication usage
+    - How to set up Euroscope and interpret the radar display.
+    - How to appropriately manage tags and lists.
+    - How to set up and use the radar display, including relevant features of TopSky and UKCP.
+    - The Identification, Verification and Validation process, including likely Tag errors and Euroscope issues.
+- Basic local knowledge
+    - The various classes of airspace and the responsibilities of controllers and pilots within each class.
+    - The structure of the local airspace, including the airspace that the controller is directly responsible for and adjacent blocks of airspace owned by neighbouring sectors, to include the RMA or local area and any adjacent controlled airspace.
+    - The ownership of blocks of airspace, including lateral and vertical constraints imposed on the student controller.
+    - General knowledge of the APC section of the vMATS, including:
+        - Definition of the local airspace, commensurate with the above.
+        - The definition of local holding points, including their vertical constraints.
+        - Basic conditions for releases.
+        - Expectations for controlling departing aircraft.
+        - Standing agreements for spacing of arriving aircraft.
+- Radiotelephony 
+    - The use of correct phraseology, focused on the handling of IFR traffic.
+    - Frequency presence and management, including communication priority.
+- Vectoring, descent management, separation and spacing.
+    - Techniques to maintain separation.
+    - Understanding Separation Standards, where the standards are derived from and when they can be applied.
+    - When and how to use Speed Control, understanding the use of ‘soft’ and ‘hard’ speeds and the impact of ‘compression’.
+    - Understanding the definitions of ‘separation’ and ‘spacing’ alongside standard spacing requirements for various airfields.
+- Establishing Aircraft on Approaches
+    - Establishing aircraft on ILS approaches.
+    - Establishing aircraft on RNP approaches.
+    - Establishing aircraft on other instrument approaches.
+    - Establishing aircraft on Visual Approaches.
+- Co-ordination
+    - Receiving or negotiating Inbound Releases and understanding the implications of various release types.
+    - Understanding the ‘PRAWNS’ pneumonic to facilitate controller handovers.
+    - Working split positions (INT/FIN splits).
+
+#### Block Two
+
+Block two aims to develop on the knowledge obtained at Block One, whilst introducing more advanced concepts for streaming arriving aircraft, managing continuous descent approaches, managing holds and providing UK Flight Information Services. A student controller should, at the end of this block, be expected to demonstrate:
+
+- The consolidation of skills and knowledge obtained at Block One.
+- How to efficiently and safely sequence aircraft, considering:
+    - How to establish an appropriate landing order for arrivals.
+    - The facilitation of Continuous Descent Approaches when appropriate.
+    - How to build streams of traffic, including merging streams of traffic from multiple terminal holds.
+    - Planning for accommodating wake vortex separation.
+    - Accommodating missed approaches.
+    - Handling departures and their interactions with inbound aircraft.
+- Hold management, including:
+    - Entry and exit from the hold.
+    - Descending aircraft within the hold.
+    - Hold management, preparation and co-ordination.
+- The provision of UK Flight Information Services, including:
+    - Handling aircraft joining, crossing and leaving controlled airspace, facilitating appropriate service upgrades and downgrades.
+    - The provision of Basic and Traffic services.
+    - The provision of Deconfliction services with consideration to limitations of the service.
+    - The provision of Radar Handovers to adjacent ATSUs.
+- Final preparation for the S3 exam.
+
+---
+
+## Criteria Requirements
+
+In order to ensure that students and mentors have a common understanding of the requirements of the syllabus and that mentoring reports are completed consistently, the requirements for progress through each rating in the syllabus are outlined below:
+
+### Block 1
+
+| Item | Covered / Fail | Developing / Partly Competent | Good / Mostly Competent | Test Standard / Fully Competent |
+|------|----------------|-------------------------------|--------------------------|---------------------------------|
+| Role of the Approach Radar Controller | The student understands the basics of their role and can define, in simple terms, their area of responsibility. They can make simple planning decisions and can handle low workload situations with prompting. | The student has a clear understanding of the basics of their role and plan the short term consistently. They can describe the lateral and vertical constraints of their AOR, but may omit some details. They handle low workload scenarios with occasional prompting but may struggle with higher workload. The student recognises when they are required to handle departures and does so appropriately, with occasional input from the mentor. | The student unthinkingly understands their role and most responsibilities, with good workload management and planning capabilities. They can describe their AOR in detail, along with any associated restrictions. They can handle high workload scenarios with limited input. The student correctly handles departures to the minimum degree required and in accordance with local procedure. | The student fully understands their role and responsibilities for both blocks, with consistent and good workload management and planning capabilities. They can describe their AOR in detail, along with any associated restrictions. They can, without prompting, handle all scenarios presented in day-to-day traffic presentations. The controller recognises that their workload may necessitate opening a split position and understands the concept of operating the split. The student correctly handles departures to the minimum degree required and in accordance with local procedure. |
+| Radar and Communications Usage | The student correctly opens the UK Controller Pack. The student can update the controller pack without support. They require mentor input to make changes to the radar display and can perform simple tag management functions. The student can interpret the radar display. | The student performs basic, irregular tag management functions. They can dynamically configure simple radar display elements. They can recall basic IVV theory and apply it without prompting, although may need support when tooling fails or uncommon IVV scenarios present. | The student consistently manages tags well and uses the radar display dynamically. They consistently perform IVV to a good standard, including managing traffic from unstaffed airspace. | The student maintains accurate tags throughout entire controlling sessions. They understand the radar display and can easily access information. They understand the intricacies of the IVV process and perform it consistently, alongside understanding a number of possible methods of identification. |
+| Radiotelephony | The student demonstrates appropriate use of basic radiotelephony. They can use the correct phraseology to issue climbs/descents, issue headings, approach clearances and handoffs, although they may make common errors with their R/T. | The student consistently applies correct radiotelephony for common scenarios. The student speaks with appropriate pace and tone. They may make mistakes when under pressure or in more complex scenarios. | The student demonstrates good use of radiotelephony, including for block 2 concepts. The student speaks with appropriate pace and tone, even under pressure, and prioritises their transmissions well. Mistakes are limited and commonly identified/corrected by the student without prompting. | The student demonstrates an appropriate understanding of all required approach radar phraseology, including recognising when to revert to normal speech. The student speaks with good pace and tone and consistently prioritises their transmissions well. |
+| Vectoring, Descent Management, Separation and Spacing | The student understands separation minima and basic techniques to achieve them, but may not understand or demonstrate the concept of ‘ensuring’ separation. The student can provide descents to aircraft with respect to local airspace in low workload situations. | The student demonstrates a basic understanding of speed control. They recognise the tools and techniques used to maintain separation. The student descends aircraft appropriately, considerate of the impact of speed. They provide reasonable vectors to multiple aircraft concurrently, ensuring compliance with local airspace restrictions, although their vectors may be rigid and lead to inconsistent spacing. | The student demonstrates consistent use of appropriate speed control, both as a technique to maintain separation and to achieve spacing. The student understands the difference between separation and spacing. The student descends aircraft with respect to local airspace. They provide dynamic vectors to achieve consistent, intentional spacing. | The student proactively achieves spacing, including for wake vortex impacted situations. They have a clear plan and use various methods to implement that plan. They consistently co-ordinate spacing changes when the situation allows. The student consistently gives accurate and dynamic instructions in challenging scenarios, without mentor support, whilst achieving consistent spacing. |
+| Establishing Aircraft on Approaches | The student can establish aircraft on ILS approaches with due consideration to the platform altitude. They understand the appropriate range and altitude to establish aircraft on a 3-degree glide path. | The student consistently establishes aircraft on ILS approaches at appropriate points on the extended centreline and with due consideration to the platform altitude. They demonstrate a basic understanding of RNP approaches. | The student can integrate aircraft on ILS, RNP and visual approaches. They demonstrate a good understanding of each approach type and issue them appropriately for the traffic volume. The student can control procedural and visual approaches, but understands the challenge this presents and is duly wary of providing them. | The student can integrate aircraft on ILS, RNP and visual approaches, utilising different establishing points for efficiency. They demonstrate the use of more advanced techniques for efficiency. |
+| Co-ordination | The student facilitates basic co-ordination for simple tasks, such as logging on and logging off, although they may not leverage the provided mnemonics. They can both accept and provide a basic handover to another controller. They demonstrate an awareness of different release types but may not recall specific detail. | The student demonstrates a good understanding of inbound release types, receives and actions them appropriately. They may not understand all restrictions with various release conditions, but understands that restrictions may exist and where they will be defined. | The student understands all inbound release types and can co-ordinate without significant impediment to their controlling. The student occasionally pro-actively co-ordinates with area controllers, but may need prompting to do so. The student maintains control of their frequency whilst co-ordinating. | The student demonstrates a good understanding of co-ordination, pro-actively co-ordinating when appropriate and rejecting or delaying co-ordination requests when busy. The student does not require prompting and continues to control whilst co-ordinating where appropriate. |
+
+### Block 2
+
+| Item | Covered | Developing | Good | Test Standard |
+|------|---------|------------|------|---------------|
+| UK Flight Information Services | The student can issue a basic service to aircraft outside of controlled airspace. They can safely process a join, leave or transit request. | The student can issue basic and traffic services. They demonstrate a basic understanding of radar handovers. They issue appropriate traffic information in good time. | The student can issue basic, traffic and deconfliction services appropriately for their workload and with respect to service limitations. They reject services appropriate to their workload. | The student competently issues the full range of UKFIS appropriately for their workload and without impediment to their controlling. They are aware they can downgrade or reject services due to non-compliance or workload. |
+| Hold Management | The student understands the location and vertical limits of terminal holds in their airspace. They can issue appropriate instructions to enter terminal holds. | The student utilises holds efficiently, recognising when to start holding and effecting appropriate co-ordination. They are aware of the different methods for leaving the hold but may not apply them. | The student manages holds well, recognising when to start holding and occasionally using holds to facilitate changes to the landing order. They appropriately use a variety of methods to make aircraft leave the hold. They are able to stop holding and have an awareness of ‘stack-swap’ STARs. | The student effectively holds, including pro-active holding to protect their workload. They can safely manage holds without impediments to regular controlling and proactively consider the use of ‘stack-swaps’ to minimise holding. |
+| Advanced Vectoring, Descent Management, Separation and Spacing | The student consistently vectors and descends aircraft with respect to local airspace. They demonstrate a capacity to plan a simple landing order. | The student can build streams of traffic from multiple arrivals, establishing a landing order in advance and fulfilling their plan; spacing may still be irregular on occasion. The student occasionally facilitates CDA. | The student streams aircraft from multiple arrivals whilst considering wake vortex implications in their landing order. Spacing is consistent and complies with local procedure or co-ordination. They are able to alter their plan to accommodate missed approaches. The student consistently facilitates CDA where appropriate. | The student can stream aircraft effectively, accommodating spacing and separation requirements in complex traffic scenarios. They can facilitate missed approaches without significant impediment to their arrival rate whilst consistently accommodating CDAs when appropriate. |
+
+---
+
+## Lessons Plans
+
+In order to ensure that students beginning their S3 training are brokered to the syllabus in a consistent manner, the department requires that students are generally taken through a number of pre-determined lesson plans. Whilst these lesson plans provide a set structure for training, they may, at the discretion of the mentor, be condensed, replaced or re-ordered, depending on an individual students approach to training. 
+
+### Lesson One
+
 </x-markdown>
