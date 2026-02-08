ce2fee3d7aa3d5db3bd453a9edfa85f15295f528
CLC0609 <cobylc.121@gmail.com>
2026-02-08 21:40:29 +0000

    Add new permission

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -80,6 +80,7 @@ class RolesAndPermissionsSeeder extends Seeder
             'role.create',
             'role.edit.*',
             'role.delete.*',
+            'role.delegate.*',
 
             // Visit Transfer System Permissions
             'vt.access',
