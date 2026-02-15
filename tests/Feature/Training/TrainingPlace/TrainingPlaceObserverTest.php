a710efb3261f46a8f5a36a30556b87d65c4c32c2
Callum Axon <git@callumaxon.uk>
2026-02-15 13:09:13 +0000

    Ensure test data for TrainingPlaceObserver multi position test is not flaky

--- tests/Feature/Training/TrainingPlace/TrainingPlaceObserverTest.php
+++ tests/Feature/Training/TrainingPlace/TrainingPlaceObserverTest.php
@@ -53,8 +53,8 @@ class TrainingPlaceObserverTest extends TestCase
     public function it_assigns_mentoring_permissions_for_multiple_cts_positions(): void
     {
         // Arrange: Create multiple CTS positions
-        $ctsPosition1 = CtsPosition::factory()->create();
-        $ctsPosition2 = CtsPosition::factory()->create();
+        $ctsPosition1 = CtsPosition::factory()->create(['callsign' => 'EGKK_TWR']);
+        $ctsPosition2 = CtsPosition::factory()->create(['callsign' => 'EGLL_TWR']);
 
         $trainingPosition = TrainingPosition::factory()->create([
             'cts_positions' => [$ctsPosition1->callsign, $ctsPosition2->callsign],
@@ -92,7 +92,7 @@ class TrainingPlaceObserverTest extends TestCase
     public function it_handles_training_place_creation_when_student_has_no_cts_member(): void
     {
         // Arrange: Create data without a CTS member
-        $ctsPosition = CtsPosition::factory()->create();
+        $ctsPosition = CtsPosition::factory()->create(['callsign' => 'EGKK_TWR']);
         $trainingPosition = TrainingPosition::factory()->create([
             'cts_positions' => [$ctsPosition->callsign],
         ]);
