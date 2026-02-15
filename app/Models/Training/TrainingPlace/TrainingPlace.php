66bb44aa7a21743b69e3d3cd39d7b4754bd944eb
Callum Axon <git@callumaxon.uk>
2026-02-15 08:48:11 +0000

    Add soft deletes to training places table

--- app/Models/Training/TrainingPlace/TrainingPlace.php
+++ app/Models/Training/TrainingPlace/TrainingPlace.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TrainingPlace extends Model
 {
@@ -15,6 +16,7 @@ class TrainingPlace extends Model
     use HasFactory;
 
     use HasUlids;
+    use SoftDeletes;
 
     protected $guarded = [];
 
