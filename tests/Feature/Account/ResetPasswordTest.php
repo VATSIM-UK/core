cd5a599cfa0b710317dad6b4a7fa08b050cd6469
CLC0609 <cobylc.121@gmail.com>
2026-02-21 17:08:08 +0000

    Fix tests

--- tests/Feature/Account/ResetPasswordTest.php
+++ tests/Feature/Account/ResetPasswordTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Account;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Password;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
@@ -28,6 +29,8 @@ class ResetPasswordTest extends TestCase
     #[Test]
     public function test_password_reset_updates_correctly()
     {
+        Http::fake();
+
         $this->user->password = 'Testing123';
 
         $now = Carbon::today();
@@ -42,10 +45,14 @@ class ResetPasswordTest extends TestCase
         // Hold time to allow for comparision
 
         // Reset the password
-        $this->followingRedirects()->actingAs($this->user, 'vatsim-sso')
+        $this->actingAs($this->user, 'vatsim-sso')
             ->from(route('password.reset', $token))
-            ->post(route('password.request'), ['token' => $token, 'password' => 'Testing234', 'password_confirmation' => 'Testing234'])
-            ->assertSuccessful();
+            ->post(route('password.request'), [
+                'token' => $token,
+                'new_password' => 'Testing234',
+                'new_password_confirmation' => 'Testing234',
+            ])
+            ->assertRedirect();
 
         $this->assertTrue(Hash::check('Testing234', $this->user->fresh()->password));
         $this->assertEquals($now, $this->user->fresh()->password_set_at);
