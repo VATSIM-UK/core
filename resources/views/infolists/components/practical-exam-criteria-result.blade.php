668c8530438069d0a4b0d7f856f2e11e5bfedef9
Callum Axon <git@callumaxon.uk>
2026-04-05 09:09:19 +0100

    Upgrade Tailwindcss to V4

--- resources/views/infolists/components/practical-exam-criteria-result.blade.php
+++ resources/views/infolists/components/practical-exam-criteria-result.blade.php
@@ -1,5 +1,5 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
-    <div class="flex items-center justify-center w-full px-3 py-1.5 text-xs font-semibold rounded-md uppercase tracking-wide shadow-sm transition-all duration-200 hover:shadow-md"
+    <div class="flex items-center justify-center w-full px-3 py-1.5 text-xs font-semibold rounded-md uppercase tracking-wide shadow-xs transition-all duration-200 hover:shadow-md"
          style="background-color: {{ $getColorForResult() }}; color: {{ $getTextColorForResult() }};">
         <span class="leading-none">{{ $getRecord()->resultHuman }}</span>
     </div>
