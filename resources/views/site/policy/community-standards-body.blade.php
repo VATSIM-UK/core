bbbfdb8c391fb7b1c2abcdc4e5a5dbccd1efb620
Coby Chapman <cobylc.121@gmail.com>
2026-04-29 20:28:36 +0100

    fix: update terms and conditions text on core (#4686)

---
+++ resources/views/site/policy/community-standards-body.blade.php
@@ -0,0 +1,75 @@
+<x-markdown>
+VATSIM UK is a Division of the VATSIM Network within the Europe, Middle East and Asia Region. We are home to a large community of flight simulation hobbyists, enthusiasts and aviation professionals from across the world. Our Community Standards outline the rules which all members must abide by in order to keep our services safe, accessible and enjoyable by everyone.
+
+Members are subject to all VATSIM Network policies when utilising any VATSIM-owned or operated service. This includes, but is not limited to:
+- VATSIM Code of Conduct
+- VATSIM Code of Regulations
+- VATSIM User Agreement
+- VATSIM Safeguarding Minors Policy
+
+The VATSIM UK Community Standards expand on VATSIM Network policy and apply to the conduct of all members whilst utilising a VATSIM UK-owned or operated service, or attending a VATSIM UK event. These Standards may be subject to future amendments, however, any changes will be notified to members before coming into effect.
+
+The VATSIM Network Supervisor Department is primarily responsible for the enforcement of VATSIM Network policies. VATSIM UK has appointed a Moderation Team that is similarly responsible for enforcing local policy, such as these Standards. Our Moderators do not wish, nor would they be able, to police all content posted to our services. If you believe that a Moderator has missed something which is in violation of our Community Standards, you may bring it to our attention in the following ways:
+- Discord - tag @moderator in the relevant channel for assistance
+- Email - <moderation@vatsim.uk>
+
+## Section 1 - Guiding Principles:
+
+1.1. Any member who is unsure of whether their actions would breach the Terms & Conditions should use caution and consider whether a different approach would be appropriate.
+
+1.2. It is expected that all members are familiarised with VATSIM UK policy. All VATSIM UK policies are available on our website.
+
+1.3. If you believe that content on a VATSIM UK service is in violation of these Standards, you should inform a Moderator immediately. You should not attempt to 'moderate' the issue yourself or take part in any retaliatory action.
+
+## Section 2 - Community Standards:
+
+2.1. Think before you type - Online discussions can take place with many participants and at great speed. Slow down and take the time to gather your thoughts into a well-considered message. Be mindful that your words and actions reflect on the Division and our community as a whole.
+
+2.2. Mind your language - Ensure that your tone, language and conduct is appropriate for the audience and medium. Members should not aim to deliberately cause offence through their words or actions; the use of slurs or discriminatory language is not permitted.
+
+2.3. Keep discussion organised - Our services include a variety of voice and text channels, many of which have a specific focus or topic of discussion. This helps us keep discussion organised and allows our members to 'opt-in' to the topics which they find most interesting. Where channel topics exist, you should ensure your content complies with these guidelines or post it elsewhere.
+
+2.4. There's no need to shout - You should not use block capitals to get the attention of others via text. Similarly, trying to disrupt the natural flow of conversation through the posting of large, unnecessary or inflammatory images, GIFs or messages is not permitted - this includes 'spam' content.
+
+2.5. Privacy please - You are not permitted to record other members on VATSIM UK services without their prior consent. This applies to audio included as part of an online livestream but does not apply to recordings for the purpose of evidencing breaches of these Standards or the VATSIM Code of Conduct.
+
+2.6. Your stream, your responsibility - While sharing your PC's screen or broadcasting your webcam through a VATSIM UK service, you are responsible for ensuring that the content of your stream is compliant with these Standards, irrespective of whether the content was intended to be included or not.
+
+2.7. No advertisements - You are not permitted to advertise products or services on VATSIM UK services. This includes the advertisement of staff positions or recruitment of members on behalf of organisations, including virtual airlines and other sub-divisions. Endorsements and reviews of products or services are permitted, provided that these take place within the context of discussion.
+
+2.8. Take the hint - Moderation action may include the removal of content which is deemed in breach of these Standards. You should not attempt to circumvent moderation action or automatic profanity filters. The discussion of moderation action with others, including the appeals process or outcomes, is not permitted under any circumstances.
+
+2.9. Don't provoke - Nobody enjoys reading an argument. Whilst healthy and constructive debate is permitted, you should not engage in petty, pointless or inflammatory conduct - this includes the use of reactions, photos or GIFs with the aim of provoking others.
+
+2.10. Inappropriate content - Posting images and videos of sexually explicit material or adult content is forbidden on all VATSIM UK services. Posting links to websites which contain such content, as well as the use of sexually explicit language, is similarly forbidden.
+
+2.11. Safeguarding - The discussion or implication of any form of abuse towards a child or vulnerable adult is forbidden. If you are made aware of a potential safeguarding concern, you should report this via the VATSIM UK Helpdesk 'Safeguarding Concerns' topic.
+
+2.12. Don't break the law - You should not discuss, promote, encourage, aide or take part in any illegal activity. This includes threatening or harassing other members, pirating software or content, and the hacking or misuse of computers.
+
+## Section 3 - Disciplinary Action:
+
+3.1. VATSIM UK reserves the right to carry out any actions deemed necessary to enforce these Standards - this includes, but is not limited, to the following:
+  - Informal Moderation - Real-time guidance provided to a member, either publicly or privately, that their conduct has violated these Standards.
+  - Removal of Content - This should be accompanied by Informal Moderation to inform a member why their content has been deleted.
+  - Temporary Timeout - Prevents a member from posting within Discord for a short period of time. This should be accompanied by Informal Moderation to inform a member why a timeout has been applied.
+  - Temporary Suspension - Prevents a member from accessing any VATSIM UK service that allows communication with other members for a set duration. The reason for suspension shall be notified to the member via email.
+  - Permanent Suspension - Prevents a member from accessing any VATSIM UK service that allows communication with other members indefinitely. The member shall be notified of a Permanent Suspension via email.
+  - Referral to the VATSIM Network Supervisor Department - Made when a member has breached VATSIM Network policy and may result in further disciplinary action, such as a Network Suspension or further referral to the Standards and Compliance Department.
+
+3.2. The public discussion of moderation action or appeals is prohibited. Retaliatory action or harassment of other members, including VATSIM UK staff, is similarly prohibited and may lead to further disciplinary action, and the removal of a member's right to appeal.
+
+3.3. VATSIM UK reserves the right to apply a Permanent Suspension to any member at its discretion.
+
+3.4. A member has the right to appeal an action of moderation or suspension from VATSIM UK services, except in the following circumstances:
+  - Removal of Content occurred but no further action was taken against a member
+  - A member is subject to a Permanent Suspension from VATSIM UK services
+  - A member's local suspension from VATSIM UK services is the result of a VATSIM Network Suspension
+  - A member has engaged in any of the activities outlined in Sections 2.10, 2.11, 2.12, and 3.2.
+
+3.5. Any appeal regarding an action of moderation or suspension from VATSIM UK services must be communicated to the Division Staff Group (DSG) via the VATSIM UK Helpdesk within 5 days of the moderation action taking place. The DSG are not obligated to hear appeals submitted beyond this time.
+
+3.6. An appeal decision shall be made and communicated by email to the appellant within 5 days of an appeal being submitted. Where this is not possible, the DSG shall notify the appellant of an estimated response date at the earliest possible opportunity.
+
+3.7. The DSG are not required to justify appeal decisions, and all decisions are final. An appellant has no further right to appeal once a decision has been made.
+</x-markdown>
