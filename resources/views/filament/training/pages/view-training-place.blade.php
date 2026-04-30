cef825cc405902f51e7e764cb70a2a74930f1fac
CLC0609 <cobylc.121@gmail.com>
2026-04-30 13:41:46 +0100

    Improve training place page

--- resources/views/filament/training/pages/view-training-place.blade.php
+++ resources/views/filament/training/pages/view-training-place.blade.php
@@ -9,8 +9,6 @@
 
     @livewire(\App\Livewire\Training\AvailabilityWarningsTable::class, ['trainingPlace' => $this->trainingPlace], key('availability-warnings-table'))
 
-    @livewire(\App\Livewire\Training\AvailabilityChecksTable::class, ['trainingPlace' => $this->trainingPlace], key('availability-checks-table'))
-
     @if($this->trainingPlace->trainingPosition->should_show_solo_endorsement ?? true)
         @livewire(\App\Livewire\Training\TrainingPlaceSoloEndorsement::class, ['trainingPlace' => $this->trainingPlace], key('training-place-solo-endorsement'))
     @endif
@@ -21,5 +19,13 @@
 
     @livewire(\App\Livewire\Training\LeaveOfAbsencesTable::class, ['trainingPlace' => $this->trainingPlace], key('leave-of-absences-table'))
 
-    @livewire(\App\Livewire\Training\TrainingPlaceExamCancellationsTable::class, ['trainingPlace' => $this->trainingPlace], key('exam-cancellations-table'))
+    @if($this->trainingPlace->hasExamCancellations())
+        @livewire(\App\Livewire\Training\TrainingPlaceExamCancellationsTable::class, ['trainingPlace' => $this->trainingPlace], key('exam-cancellations-table'))
+    @endif
+
+    <x-filament::section collapsible collapsed>
+        <x-slot name="heading">Availability checks</x-slot>
+        @livewire(\App\Livewire\Training\AvailabilityChecksTable::class, ['trainingPlace' => $this->trainingPlace], key('availability-checks-table'))
+    </x-filament::section>
+
 </x-filament-panels::page>
