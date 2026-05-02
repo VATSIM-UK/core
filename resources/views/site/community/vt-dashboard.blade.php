e5a8cf6af1fddf9d90a141ee189f2def53545081
William Shaw <39245501+WillShaw1100@users.noreply.github.com>
2026-05-02 21:15:20 +0100

    Update resources/views/site/community/vt-dashboard.blade.php
    Co-authored-by: Coby Chapman <cobylc.121@gmail.com>

--- resources/views/site/community/vt-dashboard.blade.php
+++ resources/views/site/community/vt-dashboard.blade.php
@@ -264,7 +264,7 @@
                 </p>
 
                 <p>
-                    All UK CTR + APP + ADC positions with the exception of those listed in the ATC Training Policy as requireing an additional endorsement.
+                    All UK CTR + APP + ADC positions with the exception of those listed in the ATC Training Policy as requiring an additional endorsement.
                 </p>
 
                 <p>
