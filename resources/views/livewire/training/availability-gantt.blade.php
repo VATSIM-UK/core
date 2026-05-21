ec96277ab06a87564a75be56ab01ede87f8d055b
Kristián Kunc <kristian.kunc@gmail.com>
2026-05-21 23:14:24 +0200

    prek run

--- resources/views/livewire/training/availability-gantt.blade.php
+++ resources/views/livewire/training/availability-gantt.blade.php
@@ -6,14 +6,15 @@
             <x-filament::button color="gray" wire:click="previousDay" icon="heroicon-m-chevron-left" class="!px-2" />
             <x-filament::button color="gray" wire:click="setToday">Today</x-filament::button>
             <x-filament::input.wrapper>
-                <x-filament::input type="date" wire:model.live="date" class="min-w-[140px]"/>
+                <x-filament::input type="date" wire:model.live="date" class="min-w-[140px]" />
             </x-filament::input.wrapper>
-            <x-filament::button color="gray" wire:click="nextDay" icon="heroicon-m-chevron-right" icon-alias="next" class="!px-2" />
+            <x-filament::button color="gray" wire:click="nextDay" icon="heroicon-m-chevron-right" icon-alias="next"
+                class="!px-2" />
 
             <x-filament::input.wrapper class="ml-4">
                 <x-filament::input.select wire:model.live="category">
                     <option value="">All Training Groups</option>
-                    @foreach($this->availableCategories as $cat)
+                    @foreach ($this->availableCategories as $cat)
                         <option value="{{ $cat }}">{{ $cat }}</option>
                     @endforeach
                 </x-filament::input.select>
@@ -22,9 +23,9 @@
     </div>
 
     {{-- Chart --}}
-    @if($students->isEmpty())
+    @if ($students->isEmpty())
         <div class="p-8 text-center border-t border-gray-200 dark:border-white/10 text-gray-500 dark:text-gray-400">
-            <x-filament::icon icon="heroicon-o-calendar" class="mx-auto h-8 w-8 mb-3 text-gray-400"/>
+            <x-filament::icon icon="heroicon-o-calendar" class="mx-auto h-8 w-8 mb-3 text-gray-400" />
             No availability found for this date.
         </div>
     @else
@@ -36,20 +37,12 @@
         </div>
 
         {{-- Page Arrows --}}
-        @if($students->count() > $this->studentsPerPage)
+        @if ($students->count() > $this->studentsPerPage)
             <div class="flex items-center justify-end gap-2 pt-2 border-gray-200 dark:border-white/10">
-                <x-filament::button
-                    color="gray"
-                    wire:click="previousStudentsPage"
-                    icon="heroicon-m-chevron-left"
-                    class="!px-2"
-                    :disabled="$studentsPage <= 1"/>
-                <x-filament::button
-                    color="gray"
-                    wire:click="nextStudentsPage"
-                    icon="heroicon-m-chevron-right"
-                    class="!px-2"
-                    :disabled="$studentsPage * $this->studentsPerPage >= $students->count()"/>
+                <x-filament::button color="gray" wire:click="previousStudentsPage" icon="heroicon-m-chevron-left"
+                    class="!px-2" :disabled="$studentsPage <= 1" />
+                <x-filament::button color="gray" wire:click="nextStudentsPage" icon="heroicon-m-chevron-right"
+                    class="!px-2" :disabled="$studentsPage * $this->studentsPerPage >= $students->count()" />
             </div>
         @endif
     @endif
