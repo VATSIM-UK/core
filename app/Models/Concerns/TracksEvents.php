355cdf4c92688d36a6ff8a891b694a159daf6589
Kristián Kunc <kristian.kunc@gmail.com>
2026-06-04 00:12:36 +0200

    use laravel 13 model booting

--- app/Models/Concerns/TracksEvents.php
+++ app/Models/Concerns/TracksEvents.php
@@ -22,9 +22,10 @@ trait TracksEvents
      */
     public static function bootTracksEvents()
     {
-        $instance = new static;
+        $reflection = new \ReflectionClass(static::class);
+        $trackedEvents = $reflection->getDefaultProperties()['trackedEvents'] ?? [];
 
-        foreach ($instance->trackedEvents as $event) {
+        foreach ($trackedEvents as $event) {
             static::$event(function ($model) use ($event) {
                 $model->addActivity($event);
             });
