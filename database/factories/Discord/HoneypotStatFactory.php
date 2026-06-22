d8cb50f1972a41a7dd2c9886d992136b48a56ff8
Kristián Kunc <kristian.kunc@gmail.com>
2026-06-23 00:29:00 +0200

    add honeypot stats factory

---
+++ database/factories/Discord/HoneypotStatFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories\Discord;
+
+use App\Models\Discord\HoneypotStat;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Discord\HoneypotStat>
+ */
+class HoneypotStatFactory extends Factory
+{
+    protected $model = HoneypotStat::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'account_id' => (string) fake()->unique()->randomNumber(8),
+        ];
+    }
+}
