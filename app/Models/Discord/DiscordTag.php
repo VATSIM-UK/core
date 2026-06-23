199ff2a560f65793f70e001e52b79be0214eeeaf
Kristián Kunc <kristian.kunc@gmail.com>
2026-06-23 02:29:10 +0200

    re-add tag factory

--- app/Models/Discord/DiscordTag.php
+++ app/Models/Discord/DiscordTag.php
@@ -4,11 +4,14 @@ namespace App\Models\Discord;
 
 use App\Observers\DiscordTagObserver;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 #[ObservedBy([DiscordTagObserver::class])]
 class DiscordTag extends Model
 {
+    use HasFactory;
+
     protected $table = 'discord_tags';
 
     protected $fillable = [
