070953aff04c6d13b6b099e1e81cda01fa7bf9f2
CLC0609 <cobylc.121@gmail.com>
2026-06-25 10:36:05 +0100

    Update session_cancelled_student_confirmation.blade.php

--- resources/views/emails/training/mentoring/session_cancelled_student_confirmation.blade.php
+++ resources/views/emails/training/mentoring/session_cancelled_student_confirmation.blade.php
@@ -3,7 +3,7 @@
 @section('body')
     <p>You have cancelled your mentoring session, which was due to take place on {{ $session->position }} on {{ \Carbon\Carbon::parse($session->taken_date)->format('l jS M y') }} at {{ \Carbon\Carbon::parse($session->taken_from)->format('H:i') }}Z.</p>
 
-    <p>To stop receiving these alerts, you can amend your email settings in the STUDENT menu.</p>
+    <p>To stop receiving these alerts, you can amend your email settings in the <a href="{{ route('filament.training.pages.email-settings') }}">Training Panel</a>.</p>
 @stop
 
 @section('signature')
