f499c340a4a7d2de818cf25f3698e71127b8b91d
CLC0609 <cobylc.121@gmail.com>
2026-07-16 22:15:17 +0100

    allow create and approve

--- app/Filament/Training/Resources/EndorsementRequests/Pages/CreateEndorsementRequest.php
+++ app/Filament/Training/Resources/EndorsementRequests/Pages/CreateEndorsementRequest.php
@@ -2,10 +2,55 @@
 
 namespace App\Filament\Training\Resources\EndorsementRequests\Pages;
 
+use App\Events\Training\EndorsementRequestApproved;
 use App\Filament\Training\Resources\EndorsementRequests\EndorsementRequestResource;
+use Filament\Actions\Action;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateEndorsementRequest extends CreateRecord
 {
     protected static string $resource = EndorsementRequestResource::class;
+
+    protected ?array $approvalData = null;
+
+    protected function getFormActions(): array
+    {
+        return [
+            $this->getCreateFormAction(),
+            $this->getCreateAndApproveFormAction(),
+            $this->getCancelFormAction(),
+        ];
+    }
+
+    protected function getCreateAndApproveFormAction(): Action
+    {
+        return Action::make('createAndApprove')
+            ->label('Create and Approve')
+            ->modalHeading('Approve')
+            ->modalSubmitActionLabel('Approve')
+            ->color('success')
+            ->schema(EndorsementRequestResource::approvalSchema())
+            ->action(function (array $data): void {
+                $this->approvalData = $data;
+
+                $this->create();
+            });
+    }
+
+    protected function afterCreate(): void
+    {
+        if ($this->approvalData === null) {
+            return;
+        }
+
+        event(new EndorsementRequestApproved($this->record, $this->approvalData['days'] ?? null));
+
+        Notification::make()
+            ->title('Endorsement request created')
+            ->success()
+            ->send();
+
+        $this->approvalData = null;
+    }
 }
