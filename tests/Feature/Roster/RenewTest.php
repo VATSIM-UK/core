79d5585805d16e8a9526b2d88c88255c1f698d35
Coby Chapman <cobylc.121@gmail.com>
2026-07-21 21:45:25 +0100

    feat: circumstantially inhibit manual roster re-activation (#5011)
    * adds new roster requirements
    
    * styling
    
    * Ensure we get the end of the day
    
    * test fixes
    
    * test fixes v2

--- tests/Feature/Roster/RenewTest.php
+++ tests/Feature/Roster/RenewTest.php
@@ -9,6 +9,7 @@ use App\Models\Mship\Qualification;
 use App\Models\Mship\State;
 use App\Models\NetworkData\Atc;
 use App\Models\Roster;
+use App\Models\RosterHistory;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Livewire\Livewire;
@@ -27,16 +28,21 @@ class RenewTest extends TestCase
         return $account;
     }
 
-    private function createAtcSession(Account $account, Carbon $disconnectedAt): Atc
+    private function createAtcSession(Account $account, Carbon $disconnectedAt, int $minutesDuration = 30): Atc
     {
-        return Atc::create([
+        $atc = Atc::create([
             'account_id' => $account->id,
             'callsign' => 'EGLL_TWR',
-            'connected_at' => $disconnectedAt->copy()->subMinutes(30),
+            'connected_at' => $disconnectedAt->copy()->subMinutes($minutesDuration),
             'disconnected_at' => $disconnectedAt,
             'qualification_id' => 1,
             'facility_type' => 4,
         ]);
+
+        $atc->minutes_online = $minutesDuration;
+        $atc->save();
+
+        return $atc;
     }
 
     public function test_redirects_when_already_on_roster()
@@ -95,22 +101,6 @@ class RenewTest extends TestCase
             ->assertSet('page', 1);
     }
 
-    public function test_cannot_proceed_without_recent_connection()
-    {
-        $account = $this->createEligibleAccount();
-
-        $this->mock(UKCP::class)
-            ->shouldReceive('getUnreadNotificationsForUser')
-            ->andReturn([]);
-
-        $this->createAtcSession($account, Carbon::now()->subMonths(19));
-
-        Livewire::actingAs($account)
-            ->test(Renew::class)
-            ->call('nextPage')
-            ->assertForbidden();
-    }
-
     public function test_cannot_proceed_with_no_connection_history()
     {
         $account = $this->createEligibleAccount();
@@ -122,52 +112,6 @@ class RenewTest extends TestCase
         Livewire::actingAs($account)
             ->test(Renew::class)
             ->call('nextPage')
-            ->assertForbidden();
-    }
-
-    public function test_can_proceed_with_recent_connection()
-    {
-        $account = $this->createEligibleAccount();
-
-        $this->mock(UKCP::class)
-            ->shouldReceive('getUnreadNotificationsForUser')
-            ->andReturn([]);
-
-        $this->createAtcSession($account, Carbon::now()->subMonths(6));
-
-        Livewire::actingAs($account)
-            ->test(Renew::class)
-            ->call('nextPage')
-            ->assertSet('page', 2);
-    }
-
-    public function test_can_proceed_when_within_18_month_window()
-    {
-        $account = $this->createEligibleAccount();
-
-        $this->mock(UKCP::class)
-            ->shouldReceive('getUnreadNotificationsForUser')
-            ->andReturn([]);
-
-        $this->createAtcSession($account, Carbon::now()->subMonths(18)->addDay());
-
-        Livewire::actingAs($account)
-            ->test(Renew::class)
-            ->call('nextPage')
-            ->assertSet('page', 2);
-    }
-
-    public function test_cannot_reactivate_without_recent_connection()
-    {
-        $account = $this->createEligibleAccount();
-
-        $this->mock(UKCP::class)
-            ->shouldReceive('getUnreadNotificationsForUser')
-            ->andReturn([]);
-
-        Livewire::actingAs($account)
-            ->test(Renew::class)
-            ->call('reactivate')
             ->assertForbidden();
     }
 
@@ -236,49 +180,105 @@ class RenewTest extends TestCase
             ->assertSee('You have 1 notifications to read.');
     }
 
-    public function test_mark_notification_read_removes_notification()
+    public function test_cannot_proceed_when_previous_removal_and_no_hours_in_last_two_quarters()
     {
         $account = $this->createEligibleAccount();
-        $this->createAtcSession($account, Carbon::now()->subMonths(6));
 
-        $ukcp = $this->mock(UKCP::class);
-        $ukcp->shouldReceive('getUnreadNotificationsForUser')
-            ->andReturn([
-                ['id' => 1, 'title' => 'Change 1', 'body' => 'Body 1', 'link' => null],
-            ]);
-        $ukcp->shouldReceive('markNotificationReadForUser')
-            ->with(\Mockery::type(Account::class), 1)
-            ->andReturn(true);
+        RosterHistory::create([
+            'account_id' => $account->id,
+            'original_created_at' => Carbon::now()->subMonths(12),
+            'original_updated_at' => Carbon::now()->subMonths(12),
+            'created_at' => Carbon::now()->subMonths(9),
+        ]);
+
+        $this->mock(UKCP::class)
+            ->shouldReceive('getUnreadNotificationsForUser')
+            ->andReturn([]);
+
+        $this->createAtcSession($account, Carbon::now()->subMonths(15));
 
         Livewire::actingAs($account)
             ->test(Renew::class)
-            ->assertSee('You have 1 notifications to read.')
-            ->call('markNotificationRead', 1, 1)
-            ->assertSee('You have 0 notifications to read.');
+            ->call('nextPage')
+            ->assertForbidden();
     }
 
-    public function test_mark_notification_read_failure_retains_notification()
+    public function test_can_proceed_when_previous_removal_and_met_hours_in_last_quarter()
     {
         $account = $this->createEligibleAccount();
-        $this->createAtcSession($account, Carbon::now()->subMonths(6));
 
-        $ukcp = $this->mock(UKCP::class);
-        $ukcp->shouldReceive('getUnreadNotificationsForUser')
-            ->andReturn([
-                ['id' => 1, 'title' => 'Change 1', 'body' => 'Body 1', 'link' => null],
-            ]);
-        $ukcp->shouldReceive('markNotificationReadForUser')
-            ->with(\Mockery::type(Account::class), 1)
-            ->andReturn(false);
+        RosterHistory::create([
+            'account_id' => $account->id,
+            'original_created_at' => Carbon::now()->subMonths(12),
+            'original_updated_at' => Carbon::now()->subMonths(12),
+        ]);
+
+        $currentQuarterStart = Carbon::now()->copy()->startOfQuarter();
+        $lastQuarterStart = $currentQuarterStart->copy()->subMonths(3);
+
+        $sessionTime = $lastQuarterStart->copy()->addWeeks(4)->addHours(12);
+
+        $this->mock(UKCP::class)
+            ->shouldReceive('getUnreadNotificationsForUser')
+            ->andReturn([]);
+
+        $this->createAtcSession($account, $sessionTime, 180);
 
         Livewire::actingAs($account)
             ->test(Renew::class)
-            ->assertSee('You have 1 notifications to read.')
-            ->call('markNotificationRead', 1, 1)
-            ->assertSee('You have 1 notifications to read.');
+            ->call('nextPage')
+            ->assertSet('page', 2);
     }
 
-    public function test_shows_page_two_after_proceeding()
+    public function test_can_proceed_when_previous_removal_and_met_hours_in_prior_quarter_only()
+    {
+        $account = $this->createEligibleAccount();
+
+        RosterHistory::create([
+            'account_id' => $account->id,
+            'original_created_at' => Carbon::now()->subMonths(12),
+            'original_updated_at' => Carbon::now()->subMonths(12),
+        ]);
+
+        $currentQuarterStart = Carbon::now()->copy()->startOfQuarter();
+
+        $sessionTime = $currentQuarterStart->copy()->subMonths(5)->addWeeks(2)->addHours(12);
+
+        $this->mock(UKCP::class)
+            ->shouldReceive('getUnreadNotificationsForUser')
+            ->andReturn([]);
+
+        $this->createAtcSession($account, $sessionTime, 180);
+
+        Livewire::actingAs($account)
+            ->test(Renew::class)
+            ->call('nextPage')
+            ->assertSet('page', 2);
+    }
+
+    public function test_shows_quarter_failure_message_when_no_hours_in_last_two_quarters()
+    {
+        $account = $this->createEligibleAccount();
+
+        RosterHistory::create([
+            'account_id' => $account->id,
+            'original_created_at' => Carbon::now()->subMonths(12),
+            'original_updated_at' => Carbon::now()->subMonths(12),
+            'created_at' => Carbon::now()->subMonths(9),
+        ]);
+
+        $this->mock(UKCP::class)
+            ->shouldReceive('getUnreadNotificationsForUser')
+            ->andReturn([]);
+
+        $this->createAtcSession($account, Carbon::now()->subMonths(15));
+
+        Livewire::actingAs($account)
+            ->test(Renew::class)
+            ->assertSee('minimum controlling requirements');
+    }
+
+    public function test_first_time_joiner_not_blocked_by_quarter_check()
     {
         $account = $this->createEligibleAccount();
 
@@ -291,7 +291,6 @@ class RenewTest extends TestCase
         Livewire::actingAs($account)
             ->test(Renew::class)
             ->call('nextPage')
-            ->assertSee('Reactivate')
-            ->assertSee('Add to');
+            ->assertSet('page', 2);
     }
 }
