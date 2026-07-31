ecc5a4a44f2007f6e153fc12f202b06c65278f57
Coby Chapman <cobylc.121@gmail.com>
2026-07-31 18:35:56 +0100

    Add allowlist on resources
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/Filament/Training/Pages/Endorsements/Tables/ResourceTable.php
+++ app/Filament/Training/Pages/Endorsements/Tables/ResourceTable.php
@@ -22,13 +22,21 @@ class ResourceTable extends Component implements HasActions, HasForms, HasTable
 
     public string $resource;
 
+    protected const ALLOWED_RESOURCES = [
+        \App\Filament\Training\Resources\EndorsementRequests\EndorsementRequestResource::class,
+        \App\Filament\Training\Resources\SoloEndorsements\SoloEndorsementResource::class,
+        \App\Filament\Training\Resources\PositionGroups\PositionGroupResource::class,
+    ];
+
     public function table(Table $table): Table
     {
+        abort_unless(in_array($this->resource, self::ALLOWED_RESOURCES, true), 403);
         return ($this->resource)::table($table);
     }
 
     protected function getTableQuery(): ?Builder
     {
+        abort_unless(in_array($this->resource, self::ALLOWED_RESOURCES, true), 403);
         return ($this->resource)::getEloquentQuery();
     }
 
