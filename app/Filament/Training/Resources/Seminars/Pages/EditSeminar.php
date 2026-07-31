3a5cb9e77fa019f4f473ef4ef4daed6859ecae3e
CLC0609 <cobylc.121@gmail.com>
2026-07-31 20:50:55 +0100

    Update EditSeminar.php

--- app/Filament/Training/Resources/Seminars/Pages/EditSeminar.php
+++ app/Filament/Training/Resources/Seminars/Pages/EditSeminar.php
@@ -8,9 +8,4 @@ use Filament\Resources\Pages\EditRecord;
 class EditSeminar extends EditRecord
 {
     protected static string $resource = SeminarResource::class;
-
-    public static function canAccess(): bool
-    {
-        return auth()->user()->can('training.seminars.manage.*');
-    }
 }
