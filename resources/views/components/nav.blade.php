ae5cb081d1d97fb3722336c0cbc5abbbd08144e6
William Shaw <williamrockstar110@gmail.com>
2026-08-03 20:57:23 +0100

    reverts cts link as we still use it

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -291,8 +291,7 @@
 								<li><a href="{{ route('site.community.teamspeak') }}" class="nav-dropdown-link">TeamSpeak</a></li>
 								<li><a href="{{ route('mship.manage.dashboard') }}" class="nav-dropdown-link">Discord</a></li>
 								<li class="nav-dropdown-divider"></li>
-								<li><a href="{{ route('filament.training.pages.dashboard') }}" class="nav-dropdown-link">Training System</a>
-								</li>
+								<li><a href="https://cts.vatsim.uk/" class="nav-dropdown-link">Training System (CTS)</a></li>
 								<li><a href="https://docs.vatsim.uk/" class="nav-dropdown-link">Docs Site</a></li>
 								<li><a href="https://moodle.vatsim.uk/" class="nav-dropdown-link">eLearning</a></li>
 								<li><a href="https://helpdesk.vatsim.uk/" class="nav-dropdown-link">Helpdesk</a></li>
