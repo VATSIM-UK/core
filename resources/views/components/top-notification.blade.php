f5e1e346d431083d630c512989f5fe29a7f16ca9
CLC0609 <cobylc.121@gmail.com>
2026-08-04 10:45:16 +0100

    make discord a link

--- resources/views/components/top-notification.blade.php
+++ resources/views/components/top-notification.blade.php
@@ -38,7 +38,9 @@
 					<p><strong>Meet the new Training Panel, {{ auth()->user()->name_first }}!</strong></p>
 					<p>We're moving all training management out of CTS and into the Training Panel, it'll eventually replace CTS
 						entirely, so the sooner you get familiar with it, the better. Found a bug or have any feedback? Let us know in
-						#training-panel-feedback-issues on our discord.</p>
+						<a href="https://discord.com/channels/705488311346790471/1516503043879993596" target="_blank"
+							rel="noopener">#training-panel-feedback-issues</a> on our discord.
+					</p>
 				</div>
 				<div class="col-md-3 cta text-center">
 					<a href="{{ route('filament.training.pages.dashboard') }}"
