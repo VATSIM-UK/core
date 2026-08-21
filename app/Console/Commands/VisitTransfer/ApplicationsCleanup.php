baf99a6a3ef60c01690df949f6150aab9d1ed015
Coby Chapman <cobylc.121@gmail.com>
2026-08-21 08:35:05 +0100

    feat: automatically reject ATC VT applications that fail automated checks (#5117)
    * auto reject applications
    
    * Update ApplicationsCleanup.php
    
    * copy text

--- app/Console/Commands/VisitTransfer/ApplicationsCleanup.php
+++ app/Console/Commands/VisitTransfer/ApplicationsCleanup.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands\VisitTransfer;
 
 use App\Console\Commands\Command;
+use App\Enums\VTCheckStatus;
 use App\Exceptions\VisitTransfer\Application\ApplicationCannotBeExpiredException;
 use App\Models\VisitTransfer\Application;
 
@@ -65,7 +66,25 @@ class ApplicationsCleanup extends Command
                 $application->setCheckOutcome('90_day', $application->check90DayQualification());
                 $application->setCheckOutcome('50_hours', $application->check50Hours());
 
-                $application->markAsUnderReview('Automated checks have completed.');
+                // If automated checks fail, automatically reject the application
+                if ($application->check_outcome_90_day === VTCheckStatus::Failed || $application->check_outcome_50_hours === VTCheckStatus::Failed) {
+                    $failedChecks = [];
+
+                    if ($application->check_outcome_90_day === VTCheckStatus::Failed) {
+                        $failedChecks[] = '90-day: You have not held your current rating for at least 90 days';
+                    }
+
+                    if ($application->check_outcome_50_hours === VTCheckStatus::Failed) {
+                        $failedChecks[] = '50-hours: You have not controlled a minimum of 50 hours at your current rating';
+                    }
+
+                    $application->reject(
+                        'Your application has been automatically rejected as one or more of the automated checks failed ('.implode('. ', $failedChecks).'). Please note there may additionally be other conditions not met. You can find out more about the requirements for your application in VATSIM .NETs TV Policy. Please contact the Community team via the helpdesk if you believe this is in error.',
+                        'Application automatically rejected after failing an automated check. 90-day: '.$application->check_outcome_90_day->label().', 50-hours: '.$application->check_outcome_50_hours->label().'.'
+                    );
+                } else {
+                    $application->markAsUnderReview('Automated checks have completed.');
+                }
             } else {
                 $application->markAsUnderReview('Automated checks have been disabled for this facility - requires manual checking.');
             }
