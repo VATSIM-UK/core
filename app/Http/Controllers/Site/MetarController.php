6a6faba8c71c00470cc87931631d19734ec0c4f9
Kristián Kunc <kristian.kunc@gmail.com>
2026-09-17 13:28:47 +0000

    api docs

--- app/Http/Controllers/Site/MetarController.php
+++ app/Http/Controllers/Site/MetarController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers\Site;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class MetarController
 {
-    public function get($airportIcao)
+    public function get(Request $request, $airportIcao)
     {
-        return Cache::remember("vatsim.metar.$airportIcao", 5 * 60, function () use ($airportIcao) {
+        $metar = Cache::remember("vatsim.metar.$airportIcao", 5 * 60, function () use ($airportIcao) {
             $response = Http::withUserAgent('VATSIM-UK')->get("http://metar.vatsim.net/metar.php?id=$airportIcao");
 
             if ($response->failed()) {
@@ -18,5 +19,10 @@ class MetarController
 
             return $response->body();
         });
+
+        // Defaults to html unless the client's Accept header prefers plain text.
+        $contentType = $request->prefers(['text/html', 'text/plain']) ?? 'text/html';
+
+        return response($metar, 200, ['Content-Type' => $contentType]);
     }
 }
