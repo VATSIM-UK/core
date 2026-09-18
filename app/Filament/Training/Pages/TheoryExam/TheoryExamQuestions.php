bddf0e24901bae1a8f3b12ed6e912c6ffd65dfdb
William Shaw <39245501+WillShaw1100@users.noreply.github.com>
2026-09-19 00:52:52 +0100

    Update app/Filament/Training/Pages/TheoryExam/TheoryExamQuestions.php
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/Filament/Training/Pages/TheoryExam/TheoryExamQuestions.php
+++ app/Filament/Training/Pages/TheoryExam/TheoryExamQuestions.php
@@ -51,7 +51,8 @@ class TheoryExamQuestions extends Page implements HasTable
         ];
         $this->allowedLevels = collect($this->userPermissionsTruthTable)->filter(fn ($value) => $value)->keys()->map(fn ($level) => strtoupper($level))->all();
 
-        $requestedLevel = strtoupper(request()->get('level'));
+        $levelParam = request()->get('level');
+        $requestedLevel = is_string($levelParam) ? strtoupper($levelParam) : null;
 
         $this->level = in_array($requestedLevel, $this->allowedLevels) ? $requestedLevel : $this->allowedLevels[0] ?? 'S1';
     }
