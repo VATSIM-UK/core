e2524ed2f297b556ce3cb6bf0c18a4d7538103ec
Coby Chapman <cobylc.121@gmail.com>
2026-09-20 22:09:43 +0100

    Revert "chore: add logging for availability issues- #5206 (#5206)"
    This reverts commit f9576ecd27196599b65d37880b5e94074c66d8fa.

--- tests/Feature/Bookings/CalendarWeekViewTest.php
+++ tests/Feature/Bookings/CalendarWeekViewTest.php
@@ -231,15 +231,15 @@ class CalendarWeekViewTest extends TestCase
             ->assertSeeHtml("wire:click=\"viewDayFromWeek('".Carbon::today()->toDateString()."')\"");
     }
 
-    // #[Test]
-    // public function it_marks_past_day_headers_distinctly_from_today_and_future_days(): void
-    // {
-    //     Livewire::test(Calendar::class)
-    //         ->call('setViewMode', 'week')
-    //         ->assertSeeHtml('data-day-state="past"')
-    //         ->assertSeeHtml('data-day-state="today"')
-    //         ->assertSeeHtml('data-day-state="future"');
-    // }
+    #[Test]
+    public function it_marks_past_day_headers_distinctly_from_today_and_future_days(): void
+    {
+        Livewire::test(Calendar::class)
+            ->call('setViewMode', 'week')
+            ->assertSeeHtml('data-day-state="past"')
+            ->assertSeeHtml('data-day-state="today"')
+            ->assertSeeHtml('data-day-state="future"');
+    }
 
     #[Test]
     public function it_shows_the_day_and_week_toggle_buttons(): void
