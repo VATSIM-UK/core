07ac7f72e3b121be5be94ea6af53396cbb7fb415
CLC0609 <cobylc.121@gmail.com>
2026-09-21 20:17:28 +0100

    Adds callout

--- resources/views/filament/training/pages/mentoring.blade.php
+++ resources/views/filament/training/pages/mentoring.blade.php
@@ -1,5 +1,15 @@
 <x-filament-panels::page>
 	<div class="space-y-6">
+		<x-filament::callout icon="heroicon-o-information-circle" color="info">
+			<x-slot name="heading">
+				Seeing fewer students than in CTS?
+			</x-slot>
+
+			<x-slot name="description">
+				This is to be expected. Core applies stricter rules than CTS. A student won't appear if they have a session booked in the future, have been forwarded for an exam, or are on a LOA.
+			</x-slot>
+		</x-filament::callout>
+
 		<livewire:training.availability-gantt />
 
 		<livewire:training.accepted-mentoring-sessions-table />
